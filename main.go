44585c249e1a3a7eb6adfe55bcee74c465432219
Lauren Long <laurenzlong@users.noreply.github.com>
2023-04-28 22:51:55 -0700

    feat: Create scaffolding for module.

--- main.go
+++ main.go
@@ -1,16 +1,49 @@
 package connectors
 
 import (
-	"github.com/amp-labs/connectors/salesforce"
-	"github.com/amp-labs/connectors/common"
+	"fmt"
 )
 
-func Read(spec common.ReadConfig) (common.Result, error)	{
-	if spec.API == common.Salesforce {
-		return salesforce.Read(spec)
+type API string
+
+const (
+	Salesforce API = "salesforce"
+)
+
+// ReadConfig defines what API to read from, and what we are reading.
+type ReadConfig struct {
+	API API
+	ObjectName string
+	Fields [] string
+	AccessToken string
+	// WorkspaceID is the ID of the workspace, subdomain, etc. that we are reading from.
+	WorkspaceID string
+}
+
+type Result struct {
+	// Rows is the number of total rows in the result.
+	Rows int
+	// Data is a list of maps, where each map represents a record that we read.
+	Data [] map [string] interface {}
+}
+
+type ErrorWithStatus struct {
+	// StatusCode is the HTTP status.
+	StatusCode int
+	// A human-readable error message.
+	Message string
+}
+
+func (r ErrorWithStatus) Error() string {
+	return fmt.Sprintf("status %d: message %v", r.StatusCode, r.Message)
+}
+
+func Read(config ReadConfig) (Result, error)	{
+	if config.API == Salesforce {
+		return salesforceRead(config)
 	}
 
-	return common.Result{}, common.ErrorWithStatus{
+	return Result{}, ErrorWithStatus{
 		StatusCode: 400,
 		Message: "API not supported",
 	}
---
+++ salesforce.go
@@ -0,0 +1,7 @@
+package connectors
+
+func salesforceRead(config ReadConfig) (Result, error) {
+	// TODO: implement
+	// Construct a SOQL query out of the config, make the appropriate API calls, handle pagination, and return the full list of objects.
+	return Result{}, nil
+}
