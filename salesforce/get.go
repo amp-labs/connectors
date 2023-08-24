9bdd8da34a96ef7aac4f04cde7c133139176028c
Chris Eberle <chris@withampersand.com>
2023-08-24 15:20:36 -0700

    more cleanup, and get rid of retry library

--- salesforce/get.go
+++ salesforce/get.go
@@ -6,38 +6,33 @@ import (
 	"log/slog"
 
 	"github.com/amp-labs/connectors/common"
-	"github.com/jdvr/go-again"
 	"github.com/spyzhov/ajson"
 )
 
 // get reads data from Salesforce. It handles retries and access token refreshes.
 func (c *Connector) get(ctx context.Context, url string) (*ajson.Node, error) {
-	// Retry will retry the function until it returns a nil error, or a permanent (non-retryable) error.
-	return again.Retry[*ajson.Node](ctx, func(ctx context.Context) (*ajson.Node, error) {
-		// Make the request
-		node, err := c.Client.Get(ctx, url)
-		if err != nil {
-			switch {
-			case errors.Is(err, common.ErrApiDisabled):
-				// Not retryable, so return a permanent error
-				return nil, again.NewPermanentError(err)
-			case errors.Is(err, common.ErrForbidden):
-				// Not retryable, so return a permanent error
-				return nil, again.NewPermanentError(err)
-			case errors.Is(err, common.ErrAccessToken):
-				// Retryable, so just log and retry
-				slog.Warn("Access token invalid, retrying", "error", err)
+	node, err := c.Client.Get(ctx, url)
+	if err != nil {
+		switch {
+		case errors.Is(err, common.ErrAccessToken):
+			// Retryable, so just log and retry
+			slog.Warn("Access token invalid, retrying", "error", err)
 
-				return nil, err
-			case errors.Is(err, common.ErrRetryable):
-				return nil, err
-			default:
-				// Anything else is a permanent error
-				return nil, again.NewPermanentError(err)
-			}
+			// TODO: Retry
+			return nil, err
+		case errors.Is(err, common.ErrRetryable):
+			// TODO: Retry
+			return nil, err
+		case errors.Is(err, common.ErrApiDisabled):
+			fallthrough
+		case errors.Is(err, common.ErrForbidden):
+			fallthrough
+		default:
+			// Anything else is a permanent error
+			return nil, err
 		}
+	}
 
-		// Success
-		return node, nil
-	})
+	// Success
+	return node, nil
 }
