8347c6be12e49c18c7b21b8b182cca926973a141
Lauren Long <laurenzlong@users.noreply.github.com>
2023-08-25 10:49:22 -0700

    Support Post requests

--- common/json.go
+++ common/json.go
@@ -1,7 +1,9 @@
 package common
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
@@ -39,6 +41,27 @@ func (j *JSONHTTPClient) Get(ctx context.Context,
 		return nil, err
 	}
 
+	return parseJSONResponse(res, body)
+}
+
+// Post makes a POST request to the given URL and returns the response body as a JSON object.
+// ReqBody must be JSON-serializable. If it is not, an error is returned.
+// If the response is not a 2xx, an error is returned. If the response is a 401, the caller should
+// refresh the access token and retry the request. If errorHandler is nil, then the default error
+// handler is used. If not, the caller can inject their own error handling logic.
+func (j *JSONHTTPClient) Post(ctx context.Context,
+	url string, reqBody any, headers ...Header,
+) (*ajson.Node, error) {
+	// Make the request, get the response body
+	res, body, err := j.httpPost(ctx, url, headers, reqBody) //nolint:bodyclose
+	if err != nil {
+		return nil, err
+	}
+
+	return parseJSONResponse(res, body)
+}
+
+func parseJSONResponse(res *http.Response, body []byte) (*ajson.Node, error) {
 	// Ensure the response is JSON
 	ct := res.Header.Get("Content-Type")
 	if len(ct) > 0 {
@@ -64,11 +87,26 @@ func (j *JSONHTTPClient) Get(ctx context.Context,
 func (j *JSONHTTPClient) httpGet(ctx context.Context, url string,
 	headers []Header,
 ) (*http.Response, []byte, error) {
-	req, err := makeJSONRequest(ctx, url, headers)
+	req, err := makeJSONGetRequest(ctx, url, headers)
 	if err != nil {
 		return nil, nil, err
 	}
 
+	return j.sendRequest(req)
+}
+
+func (j *JSONHTTPClient) httpPost(ctx context.Context, url string,
+	headers []Header, body any,
+) (*http.Response, []byte, error) {
+	req, err := makeJSONPostRequest(ctx, url, headers, body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return j.sendRequest(req)
+}
+
+func (j *JSONHTTPClient) sendRequest(req *http.Request) (*http.Response, []byte, error) {
 	// Send the request
 	res, err := j.Client.Do(req)
 	if err != nil {
@@ -102,13 +140,32 @@ func (j *JSONHTTPClient) httpGet(ctx context.Context, url string,
 	return res, body, nil
 }
 
-func makeJSONRequest(ctx context.Context, url string, headers []Header) (*http.Request, error) {
-	// Create a new GET request
+func makeJSONGetRequest(ctx context.Context, url string, headers []Header) (*http.Request, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error creating request: %w", err)
 	}
 
+	return addAcceptJSONHeaders(req, headers)
+}
+
+func makeJSONPostRequest(ctx context.Context, url string, headers []Header, body any) (*http.Request, error) {
+	j, err := json.Marshal(body)
+	if err != nil {
+		return nil, fmt.Errorf("request body is not valid JSON, body is %v:\n%w", body, err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBuffer(j))
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+
+	headers = append(headers, Header{Key: "Content-Type", Value: "application/json"})
+
+	return addAcceptJSONHeaders(req, headers)
+}
+
+func addAcceptJSONHeaders(req *http.Request, headers []Header) (*http.Request, error) {
 	// Request JSON
 	req.Header.Add("Accept", "application/json")
 
