ced1003378882ba59301c959da8b92173f7720a2
stevenkokada <stevenkokada@gmail.com>
2023-10-09 23:03:05 -0700

    json: remove lifted json unmarshalling logic on nil resp

--- common/json.go
+++ common/json.go
@@ -106,10 +106,6 @@ func parseJSONResponse(res *http.Response, body []byte) (*ajson.Node, error) {
 	jsonBody, err := ajson.Unmarshal(body)
 	if err != nil {
 
-		// ajson errs when trying to unmarshal empty body, even if the req was successful so we swallow error
-		if res.StatusCode == http.StatusNoContent {
-			return nil, nil
-		}
 		return nil, NewHTTPStatusError(res.StatusCode, fmt.Errorf("failed to unmarshall response body into JSON: %w", err))
 	}
 
