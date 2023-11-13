5e1616d8b242d170de8754a3096ae0edc170dce1
Rajat Pawar <18614743+RajatPawar@users.noreply.github.com>
2023-11-13 10:24:15 -0800

    [ENG-423] Add Search functionality (#26)

--- hubspot/search.go
+++ hubspot/search.go
@@ -2,34 +2,23 @@ package hubspot
 
 import (
 	"context"
-	"time"
 
 	"github.com/amp-labs/connectors/common"
 	"github.com/spyzhov/ajson"
 )
 
-// search uses the POST /search endpoint to filter object records and return the result.
-// This is used when Since is set. Otherwise, the Read endpoint is used.
+// Search uses the POST /search endpoint to filter object records and return the result.
+// This endpoint has a limit of 10,000 records. If the result has more than 10,000 records,
+// the caller should employ sorting to paginate through the result on the client side.
 // This endpoint paginates using paging.next.after which is to be used as an offset.
 // Read more @ https://developers.hubspot.com/docs/api/crm/search
-func (c *Connector) search(ctx context.Context, config common.ReadParams) (*common.ReadResult, error) {
+func (c *Connector) Search(ctx context.Context, config SearchParams) (*common.ReadResult, error) {
 	var (
 		data *ajson.Node
 		err  error
 	)
 
-	// If filtering is not required, then we have to use the read endpoint.
-	if !requiresFiltering(config) {
-		return c.Read(ctx, config)
-	}
-
-	// If the next page is set, then we have to use the next page as the offset
-	// in the filter body. As always, we attach the query values in the request.
-	data, err = c.post(
-		ctx,
-		c.BaseURL+"/objects/"+config.ObjectName+"/search"+"?"+makeQueryValues(config),
-		makeFilterBody(config),
-	)
+	data, err = c.post(ctx, c.BaseURL+"/objects/"+config.ObjectName+"/search", makeFilterBody(config))
 	if err != nil {
 		return nil, err
 	}
@@ -37,25 +26,25 @@ func (c *Connector) search(ctx context.Context, config common.ReadParams) (*comm
 	return parseResult(data, getNextRecordsAfter)
 }
 
-// makeFilterBody is specifically implemented for the Since filter currently.
-func makeFilterBody(config common.ReadParams) map[string]any {
+func makeFilterBody(config SearchParams) map[string]any {
 	filterBody := map[string]any{
-		"filterGroups": []map[string]any{
-			{
-				"filters": []map[string]any{
-					{
-						"propertyName": "lastmodifieddate",
-						"operator":     "GT",
-						"value":        config.Since.Format(time.RFC3339),
-					},
-				},
-			},
-		},
 		"limit": DefaultPageSize,
 	}
 
-	if len(config.NextPage) > 0 {
+	if config.FilterGroups != nil {
+		filterBody["filterGroups"] = config.FilterGroups
+	}
+
+	if config.NextPage != "" {
 		filterBody["after"] = config.NextPage
+	}
+
+	if config.SortBy != nil {
+		filterBody["sorts"] = config.SortBy
+	}
+
+	if config.Fields != nil {
+		filterBody["properties"] = config.Fields
 	}
 
 	return filterBody
---
+++ hubspot/types.go
@@ -0,0 +1,55 @@
+package hubspot
+
+type SearchParams struct {
+	// The name of the object we are reading, e.g. "Account"
+	ObjectName string // required
+	// NextPage is an opaque token that can be used to get the next page of results.
+	NextPage string // optional, only set this if you want to read the next page of results
+	// SortBy is the field to sort by in the direction specified by SortDirection.
+	SortBy []SortBy // optional
+	// FilterBy is the filter to apply to the search
+	FilterGroups []FilterGroup // optional
+	// Fields is the list of fields to return in the result.
+	Fields []string // optional
+}
+
+type SortBy struct {
+	// The name of the field to sort by.
+	PropertyName string `json:"propertyName,omitempty"`
+	// The direction to sort by.
+	Direction SortDirection `json:"direction,omitempty"`
+}
+
+type FilterGroup struct {
+	Filters []Filter `json:"filters,omitempty"`
+}
+
+type Filter struct {
+	FieldName string             `json:"propertyName,omitempty"`
+	Operator  FilterOperatorType `json:"operator,omitempty"`
+	Value     string             `json:"value,omitempty"`
+}
+
+type (
+	SortDirection      string
+	FilterOperatorType string
+)
+
+const (
+	SortDirectionAsc  SortDirection = "ASCENDING"
+	SortDirectionDesc SortDirection = "DESCENDING"
+
+	FilterOperatorTypeEQ           FilterOperatorType = "EQ"
+	FilterOperatorTypeNEQ          FilterOperatorType = "NEQ"
+	FilterOperatorTypeGT           FilterOperatorType = "GT"
+	FilterOperatorTypeGTE          FilterOperatorType = "GTE"
+	FilterOperatorTypeLT           FilterOperatorType = "LT"
+	FilterOperatorTypeLTE          FilterOperatorType = "LTE"
+	FilterOperatorBetween          FilterOperatorType = "BETWEEN"
+	FilterOperatorIN               FilterOperatorType = "IN"
+	FilterOperatorNIN              FilterOperatorType = "NIN"
+	FilterPropertyHasProperty      FilterOperatorType = "HAS_PROPERTY"
+	FilterPropertyNotHasProperty   FilterOperatorType = "NOT_HAS_PROPERTY"
+	FilterPropertyContainsToken    FilterOperatorType = "CONTAINS_TOKEN"
+	FilterPropertyNotContainsToken FilterOperatorType = "NOT_CONTAINS_TOKEN"
+)
