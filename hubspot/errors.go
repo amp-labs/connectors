853bcb6af889031d65d1eaf895ca5fb0e801afdb
Rajat Pawar <18614743+RajatPawar@users.noreply.github.com>
2023-12-14 17:26:30 -0800

    chore: Update hubspot error handling (#35)

--- hubspot/errors.go
+++ hubspot/errors.go
@@ -4,17 +4,19 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"mime"
 	"net/http"
 
 	"github.com/amp-labs/connectors/common"
 )
 
 var (
-	ErrMissingAPIModule = errors.New("missing Hubspot API module")
-	ErrMissingClient    = errors.New("JSON http client not set")
-	ErrNotArray         = errors.New("results is not an array")
-	ErrNotObject        = errors.New("result is not an object")
-	ErrNotString        = errors.New("link is not a string")
+	ErrMissingAPIModule    = errors.New("missing Hubspot API module")
+	ErrMissingClient       = errors.New("JSON http client not set")
+	ErrNotArray            = errors.New("results is not an array")
+	ErrNotObject           = errors.New("result is not an object")
+	ErrNotString           = errors.New("link is not a string")
+	ErrSearchLimitExceeded = errors.New("search endpoint limit exceeded")
 )
 
 type HubspotError struct {
@@ -50,7 +52,12 @@ type ErrLinks struct {
 }
 
 func (c *Connector) interpretError(res *http.Response, body []byte) error {
-	if res.Header.Get("Content-Type") == "application/json" {
+	mediaType, _, err := mime.ParseMediaType(res.Header.Get("Content-Type"))
+	if err != nil {
+		return fmt.Errorf("mime.ParseMediaType failed: %w", err)
+	}
+
+	if mediaType == "application/json" {
 		return c.interpretJSONError(res, body)
 	}
 
@@ -74,6 +81,9 @@ func (c *Connector) interpretJSONError(res *http.Response, body []byte) error {
 	}
 
 	switch res.StatusCode {
+	// Hubspot sends us a 400 when the search endpoint returns over 10K records.
+	case http.StatusBadRequest:
+		return createError(ErrSearchLimitExceeded, apiError)
 	case http.StatusUnauthorized:
 		return createError(common.ErrAccessToken, apiError)
 	case http.StatusForbidden:
