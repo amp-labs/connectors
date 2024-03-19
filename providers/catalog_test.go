50003f0cf8e935056a33f0613947b61e1c77e6c9
sanjay-a01 <163011126+sanjay-a01@users.noreply.github.com>
2024-03-19 16:36:51 +0530

    [ENG-824] feat: Add Docusign connector (#86)

--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -413,6 +413,48 @@ var testCases = []struct { // nolint
 		},
 		expectedErr: nil,
 	},
+	{
+		provider: DocuSign,
+		expected: &ProviderInfo{
+			Support: Support{
+				Read:      false,
+				Write:     false,
+				BulkWrite: false,
+				Subscribe: false,
+				Proxy:     false,
+			},
+			AuthType: Oauth2,
+			OauthOpts: OauthOpts{
+				AuthURL:                   "https://account.docusign.com/oauth/auth",
+				TokenURL:                  "https://account.docusign.com/oauth/token",
+				ExplicitScopesRequired:    true,
+				ExplicitWorkspaceRequired: true,
+			},
+			BaseURL: "https://{{.workspace}}.docusign.net",
+		},
+		expectedErr: nil,
+	},
+	{
+		provider: DocuSignDeveloper,
+		expected: &ProviderInfo{
+			Support: Support{
+				Read:      false,
+				Write:     false,
+				BulkWrite: false,
+				Subscribe: false,
+				Proxy:     false,
+			},
+			AuthType: Oauth2,
+			OauthOpts: OauthOpts{
+				AuthURL:                   "https://account-d.docusign.com/oauth/auth",
+				TokenURL:                  "https://account-d.docusign.com/oauth/token",
+				ExplicitScopesRequired:    true,
+				ExplicitWorkspaceRequired: false,
+			},
+			BaseURL: "https://demo.docusign.net",
+		},
+		expectedErr: nil,
+	},
 }
 
 func TestReadInfo(t *testing.T) { // nolint
