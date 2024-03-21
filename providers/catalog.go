ddfdbb6d8ae6f555ce92cd663628717fc8045c35
Nadezda Kalmykova <nklepik@mail.ru>
2024-03-22 03:38:00 +0600

    zoho_code

--- providers/catalog.go
+++ providers/catalog.go
@@ -12,6 +12,7 @@ const (
 	Outreach          Provider = "outreach"
 	Pipedrive         Provider = "pipedrive"
 	Copper            Provider = "copper"
+	ZohoCRM           Provider = "zohoCRM"
 	Sellsy            Provider = "sellsy"
 	Attio             Provider = "attio"
 	Close             Provider = "close"
@@ -174,6 +175,25 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
+		},
+	},
+
+	// ZohoCRM configuration
+	ZohoCRM: {
+		AuthType: Oauth2,
+		BaseURL:  "https://www.zohoapis.com",
+		OauthOpts: OauthOpts{
+			AuthURL:                   "https://accounts.zoho.com/oauth/v2/auth",
+			TokenURL:                  "https://accounts.zoho.com/oauth/v2/token",
+			ExplicitScopesRequired:    true,
+			ExplicitWorkspaceRequired: false,
+		},
+		Support: Support{
+			BulkWrite: true,
+			Proxy:     false,
+			Read:      true,
+			Subscribe: true,
+			Write:     true,
 		},
 	},
 
