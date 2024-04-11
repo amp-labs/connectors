3d53ef5b721e30fcfa20ed243cd6b27f16d29b55
Rajat Pawar <18614743+RajatPawar@users.noreply.github.com>
2024-04-11 00:31:29 -0700

    feat: Split interface by functionality (#195)

--- connector/client.go
+++ connector/client.go
@@ -1,8 +1,12 @@
-package basic
+package connector
 
 import (
 	"github.com/amp-labs/connectors/common"
 )
+
+func (c *Connector) JSONHTTPClient() *common.JSONHTTPClient {
+	return c.Client
+}
 
 func (c *Connector) HTTPClient() *common.HTTPClient {
 	return c.Client.HTTPClient
