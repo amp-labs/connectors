5a07f608d7bb9fe1efe35584d4e7865726d2b868
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-04-16 20:28:20 +0300

    feat: Deep connector MS Dynamics365 Sales (Read) (#157)

---
+++ microsoftdynamicscrm/metadata.go
@@ -0,0 +1,14 @@
+package microsoftdynamicscrm
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+)
+
+func (c *Connector) ListObjectMetadata(
+	ctx context.Context, objectNames []string,
+) (*common.ListObjectMetadataResult, error) {
+	// TODO implement me
+	panic("implement me")
+}
