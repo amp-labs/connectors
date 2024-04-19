dec974ce879fa92066fceee00f12d97e7557ef5b
Nadezda Kalmykova <nklepik@mail.ru>
2024-04-19 19:24:08 +0600

    workspace_required_fix

--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -200,7 +200,7 @@ var testCases = []struct { // nolint
 
 				TokenURL:                  "https://api.na3.echosign.com/oauth/v2/token",
 				ExplicitScopesRequired:    true,
-				ExplicitWorkspaceRequired: false,
+				ExplicitWorkspaceRequired: true,
 				TokenMetadataFields: TokenMetadataFields{
 					ConsumerRefField:  "api_access_point",
 					WorkspaceRefField: "web_access_point",
