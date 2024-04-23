62f98d83253f734f6cb938db99e443f354f5e9aa
Chris Eberle <chris@withampersand.com>
2024-04-23 12:42:03 -0700

    fix test

--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -1175,7 +1175,7 @@ var testCases = []struct { // nolint
 					Upsert: false,
 					Delete: false,
 				},
-				Proxy:     false,
+				Proxy:     true,
 				Read:      false,
 				Subscribe: false,
 				Write:     false,
