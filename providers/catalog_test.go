335aa30bdc86daf906c2835bfeeb3e4389307058
jkarage <josephbkarage@gmail.com>
2024-05-06 09:17:38 +0300

    feat: Update outreach proxy support

--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -175,7 +175,7 @@ var testCases = []struct { // nolint
 					Delete: false,
 				},
 				Subscribe: false,
-				Proxy:     false,
+				Proxy:     true,
 			},
 			AuthType: Oauth2,
 			OauthOpts: OauthOpts{
