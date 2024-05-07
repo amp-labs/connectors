18c8ba4d1ca144104e5c376d5230d64a463cc44f
mohammed <mohammedfuta@gmail.com>
2024-05-07 21:09:06 +0000

    Initial Commit

--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -1525,7 +1525,7 @@ var testCases = []struct { // nolint
 					Upsert: false,
 					Delete: false,
 				},
-				Proxy:     false,
+				Proxy:     true,
 				Read:      false,
 				Subscribe: false,
 				Write:     false,
