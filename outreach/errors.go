d92a4ff81310b3d3dd782d6aa5c00fdfc72993c9
jkarage <josephbkarage@gmail.com>
2024-05-09 17:57:03 +0300

    feat: Implement Outreach ListMetadata

--- outreach/errors.go
+++ outreach/errors.go
@@ -9,6 +9,7 @@ var (
 	ErrNotArray      = errors.New("results data is not an array")
 	ErrNotObject     = errors.New("record is not an object")
 	ErrNotString     = errors.New("next is not a string")
+	ErrEmptyResponse = errors.New("empty response body")
 )
 
 func (c *Connector) HandleError(err error) error {
---
+++ outreach/metadata.go
@@ -0,0 +1,77 @@
+package outreach
+
+import (
+	"context"
+	"encoding/json"
+	"net/url"
+
+	"github.com/amp-labs/connectors/common"
+)
+
+type ObjectOKResponse struct {
+	Data []map[string]any `json:"data"`
+}
+
+func (c *Connector) ListObjectMetadata(ctx context.Context,
+	objectNames []string,
+) (*common.ListObjectMetadataResult, error) {
+	// Ensure that objectNames is not empty
+	if len(objectNames) == 0 {
+		return nil, common.ErrMissingObjects
+	}
+
+	objMetadata := common.ListObjectMetadataResult{
+		Result: make(map[string]common.ObjectMetadata),
+		Errors: make(map[string]error),
+	}
+
+	for _, obj := range objectNames {
+		// Constructing the  request url.
+		objURL, err := url.JoinPath(c.BaseURL, obj)
+		if err != nil {
+			return nil, err
+		}
+
+		res, err := c.get(ctx, objURL)
+		if err != nil {
+			objMetadata.Errors[obj] = err
+		}
+
+		// Check nil response body, to avoid panic.
+		if res == nil || res.Body == nil {
+			objMetadata.Errors[obj] = ErrEmptyResponse
+			continue
+		}
+
+		metadata, err := metadataMapper(res.Body.Source())
+		if err != nil {
+			return nil, err
+		}
+
+		metadata.DisplayName = obj
+		objMetadata.Result[obj] = metadata
+	}
+
+	return &objMetadata, nil
+}
+
+func metadataMapper(body []byte) (common.ObjectMetadata, error) {
+	metadata := common.ObjectMetadata{
+		FieldsMap: make(map[string]string),
+	}
+
+	var response ObjectOKResponse
+
+	err := json.Unmarshal(body, &response)
+	if err != nil {
+		return metadata, err
+	}
+
+	for _, dataMap := range response.Data {
+		for attr := range dataMap {
+			metadata.FieldsMap[attr] = attr
+		}
+	}
+
+	return metadata, nil
+}
