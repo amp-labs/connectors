d67f6c1bdb6283feaa72702d43594399b11791ee
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-05-09 15:13:07 +0300

    enable proxy: Zendesk Support (#275)

--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -1031,7 +1031,7 @@ var testCases = []struct { // nolint
 					Delete: false,
 				},
 				Subscribe: false,
-				Proxy:     false,
+				Proxy:     true,
 			},
 			AuthType: Oauth2,
 			OauthOpts: OauthOpts{
