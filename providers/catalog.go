b38dbc50cb9999e934636e3196de93e1131a8c90
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-05-14 01:01:32 +0300

    release: enable Calendly connector (#309)

--- providers/catalog.go
+++ providers/catalog.go
@@ -749,7 +749,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
