2d3639f0edbb85d6fb00048f6c1c408d6228cc24
Cobalt0s <constantin.y.koval@gmail.com>
2024-05-15 00:37:00 +0300

    [Read] Error format

--- intercom/errors.go
+++ intercom/errors.go
@@ -4,17 +4,60 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/amp-labs/connectors/common/interpreter"
 )
 
+/*
+Response example:
+
+	{
+	  "type": "error.list",
+	  "request_id": "00066ltgfertncb684rg",
+	  "errors": [
+		{
+		  "code": "...",
+		  "message": "...",
+		  "field": "..."
+		}
+	  ]
+	}
+*/
+
 func (*Connector) interpretJSONError(res *http.Response, body []byte) error { //nolint:cyclop
-	payload := make(map[string]any)
+	var payload ResponseError
 	if err := json.Unmarshal(body, &payload); err != nil {
 		return fmt.Errorf("interpretJSONError general: %w %w", interpreter.ErrUnmarshal, err)
 	}
 
-	// TODO handle error
+	return payload.combineErr(interpreter.DefaultStatusCodeMappingToErr(res, body))
+}
 
-	return interpreter.DefaultStatusCodeMappingToErr(res, body)
+type ResponseError struct {
+	Type      string             `json:"type"`
+	RequestId *string            `json:"request_id"` // nolint:tagliatelle
+	Errors    []DescriptiveError `json:"errors"`
+}
+
+type DescriptiveError struct {
+	Code    string  `json:"code"`
+	Message *string `json:"message"`
+	Field   *string `json:"field"`
+}
+
+func (r ResponseError) combineErr(base error) error {
+	messages := make([]string, len(r.Errors))
+
+	for i, descr := range r.Errors {
+		var message string
+
+		if descr.Message != nil {
+			message = fmt.Sprintf("[%v]", *descr.Message)
+		}
+
+		messages[i] = descr.Code + message
+	}
+
+	return fmt.Errorf("%w: %s", base, strings.Join(messages, ","))
 }
