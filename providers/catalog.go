e635109739d1b32ddf25eee5f2ed593ea313119f
Beadko <beadko@gmail.com>
2024-05-15 20:02:42 +0700

    HelpScoutMailbox: Enable Proxy

--- providers/catalog.go
+++ providers/catalog.go
@@ -1106,7 +1106,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -1286,7 +1286,7 @@ var testCases = []struct { // nolint
 					Upsert: false,
 					Delete: false,
 				},
-				Proxy:     false,
+				Proxy:     true,
 				Read:      false,
 				Subscribe: false,
 				Write:     false,
