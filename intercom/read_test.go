f40886811fbb70df9f3a0f4e536d30e134cf9a1c
Cobalt0s <constantin.y.koval@gmail.com>
2024-05-15 18:38:31 +0300

    Add pagination, error format, success response tests

--- intercom/read_test.go
+++ intercom/read_test.go
@@ -20,14 +20,12 @@ func TestRead(t *testing.T) { //nolint:funlen,gocognit,cyclop,maintidx
 	t.Parallel()
 
 	responseErrorFormat := mockutils.DataFromFile(t, "page-req-too-large.json")
-	responseNotesFirstPage := mockutils.DataFromFile(t, "read-notes-1-first-page.json")
 	responseContactsFirstPage := mockutils.DataFromFile(t, "read-contacts-1-first-page.json")
-
-	// TODO deal with other sample files
-
-	// x := mockutils.DataFromFile(t, "read-contacts-3-last-page.json")
-	// x := mockutils.DataFromFile(t, "read-notes-2-last-page.json")
-	// x := mockutils.DataFromFile(t, "read-conversations.json")
+	responseContactsSecondPage := mockutils.DataFromFile(t, "read-contacts-2-second-page.json")
+	responseContactsThirdPage := mockutils.DataFromFile(t, "read-contacts-3-last-page.json")
+	// responseReadConversations := mockutils.DataFromFile(t, "read-conversations.json")
+	responseNotesFirstPage := mockutils.DataFromFile(t, "read-notes-1-first-page.json")
+	responseNotesSecondPage := mockutils.DataFromFile(t, "read-notes-2-last-page.json")
 
 	// TODO think about test cases
 
@@ -134,96 +132,75 @@ func TestRead(t *testing.T) { //nolint:funlen,gocognit,cyclop,maintidx
 			},
 			expectedErrs: nil,
 		},
-		//{
-		//	name: "Successful read with 25 entries, checking one row",
-		//	server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//		w.Header().Set("Content-Type", "application/json")
-		//		w.WriteHeader(http.StatusOK)
-		//		_, _ = w.Write(responseListPeople)
-		//	})),
-		//	comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
-		//		return mockutils.ReadResultComparator.SubsetRaw(actual, expected) &&
-		//			actual.Done == expected.Done &&
-		//			actual.Rows == expected.Rows
-		//	},
-		//	expected: &common.ReadResult{
-		//		Rows: 25,
-		//		// We are only interested to validate only first Read Row!
-		//		Data: []common.ReadResultRow{{
-		//			Fields: map[string]any{},
-		//			Raw: map[string]any{
-		//				"first_name":             "Lynnelle",
-		//				"email_address":          "losbourn29@paypal.com",
-		//				"full_email_address":     "\"Lynnelle new\" <losbourn29@paypal.com>",
-		//				"person_company_website": "http://paypal.com",
-		//			},
-		//		}},
-		//		Done: false,
-		//	},
-		//	expectedErrs: nil,
-		// },
-		//{
-		//	name: "Successful read with chosen fields",
-		//	input: common.ReadParams{
-		//		Fields: []string{"email_address", "person_company_website"},
-		//	},
-		//	server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//		w.Header().Set("Content-Type", "application/json")
-		//		w.WriteHeader(http.StatusOK)
-		//		_, _ = w.Write(responseListPeople)
-		//	})),
-		//	comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
-		//		return mockutils.ReadResultComparator.SubsetFields(actual, expected) &&
-		//			mockutils.ReadResultComparator.SubsetRaw(actual, expected)
-		//	},
-		//	expected: &common.ReadResult{
-		//		Data: []common.ReadResultRow{{
-		//			Fields: map[string]any{
-		//				"email_address":          "losbourn29@paypal.com",
-		//				"person_company_website": "http://paypal.com",
-		//			},
-		//			Raw: map[string]any{
-		//				"first_name":             "Lynnelle",
-		//				"email_address":          "losbourn29@paypal.com",
-		//				"full_email_address":     "\"Lynnelle new\" <losbourn29@paypal.com>",
-		//				"person_company_website": "http://paypal.com",
-		//			},
-		//		}},
-		//	},
-		//	expectedErrs: nil,
-		//},
-		//{
-		//	name: "Listing Users without pagination payload",
-		//	input: common.ReadParams{
-		//		Fields: []string{"email", "guid"},
-		//	},
-		//	server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//		w.Header().Set("Content-Type", "application/json")
-		//		w.WriteHeader(http.StatusOK)
-		//		_, _ = w.Write(responseListUsers)
-		//	})),
-		//	comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
-		//		return mockutils.ReadResultComparator.SubsetFields(actual, expected) &&
-		//			mockutils.ReadResultComparator.SubsetRaw(actual, expected)
-		//	},
-		//	expected: &common.ReadResult{
-		//		Rows: 1,
-		//		Data: []common.ReadResultRow{{
-		//			Fields: map[string]any{
-		//				"guid":  "0863ed13-7120-479b-8650-206a3679e2fb",
-		//				"email": "somebody@withampersand.com",
-		//			},
-		//			Raw: map[string]any{
-		//				"name":       "Int User",
-		//				"first_name": "Int",
-		//				"last_name":  "User",
-		//			},
-		//		}},
-		//		NextPage: "",
-		//		Done:     false,
-		//	},
-		//	expectedErrs: nil,
-		//},
+		{
+			name: "Next page URL is empty, when provided with null object",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				_, _ = w.Write(responseNotesSecondPage)
+			})),
+			comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
+				return actual.NextPage.String() == expected.NextPage.String() &&
+					actual.Done == expected.Done
+			},
+			expected:     &common.ReadResult{NextPage: "", Done: true},
+			expectedErrs: nil,
+		},
+		{
+			name: "Next page URL is empty, when provided with missing object",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				_, _ = w.Write(responseContactsThirdPage)
+			})),
+			comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
+				return actual.NextPage.String() == expected.NextPage.String() &&
+					actual.Done == expected.Done
+			},
+			expected:     &common.ReadResult{NextPage: "", Done: true},
+			expectedErrs: nil,
+		},
+		{
+			name: "Successful read with chosen fields",
+			input: common.ReadParams{
+				Fields: []string{"email", "name"},
+			},
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				_, _ = w.Write(responseContactsSecondPage)
+			})),
+			comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
+				expectedNextPage := strings.ReplaceAll(expected.NextPage.String(), "{{testServerURL}}", baseURL)
+				// custom comparison focuses on subset of fields to keep the test short
+				return mockutils.ReadResultComparator.SubsetFields(actual, expected) &&
+					mockutils.ReadResultComparator.SubsetRaw(actual, expected) &&
+					actual.NextPage.String() == expectedNextPage &&
+					actual.Done == expected.Done
+			},
+			expected: &common.ReadResult{
+				Data: []common.ReadResultRow{{
+					Fields: map[string]any{
+						"name":  "Patrick",
+						"email": "patrick@gmail.com",
+					},
+					Raw: map[string]any{
+						"type":       "contact",
+						"id":         "66439b947bb095a681f7fd9e",
+						"role":       "user",
+						"email":      "patrick@gmail.com",
+						"phone":      nil,
+						"name":       "Patrick",
+						"created_at": float64(1715706772),
+						"updated_at": float64(1715706939),
+					},
+				}},
+				NextPage: "{{testServerURL}}?per_page=50&starting_after=" +
+					"Wy0xLCI2NjQzOWI5NDdiYjA5NWE2ODFmN2ZkOWUiLDNd",
+				Done: false,
+			},
+			expectedErrs: nil,
+		},
 	}
 
 	for _, tt := range tests {
