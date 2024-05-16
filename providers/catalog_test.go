805d33319b5dfba5ab82c173a4f86184351fd736
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2024-05-16 17:15:10 +0530

    feat: Update Clickup proxy support (#333)
    * feat: Add ClickUp auth connector
    
    * feat: Update ClickUP proxy support

--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -1774,7 +1774,7 @@ var testCases = []struct { // nolint
 					Upsert: false,
 					Delete: false,
 				},
-				Proxy:     false,
+				Proxy:     true,
 				Read:      false,
 				Subscribe: false,
 				Write:     false,
