8994fd3657054970858dfb89b6218ad212b451de
Sanjay Mitrahsoft <sanjaykanth.a@mitrahsoft.com>
2024-05-16 18:47:48 +0530

    feat: Update Gmail proxy support

--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -1565,7 +1565,7 @@ var testCases = []struct { // nolint
 					Upsert: false,
 					Delete: false,
 				},
-				Proxy:     false,
+				Proxy:     true,
 				Read:      false,
 				Subscribe: false,
 				Write:     false,
