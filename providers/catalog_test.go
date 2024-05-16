574ef03a7c4b01b2d64be00df9cb27941dbe88aa
jkarage <josephbkarage@gmail.com>
2024-05-16 19:01:39 +0300

    feat: Add proxy-action support for asana

--- providers/catalog_test.go
+++ providers/catalog_test.go
@@ -570,7 +570,7 @@ var testCases = []struct { // nolint
 					Delete: false,
 				},
 				Subscribe: false,
-				Proxy:     false,
+				Proxy:     true,
 			},
 			AuthType: Oauth2,
 			OauthOpts: OauthOpts{
