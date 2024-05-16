c51eb0807809c5352583f6482e588e27d593d518
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-05-16 23:43:18 +0300

    feat: Deep connector Intercom (ListMetadata) (#363)

---
+++ common/scrapper/utils.go
@@ -0,0 +1,90 @@
+package scrapper
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// QueryHTML this method can panic.
+func QueryHTML(link string) *goquery.Document { //nolint:gocritic
+	res, err := makeRequest(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		_ = res.Body.Close()
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		_ = res.Body.Close()
+
+		log.Fatal(err)
+	}
+
+	_ = res.Body.Close()
+
+	return doc
+}
+
+func makeRequest(sourceURL string) (*http.Response, error) {
+	// must end with `/` to avoid stupid redirect with then without then again with and without slash
+	if !strings.HasSuffix(sourceURL, "/") {
+		sourceURL += "/"
+	}
+
+	client := &http.Client{
+		CheckRedirect: func() func(req *http.Request, via []*http.Request) error {
+			redirects := 0
+
+			return func(req *http.Request, via []*http.Request) error {
+				maxRedirects := 12
+				if redirects > maxRedirects {
+					return fmt.Errorf("stopped after %v redirects", maxRedirects) // nolint:goerr113
+				}
+				redirects++
+
+				return nil
+			}
+		}(),
+	}
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, sourceURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func FlushToFile(filename string, object any) error {
+	data, err := json.MarshalIndent(object, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, data, os.ModePerm)
+}
+
+func LoadFile(filename string, object any) error {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, &object)
+}
---
+++ test/intercom/metadata/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/amp-labs/connectors/common"
+	msTest "github.com/amp-labs/connectors/test/intercom"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+var (
+	objectName     = "admins"
+	objectNameMeta = "admin"
+)
+
+// we want to compare fields returned by read and schema properties provided by metadata methods
+// they must match for all such objects
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	filePath := os.Getenv("INTERCOM_CRED_FILE")
+	if filePath == "" {
+		filePath = "./intercom-creds.json"
+	}
+
+	conn := msTest.GetIntercomConnector(ctx, filePath)
+	defer utils.Close(conn)
+
+	response, err := conn.Read(ctx, common.ReadParams{
+		ObjectName: objectName,
+	})
+	if err != nil {
+		utils.Fail("error reading from Intercom", "error", err)
+	}
+
+	if response.Rows == 0 {
+		utils.Fail("expected to read at least one record", "error", err)
+	}
+
+	metadata, err := conn.ListObjectMetadata(ctx, []string{
+		objectNameMeta,
+	})
+	if err != nil {
+		utils.Fail("error listing metadata for Intercom", "error", err)
+	}
+
+	fmt.Println("Compare object metadata against endpoint response:")
+
+	mismatchErr := compareFieldsMatch(metadata, response.Data[0].Raw)
+	if mismatchErr != nil {
+		utils.Fail("schema and payload response have mismatching fields", "error", mismatchErr)
+	} else {
+		fmt.Println("==> success fields match.")
+	}
+}
+
+func compareFieldsMatch(metadata *common.ListObjectMetadataResult, response map[string]any) error {
+	fields := make(map[string]bool)
+
+	for field := range response {
+		fields[field] = false
+	}
+
+	mismatch := make([]error, 0)
+
+	for _, displayName := range metadata.Result[objectNameMeta].FieldsMap {
+		if _, found := fields[displayName]; found {
+			fields[displayName] = true
+		}
+	}
+
+	// every field from Read must be known to ListObjectMetadata
+	for name, found := range fields {
+		if !found {
+			mismatch = append(mismatch, fmt.Errorf("metadata schema is missing field %v", name))
+		}
+	}
+
+	return errors.Join(mismatch...)
+}
