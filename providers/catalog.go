bb709f0471e3023e5418c13739a3545ab28e51b5
Bea <beadko@gmail.com>
2024-05-20 17:06:04 +0800

    Aircall: Remove TokenMetadataFields (#368)

--- providers/catalog.go
+++ providers/catalog.go
@@ -1671,9 +1671,6 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 			TokenURL:                  "https://api.aircall.io/v1/oauth/token",
 			ExplicitScopesRequired:    true,
 			ExplicitWorkspaceRequired: false,
-			TokenMetadataFields: TokenMetadataFields{
-				ScopesField: "scope",
-			},
 		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
