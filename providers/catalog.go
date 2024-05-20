388a5f085cfdf2696a97b76f3dc34594a6c6d60c
Sanjay <sanjaykanth.a@mitrahsoft.com>
2024-05-20 20:43:51 +0530

    feat: Enable DropboxSign Proxy support

--- providers/catalog.go
+++ providers/catalog.go
@@ -1520,7 +1520,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
