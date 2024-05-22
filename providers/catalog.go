465773efb02d73f19784c1d2d0c92dc4edb903e1
jkarage <josephbkarage@gmail.com>
2024-05-22 15:39:31 +0300

    feat: Enable Microsoft

--- providers/catalog.go
+++ providers/catalog.go
@@ -1707,7 +1707,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
