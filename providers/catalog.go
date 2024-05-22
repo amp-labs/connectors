fe8ff36d9c9a1b73dd0809cac312f2ae83369e2f
Listener430 <95291462+Listener430@users.noreply.github.com>
2024-05-22 10:31:05 +0300

    CloseCRM auth connector update (#390)
    * grant_type_tokenmetadata_explicit_scopes_required
    
    * missing_bracket

--- providers/catalog.go
+++ providers/catalog.go
@@ -485,10 +485,16 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 		AuthType: Oauth2,
 		BaseURL:  "https://api.close.com/api",
 		OauthOpts: OauthOpts{
+			GrantType:                 AuthorizationCode,
 			AuthURL:                   "https://app.close.com/oauth2/authorize",
 			TokenURL:                  "https://api.close.com/oauth2/token",
 			ExplicitScopesRequired:    false,
 			ExplicitWorkspaceRequired: false,
+			TokenMetadataFields: TokenMetadataFields{
+				ConsumerRefField:  "user_id",
+				WorkspaceRefField: "organization_id",
+				ScopesField:       "scope",
+			},
 		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
