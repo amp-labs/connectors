0a586e84c9a08b688342c038861304d847f23a7e
Listener430 <95291462+Listener430@users.noreply.github.com>
2024-05-22 10:36:57 +0300

    fixes#366 (#372)

--- providers/catalog.go
+++ providers/catalog.go
@@ -374,7 +374,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
