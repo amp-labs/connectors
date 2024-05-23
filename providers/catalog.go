325413ac024baa5edbc9ba8094eb75a285a838e2
mohammed <mohammedfuta@gmail.com>
2024-05-23 09:52:24 +0000

    drift proxy enabled

--- providers/catalog.go
+++ providers/catalog.go
@@ -1600,7 +1600,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
