e0b9c19b1fc797eaf1816a5d497d4e8d9d1b0dd5
Lauren Long <laurenzlong@users.noreply.github.com>
2024-05-23 14:02:09 -0700

    intercom: enable proxy support (#426)

--- providers/catalog.go
+++ providers/catalog.go
@@ -697,7 +697,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      true,
 			Subscribe: false,
 			Write:     false,
