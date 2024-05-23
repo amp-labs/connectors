c02e3632be253c61d0d66f58fd53d51027dcf938
Cobalt0s <constantin.y.koval@gmail.com>
2024-05-23 19:40:08 +0300

    [Salesloft] Metadata scrapper

---
+++ salesloft/metadata/scrapping.go
@@ -0,0 +1,27 @@
+package metadata
+
+import (
+	_ "embed"
+	"path/filepath"
+	"runtime"
+
+	"github.com/amp-labs/connectors/common/scrapper"
+)
+
+var (
+	// Static file containing a list of object metadata is embedded and can be served.
+	//
+	//go:embed schemas.json
+	schemas []byte
+
+	FileManager = scrapper.NewMetadataFileManager(schemas, locator{}) // nolint:gochecknoglobals
+)
+
+type locator struct{}
+
+func (locator) AbsPathTo(filename string) string {
+	_, thisMethodsLocation, _, _ := runtime.Caller(0) // nolint:dogsled
+	localDir := filepath.Dir(thisMethodsLocation)
+
+	return localDir + "/" + filename
+}
