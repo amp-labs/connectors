bf43b62f7e7f790262a46c05409a18a255811c26
Sanjay A <sanjaykanth.a@mitrahsoft.com>
2024-05-23 12:16:12 +0530

    feat: enable Pinterest proxy support

--- providers/catalog.go
+++ providers/catalog.go
@@ -1777,7 +1777,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
