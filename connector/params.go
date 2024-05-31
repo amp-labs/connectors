a3d49bce648e99edd7d39972eb8b8be879daf660
Chris Eberle <eberle1080@gmail.com>
2024-05-31 07:45:09 -0700

    feat: Add a client which automatically appends fixed header values (#423)
    * add a client which automatically appends fixed header values. Meant to be used for API keys or basic auth.
    
    * fix linter
    
    * more linter issues
    
    * add headers
    
    * run gofumpt

--- connector/params.go
+++ connector/params.go
@@ -53,7 +53,7 @@ func WithClient(ctx context.Context, client *http.Client, config *oauth2.Config,
 ) Option {
 	return func(params *connectorParams) {
 		options := []common.OAuthOption{
-			common.WithClient(client),
+			common.WithOAuthClient(client),
 			common.WithOAuthConfig(config),
 			common.WithOAuthToken(token),
 		}
