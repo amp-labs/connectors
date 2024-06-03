942cb5055b4ac03d3c819f6a51520a86f7e55f79
Joseph Karage <josephbkarage@gmail.com>
2024-06-03 14:16:05 +0300

    fix: avoid panics incase nil response (#375)

--- common/types.go
+++ common/types.go
@@ -82,6 +82,9 @@ var (
 
 	// ErrMetadataLoadFailure is returned when files that contain metadata for a connector cannot be loaded.
 	ErrMetadataLoadFailure = errors.New("cannot load metadata")
+
+	// ErrEmptyResponse is returned when the jsonResponse is nil.
+	ErrEmptyJSONHTTPResponse = errors.New("empty json http response")
 )
 
 // ReadParams defines how we are reading data from a SaaS API.
