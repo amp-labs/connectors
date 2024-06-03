13dc3f12fe7e0a9b031fb6ceadf4f9ff74d8bbd2
Joseph Karage <josephbkarage@gmail.com>
2024-06-03 13:55:14 +0300

    feat: enable AWeber proxy connector (#420)

--- providers/catalog.go
+++ providers/catalog.go
@@ -854,7 +854,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
