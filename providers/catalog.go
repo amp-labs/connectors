30b9090091adbe634619fd502d264c87311cc3fb
Sanjay Mitrahsoft <sanjaykanth.a@mitrahsoft.com>
2024-06-06 19:56:27 +0530

    feat: enable ServiceNow proxy support

--- providers/catalog.go
+++ providers/catalog.go
@@ -2039,7 +2039,7 @@ var catalog = CatalogType{ // nolint:gochecknoglobals
 				Upsert: false,
 				Delete: false,
 			},
-			Proxy:     false,
+			Proxy:     true,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
