2e0dce54023ffd38f76a0705069cfb6ab3376212
Sanjay <sanjaykanth.a@mitrahsoft.com>
2024-06-20 19:48:51 +0530

    Added separate file for Hive connector

---
+++ providers/hive.go
@@ -0,0 +1,28 @@
+package providers
+
+const Hive Provider = "hive"
+
+func init() {
+	// Hive Connector Configuration
+	SetInfo(Hive, ProviderInfo{
+		AuthType: ApiKey,
+		BaseURL:  "https://app.hive.com",
+		ApiKeyOpts: &ApiKeyOpts{
+			Type:       InHeader,
+			HeaderName: "api_key",
+			DocsURL:    "https://developers.hive.com/reference/api-keys-and-auth",
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+}
