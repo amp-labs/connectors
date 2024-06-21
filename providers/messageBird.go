6dc25323dda52d19b8c80b588e095846f3dfc52a
Joseph Karage <josephbkarage@gmail.com>
2024-06-21 08:40:42 +0300

    fix: Update messageBird connector (#570)

--- providers/messageBird.go
+++ providers/messageBird.go
@@ -1,3 +1,29 @@
 package providers
 
 const MessageBird Provider = "messageBird"
+
+func init() {
+	// MessageBird configuration
+	SetInfo(MessageBird, ProviderInfo{
+		AuthType: ApiKey,
+		BaseURL:  "https://api.bird.com",
+		ApiKeyOpts: &ApiKeyOpts{
+			Type:        InHeader,
+			HeaderName:  "Authorization",
+			ValuePrefix: "AccessKey ",
+			DocsURL:     "https://docs.bird.com/api",
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+}
