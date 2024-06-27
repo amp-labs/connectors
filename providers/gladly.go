d9e1ef5bb36e8f2663fa73d8c153577a1cf0bd52
mohammed <mohammedfuta@gmail.com>
2024-06-27 16:49:37 +0000

    feat: Add Gladly connector

---
+++ providers/gladly.go
@@ -0,0 +1,26 @@
+package providers
+
+const Gladly Provider = "gladly"
+
+func init() {
+	// Gladly configuration
+	SetInfo(Gladly, ProviderInfo{
+        AuthType: Basic,
+        BaseURL: "https://{{.workspace}}.gladly.qa",
+        // this is my organization/workspace = partner-withampersand.us-uat
+        // for live production "https://{{organization}}.gladly.com",
+        Support: Support {
+            BulkWrite: BulkWriteSupport{
+             Insert: false,
+             Update: false,
+             Upsert: false,
+             Delete: false,
+            },
+            Proxy:     false,
+            Read:      false,
+            Subscribe: false,
+            Write:     false,
+        },
+        PostAuthInfoNeeded: false,
+    })
+}
