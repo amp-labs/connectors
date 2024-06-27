5667ab181d7a0add293d835175048097ad748ac2
mohammed <mohammedfuta@gmail.com>
2024-06-27 17:36:02 +0000

    included gladly dev and prod

--- providers/gladly.go
+++ providers/gladly.go
@@ -1,14 +1,34 @@
 package providers
 
-const Gladly Provider = "gladly"
+const (
+	GladlyDev  Provider = "gladlyDev"
+	GladlyProd Provider = "gladlyProd"
+)
 
 func init() {
-	// Gladly configuration
-	SetInfo(Gladly, ProviderInfo{
+	// GladlyDev configuration
+	SetInfo(GladlyDev, ProviderInfo{
 		AuthType: Basic,
 		BaseURL:  "https://{{.workspace}}.gladly.qa",
-		// this is my organization/workspace = partner-withampersand.us-uat
-		// for live production "https://{{organization}}.gladly.com",
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		PostAuthInfoNeeded: false,
+	})
+
+	// GladlyProd configuration
+	SetInfo(GladlyProd, ProviderInfo{
+		AuthType: Basic,
+		BaseURL:  "https://{{.workspace}}.gladly.com",
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
