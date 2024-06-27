6d6a330080c1aa8febb3550d0118288dccb0de8f
mohammed <mohammedfuta@gmail.com>
2024-06-27 16:57:23 +0000

    lint corrected

--- providers/gladly.go
+++ providers/gladly.go
@@ -5,22 +5,22 @@ const Gladly Provider = "gladly"
 func init() {
 	// Gladly configuration
 	SetInfo(Gladly, ProviderInfo{
-        AuthType: Basic,
-        BaseURL: "https://{{.workspace}}.gladly.qa",
-        // this is my organization/workspace = partner-withampersand.us-uat
-        // for live production "https://{{organization}}.gladly.com",
-        Support: Support {
-            BulkWrite: BulkWriteSupport{
-             Insert: false,
-             Update: false,
-             Upsert: false,
-             Delete: false,
-            },
-            Proxy:     false,
-            Read:      false,
-            Subscribe: false,
-            Write:     false,
-        },
-        PostAuthInfoNeeded: false,
-    })
+		AuthType: Basic,
+		BaseURL:  "https://{{.workspace}}.gladly.qa",
+		// this is my organization/workspace = partner-withampersand.us-uat
+		// for live production "https://{{organization}}.gladly.com",
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		PostAuthInfoNeeded: false,
+	})
 }
