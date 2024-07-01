5a2592a0ceddcf1032ded737ccbe546282ff4020
Cobalt0s <constantin.y.koval@gmail.com>
2024-07-02 00:05:38 +0300

    tidy: remove restApiUrl provider option (Outreach)

--- outreach/read.go
+++ outreach/read.go
@@ -2,41 +2,36 @@ package outreach
 
 import (
 	"context"
-	"net/url"
 
 	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
 )
 
 func (c *Connector) Read(ctx context.Context, config common.ReadParams) (*common.ReadResult, error) {
-	var (
-		res    *common.JSONHTTPResponse
-		err    error
-		fields []string
-	)
+	url, err := c.buildReadURL(config)
+	if err != nil {
+		return nil, err
+	}
 
-	if len(config.NextPage) > 0 {
-		// If NextPage is set, then we're reading the next page of results.
-		// The NextPage URL has all the necessary parameters.
-		res, err = c.Client.Get(ctx, config.NextPage.String())
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		fullURL, err := url.JoinPath(c.BaseURL, config.ObjectName)
-		if err != nil {
-			return nil, err
-		}
-
-		res, err = c.Client.Get(ctx, fullURL)
-		if err != nil {
-			return nil, err
-		}
+	res, err := c.Client.Get(ctx, url.String())
+	if err != nil {
+		return nil, err
 	}
 
 	return common.ParseResult(res, getTotalSize,
 		getRecords,
 		getNextRecordsURL,
 		getMarshaledData,
-		fields,
+		config.Fields,
 	)
+}
+
+func (c *Connector) buildReadURL(config common.ReadParams) (*urlbuilder.URL, error) {
+	if len(config.NextPage) > 0 {
+		// If NextPage is set, then we're reading the next page of results.
+		// The NextPage URL has all the necessary parameters.
+		return constructURL(config.NextPage.String())
+	}
+
+	return c.getURL(config.ObjectName)
 }
