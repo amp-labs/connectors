b08afe1ce87d862c879876116cbc985e49c4cb04
mohammed <mohammedfuta@gmail.com>
2024-07-02 15:09:46 +0000

    feat: Add Coda connector

---
+++ providers/coda.go
@@ -0,0 +1,31 @@
+package providers
+
+const Coda Provider = "coda"
+
+func init() {
+	// Coda Configuration
+	SetInfo(Coda, ProviderInfo{
+		AuthType: ApiKey,
+		BaseURL:  "https://coda.io/apis",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Header,
+			Header: &ApiKeyOptsHeader{
+				Name:        "Authorization",
+				ValuePrefix: "Bearer ",
+			},
+			DocsURL: "https://coda.io/developers/apis/v1#section/Introduction",
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+}
