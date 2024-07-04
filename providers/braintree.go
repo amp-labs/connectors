825b2b6324358a4a9d561e3b2461a65379d45888
mohammed <mohammedfuta@gmail.com>
2024-07-04 21:06:27 +0000

    lint corrected

--- providers/braintree.go
+++ providers/braintree.go
@@ -5,20 +5,20 @@ const Braintree Provider = "braintree"
 func init() {
 	// Braintree Configuration
 	SetInfo(Braintree, ProviderInfo{
-        AuthType: Basic,
-        BaseURL: "https://payments.sandbox.braintree-api.com/graphql",
-		Support: Support {
+		AuthType: Basic,
+		BaseURL:  "https://payments.sandbox.braintree-api.com/graphql",
+		Support: Support{
 			BulkWrite: BulkWriteSupport{
-             Insert: false,
-             Update: false,
-             Upsert: false,
-             Delete: false,
-            },
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
 			Proxy:     false,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
 		},
-        PostAuthInfoNeeded: false,
-    })
+		PostAuthInfoNeeded: false,
+	})
 }
