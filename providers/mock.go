94cb2593500852ac85dde1d6f3770a91508e906c
RajatPawar <18614743+RajatPawar@users.noreply.github.com>
2024-07-08 17:57:26 +0530

    Test out action work after role update

--- providers/mock.go
+++ providers/mock.go
@@ -2,5 +2,5 @@ package providers
 
 // Mock is a mock provider that can be used for testing.
 // When the mock connector is used, it's added to the catalog
-// manually. That's why there's no init() function in this file.
+// manually. That is why there's no init() function in this file.
 const Mock Provider = "mock"
