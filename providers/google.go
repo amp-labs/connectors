6f6e2e98bf817a616cf21a1394215a925588df91
Sanjay <sanjaykanth.a@mitrahsoft.com>
2024-07-09 11:57:32 +0530

    Added AuthURLParams options for google ads

--- providers/google.go
+++ providers/google.go
@@ -75,6 +75,7 @@ func init() {
 		Oauth2Opts: &Oauth2Opts{
 			GrantType:                 AuthorizationCode,
 			AuthURL:                   "https://accounts.google.com/o/oauth2/v2/auth",
+			AuthURLParams:             map[string]string{"access_type": "offline"},
 			TokenURL:                  "https://oauth2.googleapis.com/token",
 			ExplicitScopesRequired:    true,
 			ExplicitWorkspaceRequired: false,
