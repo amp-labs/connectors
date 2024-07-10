a73307feb3be7f1772f2885000db46c3083aa43b
RajatPawar <18614743+RajatPawar@users.noreply.github.com>
2024-07-10 13:28:07 +0530

    ignore: Test PR

--- providers/mock.go
+++ providers/mock.go
@@ -3,4 +3,5 @@ package providers
 // Mock is a mock provider that can be used for testing.
 // When the mock connector is used, it's added to the catalog
 // manually. That's why there's no init() function in this file.
+// Test.
 const Mock Provider = "mock"
