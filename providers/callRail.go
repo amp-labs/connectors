b61527c3cdd24ba4f2f39b409452a1dffeb4a274
prabhsimran@identityscience.ai <mohammedfuta@gmail.com>
2024-07-12 00:29:30 +0000

    lint corrected

--- providers/callRail.go
+++ providers/callRail.go
@@ -5,37 +5,27 @@ const CallRail Provider = "callRail"
 func init() {
 	// CallRail Configuration
 	SetInfo(CallRail, ProviderInfo{
-
-        AuthType: ApiKey,
-        BaseURL: "https://api.callrail.com",
-        // For 6sense, the header needs to be 'Authorization: Token {your_api_key}'
-        ApiKeyOpts: &ApiKeyOpts{
+		AuthType: ApiKey,
+		BaseURL:  "https://api.callrail.com",
+		ApiKeyOpts: &ApiKeyOpts{
 			AttachmentType: Header,
 			Header: &ApiKeyOptsHeader{
-				Name: "Authorization",
+				Name:        "Authorization",
 				ValuePrefix: "Token token=",
 			},
-            DocsURL: "https://apidocs.callrail.com/#getting-started",
+			DocsURL: "https://apidocs.callrail.com/#getting-started",
 		},
-        // For another provider, ValuePrefix may not be needed
-        // For example, if the expected header is 'X-Api-Key: {your_api_key}'
-        /*
-        ApiKeyOpts: &ApiKeyOpts{
-			HeaderName: "X-Api-Key",
-            DocsURL: "https://api.6sense.com/docs/#get-your-api-token",
-		}, */
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
-             Insert: false,
-             Update: false,
-             Upsert: false,
-             Delete: false,
-            },
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
 			Proxy:     false,
 			Read:      false,
 			Subscribe: false,
 			Write:     false,
 		},
-   
 	})
 }
