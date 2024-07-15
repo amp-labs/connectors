e847ce19ea48f30cebf820a65d893a153e1b9ecb
Joseph Karage <josephbkarage@gmail.com>
2024-07-15 08:25:27 +0300

    feat: Add Chargebee auth connector (#651)
    Co-authored-by: Rajat Pawar <18614743+RajatPawar@users.noreply.github.com>

---
+++ providers/chargebee.go
@@ -0,0 +1,25 @@
+package providers
+
+const Chargebee Provider = "chargebee"
+
+func init() {
+	// Chargebee connfiguration
+	// workspace maps to site
+	SetInfo(Chargebee, ProviderInfo{
+		AuthType: Basic,
+		BaseURL:  "https://{{.workspace}}.chargebee.com/api",
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		PostAuthInfoNeeded: false,
+	})
+}
