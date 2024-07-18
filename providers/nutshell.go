1de5f20cd2bc489af2d0d50c031a80d2512d0921
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2024-07-18 20:28:47 +0530

    feat: Add Nutshell Connector (#613)
    Co-authored-by: Rajat Pawar <18614743+RajatPawar@users.noreply.github.com>

---
+++ providers/nutshell.go
@@ -0,0 +1,24 @@
+package providers
+
+const Nutshell Provider = "nutshell"
+
+func init() {
+	// Nutshell Configuration
+	SetInfo(Nutshell, ProviderInfo{
+		AuthType: Basic,
+		BaseURL:  "https://app.nutshell.com",
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		PostAuthInfoNeeded: false,
+	})
+}
