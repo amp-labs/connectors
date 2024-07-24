e2f9358196717f869668bcf3d348921becddf2a3
Cobalt0s <constantin.y.koval@gmail.com>
2024-07-24 18:41:05 +0300

    feat: Deep connector Pipeliner (Metadata)

--- test/pipeliner/metadata/main.go
+++ test/pipeliner/metadata/main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log/slog"
 	"os/signal"
 	"strings"
 	"syscall"
 
 	"github.com/amp-labs/connectors/common"
-	connTest "github.com/amp-labs/connectors/test/salesforce"
+	connTest "github.com/amp-labs/connectors/test/pipeliner"
 	"github.com/amp-labs/connectors/test/utils"
 	"github.com/amp-labs/connectors/test/utils/mockutils"
 )
 
 var (
-	objectName = "Organization" // nolint: gochecknoglobals
+	objectName = "Leads" // nolint: gochecknoglobals
 )
 
 // We want to compare fields returned by read and schema properties provided by metadata methods.
@@ -27,15 +27,14 @@ func main() {
 	// Set up slog logging.
 	utils.SetupLogging()
 
-	conn := connTest.GetSalesforceConnector(ctx)
+	conn := connTest.GetPipelinerConnector(ctx)
 	defer utils.Close(conn)
 
 	response, err := conn.Read(ctx, common.ReadParams{
 		ObjectName: objectName,
-		Fields:     []string{"*"},
 	})
 	if err != nil {
-		utils.Fail("error reading from Salesforce", "error", err)
+		utils.Fail("error reading from Pipeliner", "error", err)
 	}
 
 	if response.Rows == 0 {
@@ -46,28 +45,31 @@ func main() {
 		objectName,
 	})
 	if err != nil {
-		utils.Fail("error listing metadata for Salesforce", "error", err)
+		utils.Fail("error listing metadata for Pipeliner", "error", err)
 	}
 
-	fmt.Println("Compare object metadata against endpoint response:")
+	slog.Info("Compare object metadata against endpoint response:")
 
 	data := sanitizeReadResponse(response.Data[0].Raw)
 
-	mismatchErr := mockutils.ValidateReadConformsMetadata(strings.ToLower(objectName), data, metadata)
+	mismatchErr := mockutils.ValidateReadConformsMetadata(objectName, data, metadata)
 	if mismatchErr != nil {
 		utils.Fail("schema and payload response have mismatching fields", "error", mismatchErr)
 	} else {
-		fmt.Println("==> success fields match.")
+		slog.Info("==> success fields match.")
 	}
 }
 
 func sanitizeReadResponse(response map[string]any) map[string]any {
-	// every Salesforce response attached attributes object with type and url of a resource.
-	// this attribute field will not appear in metadata response, so we shall remove it.
+	// Pipeliner has some extra fields attached starting with `cf_` prefix.
+	// Example Leads:
+	//	cf_other_lead_source
+	//	cf_lead_source1
+	//	cf_lead_source1_id
 	crucialFields := make(map[string]any)
 
 	for field, v := range response {
-		if field != "attributes" {
+		if !strings.HasPrefix(field, "cf_") {
 			crucialFields[field] = v
 		}
 	}
