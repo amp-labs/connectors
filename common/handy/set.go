1e5cb8cc14e3bf6fdf01abcc2ca8d319b43c6073
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-07-25 16:26:55 +0300

    tidy: Metadata locator becomes general locator (#717)

--- common/handy/set.go
+++ common/handy/set.go
@@ -1,17 +1,21 @@
 package handy
 
+// Set data structure that can hold any type of data.
 type Set[T comparable] map[T]bool
 
+// Add will add a list of values to the unique set. Repetitions will be omitted.
 func (s Set[T]) Add(values []T) {
 	for _, value := range values {
 		s.AddOne(value)
 	}
 }
 
+// AddOne will add single value to the unique set.
 func (s Set[T]) AddOne(value T) {
 	s[value] = true
 }
 
+// List returns unique set in a shape of slice.
 func (s Set[T]) List() []T {
 	list := make([]T, 0)
 	for v := range s {
