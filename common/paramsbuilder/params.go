7b955e396bab5c34b3feb9bbed605c04db13dcdb
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-07-27 18:24:35 +0300

    add: Build connector options, genrics (#711)

--- common/paramsbuilder/params.go
+++ common/paramsbuilder/params.go
@@ -22,6 +22,17 @@ var (
 	ErrNoSupportedModule = errors.New("no supported module was chosen")
 )
 
+// Apply will apply options to construct a ready to go set of parameters.
+// This is a generalized constructor of parameters used to initialize any connector.
+// To qualify as a parameter one must have data validation laid out.
+func Apply[P ParamAssurance](params P, opts []func(params *P)) (*P, error) {
+	for _, opt := range opts {
+		opt(&params)
+	}
+
+	return &params, params.ValidateParams()
+}
+
 // Client params sets up authenticated proxy HTTP client
 // This can be reused among other param builders by composition.
 type Client struct {
