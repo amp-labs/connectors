e7d8cb999ef0b150d456466dadff76e4da0148ee
Cobalt0s <constantin.y.koval@gmail.com>
2024-07-28 18:54:54 +0300

    add: Logo - Zendesk Support

--- providers/zendesk.go
+++ providers/zendesk.go
@@ -1,11 +1,11 @@
 package providers
 
 const (
-	ZendeskChat    Provider = "zendeskChat"
+	ZendeskChat    Provider = "zendeskSupport"
 	ZendeskSupport Provider = "zendeskSupport"
 )
 
-func init() {
+func init() { // nolint:funlen
 	// Zendesk Support configuration
 	SetInfo(ZendeskSupport, ProviderInfo{
 		DisplayName: "Zendesk Support",
@@ -17,6 +17,16 @@ func init() {
 			TokenURL:                  "https://{{.workspace}}.zendesk.com/oauth/tokens",
 			ExplicitScopesRequired:    true,
 			ExplicitWorkspaceRequired: true,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722102329/media/zendeskSupport_1722102328.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722102329/media/zendeskSupport_1722102328.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722102362/media/zendeskSupport_1722102361.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722102362/media/zendeskSupport_1722102361.svg",
+			},
 		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
@@ -42,6 +52,16 @@ func init() {
 			TokenURL:                  "https://www.zopim.com/oauth2/token",
 			ExplicitScopesRequired:    true,
 			ExplicitWorkspaceRequired: true,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722102329/media/zendeskSupport_1722102328.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722102329/media/zendeskSupport_1722102328.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722102362/media/zendeskSupport_1722102361.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722102362/media/zendeskSupport_1722102361.svg",
+			},
 		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
