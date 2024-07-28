e5db195129504892715eaa86c4ff0f29903b3a13
Cobalt0s <constantin.y.koval@gmail.com>
2024-07-28 19:35:34 +0300

    add: Icon - Anthropic

--- providers/anthropic.go
+++ providers/anthropic.go
@@ -14,6 +14,16 @@ func init() {
 			},
 			DocsURL: "https://docs.anthropic.com/en/api/getting-started#authentication",
 		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722184511/media/anthropic_1722184511.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722184511/media/anthropic_1722184511.jpg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722184511/media/anthropic_1722184511.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722184511/media/anthropic_1722184511.jpg",
+			},
+		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
