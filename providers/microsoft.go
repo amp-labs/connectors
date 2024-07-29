a9e8f1ec272a7193228d8149d45aa8a38fe7a5e7
Lauren Long <laurenzlong@users.noreply.github.com>
2024-07-29 16:48:57 -0700

    remove powerBI and sharePoint

--- providers/microsoft.go
+++ providers/microsoft.go
@@ -3,12 +3,12 @@ package providers
 // Supported Microsoft Products includes OneDrive Outlook Excel
 // Edge Extensions Sharepoint OneNote Notifications Todos Teams Insights
 // Planner and Personal Contacts.
-const MicrosoftOffice365 Provider = "microsoftOffice365"
+const Microsoft Provider = "microsoft"
 
 func init() {
 	// Microsoft Office 365 Configuration
-	SetInfo(MicrosoftOffice365, ProviderInfo{
-		DisplayName: "Microsoft Office 365",
+	SetInfo(Microsoft, ProviderInfo{
+		DisplayName: "Microsoft",
 		AuthType:    Oauth2,
 		BaseURL:     "https://graph.microsoft.com",
 		Oauth2Opts: &Oauth2Opts{
