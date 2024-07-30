47fc54d53c04b6af1a576122d164d3053d8fd035
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-07-30 21:30:01 +0300

    feat: Enable Read+Write: Dynamics CRM (#699)

--- providers/dynamics.go
+++ providers/dynamics.go
@@ -60,9 +60,9 @@ func init() {
 				Delete: false,
 			},
 			Proxy:     true,
-			Read:      false,
+			Read:      true,
 			Subscribe: false,
-			Write:     false,
+			Write:     true,
 		},
 	})
 }
