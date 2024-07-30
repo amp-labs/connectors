78c635dfebc3052d4c764d5248e32839b77ec5de
Cobalt0s <constantin.y.koval@gmail.com>
2024-07-30 16:59:49 +0300

    save

--- providers/anthropic.go
+++ providers/anthropic.go
@@ -16,12 +16,12 @@ func init() {
 		},
 		Media: &Media{
 			DarkMode: &MediaTypeDarkMode{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722184511/media/anthropic_1722184511.jpg",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722184511/media/anthropic_1722184511.jpg",
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722347823/media/anthropic_1722347823.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722347778/media/anthropic_1722347778.svg",
 			},
 			Regular: &MediaTypeRegular{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722184511/media/anthropic_1722184511.jpg",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722184511/media/anthropic_1722184511.jpg",
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722347823/media/anthropic_1722347823.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722347800/media/anthropic_1722347800.svg",
 			},
 		},
 		Support: Support{
