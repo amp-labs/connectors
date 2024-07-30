5dd94479809e7e4d058894341b73e30e9f10f594
Bea <beadko@gmail.com>
2024-07-31 03:26:47 +0700

    Feat: Add media URLs to Mailgun (#734)

--- providers/mailgun.go
+++ providers/mailgun.go
@@ -20,5 +20,15 @@ func init() {
 			Write:     false,
 		},
 		PostAuthInfoNeeded: false,
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722071456/media/mailgun_1722071455.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722071433/media/mailgun_1722071431.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722071495/media/mailgun_1722071493.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722071474/media/mailgun_1722071473.svg",
+			},
+		},
 	})
 }
