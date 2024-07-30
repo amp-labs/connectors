4dab5b772d62e4931842f3ccad054e39fa7a2402
Bea <beadko@gmail.com>
2024-07-31 03:27:29 +0700

    Feat: Add media URLs for Survey Monkey (#731)
    * Add media for StackExchange
    
    * Add media for SurveyMonkey

--- providers/surveyMonkey.go
+++ providers/surveyMonkey.go
@@ -27,5 +27,15 @@ func init() {
 			Subscribe: false,
 			Write:     false,
 		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722064886/media/surveyMonkey_1722064885.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722064863/media/surveyMonkey_1722064862.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722064941/media/surveyMonkey_1722064939.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722064920/media/surveyMonkey_1722064919.svg",
+			},
+		},
 	})
 }
