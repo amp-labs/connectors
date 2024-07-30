500b7049b88516dfee7e9039215967d16fcf768e
Sanjay <sanjaykanth.a@mitrahsoft.com>
2024-07-30 17:33:58 +0530

    feat: Add media to zoom

--- providers/zoom.go
+++ providers/zoom.go
@@ -18,6 +18,17 @@ func init() {
 				ScopesField: "scope",
 			},
 		},
+		//nolint:lll
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722325775/media/const%20Zoom%20Provider%20%3D%20%22zoom%22_1722325765.png",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722325874/media/const%20Zoom%20Provider%20%3D%20%22zoom%22_1722325874.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722325775/media/const%20Zoom%20Provider%20%3D%20%22zoom%22_1722325765.png",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722325900/media/const%20Zoom%20Provider%20%3D%20%22zoom%22_1722325899.svg",
+			},
+		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
