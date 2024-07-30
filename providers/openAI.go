ef989e6d7fb1b79b6ff6ebae422ad385730537e2
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-07-31 00:08:30 +0300

    add: Logo - OpenAI (#751)
    * add: Logo - OpenAI
    
    * save

--- providers/openAI.go
+++ providers/openAI.go
@@ -15,6 +15,16 @@ func init() {
 			},
 			DocsURL: "https://platform.openai.com/docs/api-reference/api-keys",
 		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722348143/media/openAI_1722348141.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722348197/media/openAI_1722348196.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722348143/media/openAI_1722348141.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722348211/media/openAI_1722348211.svg",
+			},
+		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
