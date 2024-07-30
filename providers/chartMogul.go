39b86eebdd86745d2313e08cbdb25decd22ac0a3
Bea <beadko@gmail.com>
2024-07-31 03:24:27 +0700

    Feat: Add media to ChartMogul (#741)

--- providers/chartMogul.go
+++ providers/chartMogul.go
@@ -20,5 +20,15 @@ func init() {
 			Write:     false,
 		},
 		PostAuthInfoNeeded: false,
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722071123/media/chartMogul_1722071122.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722071123/media/chartMogul_1722071122.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722071196/media/chartMogul_1722071194.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722071151/media/chartMogul_1722071150.svg",
+			},
+		},
 	})
 }
