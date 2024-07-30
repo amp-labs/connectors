949ab23cf4b9e35049e4e805e2777b78ade0f252
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-07-31 00:08:43 +0300

    add: Icon - Anthropic (#750)
    * add: Icon - Anthropic
    
    * save

--- providers/anthropic.go
+++ providers/anthropic.go
@@ -14,6 +14,16 @@ func init() {
 			},
 			DocsURL: "https://docs.anthropic.com/en/api/getting-started#authentication",
 		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722347823/media/anthropic_1722347823.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722347778/media/anthropic_1722347778.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722347823/media/anthropic_1722347823.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722347800/media/anthropic_1722347800.svg",
+			},
+		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
