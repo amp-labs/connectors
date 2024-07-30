2dab1f1ed6385eb00b99ff29b6bfe53a8b8b3ad4
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-07-31 00:01:45 +0300

    add: Logo - Monday (#745)
    * add: Logo - Monday
    
    * save

--- providers/monday.go
+++ providers/monday.go
@@ -18,6 +18,16 @@ func init() {
 				ScopesField: "scope",
 			},
 		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722345745/media/monday_1722345745.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722345579/media/monday_1722345579.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722345745/media/monday_1722345745.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722345545/media/monday_1722345544.svg",
+			},
+		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
