efa8a91503582e5e81f651d791e08747e3fa9481
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2024-08-01 04:07:19 +0530

    feat: Add media to rebilly (#785)

--- providers/rebilly.go
+++ providers/rebilly.go
@@ -15,6 +15,17 @@ func init() {
 			},
 			DocsURL: "https://www.rebilly.com/catalog/all/section/authentication/manage-api-keys",
 		},
+		//nolint:lll
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722408171/media/const%20Rebilly%20Provider%20%3D%20%22rebilly%22_1722408170.png",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722408423/media/const%20Rebilly%20Provider%20%3D%20%22rebilly%22_1722408423.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722408171/media/const%20Rebilly%20Provider%20%3D%20%22rebilly%22_1722408170.png",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722408423/media/const%20Rebilly%20Provider%20%3D%20%22rebilly%22_1722408423.svg",
+			},
+		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
