0b012a98a61b87bf900222755925e6cebee9e084
Sanjay <sanjaykanth.a@mitrahsoft.com>
2024-07-31 16:51:30 +0530

    feat: Add media to paddle

--- providers/paddle.go
+++ providers/paddle.go
@@ -43,6 +43,17 @@ func init() {
 			},
 			DocsURL: "https://developer.paddle.com/api-reference/about/authentication",
 		},
+		//nolint:lll
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722407082/media/const%20%20%20%20Paddle%20%20%20Provider%20%3D%20%22paddle%22_1722407081.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722407118/media/const%20%20%20%20Paddle%20%20%20Provider%20%3D%20%22paddle%22_1722407117.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722407082/media/const%20%20%20%20Paddle%20%20%20Provider%20%3D%20%22paddle%22_1722407081.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722407118/media/const%20%20%20%20Paddle%20%20%20Provider%20%3D%20%22paddle%22_1722407117.svg",
+			},
+		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
