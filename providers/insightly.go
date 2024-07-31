0cb895de0fd67f02dc773b035c72cfab8f5fedef
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2024-07-31 23:06:53 +0530

    feat: Add media to Insightly (#790)

--- providers/insightly.go
+++ providers/insightly.go
@@ -8,6 +8,17 @@ func init() {
 		DisplayName: "Insightly",
 		AuthType:    Basic,
 		BaseURL:     "https://api.insightly.com",
+		//nolint:lll
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722411056/media/const%20Insightly%20%3D%20%22insightly%22_1722411055.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722411001/media/const%20Insightly%20%3D%20%22insightly%22_1722411000.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722411056/media/const%20Insightly%20%3D%20%22insightly%22_1722411055.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722411001/media/const%20Insightly%20%3D%20%22insightly%22_1722411000.svg",
+			},
+		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
