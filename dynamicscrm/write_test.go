a81c7f1d6f5aaf30a83fa356ebab7902650dbbe5
Cobalt0s <constantin.y.koval@gmail.com>
2024-08-01 18:01:42 +0300

    add: Define Write testing, dynamicsCRM

--- dynamicscrm/write_test.go
+++ dynamicscrm/write_test.go
@@ -1,47 +1,39 @@
 package dynamicscrm
 
 import (
-	"context"
 	"errors"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
-	"strings"
 	"testing"
 
+	"github.com/amp-labs/connectors"
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/interpreter"
 	"github.com/amp-labs/connectors/test/utils/mockutils"
 	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
-	"github.com/go-test/deep"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
 )
 
 func TestWrite(t *testing.T) { // nolint:funlen,cyclop
 	t.Parallel()
 
-	tests := []struct {
-		name         string
-		input        common.WriteParams
-		server       *httptest.Server
-		connector    Connector
-		expected     *common.WriteResult
-		expectedErrs []error
-	}{
+	tests := []testroutines.Write{
 		{
-			name:         "Write object must be included",
-			server:       mockserver.Dummy(),
-			expectedErrs: []error{common.ErrMissingObjects},
+			Name:         "Write object must be included",
+			Input:        common.WriteParams{},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
 		},
 		{
-			name:         "Mime response header expected",
-			input:        common.WriteParams{ObjectName: "fax"},
-			server:       mockserver.Dummy(),
-			expectedErrs: []error{interpreter.ErrMissingContentType},
+			Name:         "Mime response header expected",
+			Input:        common.WriteParams{ObjectName: "fax"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{interpreter.ErrMissingContentType},
 		},
 		{
-			name:  "Correct error message is understood from JSON response",
-			input: common.WriteParams{ObjectName: "fax"},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Correct error message is understood from JSON response",
+			Input: common.WriteParams{ObjectName: "fax"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusBadRequest)
 				mockutils.WriteBody(w, `{
@@ -51,74 +43,42 @@ func TestWrite(t *testing.T) { // nolint:funlen,cyclop
 					}
 				}`)
 			})),
-			expectedErrs: []error{
+			ExpectedErrs: []error{
 				common.ErrBadRequest,
 				errors.New("Resource not found for the segment 'conacs'"), // nolint:goerr113
 			},
 		},
 		{
-			name:  "Write must act as a Create",
-			input: common.WriteParams{ObjectName: "fax"},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Write must act as a Create",
+			Input: common.WriteParams{ObjectName: "fax"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				mockutils.RespondNoContentForMethod(w, r, "POST")
 			})),
-			expected:     &common.WriteResult{Success: true},
-			expectedErrs: nil,
+			Expected:     &common.WriteResult{Success: true},
+			ExpectedErrs: nil,
 		},
 		{
-			name:  "Write must act as an Update",
-			input: common.WriteParams{ObjectName: "fax", RecordId: "dd2f7870-3fe8-ee11-a204-0022481f9e3c"},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Write must act as an Update",
+			Input: common.WriteParams{ObjectName: "fax", RecordId: "dd2f7870-3fe8-ee11-a204-0022481f9e3c"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				mockutils.RespondNoContentForMethod(w, r, "PATCH")
 			})),
-			expected:     &common.WriteResult{Success: true},
-			expectedErrs: nil,
+			Expected:     &common.WriteResult{Success: true},
+			ExpectedErrs: nil,
 		},
 	}
 
 	for _, tt := range tests { // nolint:dupl
 		// nolint:varnamelen
-		tt := tt // rebind, omit loop side effects for parallel goroutine
-		t.Run(tt.name, func(t *testing.T) {
+		tt := tt
+		t.Run(tt.Name, func(t *testing.T) {
 			t.Parallel()
 
-			defer tt.server.Close()
-
-			ctx := context.Background()
-
-			connector, err := NewConnector(
-				WithAuthenticatedClient(http.DefaultClient),
-				WithWorkspace("test-workspace"),
-			)
-			if err != nil {
-				t.Fatalf("%s: error in test while constructing connector %v", tt.name, err)
-			}
-
-			// for testing we want to redirect calls to our server
-			connector.setBaseURL(tt.server.URL)
-
-			// start of tests
-			output, err := connector.Write(ctx, tt.input)
-			if len(tt.expectedErrs) == 0 && err != nil {
-				t.Fatalf("%s: expected no errors, got: (%v)", tt.name, err)
-			}
-
-			if len(tt.expectedErrs) != 0 && err == nil {
-				t.Fatalf("%s: expected errors (%v), but got nothing", tt.name, tt.expectedErrs)
-			}
-
-			for _, expectedErr := range tt.expectedErrs {
-				if !errors.Is(err, expectedErr) && !strings.Contains(err.Error(), expectedErr.Error()) {
-					t.Fatalf("%s: expected Error: (%v), got: (%v)", tt.name, expectedErr, err)
-				}
-			}
-
-			if !reflect.DeepEqual(output, tt.expected) {
-				diff := deep.Equal(output, tt.expected)
-				t.Fatalf("%s:, \nexpected: (%v), \ngot: (%v), \ndiff: (%v)", tt.name, tt.expected, output, diff)
-			}
+			tt.Run(t, func() (connectors.WriteConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
 		})
 	}
 }
