772480c5808cb002a609f20b823aa04b4ebbd592
Lauren Long <laurenzlong@users.noreply.github.com>
2024-08-01 11:40:05 -0700

    add logo for Clickup

--- providers/clickup.go
+++ providers/clickup.go
@@ -27,5 +27,15 @@ func init() {
 			Subscribe: false,
 			Write:     false,
 		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722537393/media/clickup.com_1722537393.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722537424/media/clickup.com_1722537424.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722536894/media/clickup.com_1722536893.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722537034/media/clickup.com_1722537033.svg",
+			},
+		},
 	})
 }
