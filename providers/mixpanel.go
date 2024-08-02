47a2f046daec19cf04a32a9fcbe21bcdb13e07cb
jkarage <josephbkarage@gmail.com>
2024-08-02 14:13:17 +0300

    feat: Add icon/logos

--- providers/mixpanel.go
+++ providers/mixpanel.go
@@ -4,7 +4,7 @@ const Mixpanel Provider = "mixpanel"
 
 func init() {
 	// Mixpanel configuration
-	// serviceSubdomain cab either be [api, api-eu, data,data-eu].
+	// apiserviceSubdomain cab either be [api, api-eu, data,data-eu].
 	// Supported Mixpanel APIs
 	// -	Ingestion API
 	// -	Identity API
@@ -12,7 +12,17 @@ func init() {
 	// -	Data Pipelines API
 	SetInfo(Mixpanel, ProviderInfo{
 		AuthType: Basic,
-		BaseURL:  "https://{{.serviceSubdomain}}.mixpanel.com",
+		BaseURL:  "https://{{.apiserviceSubdomain}}.mixpanel.com",
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722597081/media/mixpanel.com_1722597079.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722597119/media/mixpanel.com_1722597118.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722597101/media/mixpanel.com_1722597100.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722597140/media/mixpanel.com_1722597139.svg",
+			},
+		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
