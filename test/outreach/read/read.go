31dcf87e42fac421ea85ce0f04c798285cd32030
JKarage <josephbkarage@gmail.com>
2024-08-05 20:44:12 +0300

    fix: Outreach Read to include Id (#843)

--- test/outreach/read/read.go
+++ test/outreach/read/read.go
@@ -20,17 +20,76 @@ func main() {
 
 	conn := outreach.GetOutreachConnector(context.Background(), DefaultCredsFile)
 
-	err = testReadConnector(context.Background(), conn)
+	err = testReadSequences(context.Background(), conn)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	err = testReadMailings(context.Background(), conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = testReadProspects(context.Background(), conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
-func testReadConnector(ctx context.Context, conn connectors.ReadConnector) error {
+func testReadSequences(ctx context.Context, conn connectors.ReadConnector) error {
 	config := connectors.ReadParams{
 		ObjectName: "sequences",
 		Since:      time.Now().Add(-720 * time.Hour),
-		Fields:     []string{"openCount", "description"},
+		Fields:     []string{"openCount", "description", "id"},
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
+
+func testReadMailings(ctx context.Context, conn connectors.ReadConnector) error {
+	config := connectors.ReadParams{
+		ObjectName: "mailings",
+		Since:      time.Now().Add(-720 * time.Hour),
+		Fields:     []string{"bodyHtml", "errorReason", "id"},
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
+
+func testReadProspects(ctx context.Context, conn connectors.ReadConnector) error {
+	config := connectors.ReadParams{
+		ObjectName: "prospects",
+		Since:      time.Now().Add(-720 * time.Hour),
+		Fields:     []string{"addressCountry", "campaignName", "id"},
 	}
 
 	result, err := conn.Read(ctx, config)
