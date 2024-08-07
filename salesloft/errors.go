b68d2eac73edd3cc5832b339c9a60aee7b4b2a0a
Cobalt0s <constantin.y.koval@gmail.com>
2024-08-07 20:28:27 +0300

    salesloft

--- salesloft/errors.go
+++ salesloft/errors.go
@@ -9,30 +9,20 @@ import (
 	"github.com/amp-labs/connectors/common/interpreter"
 )
 
-func (*Connector) interpretJSONError(res *http.Response, body []byte) error { //nolint:cyclop
-	formats := interpreter.NewFormatSwitch(
-		[]interpreter.FormatTemplate{
-			{
-				MustKeys: []string{"errors"},
-				Template: &ResponseListError{},
-			}, {
-				MustKeys: nil,
-				Template: &ResponseSingleError{},
-			},
-		}...,
-	)
+var errorFormats = interpreter.NewFormatSwitch( // nolint:gochecknoglobals
+	[]interpreter.FormatTemplate{
+		{
+			MustKeys: []string{"errors"},
+			Template: &ResponseListError{},
+		}, {
+			MustKeys: nil,
+			Template: &ResponseSingleError{},
+		},
+	}...,
+)
 
-	schema := formats.ParseJSON(body)
-
-	return schema.CombineErr(statusCodeMapping(res, body))
-}
-
-func statusCodeMapping(res *http.Response, body []byte) error {
-	if res.StatusCode == http.StatusUnprocessableEntity {
-		return common.ErrBadRequest
-	}
-
-	return interpreter.DefaultStatusCodeMappingToErr(res, body)
+var statusCodeMapping = map[int]error{ // nolint:gochecknoglobals
+	http.StatusUnprocessableEntity: common.ErrBadRequest,
 }
 
 type ResponseListError struct {
