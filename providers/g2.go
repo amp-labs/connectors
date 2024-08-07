cc28c25df52f17be4912f9fa98a2d2f62e50b957
captain_mo <96528303+mohammedfuta2000@users.noreply.github.com>
2024-08-07 17:44:14 +0000

    feat: Add G2 connector (#851)

---
+++ providers/g2.go
@@ -0,0 +1,42 @@
+package providers
+
+const G2 Provider = "g2"
+
+func init() {
+	// G2 configuration
+	SetInfo(G2, ProviderInfo{
+		DisplayName: "G2",
+		AuthType:    ApiKey,
+		BaseURL:     "https://data.g2.com",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Header,
+			Header: &ApiKeyOptsHeader{
+				Name:        "Authorization",
+				ValuePrefix: "Token token=",
+			},
+			DocsURL: "https://data.g2.com/api/docs?shell#g2-v2-api",
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722888708/media/data.g2.com_1722888706.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722888708/media/data.g2.com_1722888706.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722888708/media/data.g2.com_1722888706.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722888708/media/data.g2.com_1722888706.svg",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+}
