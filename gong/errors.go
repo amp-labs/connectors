530d0dda9afe6cd78116cab4919854ce53cc8de0
Cobalt0s <constantin.y.koval@gmail.com>
2024-08-07 20:23:49 +0300

    gong

--- gong/errors.go
+++ gong/errors.go
@@ -2,26 +2,19 @@ package gong
 
 import (
 	"fmt"
-	"net/http"
 	"strings"
 
 	"github.com/amp-labs/connectors/common/interpreter"
 )
 
-func (*Connector) interpretJSONError(res *http.Response, body []byte) error { //nolint:cyclop
-	formats := interpreter.NewFormatSwitch(
-		[]interpreter.FormatTemplate{
-			{
-				MustKeys: nil,
-				Template: &ResponseError{},
-			},
-		}...,
-	)
-
-	schema := formats.ParseJSON(body)
-
-	return schema.CombineErr(interpreter.DefaultStatusCodeMappingToErr(res, body))
-}
+var errorFormats = interpreter.NewFormatSwitch( // nolint:gochecknoglobals
+	[]interpreter.FormatTemplate{
+		{
+			MustKeys: nil,
+			Template: &ResponseError{},
+		},
+	}...,
+)
 
 type ResponseError struct {
 	RequestId string   `json:"requestId"`
