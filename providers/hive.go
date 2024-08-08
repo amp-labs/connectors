0044c9a9446d4b1f9b0f36b494fc2bba80492646
Sanjay <sanjaykanth.a@mitrahsoft.com>
2024-08-08 11:03:05 +0530

    Added the darkmode logo and upsert field in BulkWriteSupport

--- providers/hive.go
+++ providers/hive.go
@@ -17,7 +17,10 @@ func init() {
 		},
 		//nolint:lll
 		Media: &Media{
-			DarkMode: &MediaTypeDarkMode{},
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722410295/media/const%20Hive%20Provider%20%3D%20%22hive%22_1722410295.png",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722410346/media/const%20Hive%20Provider%20%3D%20%22hive%22_1722410346.svg",
+			},
 			Regular: &MediaTypeRegular{
 				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722410295/media/const%20Hive%20Provider%20%3D%20%22hive%22_1722410295.png",
 				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722410346/media/const%20Hive%20Provider%20%3D%20%22hive%22_1722410346.svg",
@@ -27,6 +30,7 @@ func init() {
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
 				Update: false,
+				Upsert: false,
 				Delete: false,
 			},
 			Proxy:     true,
