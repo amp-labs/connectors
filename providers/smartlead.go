b83d9f71b9ebc3bf9013848e74ac0f578ddd6045
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-08-12 21:24:38 +0300

    feat: Add Smartlead connector (#896)

---
+++ providers/smartlead.go
@@ -0,0 +1,40 @@
+package providers
+
+const Smartlead Provider = "smartlead"
+
+func init() {
+	SetInfo(Smartlead, ProviderInfo{
+		DisplayName: "Smartlead AI",
+		AuthType:    ApiKey,
+		BaseURL:     "https://server.smartlead.ai/api",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Query,
+			Query: &ApiKeyOptsQuery{
+				Name: "api_key",
+			},
+			DocsURL: "https://api.smartlead.ai/reference/authentication",
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1723475823/media/smartlead_1723475823.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1723475838/media/smartlead_1723475837.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1723475823/media/smartlead_1723475823.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1723475838/media/smartlead_1723475837.svg",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+}
