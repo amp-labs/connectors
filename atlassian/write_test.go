23bcfcf8056f1df1f3f973df3118a056c1db0384
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-08-13 15:51:16 +0300

    tidy: Mocktests to use Testroutines (#875)

--- atlassian/write_test.go
+++ atlassian/write_test.go
@@ -5,16 +5,15 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
-	"strings"
 	"testing"
 
+	"github.com/amp-labs/connectors"
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/interpreter"
 	"github.com/amp-labs/connectors/test/utils/mockutils"
 	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
 	"github.com/amp-labs/connectors/test/utils/testutils"
-	"github.com/go-test/deep"
 )
 
 func TestWrite(t *testing.T) { // nolint:funlen,cyclop
@@ -24,152 +23,95 @@ func TestWrite(t *testing.T) { // nolint:funlen,cyclop
 	responseInvalidTypeError := testutils.DataFromFile(t, "create-issue-invalid-type.json")
 	createIssueResponse := testutils.DataFromFile(t, "create-issue.json")
 
-	tests := []struct {
-		name         string
-		input        common.WriteParams
-		server       *httptest.Server
-		connector    Connector
-		comparator   func(actual, expected *common.WriteResult) bool // custom comparison
-		expected     *common.WriteResult
-		expectedErrs []error
-	}{
+	tests := []testroutines.Write{
 		{
-			name:         "Mime response header expected",
-			input:        common.WriteParams{},
-			server:       mockserver.Dummy(),
-			expectedErrs: []error{interpreter.ErrMissingContentType},
+			Name:         "Mime response header expected",
+			Input:        common.WriteParams{},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{interpreter.ErrMissingContentType},
 		},
 		{
-			name:  "Error missing project during write",
-			input: common.WriteParams{RecordId: "10003"},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Error missing project during write",
+			Input: common.WriteParams{RecordId: "10003"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusBadRequest)
 				_, _ = w.Write(responseInvalidProjectError)
 			})),
-			expectedErrs: []error{
+			ExpectedErrs: []error{
 				common.ErrBadRequest,
 				errors.New("project:Specify a valid project ID or key"), // nolint:goerr113
 			},
 		},
 		{
-			name:  "Error missing issue type during write",
-			input: common.WriteParams{RecordId: "10003"},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Error missing issue type during write",
+			Input: common.WriteParams{RecordId: "10003"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusBadRequest)
 				_, _ = w.Write(responseInvalidTypeError)
 			})),
-			expectedErrs: []error{
+			ExpectedErrs: []error{
 				common.ErrBadRequest,
 				errors.New("issuetype:Specify an issue type"), // nolint:goerr113
 			},
 		},
 		{
-			name:  "Write must act as a Create",
-			input: common.WriteParams{},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Write must act as a Create",
+			Input: common.WriteParams{},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				mockutils.RespondToMethod(w, r, "POST", func() {
 					w.WriteHeader(http.StatusOK)
 				})
 			})),
-			expected:     &common.WriteResult{Success: true},
-			expectedErrs: nil,
+			Expected:     &common.WriteResult{Success: true},
+			ExpectedErrs: nil,
 		},
 		{
-			name:  "Write must act as an Update",
-			input: common.WriteParams{RecordId: "10003"},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Write must act as an Update",
+			Input: common.WriteParams{RecordId: "10003"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				mockutils.RespondToMethod(w, r, "PUT", func() {
 					w.WriteHeader(http.StatusNoContent)
 				})
 			})),
-			expected:     &common.WriteResult{Success: true},
-			expectedErrs: nil,
+			Expected:     &common.WriteResult{Success: true},
+			ExpectedErrs: nil,
 		},
 		{
-			name:  "Valid creation of an Issue",
-			input: common.WriteParams{ObjectName: "accounts"},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Valid creation of an Issue",
+			Input: common.WriteParams{ObjectName: "accounts"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				mockutils.RespondToMethod(w, r, "POST", func() {
 					w.WriteHeader(http.StatusOK)
 					_, _ = w.Write(createIssueResponse)
 				})
 			})),
-			comparator: func(actual, expected *common.WriteResult) bool {
+			Comparator: func(serverURL string, actual, expected *common.WriteResult) bool {
 				return mockutils.WriteResultComparator.SubsetData(actual, expected)
 			},
-			expected: &common.WriteResult{
+			Expected: &common.WriteResult{
 				Success:  true,
 				RecordId: "10004",
 				Errors:   nil,
 				Data:     nil,
 			},
-			expectedErrs: nil,
+			ExpectedErrs: nil,
 		},
 	}
 
-	for _, tt := range tests { // nolint:dupl
+	for _, tt := range tests {
 		// nolint:varnamelen
 		tt := tt // rebind, omit loop side effects for parallel goroutine
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.Name, func(t *testing.T) {
 			t.Parallel()
 
-			defer tt.server.Close()
-
-			ctx := context.Background()
-
-			connector, err := NewConnector(
-				WithAuthenticatedClient(http.DefaultClient),
-				WithWorkspace("test-workspace"),
-				WithModule(ModuleJira),
-				WithMetadata(map[string]string{
-					"cloudId": "ebc887b2-7e61-4059-ab35-71f15cc16e12", // any value will work for the test
-				}),
-			)
-			if err != nil {
-				t.Fatalf("%s: error in test while constructing connector %v", tt.name, err)
-			}
-
-			// for testing we want to redirect calls to our server
-			connector.setBaseURL(tt.server.URL)
-
-			if err != nil {
-				t.Fatalf("%s: failed to setup auth metadata connector %v", tt.name, err)
-			}
-
-			// start of tests
-			output, err := connector.Write(ctx, tt.input)
-			if len(tt.expectedErrs) == 0 && err != nil {
-				t.Fatalf("%s: expected no errors, got: (%v)", tt.name, err)
-			}
-
-			if len(tt.expectedErrs) != 0 && err == nil {
-				t.Fatalf("%s: expected errors (%v), but got nothing", tt.name, tt.expectedErrs)
-			}
-
-			for _, expectedErr := range tt.expectedErrs {
-				if !errors.Is(err, expectedErr) && !strings.Contains(err.Error(), expectedErr.Error()) {
-					t.Fatalf("%s: expected Error: (%v), got: (%v)", tt.name, expectedErr, err)
-				}
-			}
-
-			// compare desired output
-			var ok bool
-			if tt.comparator == nil {
-				// default comparison is concerned about all fields
-				ok = reflect.DeepEqual(output, tt.expected)
-			} else {
-				ok = tt.comparator(output, tt.expected)
-			}
-
-			if !ok {
-				diff := deep.Equal(output, tt.expected)
-				t.Fatalf("%s:, \nexpected: (%v), \ngot: (%v), \ndiff: (%v)", tt.name, tt.expected, output, diff)
-			}
+			tt.Run(t, func() (connectors.WriteConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
 		})
 	}
 }
--- salesloft/delete_test.go
+++ salesloft/delete_test.go
@@ -1,20 +1,18 @@
 package salesloft
 
 import (
-	"context"
 	"errors"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
-	"strings"
 	"testing"
 
+	"github.com/amp-labs/connectors"
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/interpreter"
 	"github.com/amp-labs/connectors/test/utils/mockutils"
 	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
 	"github.com/amp-labs/connectors/test/utils/testutils"
-	"github.com/go-test/deep"
 )
 
 func TestDelete(t *testing.T) { // nolint:funlen,cyclop
@@ -22,96 +20,58 @@ func TestDelete(t *testing.T) { // nolint:funlen,cyclop
 
 	listSchema := testutils.DataFromFile(t, "write-signals-error.json")
 
-	tests := []struct {
-		name         string
-		input        common.DeleteParams
-		server       *httptest.Server
-		connector    Connector
-		expected     *common.DeleteResult
-		expectedErrs []error
-	}{
+	tests := []testroutines.Delete{
 		{
-			name:         "Delete param object must be included",
-			server:       mockserver.Dummy(),
-			expectedErrs: []error{common.ErrMissingObjects},
+			Name:         "Delete param object must be included",
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
 		},
 		{
-			name:         "Delete param object and its ID must be included",
-			input:        common.DeleteParams{ObjectName: "signals"},
-			server:       mockserver.Dummy(),
-			expectedErrs: []error{common.ErrMissingRecordID},
+			Name:         "Delete param object and its ID must be included",
+			Input:        common.DeleteParams{ObjectName: "signals"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingRecordID},
 		},
 		{
-			name:         "Mime response header expected",
-			input:        common.DeleteParams{ObjectName: "signals", RecordId: "22165"},
-			server:       mockserver.Dummy(),
-			expectedErrs: []error{interpreter.ErrMissingContentType},
+			Name:         "Mime response header expected",
+			Input:        common.DeleteParams{ObjectName: "signals", RecordId: "22165"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{interpreter.ErrMissingContentType},
 		},
 		{
-			name:  "Correct error message is understood from JSON response",
-			input: common.DeleteParams{ObjectName: "signals", RecordId: "22165"},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Correct error message is understood from JSON response",
+			Input: common.DeleteParams{ObjectName: "signals", RecordId: "22165"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusUnprocessableEntity)
 				_, _ = w.Write(listSchema)
 			})),
-			expectedErrs: []error{
+			ExpectedErrs: []error{
 				common.ErrBadRequest,
 				errors.New("no Signal Registration found for integration id 5167 and given type"), // nolint:goerr113
 			},
 		},
 		{
-			name:  "Successful delete",
-			input: common.DeleteParams{ObjectName: "signals", RecordId: "22165"},
-			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Name:  "Successful delete",
+			Input: common.DeleteParams{ObjectName: "signals", RecordId: "22165"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "application/json")
 				mockutils.RespondNoContentForMethod(w, r, "DELETE")
 			})),
-			expected:     &common.DeleteResult{Success: true},
-			expectedErrs: nil,
+			Expected:     &common.DeleteResult{Success: true},
+			ExpectedErrs: nil,
 		},
 	}
 
-	for _, tt := range tests { // nolint:dupl
+	for _, tt := range tests {
 		// nolint:varnamelen
 		tt := tt // rebind, omit loop side effects for parallel goroutine
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.Name, func(t *testing.T) {
 			t.Parallel()
 
-			defer tt.server.Close()
-
-			ctx := context.Background()
-
-			connector, err := NewConnector(
-				WithAuthenticatedClient(http.DefaultClient),
-			)
-			if err != nil {
-				t.Fatalf("%s: error in test while constructing connector %v", tt.name, err)
-			}
-
-			// for testing we want to redirect calls to our server
-			connector.setBaseURL(tt.server.URL)
-
-			// start of tests
-			output, err := connector.Delete(ctx, tt.input)
-			if len(tt.expectedErrs) == 0 && err != nil {
-				t.Fatalf("%s: expected no errors, got: (%v)", tt.name, err)
-			}
-
-			if len(tt.expectedErrs) != 0 && err == nil {
-				t.Fatalf("%s: expected errors (%v), but got nothing", tt.name, tt.expectedErrs)
-			}
-
-			for _, expectedErr := range tt.expectedErrs {
-				if !errors.Is(err, expectedErr) && !strings.Contains(err.Error(), expectedErr.Error()) {
-					t.Fatalf("%s: expected Error: (%v), got: (%v)", tt.name, expectedErr, err)
-				}
-			}
-
-			if !reflect.DeepEqual(output, tt.expected) {
-				diff := deep.Equal(output, tt.expected)
-				t.Fatalf("%s:, \nexpected: (%v), \ngot: (%v), \ndiff: (%v)", tt.name, tt.expected, output, diff)
-			}
+			tt.Run(t, func() (connectors.DeleteConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
 		})
 	}
 }
