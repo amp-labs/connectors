90b0c0b1081d6cc831d4f9e3b4cd1a42c3bced61
Cobalt0s <constantin.y.koval@gmail.com>
2024-08-13 23:58:44 +0300

    feat: Add Deep connector Smartlead (Metadata)

---
+++ smartlead/metadata_test.go
@@ -0,0 +1,79 @@
+package smartlead
+
+import (
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/test/utils/mockutils"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+	"github.com/amp-labs/connectors/tools/scrapper"
+)
+
+func TestListObjectMetadata(t *testing.T) { // nolint:funlen,gocognit,cyclop
+	t.Parallel()
+
+	tests := []testroutines.Metadata{
+		{
+			Name:         "At least one object name must be queried",
+			Input:        nil,
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:         "Unknown object requested",
+			Input:        []string{"butterflies"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{scrapper.ErrObjectNotFound},
+		},
+		{
+			Name:   "Successfully describe multiple objects with metadata",
+			Input:  []string{"campaigns", "leads"},
+			Server: mockserver.Dummy(),
+			Comparator: func(baseURL string, actual, expected *common.ListObjectMetadataResult) bool {
+				return mockutils.MetadataResultComparator.SubsetFields(actual, expected)
+			},
+			Expected: &common.ListObjectMetadataResult{
+				Result: map[string]common.ObjectMetadata{
+					"campaigns": {
+						DisplayName: "Campaigns",
+						FieldsMap: map[string]string{
+							"id":         "ID",
+							"user_id":    "User ID",
+							"created_at": "Created at",
+							"updated_at": "Updated at",
+							"status":     "Status",
+							"name":       "Name",
+						},
+					},
+					"leads": {
+						DisplayName: "Leads",
+						FieldsMap: map[string]string{
+							"first_name":   "First name",
+							"last_name":    "Last name",
+							"email":        "Email",
+							"created_at":   "Created at",
+							"phone_number": "Phone number",
+							"company_name": "Company name",
+						},
+					},
+				},
+				Errors: nil,
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		// nolint:varnamelen
+		tt := tt // rebind, omit loop side effects for parallel goroutine
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.ObjectMetadataConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
