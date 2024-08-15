4c963c738dde4a3485e587164c47770ceb976707
jkarage <josephbkarage@gmail.com>
2024-08-15 18:26:36 +0300

    fix: logic

--- common/error.go
+++ common/error.go
@@ -11,7 +11,7 @@ import (
 // way) and returns an error that can be handled by the caller.
 func InterpretError(res *http.Response, body []byte) error { //nolint:cyclop
 	// A must check.
-	if res.StatusCode < 200 || res.StatusCode > 299 {
+	if res.StatusCode >= 200 || res.StatusCode <= 299 {
 		return nil
 	}
 
