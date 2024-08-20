08c796de2923ff3f756712be7d46056d692a7756
jkarage <josephbkarage@gmail.com>
2024-08-20 13:53:41 +0300

    remove unwanted comment

--- common/http.go
+++ common/http.go
@@ -264,7 +264,7 @@ func makeDeleteRequest(ctx context.Context, url string, headers []Header) (*http
 }
 
 // sendRequest sends the given request and returns the response & response body.
-func (h *HTTPClient) sendRequest(req *http.Request) (*http.Response, []byte, error) { //nolint:cyclop
+func (h *HTTPClient) sendRequest(req *http.Request) (*http.Response, []byte, error) {
 	// Send the request
 	res, err := h.Client.Do(req)
 	if err != nil {
