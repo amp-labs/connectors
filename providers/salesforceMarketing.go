08d082b0fa440f15422d29907ee697abdedeea98
JKarage <josephbkarage@gmail.com>
2024-08-23 06:45:55 +0300

    feat: Add Salesforce Marketing Cloud (#932)
    * feat: Add Salesforce Marketing Cloud
    
    * fix: update scope requirement

---
+++ providers/salesforceMarketing.go
@@ -0,0 +1,44 @@
+package providers
+
+const SalesforceMarketing Provider = "salesforceMarketing"
+
+func init() {
+	// SalesforceMarketing configuration
+	SetInfo(SalesforceMarketing, ProviderInfo{
+		DisplayName: "Salesforce Marketing Cloud",
+		AuthType:    Oauth2,
+		BaseURL:     "https://{{.workspace}}.rest.marketingcloudapis.com",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType:                 AuthorizationCode, // There is also a s2s clientcredentials option.
+			AuthURL:                   "https://{{.workspace}}.auth.marketingcloudapis.com/v2/authorize",
+			TokenURL:                  "https://{{.workspace}}.auth.marketingcloudapis.com/v2/token",
+			ExplicitScopesRequired:    false,
+			ExplicitWorkspaceRequired: true,
+			TokenMetadataFields: TokenMetadataFields{
+				ScopesField: "scope",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722470590/media/salesforce_1722470589.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722470590/media/salesforce_1722470589.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722470590/media/salesforce_1722470589.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722470590/media/salesforce_1722470589.svg",
+			},
+		},
+	})
+}
