b0a72ebab6fd37fd1ce22456ef251f85e669fc47
Cobalt0s <constantin.y.koval@gmail.com>
2024-08-28 19:05:01 +0300

    feat: Deep connector Instantly (Write)

---
+++ providers/instantly/delete.go
@@ -0,0 +1,39 @@
+package instantly
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+)
+
+// Delete removes object. As of now only removal of Tags are allowed.
+func (c *Connector) Delete(ctx context.Context, config common.DeleteParams) (*common.DeleteResult, error) {
+	if len(config.ObjectName) == 0 {
+		return nil, common.ErrMissingObjects
+	}
+
+	if len(config.RecordId) == 0 {
+		return nil, common.ErrMissingRecordID
+	}
+
+	if !supportedObjectsByDelete.Has(config.ObjectName) {
+		// Removing tags is the only to be supported at this time.
+		// https://developer.instantly.ai/tags/delete-a-tag
+		return nil, common.ErrOperationNotSupportedForObject
+	}
+
+	url, err := c.getURL("custom-tag", config.RecordId)
+	if err != nil {
+		return nil, err
+	}
+
+	// 200 OK is expected
+	_, err = c.Client.Delete(ctx, url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.DeleteResult{
+		Success: true,
+	}, nil
+}
---
+++ providers/instantly/write_test.go
@@ -0,0 +1,182 @@
+package instantly
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/interpreter"
+	"github.com/amp-labs/connectors/test/utils/mockutils"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+	"github.com/amp-labs/connectors/test/utils/testutils"
+)
+
+func TestWrite(t *testing.T) { // nolint:funlen,cyclop
+	t.Parallel()
+
+	responseBlocklistEntry := testutils.DataFromFile(t, "write-blocklist-entry.json")
+
+	responseReply := testutils.DataFromFile(t, "write-unibox-reply.json")
+
+	responseLeadErr := testutils.DataFromFile(t, "write-lead-bad-request.json")
+	responseLead := testutils.DataFromFile(t, "write-lead.json")
+
+	responseTagErr := testutils.DataFromFile(t, "write-tag-bad-request.json")
+	responseTag := testutils.DataFromFile(t, "write-tag.json")
+
+	tests := []testroutines.Write{
+		{
+			Name:         "Write object must be included",
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:         "Mime response header expected",
+			Input:        common.WriteParams{ObjectName: "unibox-replies"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{interpreter.ErrMissingContentType},
+		},
+		{
+			Name:     "Unknown object name is not supported",
+			Input:    common.WriteParams{ObjectName: "orders"},
+			Server:   mockserver.Dummy(),
+			Expected: nil,
+			ExpectedErrs: []error{
+				common.ErrOperationNotSupportedForObject,
+			},
+		},
+		{
+			Name:  "Create Blocklist Entry",
+			Input: common.WriteParams{ObjectName: "blocklist-entries"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				mockutils.RespondToMethod(w, r, "POST", func() {
+					w.WriteHeader(http.StatusOK)
+					_, _ = w.Write(responseBlocklistEntry)
+				})
+			})),
+			Expected: &common.WriteResult{
+				Success:  true,
+				RecordId: "cf8fd143-08c3-438e-a396-491aa1ced9d4",
+				Errors:   nil,
+				Data:     nil,
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name:  "Create Unibox Reply",
+			Input: common.WriteParams{ObjectName: "unibox-replies"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				mockutils.RespondToMethod(w, r, "POST", func() {
+					w.WriteHeader(http.StatusOK)
+					_, _ = w.Write(responseReply)
+				})
+			})),
+			Expected: &common.WriteResult{
+				Success:  true,
+				RecordId: "19e9d7f9-bd4f-45aa-8d77-9eecc9bd3f9a",
+				Errors:   nil,
+				Data:     nil,
+			},
+			ExpectedErrs: nil,
+		},
+
+		{
+			Name:  "Invalid Lead creation",
+			Input: common.WriteParams{ObjectName: "leads"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusNotFound)
+				_, _ = w.Write(responseLeadErr)
+			})),
+			ExpectedErrs: []error{
+				common.ErrBadRequest,
+				errors.New("Leads array is empty"), // nolint:goerr113
+			},
+		},
+		{
+			Name:  "Create Lead",
+			Input: common.WriteParams{ObjectName: "leads"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				mockutils.RespondToMethod(w, r, "POST", func() {
+					w.WriteHeader(http.StatusOK)
+					_, _ = w.Write(responseLead)
+				})
+			})),
+			Expected: &common.WriteResult{
+				Success:  true,
+				RecordId: "", // lead doesn't have ID
+				Errors:   nil,
+				Data:     nil,
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name:  "Invalid Tag creation",
+			Input: common.WriteParams{ObjectName: "tags"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusNotFound)
+				_, _ = w.Write(responseTagErr)
+			})),
+			ExpectedErrs: []error{
+				common.ErrBadRequest,
+				errors.New("Bad Request"), // nolint:goerr113
+			},
+		},
+		{
+			Name:  "Create Tag acts as POST",
+			Input: common.WriteParams{ObjectName: "tags"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				mockutils.RespondToMethod(w, r, "POST", func() {
+					w.WriteHeader(http.StatusOK)
+					_, _ = w.Write(responseTag)
+				})
+			})),
+			Expected: &common.WriteResult{
+				Success:  true,
+				RecordId: "f6825fcf-c51b-4724-937b-0814ed02af83",
+				Errors:   nil,
+				Data:     nil,
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name:  "Update tag acts as PATCH",
+			Input: common.WriteParams{ObjectName: "tags", RecordId: "885633"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				mockutils.RespondToMethod(w, r, "PATCH", func() {
+					w.WriteHeader(http.StatusOK)
+					_, _ = w.Write(responseTag)
+				})
+			})),
+			Expected: &common.WriteResult{
+				Success:  true,
+				RecordId: "f6825fcf-c51b-4724-937b-0814ed02af83",
+				Errors:   nil,
+				Data:     nil,
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		// nolint:varnamelen
+		tt := tt // rebind, omit loop side effects for parallel goroutine
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.WriteConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
