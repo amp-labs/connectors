eaf3ffe34d740236d0ddca5016133217802cbaea
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-08-30 18:08:50 +0300

    tidy: Create common.GetRecords (#1004)

--- providers/gong/parse.go
+++ providers/gong/parse.go
@@ -1,22 +1,9 @@
 package gong
 
 import (
-	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/jsonquery"
 	"github.com/spyzhov/ajson"
 )
-
-func makeGetRecords(objectName string) common.RecordsFunc {
-	return func(node *ajson.Node) ([]map[string]any, error) {
-		// items are stored in array named after the API object
-		arr, err := jsonquery.New(node).Array(objectName, false)
-		if err != nil {
-			return nil, err
-		}
-
-		return jsonquery.Convertor.ArrayToMap(arr)
-	}
-}
 
 // getNextRecords returns the token or empty string if there are no more records.
 func getNextRecordsURL(node *ajson.Node) (string, error) {
