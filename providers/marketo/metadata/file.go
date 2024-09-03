a2ce180d6be69ab2ef22c95aa5f5dbf1aba86044
Cobalt0s <constantin.y.koval@gmail.com>
2024-09-03 18:03:51 +0300

    feat: Explicit loading of Metadata Schema

--- providers/marketo/metadata/file.go
+++ providers/marketo/metadata/file.go
@@ -12,4 +12,7 @@ var (
 	schemas []byte
 
 	FileManager = scrapper.NewMetadataFileManager(schemas, fileconv.NewSiblingFileLocator()) // nolint:gochecknoglobals
+
+	// Schemas is cached Object schemas.
+	Schemas = FileManager.MustLoadSchemas() // nolint:gochecknoglobals
 )
