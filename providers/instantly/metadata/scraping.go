1652b6af243a65a59747be19eba80f28f4429ccc
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-09-04 21:30:23 +0300

    feat: Deep connector Instantly (Metadata) (#925)

---
+++ providers/instantly/metadata/scraping.go
@@ -0,0 +1,17 @@
+package metadata
+
+import (
+	_ "embed"
+
+	"github.com/amp-labs/connectors/tools/fileconv"
+	"github.com/amp-labs/connectors/tools/scrapper"
+)
+
+var (
+	// Static file containing a list of object metadata is embedded and can be served.
+	//
+	//go:embed schemas.json
+	schemas []byte
+
+	FileManager = scrapper.NewMetadataFileManager(schemas, fileconv.NewSiblingFileLocator()) // nolint:gochecknoglobals
+)
---
+++ providers/instantly/metadata_test.go
@@ -0,0 +1,75 @@
+package instantly
+
+import (
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/test/utils/mockutils"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+	"github.com/amp-labs/connectors/tools/scrapper"
+)
+
+func TestListObjectMetadata(t *testing.T) { // nolint:funlen,gocognit,cyclop
+	t.Parallel()
+
+	tests := []testroutines.Metadata{
+		{
+			Name:         "At least one object name must be queried",
+			Input:        nil,
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:         "Unknown object requested",
+			Input:        []string{"butterflies"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{scrapper.ErrObjectNotFound},
+		},
+		{
+			Name:   "Successfully describe multiple objects with metadata",
+			Input:  []string{"campaigns", "emails"},
+			Server: mockserver.Dummy(),
+			Comparator: func(baseURL string, actual, expected *common.ListObjectMetadataResult) bool {
+				return mockutils.MetadataResultComparator.SubsetFields(actual, expected)
+			},
+			Expected: &common.ListObjectMetadataResult{
+				Result: map[string]common.ObjectMetadata{
+					"emails": {
+						DisplayName: "Emails",
+						FieldsMap: map[string]string{
+							"is_unread":             "Is Unread",
+							"ue_type":               "Email Type",
+							"message_id":            "Message ID",
+							"campaign_id":           "Campaign ID",
+							"from_address_email":    "Sender's Email Address",
+							"to_address_email_list": "Receivers Email Address",
+						},
+					},
+					"campaigns": {
+						DisplayName: "Campaigns",
+						FieldsMap: map[string]string{
+							"id":   "ID",
+							"name": "Name",
+						},
+					},
+				},
+				Errors: nil,
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		// nolint:varnamelen
+		tt := tt // rebind, omit loop side effects for parallel goroutine
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.ObjectMetadataConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
