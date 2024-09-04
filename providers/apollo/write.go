8dd80432e538e7f5500d70a4b72e20ae64a3b8e4
JKarage <josephbkarage@gmail.com>
2024-09-04 23:21:18 +0300

    feat: Add Apollo Write connector (#1012)
    * feat: Add Apollo Write connector
    
    * fix: Address review

---
+++ providers/apollo/write.go
@@ -0,0 +1,65 @@
+package apollo
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/jsonquery"
+	"github.com/amp-labs/connectors/common/naming"
+	"github.com/spyzhov/ajson"
+)
+
+// Write creates/updates records in apolllo.
+func (c *Connector) Write(ctx context.Context, config common.WriteParams) (*common.WriteResult, error) {
+	var write common.WriteMethod
+
+	url, err := c.getAPIURL(config.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	// sets post as default
+	write = c.Client.Post
+
+	// prepares the updating data request.
+	if len(config.RecordId) > 0 {
+		url = url.AddPath(config.RecordId)
+
+		write = c.Client.Patch
+	}
+
+	json, err := write(ctx, url.String(), config.RecordData)
+	if err != nil {
+		return nil, err
+	}
+
+	return constructWriteResult(json.Body, config.ObjectName)
+}
+
+func constructWriteResult(body *ajson.Node, objName string) (*common.WriteResult, error) {
+	// API Response contains a json object having a singular objectName key with the
+	// created/updated details in it.
+	obj := naming.NewSingularString(objName)
+
+	respObject, err := jsonquery.New(body).Object(obj.String(), false)
+	if err != nil {
+		return nil, err
+	}
+
+	recordID, err := jsonquery.New(respObject).StrWithDefault("id", "")
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := jsonquery.Convertor.ObjectToMap(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.WriteResult{
+		Success:  true,
+		RecordId: recordID,
+		Errors:   nil,
+		Data:     data,
+	}, nil
+}
