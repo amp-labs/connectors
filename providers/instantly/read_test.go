93d48d31718c932c4800da868dc8db46179eba25
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-09-04 21:12:54 +0300

    feat: Add Deep connector Instantly (Read) (#923)

---
+++ providers/instantly/read_test.go
@@ -0,0 +1,244 @@
+package instantly
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/interpreter"
+	"github.com/amp-labs/connectors/common/jsonquery"
+	"github.com/amp-labs/connectors/test/utils/mockutils"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+	"github.com/amp-labs/connectors/test/utils/testutils"
+)
+
+func TestRead(t *testing.T) { //nolint:funlen,gocognit,cyclop,maintidx
+	t.Parallel()
+
+	responseInvalidPath := testutils.DataFromFile(t, "invalid-path.json")
+	responseCampaigns := testutils.DataFromFile(t, "read-campaigns.json")
+	responseTags := testutils.DataFromFile(t, "read-tags.json")
+
+	tests := []testroutines.Read{
+		{
+			Name:         "Read object must be included",
+			Input:        common.ReadParams{},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:         "Mime response header expected",
+			Input:        common.ReadParams{ObjectName: "campaigns"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{interpreter.ErrMissingContentType},
+		},
+		{
+			Name:     "Unknown object name is not supported",
+			Input:    common.ReadParams{ObjectName: "orders"},
+			Server:   mockserver.Dummy(),
+			Expected: nil,
+			ExpectedErrs: []error{
+				common.ErrOperationNotSupportedForObject,
+			},
+		},
+		{
+			Name:  "Correct error message is understood from JSON response",
+			Input: common.ReadParams{ObjectName: "campaigns"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusNotFound)
+				_, _ = w.Write(responseInvalidPath)
+			})),
+			ExpectedErrs: []error{
+				common.ErrBadRequest,
+				errors.New("Not Found"), // nolint:goerr113
+			},
+		},
+		{
+			Name:  "Incorrect key in payload",
+			Input: common.ReadParams{ObjectName: "emails"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				mockutils.WriteBody(w, `{
+					"yourEmails": []
+				}`)
+			})),
+			ExpectedErrs: []error{jsonquery.ErrKeyNotFound},
+		},
+		{
+			Name:  "Incorrect data type in payload",
+			Input: common.ReadParams{ObjectName: "emails"},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				mockutils.WriteBody(w, `{
+					"data": {}
+				}`)
+			})),
+			ExpectedErrs: []error{jsonquery.ErrNotArray},
+		},
+		{
+			Name: "Next page is correctly calculated",
+			Input: common.ReadParams{
+				ObjectName: "campaigns",
+				NextPage:   "test-placeholder?skip=700",
+			},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				// Create fake response big enough to conclude that next page exists.
+				manyCampaigns := make([]string, DefaultPageSize)
+				for i := 0; i < DefaultPageSize; i++ {
+					manyCampaigns[i] = "{}"
+				}
+				data := fmt.Sprintf("[%v]", strings.Join(manyCampaigns, ","))
+				mockutils.WriteBody(w, data)
+			})),
+			Comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
+				return actual.NextPage.String() == expected.NextPage.String() &&
+					actual.Done == expected.Done
+			},
+			Expected: &common.ReadResult{
+				NextPage: "test-placeholder?limit=100&skip=800",
+				Done:     false,
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name: "Current empty page signifies no next page",
+			Input: common.ReadParams{
+				ObjectName: "campaigns",
+			},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				mockutils.WriteBody(w, "[]")
+			})),
+			Comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
+				expectedNextPage := strings.ReplaceAll(expected.NextPage.String(), "{{testServerURL}}", baseURL)
+
+				return actual.NextPage.String() == expectedNextPage &&
+					actual.Done == expected.Done
+			},
+			Expected: &common.ReadResult{
+				NextPage: "{{testServerURL}}/v1/campaign/list?limit=100&skip=100",
+				Done:     true,
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name: "Read campaigns with chosen fields",
+			Input: common.ReadParams{
+				ObjectName: "campaigns",
+				Fields:     []string{"name"},
+			},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				_, _ = w.Write(responseCampaigns)
+			})),
+			Comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
+				return mockutils.ReadResultComparator.SubsetFields(actual, expected) &&
+					mockutils.ReadResultComparator.SubsetRaw(actual, expected) &&
+					actual.Done == expected.Done
+			},
+			Expected: &common.ReadResult{
+				Rows: 2,
+				Data: []common.ReadResultRow{{
+					Fields: map[string]any{
+						"name": "Second Campaign",
+					},
+					Raw: map[string]any{
+						"id":   "27dd47ff-5a78-4377-a1eb-98f593f37219",
+						"name": "Second Campaign",
+					},
+				}, {
+					Fields: map[string]any{
+						"name": "My Campaign",
+					},
+					Raw: map[string]any{
+						"id":   "65d890fe-ae4d-43d0-b014-af0e89b6281f",
+						"name": "My Campaign",
+					},
+				}},
+				Done: false,
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name: "Read tags with chosen fields",
+			Input: common.ReadParams{
+				ObjectName: "tags",
+				Fields:     []string{"label", "description"},
+			},
+			Server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				_, _ = w.Write(responseTags)
+			})),
+			Comparator: func(baseURL string, actual, expected *common.ReadResult) bool {
+				return mockutils.ReadResultComparator.SubsetFields(actual, expected) &&
+					mockutils.ReadResultComparator.SubsetRaw(actual, expected) &&
+					actual.Done == expected.Done
+			},
+			Expected: &common.ReadResult{
+				Data: []common.ReadResultRow{{
+					Fields: map[string]any{
+						"label":       "High Delivery 3",
+						"description": "High Delivery Accounts 3",
+					},
+					Raw: map[string]any{
+						"organization_id": "803a064c-a636-49fe-bc45-5043da7a4ee7",
+						"label":           "High Delivery 3",
+						"description":     "High Delivery Accounts 3",
+					},
+				}, {
+					Fields: map[string]any{
+						"label":       "High Delivery 2",
+						"description": "High Delivery Accounts 2",
+					},
+					Raw: map[string]any{
+						"organization_id": "803a064c-a636-49fe-bc45-5043da7a4ee7",
+						"label":           "High Delivery 2",
+						"description":     "High Delivery Accounts 2",
+					},
+				}},
+				Done: false,
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		// nolint:varnamelen
+		tt := tt // rebind, omit loop side effects for parallel goroutine
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.ReadConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
+
+func constructTestConnector(serverURL string) (*Connector, error) {
+	connector, err := NewConnector(
+		WithAuthenticatedClient(http.DefaultClient),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// for testing we want to redirect calls to our mock server
+	connector.setBaseURL(serverURL)
+
+	return connector, nil
+}
---
+++ test/instantly/read/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/providers/instantly"
+	connTest "github.com/amp-labs/connectors/test/instantly"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+var (
+	objectName = "campaigns" // nolint: gochecknoglobals
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := connTest.GetInstantlyConnector(ctx)
+	defer utils.Close(conn)
+
+	res, err := conn.Read(ctx, common.ReadParams{
+		ObjectName: objectName,
+		Fields: []string{
+			"name",
+		},
+	})
+	if err != nil {
+		utils.Fail("error reading from Instantly", "error", err)
+	}
+
+	slog.Info("Reading campaigns..")
+	utils.DumpJSON(res, os.Stdout)
+
+	if res.Rows > instantly.DefaultPageSize {
+		utils.Fail(fmt.Sprintf("expected max %v rows", instantly.DefaultPageSize))
+	}
+}
