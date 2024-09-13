1754dfd0ca832972612066e44fc0da77c345e6bc
Chris Eberle <eberle1080@gmail.com>
2024-09-13 11:23:24 -0700

    feat: Add UrlConnector interface, and implement it for Salesforce (#1042)
    * add UrlConnector interface, and implement it for Salesforce
    
    * make linter happy

---
+++ providers/salesforce/url.go
@@ -0,0 +1,25 @@
+package salesforce
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/amp-labs/connectors/common/urlbuilder"
+)
+
+const OAuthIntrospectResource = "oauth-introspect"
+
+var ErrUnknownURLResource = errors.New("unknown URL resource")
+
+func (c *Connector) GetURL(resource string, _ map[string]any) (string, error) {
+	if resource == OAuthIntrospectResource {
+		u, err := urlbuilder.New(c.BaseURL, "/services/oauth2/introspect")
+		if err != nil {
+			return "", err
+		}
+
+		return u.String(), nil
+	}
+
+	return "", fmt.Errorf("%w: %s", ErrUnknownURLResource, resource)
+}
