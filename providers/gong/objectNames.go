a3f739d4eedc4bb0c1993ea1c336c54ed948b85b
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-10-04 00:07:45 +0300

    feat: Add Deep Connector Gong (Write) (#1036)

--- providers/gong/objectNames.go
+++ providers/gong/objectNames.go
@@ -5,7 +5,15 @@ import (
 	"github.com/amp-labs/connectors/providers/gong/metadata"
 )
 
+const (
+	objectNameCalls = "calls"
+)
+
 // Supported object names can be found under schemas.json.
 var supportedObjectsByRead = handy.NewSetFromList( //nolint:gochecknoglobals
 	metadata.Schemas.GetObjectNames(),
+)
+
+var supportedObjectsByWrite = handy.NewSet( //nolint:gochecknoglobals
+	objectNameCalls,
 )
