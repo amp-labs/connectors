6240433f019dccd040413fed1658caf8de62ed6c
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-10-04 00:07:45 +0300

    feat: Add Deep Connector Gong (Write) (#1036)

---
+++ providers/gong/write.go
@@ -0,0 +1,55 @@
+package gong
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/jsonquery"
+	"github.com/spyzhov/ajson"
+)
+
+// Write only supports creating Calls.
+func (c *Connector) Write(ctx context.Context, config common.WriteParams) (*common.WriteResult, error) {
+	if err := config.ValidateParams(); err != nil {
+		return nil, err
+	}
+
+	if !supportedObjectsByWrite.Has(config.ObjectName) {
+		return nil, common.ErrOperationNotSupportedForObject
+	}
+
+	url, err := c.getURL(config.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.Client.Post(ctx, url.String(), config.RecordData)
+	if err != nil {
+		return nil, err
+	}
+
+	body, ok := res.Body()
+	if !ok {
+		// it is unlikely to have no payload
+		return &common.WriteResult{
+			Success: true,
+		}, nil
+	}
+
+	// write response was with payload
+	return constructWriteResult(body)
+}
+
+func constructWriteResult(body *ajson.Node) (*common.WriteResult, error) {
+	recordID, err := jsonquery.New(body).Str("callId", false)
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.WriteResult{
+		Success:  true,
+		RecordId: *recordID,
+		Errors:   nil,
+		Data:     nil,
+	}, nil
+}
