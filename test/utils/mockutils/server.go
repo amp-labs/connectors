ef6a600df18ec9b0e7f4c23f36fca40ba2baaa89
Cobalt0s <constantin.y.koval@gmail.com>
2024-10-04 02:36:53 +0300

    remove RespondToHeader

--- test/utils/mockutils/server.go
+++ test/utils/mockutils/server.go
@@ -30,21 +30,6 @@ func RespondToMethod(w http.ResponseWriter, r *http.Request, methodName string, 
 	}
 }
 
-func RespondToHeader(w http.ResponseWriter, r *http.Request, header http.Header, onSuccess func()) {
-	// if some headers are missing we return error code so the test will fail
-	if missingHeader, ok := headerIsSubset(r.Header, header); ok {
-		// if method is matching headers
-		onSuccess()
-	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		WriteBody(w, fmt.Sprintf(`{
-			"error": {
-				"code": "from test",
-				"message": "test server mismatching [%v] header"
-			}}`, missingHeader))
-	}
-}
-
 func RespondToBody(w http.ResponseWriter, r *http.Request, body string, onSuccess func()) {
 	if ok := bodiesMatch(r.Body, body); ok {
 		// if method is matching bodies
@@ -91,23 +76,6 @@ func RespondToMissingQueryParameters(w http.ResponseWriter, r *http.Request, mis
 
 func WriteBody(w http.ResponseWriter, body string) {
 	_, _ = w.Write([]byte(body))
-}
-
-func headerIsSubset(superset, subset http.Header) (string, bool) {
-	for name, values := range subset {
-		superValues := make(map[string]bool)
-		for _, v := range superset.Values(name) {
-			superValues[v] = true
-		}
-		// every value of this header must be part of superset
-		for _, value := range values {
-			if _, found := superValues[value]; !found {
-				return name, false
-			}
-		}
-	}
-
-	return "", true
 }
 
 func queryParamsAreSubset(superset, subset url.Values) (string, bool) {
