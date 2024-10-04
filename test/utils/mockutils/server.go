875449e0508348a64661f9cca0546e8febe9bb2e
Cobalt0s <constantin.y.koval@gmail.com>
2024-10-04 03:07:26 +0300

    remove RespondToMethod

--- test/utils/mockutils/server.go
+++ test/utils/mockutils/server.go
@@ -1,33 +1,10 @@
 package mockutils
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"strings"
 )
-
-func RespondNoContentForMethod(w http.ResponseWriter, r *http.Request, methodName string) {
-	RespondToMethod(w, r, methodName, func() {
-		w.WriteHeader(http.StatusNoContent)
-	})
-}
-
-func RespondToMethod(w http.ResponseWriter, r *http.Request, methodName string, onSuccess func()) {
-	// if method is not as expected we return error code so the test will fail
-	// and with response payload which will be a helpful message for debugging
-	if r.Method == methodName {
-		// if method is matching execute callback
-		onSuccess()
-	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		WriteBody(w, fmt.Sprintf(`{
-			"error": {
-				"code": "from test",
-				"message": "test server expected %v request"
-			}}`, methodName))
-	}
-}
 
 func RespondToBody(w http.ResponseWriter, r *http.Request, body string, onSuccess func()) {
 	if ok := bodiesMatch(r.Body, body); ok {
