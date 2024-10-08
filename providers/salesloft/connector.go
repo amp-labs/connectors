b56082ea9ce095c0d95a91f13edb41b4f0bdb523
Cobalt0s <constantin.y.koval@gmail.com>
2024-10-09 02:35:03 +0300

    concrete "callbacks" implement Requirement Interface

--- providers/salesloft/connector.go
+++ providers/salesloft/connector.go
@@ -23,6 +23,7 @@ type Connector struct {
 	deep.Clients
 	deep.EmptyCloser
 	deep.Reader
+	//deep.Writer // TODO create writer
 	deep.StaticMetadata
 	deep.Remover
 }
@@ -52,7 +53,51 @@ func NewConnector(opts ...Option) (*Connector, error) {
 	errorHandler := interpreter.ErrorHandler{
 		JSON: interpreter.NewFaultyResponder(errorFormats, statusCodeMapping),
 	}
-	urlResolver := &deep.URLResolver{
+	firstPage := deep.FirstPageBuilder{
+		Build: func(config common.ReadParams, url *urlbuilder.URL) (*urlbuilder.URL, error) {
+			url.WithQueryParam("per_page", strconv.Itoa(DefaultPageSize))
+
+			if !config.Since.IsZero() {
+				// Documentation states ISO8601, while server accepts different formats
+				// but for consistency we are sticking to one format to be sent.
+				// For the reference any API resource that includes time data type mentions iso8601 string format.
+				// One example, say accounts is https://developers.salesloft.com/docs/api/accounts-index
+				updatedSince := config.Since.Format(time.RFC3339Nano)
+				url.WithQueryParam("updated_at[gte]", updatedSince)
+			}
+
+			return url, nil
+		},
+	}
+	nextPage := deep.NextPageBuilder{
+		Build: func(config common.ReadParams, previousPage *urlbuilder.URL, node *ajson.Node) (*urlbuilder.URL, error) {
+			nextPageNum, err := jsonquery.New(node, "metadata", "paging").Integer("next_page", true)
+			if err != nil {
+				if errors.Is(err, jsonquery.ErrKeyNotFound) {
+					// list resource doesn't support pagination, hence no next page
+					return nil, nil
+				}
+
+				return nil, err
+			}
+
+			if nextPageNum == nil {
+				// next page doesn't exist
+				return nil, nil
+			}
+
+			// use request URL to infer the next page URL
+			previousPage.WithQueryParam("page", strconv.FormatInt(*nextPageNum, 10))
+
+			return previousPage, nil
+		},
+	}
+	readObjectLocator := deep.ReadObjectLocator{
+		Locate: func(config common.ReadParams) string {
+			return "data"
+		},
+	}
+	urlResolver := deep.URLResolver{
 		Resolve: func(baseURL, objectName string) (*urlbuilder.URL, error) {
 			return urlbuilder.New(baseURL, apiVersion, objectName)
 		},
@@ -64,67 +109,10 @@ func NewConnector(opts ...Option) (*Connector, error) {
 				return metadata.Schemas
 			},
 		},
-		deep.Dependency{
-			Constructor: func() *deep.URLResolver {
-				return urlResolver
-			},
-		},
-		deep.Dependency{
-			Constructor: func() *deep.FirstPageBuilder {
-				return &deep.FirstPageBuilder{
-					Build: func(config common.ReadParams, url *urlbuilder.URL) *urlbuilder.URL {
-						url.WithQueryParam("per_page", strconv.Itoa(DefaultPageSize))
-
-						if !config.Since.IsZero() {
-							// Documentation states ISO8601, while server accepts different formats
-							// but for consistency we are sticking to one format to be sent.
-							// For the reference any API resource that includes time data type mentions iso8601 string format.
-							// One example, say accounts is https://developers.salesloft.com/docs/api/accounts-index
-							updatedSince := config.Since.Format(time.RFC3339Nano)
-							url.WithQueryParam("updated_at[gte]", updatedSince)
-						}
-
-						return url
-					},
-				}
-			},
-		},
-		deep.Dependency{
-			Constructor: func() *deep.NextPageBuilder {
-				return &deep.NextPageBuilder{
-					Build: func(config common.ReadParams, previousPage *urlbuilder.URL, node *ajson.Node) (string, error) {
-						nextPageNum, err := jsonquery.New(node, "metadata", "paging").Integer("next_page", true)
-						if err != nil {
-							if errors.Is(err, jsonquery.ErrKeyNotFound) {
-								// list resource doesn't support pagination, hence no next page
-								return "", nil
-							}
-
-							return "", err
-						}
-
-						if nextPageNum == nil {
-							// next page doesn't exist
-							return "", nil
-						}
-
-						// use request URL to infer the next page URL
-						previousPage.WithQueryParam("page", strconv.FormatInt(*nextPageNum, 10))
-
-						return previousPage.String(), nil
-					},
-				}
-			},
-		},
-		deep.Dependency{
-			Constructor: func() *deep.ReadObjectLocator {
-				return &deep.ReadObjectLocator{
-					Locate: func(config common.ReadParams) string {
-						return "data"
-					},
-				}
-			},
-		},
+		urlResolver,
+		firstPage,
+		nextPage,
+		readObjectLocator,
 	)
 }
 
