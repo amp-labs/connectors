61e7bfa14d774509a68394bd964307e9300d6be8
Cobalt0s <constantin.y.koval@gmail.com>
2024-10-11 00:58:07 +0300

    zendesksupport - deep.Writer

--- providers/zendesksupport/connector.go
+++ providers/zendesksupport/connector.go
@@ -4,12 +4,14 @@ import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/interpreter"
 	"github.com/amp-labs/connectors/common/jsonquery"
+	"github.com/amp-labs/connectors/common/naming"
 	"github.com/amp-labs/connectors/common/paramsbuilder"
 	"github.com/amp-labs/connectors/common/urlbuilder"
 	"github.com/amp-labs/connectors/internal/deep"
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/providers/zendesksupport/metadata"
 	"github.com/spyzhov/ajson"
+	"strconv"
 )
 
 const apiVersion = "v2"
@@ -18,6 +20,7 @@ type Connector struct {
 	deep.Clients
 	deep.EmptyCloser
 	deep.Reader
+	deep.Writer
 	deep.StaticMetadata
 }
 
@@ -31,12 +34,14 @@ func NewConnector(opts ...Option) (*Connector, error) {
 		clients *deep.Clients,
 		closer *deep.EmptyCloser,
 		reader *deep.Reader,
+		writer *deep.Writer,
 		staticMetadata *deep.StaticMetadata,
 	) *Connector {
 		return &Connector{
 			Clients:        *clients,
 			EmptyCloser:    *closer,
 			Reader:         *reader,
+			Writer:         *writer,
 			StaticMetadata: *staticMetadata,
 		}
 	}
@@ -64,6 +69,50 @@ func NewConnector(opts ...Option) (*Connector, error) {
 			return ObjectNameToResponseField.Get(config.ObjectName)
 		},
 	}
+	writeResultBuilder := deep.WriteResultBuilder{
+		Build: func(config common.WriteParams, body *ajson.Node) (*common.WriteResult, error) {
+			nested, err := jsonquery.New(body).Object(config.ObjectName, true)
+			if err != nil {
+				return nil, err
+			}
+
+			if nested == nil {
+				// Field should be in singular form. Either one will be matched.
+				// This one is NOT optional.
+				nested, err = jsonquery.New(body).Object(
+					naming.NewSingularString(config.ObjectName).String(),
+					false,
+				)
+				if err != nil {
+					return nil, err
+				}
+			}
+			// nested node now must be not null, carry on
+
+			rawID, err := jsonquery.New(nested).Integer("id", true)
+			if err != nil {
+				return nil, err
+			}
+
+			recordID := ""
+			if rawID != nil {
+				// optional
+				recordID = strconv.FormatInt(*rawID, 10)
+			}
+
+			data, err := jsonquery.Convertor.ObjectToMap(nested)
+			if err != nil {
+				return nil, err
+			}
+
+			return &common.WriteResult{
+				Success:  true,
+				RecordId: recordID,
+				Errors:   nil,
+				Data:     data,
+			}, nil
+		},
+	}
 
 	return deep.Connector[Connector, parameters](constructor, providers.ZendeskSupport, opts,
 		meta,
@@ -72,6 +121,7 @@ func NewConnector(opts ...Option) (*Connector, error) {
 		objectSupport,
 		nextPage,
 		readObjectLocator,
+		writeResultBuilder,
 	)
 }
 
