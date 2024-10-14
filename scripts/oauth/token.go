d64f02e326f2a2c42c670a82796c0d59216152af
jkarage <josephbkarage@gmail.com>
2024-10-14 16:16:18 +0300

    feat: Add Pipedrive ListMetadata connector

--- scripts/oauth/token.go
+++ scripts/oauth/token.go
@@ -316,7 +316,8 @@ func setup() *OAuthApp {
 		}
 
 		// Determine the OAuth redirect URL.
-		redirect := fmt.Sprintf("%s://localhost:%d%s", *proto, *port, *callback)
+		// redirect := fmt.Sprintf("%s://localhost:%d%s", *proto, *port, *callback)
+		redirect := "https://dev-api.withampersand.com/callbacks/v1/oauth/wewe"
 
 		state, err := registry.GetString(credscanning.Fields.State.Name)
 		if err != nil {
