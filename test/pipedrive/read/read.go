652d05171cc94078dcede0eeab6e84ba927e4438
jkarage <josephbkarage@gmail.com>
2024-10-15 14:53:20 +0300

    feat: Add Pipedrive Read Connector

---
+++ test/pipedrive/read/read.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/test/pipedrive"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := pipedrive.GetPipedriveConnector(ctx)
+	defer utils.Close(conn)
+
+	if err := readActivities(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+	// if err := readDeals(ctx, conn); err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	if err := readLeads(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+}
+
+func readActivities(ctx context.Context, conn connectors.ReadConnector) error {
+	config := connectors.ReadParams{
+		ObjectName: "activities",
+		Since:      time.Now().Add(-720 * time.Hour),
+		Fields:     connectors.Fields("user_id", "done", "id"),
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
+
+// func readDeals(ctx context.Context, conn connectors.ReadConnector) error {
+// 	config := connectors.ReadParams{
+// 		ObjectName: "deals",
+// 		Since:      time.Now().Add(-720 * time.Hour),
+// 		Fields:     connectors.Fields("duration", "start_time", "has_recording", "id"),
+// 	}
+
+// 	result, err := conn.Read(ctx, config)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Print the results
+// 	jsonStr, err := json.MarshalIndent(result, "", "  ")
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	_, _ = os.Stdout.Write(jsonStr)
+// 	_, _ = os.Stdout.WriteString("\n")
+
+// 	return nil
+// }
+
+func readLeads(ctx context.Context, conn connectors.ReadConnector) error {
+	config := connectors.ReadParams{
+		ObjectName: "leads",
+		Since:      time.Now().Add(-720 * time.Hour),
+		Fields:     connectors.Fields("origin", "channel", "title", "id"),
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
