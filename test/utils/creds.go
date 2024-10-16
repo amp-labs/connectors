7ece3e8a527223ef078b7d11028ac68739f7ea87
jlimatampersand <135054647+jlimatampersand@users.noreply.github.com>
2024-10-16 13:34:38 -0700

    mc

--- test/utils/creds.go
+++ test/utils/creds.go
@@ -4,6 +4,47 @@ import (
 	"github.com/amp-labs/connectors/common/scanning/credscanning"
 )
 
+<<<<<<< HEAD
+=======
+// preset values from JSON file from schema equal to response from reference: https://docs.withampersand.com/reference/getconnection
+func JSONFileReaders(filePath string) []scanning.Reader {
+	schema := []scanning.Reader{
+		&scanning.JSONReader{
+			FilePath: filePath,
+			JSONPath: "$.AccessToken.token",
+			KeyName:  utils.AccessToken,
+		},
+		&scanning.JSONReader{
+			FilePath: filePath,
+			JSONPath: "$.RefreshToken.token",
+			KeyName:  utils.RefreshToken,
+		},
+		&scanning.JSONReader{
+			FilePath: filePath,
+			JSONPath: "$.providerApp.clientId",
+			KeyName:  utils.ClientId,
+		},
+		&scanning.JSONReader{
+			FilePath: filePath,
+			JSONPath: "$.providerApp.clientSecret",
+			KeyName:  utils.ClientSecret,
+		},
+		&scanning.JSONReader{
+			FilePath: filePath,
+			JSONPath: "$.providerWorkspaceRef",
+			KeyName:  utils.WorkspaceRef,
+		},
+		&scanning.JSONReader{
+			FilePath: filePath,
+			JSONPath: "$.providerApp.provider",
+			KeyName:  utils.Provider,
+		},
+	}
+
+	return schema
+}
+
+>>>>>>> 1076d84 (fix sf subscribe related methods)
 func MustCreateProvCredJSON(filePath string,
 	withRequiredAccessToken, withRequiredWorkspace bool,
 ) *credscanning.ProviderCredentials {
