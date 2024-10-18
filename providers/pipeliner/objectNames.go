9924560de6320b3ff72f3ae643814618176dbe85
Cobalt0s <constantin.y.koval@gmail.com>
2024-10-18 20:38:23 +0300

    pipeliner

--- providers/pipeliner/objectNames.go
+++ providers/pipeliner/objectNames.go
@@ -1,11 +1,6 @@
 package pipeliner
 
-import (
-	"github.com/amp-labs/connectors/common/handy"
-	"github.com/amp-labs/connectors/providers/pipeliner/metadata"
-)
+import "github.com/amp-labs/connectors/providers/pipeliner/metadata"
 
 // Supported object names can be found under schemas.json.
-var supportedObjectsByRead = handy.NewSetFromList( //nolint:gochecknoglobals
-	metadata.Schemas.GetObjectNames(),
-)
+var supportedObjectsByRead = metadata.Schemas.ObjectNames() //nolint:gochecknoglobals
