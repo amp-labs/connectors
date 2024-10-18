2b015178b288fc72da82131942ef8200067610da
Cobalt0s <constantin.y.koval@gmail.com>
2024-10-18 20:37:36 +0300

    gong

--- providers/gong/objectNames.go
+++ providers/gong/objectNames.go
@@ -10,9 +10,7 @@ const (
 )
 
 // Supported object names can be found under schemas.json.
-var supportedObjectsByRead = handy.NewSetFromList( //nolint:gochecknoglobals
-	metadata.Schemas.GetObjectNames(),
-)
+var supportedObjectsByRead = metadata.Schemas.ObjectNames() //nolint:gochecknoglobals
 
 var supportedObjectsByWrite = handy.NewSet( //nolint:gochecknoglobals
 	objectNameCalls,
