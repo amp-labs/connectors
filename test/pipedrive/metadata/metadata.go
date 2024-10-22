6fad67462670bd550689ff246217cbac1ff687b6
JKarage <josephbkarage@gmail.com>
2024-10-22 08:14:46 +0300

    feat: Add Pipedrive ListMetadata connector (#1068)
    * feat: Add Pipedrive ListMetadata connector

---
+++ test/pipedrive/metadata/metadata.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os/signal"
+	"syscall"
+
+	"github.com/amp-labs/connectors/test/pipedrive"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := pipedrive.GetPipedriveConnector(ctx)
+	defer utils.Close(conn)
+
+	m, err := conn.ListObjectMetadata(ctx, []string{"activities", "callLogs", "currencies", "deals"})
+	if err != nil {
+		utils.Fail("error listing metadata for Pipedrive", "error", err)
+	}
+
+	// Print the results
+	fmt.Println("Results: ", m.Result)
+	fmt.Println("Errors: ", m.Errors)
+}
