b0e032906f4250cf01fd1f342e0e32f98e4a2403
Cobalt0s <constantin.y.koval@gmail.com>
2024-10-23 01:38:22 +0300

    feat: CustomerJourneysApp (Metadata)

---
+++ providers/customerapp/metadata.go
@@ -0,0 +1,18 @@
+package customerapp
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/providers/customerapp/metadata"
+)
+
+func (c *Connector) ListObjectMetadata(
+	ctx context.Context, objectNames []string,
+) (*common.ListObjectMetadataResult, error) {
+	if len(objectNames) == 0 {
+		return nil, common.ErrMissingObjects
+	}
+
+	return metadata.Schemas.Select(c.Module.ID, objectNames)
+}
