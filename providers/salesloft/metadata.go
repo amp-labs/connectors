8c532f529f3df10b477d139ceec3af653ecff6f5
Cobalt0s <constantin.y.koval@gmail.com>
2024-10-23 17:49:54 +0300

    salesloft

--- providers/salesloft/metadata.go
+++ providers/salesloft/metadata.go
@@ -10,9 +10,5 @@ import (
 func (c *Connector) ListObjectMetadata(
 	ctx context.Context, objectNames []string,
 ) (*common.ListObjectMetadataResult, error) {
-	if len(objectNames) == 0 {
-		return nil, common.ErrMissingObjects
-	}
-
-	return metadata.Schemas.Select(objectNames)
+	return metadata.Schemas.Select(c.Module.ID, objectNames)
 }
