372d97222bd8d70e7d9c94810f3ce75944bd815f
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-10-28 16:34:45 +0200

    feat: CustomerJourneysApp (Metadata) (#1095)

---
+++ providers/customerapp/metadata_test.go
@@ -0,0 +1,77 @@
+package customerapp
+
+import (
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/staticschema"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+)
+
+func TestListObjectMetadata(t *testing.T) { // nolint:funlen,gocognit,cyclop
+	t.Parallel()
+
+	tests := []testroutines.Metadata{
+		{
+			Name:         "At least one object name must be queried",
+			Input:        nil,
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:         "Unknown object requested",
+			Input:        []string{"butterflies"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{staticschema.ErrObjectNotFound},
+		},
+		{
+			Name:   "Successfully describe multiple objects with metadata",
+			Input:  []string{"reporting_webhooks", "workspaces"},
+			Server: mockserver.Dummy(),
+			Expected: &common.ListObjectMetadataResult{
+				Result: map[string]common.ObjectMetadata{
+					"reporting_webhooks": {
+						DisplayName: "Reporting Webhooks",
+						FieldsMap: map[string]string{
+							"disabled":        "disabled",
+							"endpoint":        "endpoint",
+							"events":          "events",
+							"full_resolution": "full_resolution",
+							"id":              "id",
+							"name":            "name",
+							"with_content":    "with_content",
+						},
+					},
+					"workspaces": {
+						DisplayName: "Workspaces",
+						FieldsMap: map[string]string{
+							"billable_messages_sent": "billable_messages_sent",
+							"id":                     "id",
+							"messages_sent":          "messages_sent",
+							"name":                   "name",
+							"object_types":           "object_types",
+							"objects":                "objects",
+							"people":                 "people",
+						},
+					},
+				},
+				Errors: nil,
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		// nolint:varnamelen
+		tt := tt // rebind, omit loop side effects for parallel goroutine
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.ObjectMetadataConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
---
+++ scripts/openapi/customerio/journeysapp/metadata/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+
+	"github.com/amp-labs/connectors/common/handy"
+	"github.com/amp-labs/connectors/internal/staticschema"
+	"github.com/amp-labs/connectors/providers/customerapp"
+	"github.com/amp-labs/connectors/providers/customerapp/metadata"
+	"github.com/amp-labs/connectors/providers/customerapp/openapi"
+	"github.com/amp-labs/connectors/tools/fileconv/api3"
+	"github.com/amp-labs/connectors/tools/scrapper"
+)
+
+var (
+	ignoreEndpoints = []string{ // nolint:gochecknoglobals
+		"/v1/customers",
+		"/v1/exports/customers",
+	}
+	displayNameOverride = map[string]string{ // nolint:gochecknoglobals
+
+	}
+)
+
+func main() {
+	explorer, err := openapi.FileManager.GetExplorer(
+		api3.WithDisplayNamePostProcessors(
+			api3.CamelCaseToSpaceSeparated,
+			api3.CapitalizeFirstLetterEveryWord,
+		))
+	must(err)
+
+	objects, err := explorer.ReadObjectsGet(
+		api3.NewDenyPathStrategy(ignoreEndpoints),
+		nil, displayNameOverride,
+		api3.CustomMappingObjectCheck(customerapp.ObjectNameToResponseField),
+	)
+	must(err)
+
+	schemas := staticschema.NewMetadata()
+	registry := handy.NamedLists[string]{}
+
+	for _, object := range objects {
+		if object.Problem != nil {
+			slog.Error("schema not extracted",
+				"objectName", object.ObjectName,
+				"error", object.Problem,
+			)
+		}
+
+		for _, field := range object.Fields {
+			schemas.Add("", object.ObjectName, object.DisplayName, field, object.URLPath, nil)
+		}
+
+		for _, queryParam := range object.QueryParams {
+			registry.Add(queryParam, object.ObjectName)
+		}
+	}
+
+	must(metadata.FileManager.SaveSchemas(schemas))
+	must(metadata.FileManager.SaveQueryParamStats(scrapper.CalculateQueryParamStats(registry)))
+
+	log.Println("Completed.")
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
