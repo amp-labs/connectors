67a438a5514d6e8f3bf44aa4abcdb421cc9ba5fb
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-10-28 16:12:49 +0200

    feat: Infer params for API Key auth from catalog (#1078)

---
+++ common/paramsbuilder/authclient.go
@@ -0,0 +1,38 @@
+package paramsbuilder
+
+import (
+	"errors"
+
+	"github.com/amp-labs/connectors/common"
+)
+
+var ErrMissingClient = errors.New("http client not set")
+
+// AuthClient params sets up authenticated proxy HTTP client.
+type AuthClient struct {
+	// Caller is an HTTP client that knows how to make authenticated requests.
+	// It also knows how to handle authentication and API response errors.
+	Caller *common.HTTPClient
+}
+
+func (p *AuthClient) ValidateParams() error {
+	// http client must be defined.
+	if p.Caller == nil {
+		return ErrMissingClient
+	}
+
+	// authentication client should be present.
+	if p.Caller.Client == nil {
+		return ErrMissingClient
+	}
+
+	return nil
+}
+
+// WithAuthenticatedClient sets up an HTTP client that uses your implementation of authentication.
+func (p *AuthClient) WithAuthenticatedClient(client common.AuthenticatedHTTPClient) {
+	p.Caller = &common.HTTPClient{
+		Client:       client,
+		ErrorHandler: common.InterpretError,
+	}
+}
