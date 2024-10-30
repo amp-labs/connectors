035da1c5f1011615024cf25aa87a63d628ceadb6
Cobalt0s <constantin.y.koval@gmail.com>
2024-10-30 22:16:20 +0200

    feat: ConstantContract (Metadata)

---
+++ providers/constantcontact/metadata_test.go
@@ -0,0 +1,74 @@
+package constantcontact
+
+import (
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/staticschema"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+)
+
+func TestListObjectMetadata(t *testing.T) { // nolint:funlen,gocognit,cyclop
+	t.Parallel()
+
+	tests := []testroutines.Metadata{
+		{
+			Name:         "At least one object name must be queried",
+			Input:        nil,
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:         "Unknown object requested",
+			Input:        []string{"butterflies"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{staticschema.ErrObjectNotFound},
+		},
+		{
+			Name:   "Successfully describe multiple objects with metadata",
+			Input:  []string{"email_campaigns", "contact_tags"},
+			Server: mockserver.Dummy(),
+			Expected: &common.ListObjectMetadataResult{
+				Result: map[string]common.ObjectMetadata{
+					"email_campaigns": {
+						DisplayName: "Email Campaigns",
+						FieldsMap: map[string]string{
+							"campaign_id":    "campaign_id",
+							"created_at":     "created_at",
+							"current_status": "current_status",
+							"name":           "name",
+							"type":           "type",
+							"type_code":      "type_code",
+							"updated_at":     "updated_at",
+						},
+					},
+					"contact_tags": {
+						DisplayName: "Contact Tags",
+						FieldsMap: map[string]string{
+							"contacts_count": "contacts_count",
+							"created_at":     "created_at",
+							"name":           "name",
+							"tag_id":         "tag_id",
+							"tag_source":     "tag_source",
+							"updated_at":     "updated_at",
+						},
+					},
+				},
+				Errors: nil,
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.ObjectMetadataConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
--- providers/constantcontact/objectNames.go
+++ providers/constantcontact/objectNames.go
@@ -1,29 +1,6 @@
 package constantcontact
 
-import (
-	"github.com/amp-labs/connectors/internal/datautils"
-	"github.com/amp-labs/connectors/providers/constantcontact/metadata"
-)
+import "github.com/amp-labs/connectors/providers/constantcontact/metadata"
 
 // Supported object names can be found under schemas.json.
 var supportedObjectsByRead = metadata.Schemas.ObjectNames() //nolint:gochecknoglobals
-
-// ObjectNameToResponseField maps ObjectName to the response field name which contains that object.
-var ObjectNameToResponseField = datautils.NewDefaultMap(map[string]string{ //nolint:gochecknoglobals
-	"campaign_id_xrefs":        "xrefs",
-	"contact_id_xrefs":         "xrefs",
-	"list_id_xrefs":            "xrefs",
-	"accounts":                 "site_owner_list",
-	"email_campaign_summaries": "bulk_email_campaign_summaries",
-	"contact_tags":             "tags",
-	"contact_lists":            "lists",
-	"contact_custom_fields":    "custom_fields",
-	"email_campaigns":          "campaigns",
-	"account_emails":           "", // response is already an array, empty refers to current
-	"privileges":               "",
-	"subscriptions":            "",
-},
-	func(objectName string) (fieldName string) {
-		return objectName
-	},
-)
