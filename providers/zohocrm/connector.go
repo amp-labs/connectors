9897f3253e1564f6d5bca2e39311104255da2eab
JKarage <josephbkarage@gmail.com>
2024-10-30 09:16:43 +0300

    feat: Add ZohoCRM ListMetadata connnector (#1090)
    * feat: Add ZohoCRM ListMetadata connnector
    
    * fix: provider details
    
    * Address review 1
    
    * Address review 2

---
+++ providers/zohocrm/connector.go
@@ -0,0 +1,60 @@
+package zohocrm
+
+import (
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/paramsbuilder"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/providers"
+)
+
+const apiVersion = "crm/v6"
+
+type Connector struct {
+	BaseURL string
+	Client  *common.JSONHTTPClient
+}
+
+func NewConnector(opts ...Option) (conn *Connector, outErr error) {
+	defer common.PanicRecovery(func(cause error) {
+		outErr = cause
+		conn = nil
+	})
+
+	params, err := paramsbuilder.Apply(parameters{}, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	conn = &Connector{
+		Client: &common.JSONHTTPClient{
+			HTTPClient: params.Client.Caller,
+		},
+	}
+
+	providerInfo, err := providers.ReadInfo(conn.Provider())
+	if err != nil {
+		return nil, err
+	}
+
+	conn.setBaseURL(providerInfo.BaseURL)
+
+	return conn, nil
+}
+
+func (c *Connector) setBaseURL(newURL string) {
+	c.BaseURL = newURL
+	c.Client.HTTPClient.Base = newURL
+}
+
+// Provider returns the connector provider.
+func (c *Connector) Provider() providers.Provider {
+	return providers.Zoho
+}
+
+func (c *Connector) getAPIURL(prefix string) (*urlbuilder.URL, error) {
+	return urlbuilder.New(c.BaseURL, apiVersion, prefix)
+}
+
+func (c *Connector) String() string {
+	return c.Provider() + ".Connector"
+}
---
+++ providers/zohocrm/metadata.go
@@ -0,0 +1,113 @@
+package zohocrm
+
+import (
+	"context"
+	"sync"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/naming"
+)
+
+// restMetadataEndpoint is the resource for retrieving metadata details.
+// doc: https://www.zoho.com/crm/developer/docs/api/v6/field-meta.html
+const restMetadataEndpoint = "settings/fields"
+
+// apiKeyField is the key holding the metadata field name.
+const apiKeyField = "api_name"
+
+type metadataFields struct {
+	Fields []map[string]any `json:"fields"`
+}
+
+func (c *Connector) ListObjectMetadata(ctx context.Context,
+	objectNames []string,
+) (*common.ListObjectMetadataResult, error) {
+	var (
+		wg sync.WaitGroup //nolint: varnamelen
+		mu sync.Mutex     //nolint: varnamelen
+	)
+
+	if len(objectNames) == 0 {
+		return nil, common.ErrMissingObjects
+	}
+
+	objectMetadata := common.ListObjectMetadataResult{
+		Result: make(map[string]common.ObjectMetadata, len(objectNames)),
+		Errors: make(map[string]error, len(objectNames)),
+	}
+
+	wg.Add(len(objectNames))
+
+	for _, object := range objectNames {
+		go func(object string) {
+			metadata, err := c.getMetadata(ctx, object)
+			if err != nil {
+				mu.Lock()
+				objectMetadata.Errors[object] = err
+				mu.Unlock()
+				wg.Done()
+
+				return
+			}
+
+			mu.Lock()
+			objectMetadata.Result[object] = *metadata
+			mu.Unlock()
+
+			wg.Done()
+		}(object)
+	}
+
+	// Wait for all goroutines to finish their calls.
+	wg.Wait()
+
+	return &objectMetadata, nil
+}
+
+func (c *Connector) getMetadata(ctx context.Context, objectName string) (*common.ObjectMetadata, error) {
+	url, err := c.getAPIURL(restMetadataEndpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	capObj := naming.CapitalizeFirstLetterEveryWord(objectName)
+
+	// setting this, returns both used and unused fields
+	url.WithQueryParam("type", "all")
+	url.WithQueryParam("module", capObj)
+
+	resp, err := c.Client.Get(ctx, url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	metadata, err := parseMetadataResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	metadata.DisplayName = capObj
+
+	return metadata, nil
+}
+
+func parseMetadataResponse(resp *common.JSONHTTPResponse) (*common.ObjectMetadata, error) {
+	response, err := common.UnmarshalJSON[metadataFields](resp)
+	if err != nil {
+		return nil, err
+	}
+
+	metadata := &common.ObjectMetadata{
+		FieldsMap: make(map[string]string),
+	}
+
+	// Ranging on the fields Slice, to construct the metadata fields.
+	for _, f := range response.Fields {
+		apiField, ok := f[apiKeyField].(string)
+		if ok {
+			metadata.FieldsMap[apiField] = apiField
+		}
+	}
+
+	return metadata, nil
+}
