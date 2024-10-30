eed56efa1470fe342b3c88aa199ef8a8785672ff
JKarage <josephbkarage@gmail.com>
2024-10-30 12:02:49 +0300

    feat:Add ZohoCRM Read connector (#1096)
    * feat: Add ZohoCRM Read connector
    
    * fix: since when not provided
    
    * fix: Since Headers bug
    
    * Address Review 1
    
    * update printing tests results
    
    * Remove custom since query param.
    
    * Address header return review

---
+++ providers/zohocrm/read.go
@@ -0,0 +1,50 @@
+package zohocrm
+
+import (
+	"context"
+	"time"
+
+	"github.com/amp-labs/connectors/common"
+)
+
+// Read retrieves data based on the provided common.ReadParams configuration parameters.
+// ref: https://www.zoho.com/crm/developer/docs/api/v6/get-records.html
+func (c *Connector) Read(ctx context.Context, config common.ReadParams) (*common.ReadResult, error) {
+	if err := config.ValidateParams(true); err != nil {
+		return nil, err
+	}
+
+	url, err := c.buildReadURL(config)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := constructHeaders(config)
+
+	res, err := c.Client.Get(ctx, url.String(), headers...)
+	if err != nil {
+		return nil, err
+	}
+
+	return common.ParseResult(res,
+		common.GetRecordsUnderJSONPath("data"),
+		getNextRecordsURL(url),
+		common.GetMarshaledData,
+		config.Fields,
+	)
+}
+
+func constructHeaders(config common.ReadParams) []common.Header {
+	// Add the `If-Modified-Since` header if provided.
+	// All Objects(or Modules in ZohoCRM terms) supports this.
+	if !config.Since.IsZero() {
+		return []common.Header{
+			{
+				Key:   "If-Modified-Since",
+				Value: config.Since.Format(time.RFC3339),
+			},
+		}
+	}
+
+	return []common.Header{}
+}
---
+++ test/zohocrm/read/read.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/test/utils"
+	"github.com/amp-labs/connectors/test/zohocrm"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := zohocrm.GetZohoConnector(ctx)
+	defer utils.Close(conn)
+
+	if err := readContacts(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := readDeals(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := readLeads(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+}
+
+func readContacts(ctx context.Context, conn connectors.ReadConnector) error {
+	config := connectors.ReadParams{
+		ObjectName: "contacts",
+		Since:      time.Now().Add(-30 * time.Hour),
+		Fields:     connectors.Fields("Assistant", "Created_By", "Full_Name", "id", "Created_Time"),
+		// NextPage:   "https://www.zohoapis.com/crm/v6/Contacts?fields=Assistant%2CCreated_By%2CFull_Name%2Cid%2CCreated_Time\u0026page_token=089df74ef7734aa9f877fa670550bcbafc9c43567bb2f2e2404aa4d2a466a0b2c9432951d4eb3ffe73094c25e18b4c6290eedc53160535ab40b8ed204dd80e7247a90a4d5cd8d69a348dcaeefbccd8087f658d4a72cfa6aaab8ae8e7065246bf6d1fffce6a3eb2a06bab02e3ae935bc3fb63067b3e0da43e421e36b71b7c1bb843d3af99c7679b53100a8f7b8343f012\u0026since=2024-10-22T16%3A15%3A55%2B03%3A00",
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(jsonStr))
+
+	return nil
+}
+
+func readDeals(ctx context.Context, conn connectors.ReadConnector) error {
+	config := connectors.ReadParams{
+		ObjectName: "deals",
+		Since:      time.Now().Add(-720 * time.Hour),
+		Fields:     connectors.Fields("Account_Name", "Closing_Date", "id"),
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(jsonStr))
+
+	return nil
+}
+
+func readLeads(ctx context.Context, conn connectors.ReadConnector) error {
+	config := connectors.ReadParams{
+		ObjectName: "leads",
+		Since:      time.Now().Add(-720 * time.Hour),
+		Fields:     connectors.Fields("Converted_Date_Time", "Email", "Record_Status__s", "id"),
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(jsonStr))
+
+	return nil
+}
