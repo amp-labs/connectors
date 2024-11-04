8bbc0c1887e38b0e6bdf3957229419fee5f2bf9b
jlimatampersand <135054647+jlimatampersand@users.noreply.github.com>
2024-11-04 15:31:10 -0800

    todo for url

--- providers/hubspot/url.go
+++ providers/hubspot/url.go
@@ -6,5 +6,6 @@ import (
 
 // getURL is a helper to return the full URL considering the base URL & module.
 func (c *Connector) getURL(arg string) string {
+	// TODO: use url package to join paths and avoid issues with slashes in another PR
 	return c.BaseURL + "/" + path.Join(c.Module.Path(), arg)
 }
