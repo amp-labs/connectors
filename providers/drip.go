dfa926e0b00e7b61c9c4dc6bcb9c82189be1367b
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2024-11-18 11:46:45 +0530

    feat: Add Drip connector (#1141)

---
+++ providers/drip.go
@@ -0,0 +1,41 @@
+package providers
+
+const Drip Provider = "drip"
+
+func init() {
+	// Drip configuration
+	SetInfo(Drip, ProviderInfo{
+		DisplayName: "Drip",
+		AuthType:    Oauth2,
+		BaseURL:     "https://api.getdrip.com",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType:                 AuthorizationCode,
+			AuthURL:                   "https://www.getdrip.com/oauth/authorize",
+			TokenURL:                  "https://www.getdrip.com/oauth/token",
+			ExplicitScopesRequired:    false,
+			ExplicitWorkspaceRequired: false,
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1731568403/media/drip.com_1731568403.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1731568457/media/drip.com_1731568458.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1731568403/media/drip.com_1731568403.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1731568430/media/drip.com_1731568431.svg",
+			},
+		},
+	})
+}
