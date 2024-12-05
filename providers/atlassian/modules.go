572c9a5969cc2c10db9dd9d7ed78d77ab94ed3c0
Rajat Pawar <18614743+RajatPawar@users.noreply.github.com>
2024-12-06 03:41:37 +0530

    feat: Allow a module to override base URL (#1189)

--- providers/atlassian/modules.go
+++ providers/atlassian/modules.go
@@ -9,6 +9,8 @@ const (
 	ModuleEmpty common.ModuleID = ""
 	// ModuleJira is the module used for listing Jira issues.
 	ModuleJira common.ModuleID = "jira"
+	// ModuleAtlassianJiraConnect is the module used for Atlassian Connect.
+	ModuleAtlassianJiraConnect common.ModuleID = "atlassian-connect"
 )
 
 // supportedModules represents currently working and supported modules within the Atlassian connector.
@@ -21,6 +23,11 @@ var supportedModules = common.Modules{ // nolint: gochecknoglobals
 	},
 	ModuleJira: {
 		ID:      ModuleJira,
+		Label:   "rest/api",
+		Version: "3",
+	},
+	ModuleAtlassianJiraConnect: {
+		ID:      ModuleAtlassianJiraConnect,
 		Label:   "rest/api",
 		Version: "3",
 	},
