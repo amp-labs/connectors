084c156433653b8f82fa18d254e2a6f22b37f144
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2024-12-16 21:06:34 +0200

    [CON-185] Keap - metadata custom fields (#1151)

--- providers/keap/metadata_test.go
+++ providers/keap/metadata_test.go
@@ -1,17 +1,24 @@
 package keap
 
 import (
+	"errors"
+	"log/slog"
+	"net/http"
 	"testing"
 
 	"github.com/amp-labs/connectors"
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/staticschema"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockcond"
 	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
 	"github.com/amp-labs/connectors/test/utils/testroutines"
+	"github.com/amp-labs/connectors/test/utils/testutils"
 )
 
 func TestListObjectMetadataV1(t *testing.T) { // nolint:funlen,gocognit,cyclop
 	t.Parallel()
+
+	responseContactsModel := testutils.DataFromFile(t, "custom-fields-contacts.json")
 
 	tests := []testroutines.Metadata{
 		{
@@ -27,9 +34,13 @@ func TestListObjectMetadataV1(t *testing.T) { // nolint:funlen,gocognit,cyclop
 			ExpectedErrs: []error{staticschema.ErrObjectNotFound},
 		},
 		{
-			Name:       "Successfully describe multiple objects with metadata",
-			Input:      []string{"campaigns", "products"},
-			Server:     mockserver.Dummy(),
+			Name:  "Successfully describe multiple objects with metadata",
+			Input: []string{"campaigns", "products", "contacts"},
+			Server: mockserver.Conditional{
+				Setup: mockserver.ContentJSON(),
+				If:    mockcond.PathSuffix("/crm/rest/v1/contacts/model"),
+				Then:  mockserver.Response(http.StatusOK, responseContactsModel),
+			}.Server(),
 			Comparator: testroutines.ComparatorSubsetMetadata,
 			Expected: &common.ListObjectMetadataResult{
 				Result: map[string]common.ObjectMetadata{
@@ -49,6 +60,73 @@ func TestListObjectMetadataV1(t *testing.T) { // nolint:funlen,gocognit,cyclop
 							"sku":           "sku",
 							"status":        "status",
 							"product_price": "product_price",
+						},
+					},
+					"contacts": {
+						DisplayName: "Contacts",
+						FieldsMap: map[string]string{
+							"ScoreValue":       "ScoreValue",
+							"addresses":        "addresses",
+							"anniversary":      "anniversary",
+							"birthday":         "birthday",
+							"company":          "company",
+							"company_name":     "company_name",
+							"contact_type":     "contact_type",
+							"custom_fields":    "custom_fields",
+							"date_created":     "date_created",
+							"email_addresses":  "email_addresses",
+							"email_opted_in":   "email_opted_in",
+							"email_status":     "email_status",
+							"family_name":      "family_name",
+							"fax_numbers":      "fax_numbers",
+							"given_name":       "given_name",
+							"id":               "id",
+							"job_title":        "job_title",
+							"last_updated":     "last_updated",
+							"lead_source_id":   "lead_source_id",
+							"middle_name":      "middle_name",
+							"owner_id":         "owner_id",
+							"phone_numbers":    "phone_numbers",
+							"preferred_locale": "preferred_locale",
+							"preferred_name":   "preferred_name",
+							"prefix":           "prefix",
+							"social_accounts":  "social_accounts",
+							"source_type":      "source_type",
+							"spouse_name":      "spouse_name",
+							"suffix":           "suffix",
+							"time_zone":        "time_zone",
+							"website":          "website",
+							// Custom fields.
+							"jobtitle":       "title",
+							"jobdescription": "job_description",
+							"experience":     "experience",
+							"age":            "age",
+						},
+					},
+				},
+				Errors: nil,
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name:  "Partial metadata due to failed custom data requests",
+			Input: []string{"contacts"},
+			Server: mockserver.Conditional{
+				Setup: mockserver.ContentJSON(),
+				If:    mockcond.PathSuffix("/crm/rest/v1/contacts/model"),
+				Then:  mockserver.Response(http.StatusInternalServerError),
+			}.Server(),
+			Comparator: metadataExpectAbsentFields,
+			Expected: &common.ListObjectMetadataResult{
+				Result: map[string]common.ObjectMetadata{
+					"contacts": {
+						DisplayName: "Contacts",
+						FieldsMap: map[string]string{
+							// These custom fields MUST be absent due to not responding server.
+							"jobtitle":       "title",
+							"jobdescription": "job_description",
+							"experience":     "experience",
+							"age":            "age",
 						},
 					},
 				},
@@ -115,4 +193,24 @@ func TestListObjectMetadataV2(t *testing.T) { // nolint:funlen,gocognit,cyclop
 			})
 		})
 	}
+}
+
+func metadataExpectAbsentFields(serverURL string, actual, expected *common.ListObjectMetadataResult) bool {
+	const contacts = "contacts"
+	if !errors.Is(actual.Errors[contacts], ErrResolvingCustomFields) {
+		slog.Info("missing metadata error", "errors", ErrResolvingCustomFields)
+
+		return false
+	}
+
+	for fieldName := range expected.Result[contacts].FieldsMap {
+		_, present := actual.Result[contacts].FieldsMap[fieldName]
+		if present {
+			slog.Info("custom field should NOT be present", "field", fieldName)
+
+			return false
+		}
+	}
+
+	return true
 }
