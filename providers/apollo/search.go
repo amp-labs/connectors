11076023182f070748c4dfdcb9bfe06bb9a49f66
JKarage <josephbkarage@gmail.com>
2024-12-18 19:10:37 +0300

    [CON-815] feat: (apollo) support reading route to searching  (#1229)

--- providers/apollo/search.go
+++ providers/apollo/search.go
@@ -4,40 +4,23 @@ import (
 	"context"
 
 	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
 )
 
 // search uses POST method to read data.It has a display limit of 50,000 records.
 // It's recommended to filter the results so as to narrow down the results as much as possible.
 // Most of the Filtering would need client's input so we don't exhaust calls by paging through all 50k records.
 // Using this as is may lead to that issue.
-func (c *Connector) Search(ctx context.Context, config common.ReadParams,
+func (c *Connector) Search(ctx context.Context, config common.ReadParams, url *urlbuilder.URL,
 ) (*common.ReadResult, error) {
-	url, err := c.getAPIURL(config.ObjectName, readOp)
-	if err != nil {
-		return nil, err
-	}
-
-	// Adds searching path
-	url.AddPath(searchingPath)
-
-	// Check if searching the next page
-	if len(config.NextPage) > 0 {
-		url.WithQueryParam("page", config.NextPage.String())
-	}
-
-	// Add sorting & filtering criteria
-	// Currently the default values, are what we needed
-	// API sorts by the last activity or creation date timestamp.
-	// So need to change the param details here.
-
-	json, err := c.Client.Post(ctx, url.String(), []byte{})
+	resp, err := c.Client.Post(ctx, url.String(), []byte{})
 	if err != nil {
 		return nil, err
 	}
 
 	return common.ParseResult(
-		json,
-		searchRecords(responseKey[config.ObjectName]),
+		resp,
+		searchRecords(config.ObjectName),
 		getNextRecords,
 		common.GetMarshaledData,
 		config.Fields,
--- test/apollo/read/read.go
+++ test/apollo/read/read.go
@@ -42,6 +42,11 @@ func MainFn() int {
 		return 1
 	}
 
+	err = testReadContacts(ctx, conn)
+	if err != nil {
+		return 1
+	}
+
 	return 0
 }
 
@@ -118,6 +123,29 @@ func testReadSequences(ctx context.Context, conn *ap.Connector) error {
 	params := common.ReadParams{
 		ObjectName: "emailer_campaigns",
 		Fields:     connectors.Fields("id", "name", "archived"),
+	}
+
+	res, err := conn.Read(ctx, params)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
+
+func testReadContacts(ctx context.Context, conn *ap.Connector) error {
+	params := common.ReadParams{
+		ObjectName: "contacts",
+		Fields:     connectors.Fields("id", "first_name", "name"),
 	}
 
 	res, err := conn.Read(ctx, params)
