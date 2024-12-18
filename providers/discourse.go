3419547084c75655a261ca264ed6a3f198cabb67
Dipu <dipuchaurasiya91@gmail.com>
2024-12-19 03:26:14 +0545

    feat: Add Discourse provider integration

---
+++ providers/discourse.go
@@ -0,0 +1,40 @@
+package providers
+
+const Discourse Provider = "discourse"
+
+func init() {
+	SetInfo(Discourse, ProviderInfo{
+		DisplayName: "Discourse",
+		AuthType:    ApiKey,
+		BaseURL:     "https://{{.workspace}}",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Header,
+			Header: &ApiKeyOptsHeader{
+				Name: "Api-Key",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1734557159/media/discourse.org_1734557159.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1734557186/media/discourse.org_1734557186.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: " https://res.cloudinary.com/dycvts6vp/image/upload/v1734557116/media/discourse.org_1734557115.jpg",
+				LogoURL: " https://res.cloudinary.com/dycvts6vp/image/upload/v1734557138/media/discourse.org_1734557138.svg",
+			},
+		},
+	})
+}
