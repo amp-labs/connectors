91d68bf4c65ae7f8adb3d8a73a5979e3f648e63c
Chris Eberle <chris@withampersand.com>
2024-12-19 12:32:20 -0800

    change url

--- providers/hubspot/url.go
+++ providers/hubspot/url.go
@@ -4,16 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"path"
 )
 
 var errMissingValue = errors.New("missing value for query parameter")
 
 // getURL is a helper to return the full URL considering the base URL & module.
 func (c *Connector) getURL(arg string, queryArgs ...string) (string, error) {
-	urlBase, err := url.JoinPath(c.BaseURL, c.Module.Path(), arg)
-	if err != nil {
-		return "", err
-	}
+	urlBase := c.BaseURL + "/" + path.Join(c.Module.Path(), arg)
 
 	if len(queryArgs) > 0 {
 		vals := url.Values{}
