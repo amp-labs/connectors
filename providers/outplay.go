70260d65656edc1b621542c1a8ace3cbb9c4d2c9
Dipu <dipuchaurasiya91@gmail.com>
2025-01-02 08:25:55 +0545

    feat: Add Outplay provider

---
+++ providers/outplay.go
@@ -0,0 +1,34 @@
+package providers
+
+const Outplay Provider = "outplay"
+
+func init() {
+	SetInfo(Outplay, ProviderInfo{
+		DisplayName: "Outplay",
+		AuthType:    Basic,
+		BaseURL:     "https://{{.workspace}}-api.outplayhq.com",
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: " https://res.cloudinary.com/dycvts6vp/image/upload/v1735777081/media/outplayhq.com_1735777080.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1735777173/media/outplayhq.com_1735777172.png",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: " https://res.cloudinary.com/dycvts6vp/image/upload/v1735777081/media/outplayhq.com_1735777080.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1735777173/media/outplayhq.com_1735777172.png",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     true,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		PostAuthInfoNeeded: false,
+	})
+}
