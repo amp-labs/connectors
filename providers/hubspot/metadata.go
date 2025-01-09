795e69bbad5eb1c0eaedec963b99534c5662031e
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-01-09 19:00:00 +0200

    [CON-967] Hubpsot with FieldsMetadata (#1270)

--- providers/hubspot/metadata.go
+++ providers/hubspot/metadata.go
@@ -2,14 +2,13 @@ package hubspot
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/logging"
-	"github.com/spyzhov/ajson"
 )
 
 type objectMetadataResult struct {
@@ -78,15 +77,6 @@ func (c *Connector) ListObjectMetadata( // nolint:cyclop,funlen
 	return objectsMap, nil
 }
 
-type describeObjectResponse struct {
-	Results []describeObjectResult `json:"results"`
-}
-
-type describeObjectResult struct {
-	Name  string `json:"name"`
-	Label string `json:"label"`
-}
-
 // describeObject returns object metadata for the given object name.
 func (c *Connector) describeObject(ctx context.Context, objectName string) (*common.ObjectMetadata, error) {
 	relativeURL := strings.Join([]string{"properties", objectName}, "/")
@@ -101,41 +91,14 @@ func (c *Connector) describeObject(ctx context.Context, objectName string) (*com
 		return nil, fmt.Errorf("error fetching HubSpot fields: %w", err)
 	}
 
-	body, ok := rsp.Body()
-	if !ok {
-		return nil, fmt.Errorf("cannot get HubSpot fields %w", common.ErrEmptyJSONHTTPResponse)
-	}
-
-	rawResponse, err := ajson.Marshal(body)
-	if err != nil {
-		return nil, fmt.Errorf("error marshalling object metadata response into byte array: %w", err)
-	}
-
-	resp := &describeObjectResponse{}
-
-	err = json.Unmarshal(rawResponse, resp)
+	resp, err := common.UnmarshalJSON[fieldDescriptionResponse](rsp)
 	if err != nil {
 		return nil, fmt.Errorf("error unmarshalling object metadata response into JSON: %w", err)
 	}
 
-	return &common.ObjectMetadata{
-		DisplayName: objectName,
-		FieldsMap:   makeFieldsMap(resp),
-	}, nil
-}
-
-// makeFieldsMap returns a map of field name to field label.
-func makeFieldsMap(data *describeObjectResponse) map[string]string {
-	fieldsMap := make(map[string]string)
-
-	for _, field := range data.Results {
-		fieldName := strings.ToLower(field.Name)
-
-		// Add entry to fieldsMap
-		fieldsMap[fieldName] = field.Label
-	}
-
-	return fieldsMap
+	return common.NewObjectMetadata(
+		objectName, resp.transformToFields(),
+	), nil
 }
 
 func (c *Connector) GetPostAuthInfo(
@@ -177,4 +140,119 @@ func (c *Connector) GetAccountInfo(ctx context.Context) (*AccountInfo, *common.J
 	}
 
 	return accountInfo, resp, nil
+}
+
+type fieldDescriptionResponse struct {
+	Results []fieldDescription `json:"results"`
+}
+
+type fieldDescription struct {
+	UpdatedAt            time.Time                 `json:"updatedAt"`
+	CreatedAt            time.Time                 `json:"createdAt"`
+	Name                 string                    `json:"name"`
+	Label                string                    `json:"label"`
+	Type                 string                    `json:"type"`
+	FieldType            string                    `json:"fieldType"`
+	Description          string                    `json:"description"`
+	GroupName            string                    `json:"groupName"`
+	Options              []fieldEnumerationOption  `json:"options"`
+	DisplayOrder         int                       `json:"displayOrder"`
+	Calculated           bool                      `json:"calculated"`
+	ExternalOptions      bool                      `json:"externalOptions"`
+	HasUniqueValue       bool                      `json:"hasUniqueValue"`
+	Hidden               bool                      `json:"hidden"`
+	HubspotDefined       bool                      `json:"hubspotDefined"`
+	ModificationMetadata fieldModificationMetadata `json:"modificationMetadata"`
+	FormField            bool                      `json:"formField"`
+	DataSensitivity      string                    `json:"dataSensitivity"`
+}
+
+type fieldEnumerationOption struct {
+	Label        string `json:"label"`
+	Value        string `json:"value"`
+	Description  string `json:"description"`
+	DisplayOrder int    `json:"displayOrder"`
+	Hidden       bool   `json:"hidden"`
+}
+
+type fieldModificationMetadata struct {
+	Archivable         bool `json:"archivable"`
+	ReadOnlyDefinition bool `json:"readOnlyDefinition"`
+	ReadOnlyValue      bool `json:"readOnlyValue"`
+}
+
+func (r fieldDescriptionResponse) transformToFields() map[string]common.FieldMetadata {
+	fieldsMap := make(map[string]common.FieldMetadata)
+
+	for _, field := range r.Results {
+		fieldName := strings.ToLower(field.Name)
+		fieldsMap[fieldName] = field.transformToFieldMetadata()
+	}
+
+	return fieldsMap
+}
+
+// transformToFieldMetadata converts Provider model of a field into Ampersand's common.FieldMetadata.
+// This normalizes provider response to the unified standard across all providers.
+func (o fieldDescription) transformToFieldMetadata() common.FieldMetadata {
+	var (
+		valueType common.ValueType
+		values    []common.FieldValue
+	)
+
+	// Based on type and field type properties from Hubspot object model map value to Ampersand value type.
+	switch o.Type {
+	case "string":
+		valueType = common.ValueTypeString
+	case "number":
+		valueType = common.ValueTypeFloat
+	case "bool":
+		valueType = common.ValueTypeBoolean
+	case "datetime":
+		valueType = common.ValueTypeDateTime
+	case "enumeration":
+		valueType, values = o.implyEnumerationType()
+		// Enumeration type means there are predefined field values.
+	default:
+		// ex: object_coordinates, phone_number
+		valueType = common.ValueTypeOther
+	}
+
+	return common.FieldMetadata{
+		DisplayName:  o.Label,
+		ValueType:    valueType,
+		ProviderType: o.Type + "." + o.FieldType,
+		ReadOnly:     o.ModificationMetadata.ReadOnlyValue,
+		Values:       values,
+	}
+}
+
+func (o fieldDescription) implyEnumerationType() (common.ValueType, []common.FieldValue) {
+	var values []common.FieldValue
+
+	if len(o.Options) != 0 {
+		// List of values is not nil, at least one option exists.
+		values = make([]common.FieldValue, len(o.Options))
+		for index, option := range o.Options {
+			values[index] = common.FieldValue{
+				Value:        option.Value,
+				DisplayValue: option.Label,
+			}
+		}
+	}
+
+	switch o.FieldType {
+	case "checkbox":
+		return common.ValueTypeMultiSelect, values
+	case "booleancheckbox":
+		// Boolean values are ignored.
+		return common.ValueTypeBoolean, nil
+	case "radio":
+		return common.ValueTypeSingleSelect, values
+	case "select":
+		return common.ValueTypeSingleSelect, values
+	default:
+		// ex: enumeration.calculation_equation
+		return common.ValueTypeOther, values
+	}
 }
--- providers/hubspot/metadata_test.go
+++ providers/hubspot/metadata_test.go
@@ -37,6 +37,128 @@ func TestListObjectMetadata(t *testing.T) { // nolint:funlen,gocognit,cyclop
 				Result: map[string]common.ObjectMetadata{
 					"contacts": {
 						DisplayName: "contacts",
+						Fields: map[string]common.FieldMetadata{
+							// String
+							"address": {
+								DisplayName:  "Street Address",
+								ValueType:    "string",
+								ProviderType: "string.text",
+								ReadOnly:     false,
+								Values:       nil,
+							},
+							"mobilephone": {
+								DisplayName:  "Mobile Phone Number",
+								ValueType:    "string",
+								ProviderType: "string.phonenumber",
+								ReadOnly:     false,
+								Values:       nil,
+							},
+
+							// Boolean.
+							"hs_contact_enrichment_opt_out": {
+								DisplayName:  "Enrichment opt out",
+								ValueType:    "boolean",
+								ProviderType: "bool.booleancheckbox",
+								ReadOnly:     true,
+								Values:       nil,
+							},
+							"autogen": {
+								DisplayName:  "autogen",
+								ValueType:    "boolean",
+								ProviderType: "enumeration.booleancheckbox",
+								ReadOnly:     false,
+								Values:       nil,
+							},
+
+							// Float.
+							"associatedcompanyid": {
+								DisplayName:  "Primary Associated Company ID",
+								ValueType:    "float",
+								ProviderType: "number.number",
+								ReadOnly:     false,
+								Values:       nil,
+							},
+							"hubspotscore": {
+								DisplayName:  "HubSpot Score",
+								ValueType:    "float",
+								ProviderType: "number.calculation_score",
+								ReadOnly:     true,
+								Values:       nil,
+							},
+							"hs_associated_target_accounts": {
+								DisplayName:  "Associated Target Accounts",
+								ValueType:    "float",
+								ProviderType: "number.calculation_rollup",
+								ReadOnly:     true,
+								Values:       nil,
+							},
+
+							// Single/Multi Select.
+							"hs_content_membership_status": {
+								DisplayName:  "Status",
+								ValueType:    "singleSelect",
+								ProviderType: "enumeration.select",
+								ReadOnly:     false,
+								Values: []common.FieldValue{{
+									Value:        "active",
+									DisplayValue: "Active",
+								}, {
+									Value:        "inactive",
+									DisplayValue: "Inactive",
+								}},
+							},
+							"hs_predictivecontactscorebucket": {
+								DisplayName:  "Lead Rating",
+								ValueType:    "singleSelect",
+								ProviderType: "enumeration.radio",
+								ReadOnly:     true,
+								Values: []common.FieldValue{{
+									Value:        "bucket_1",
+									DisplayValue: "1 Star",
+								}, {
+									Value:        "bucket_2",
+									DisplayValue: "2 Stars",
+								}, {
+									Value:        "bucket_3",
+									DisplayValue: "3 Stars",
+								}, {
+									Value:        "bucket_4",
+									DisplayValue: "4 Stars",
+								}},
+							},
+							"hs_all_assigned_business_unit_ids": {
+								DisplayName:  "Business units",
+								ValueType:    "multiSelect",
+								ProviderType: "enumeration.checkbox",
+								ReadOnly:     false,
+								Values:       nil,
+							},
+
+							// Datetime.
+							"hs_first_subscription_create_date": {
+								DisplayName:  "First subscription create date",
+								ValueType:    "datetime",
+								ProviderType: "datetime.calculation_rollup",
+								ReadOnly:     true,
+								Values:       nil,
+							},
+							"hs_date_entered_customer": {
+								DisplayName:  "Date entered 'Customer (Lifecycle Stage Pipeline)'",
+								ValueType:    "datetime",
+								ProviderType: "datetime.calculation_read_time",
+								ReadOnly:     true,
+								Values:       nil,
+							},
+
+							// Others.
+							"hs_notes_last_activity": {
+								DisplayName:  "Last Activity",
+								ValueType:    "other",
+								ProviderType: "object_coordinates.text",
+								ReadOnly:     true,
+								Values:       nil,
+							},
+						},
 						FieldsMap: map[string]string{
 							"address":                         "Street Address",
 							"associatedcompanyid":             "Primary Associated Company ID",
