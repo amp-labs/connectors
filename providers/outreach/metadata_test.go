0da6365ac2802a9dd41b74f11633e2e559fac2fb
JKarage <josephbkarage@gmail.com>
2025-01-14 10:43:34 +0300

    feat: Add MockTests Outreach connector (#1276)
    * feat: Add MockTests Outreach connector
    
    * Address C0sReview

---
+++ providers/outreach/metadata_test.go
@@ -0,0 +1,88 @@
+package outreach
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockcond"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+	"github.com/amp-labs/connectors/test/utils/testutils"
+)
+
+func TestListObjectMetadata(t *testing.T) { // nolint:funlen,gocognit,cyclop
+	t.Parallel()
+
+	opportunityResponse := testutils.DataFromFile(t, "opportunities.json")
+
+	tests := []testroutines.Metadata{
+		{
+			Name:         "At least one object name must be provided",
+			Input:        nil,
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:  "Successfully describe supported objects",
+			Input: []string{"opportunities"},
+			Server: mockserver.Switch{
+				Setup: mockserver.ContentJSON(),
+				Cases: []mockserver.Case{{
+					If:   mockcond.PathSuffix("/v2/opportunities"),
+					Then: mockserver.Response(http.StatusOK, opportunityResponse),
+				}},
+			}.Server(),
+			Comparator: testroutines.ComparatorSubsetMetadata,
+			Expected: &common.ListObjectMetadataResult{
+				Result: map[string]common.ObjectMetadata{
+					"opportunities": {
+						DisplayName: "opportunities",
+						FieldsMap: map[string]string{
+							"mapNumberOfOverdueTasks": "mapNumberOfOverdueTasks",
+							"mapStatus":               "mapStatus",
+							"name":                    "name",
+							"nextStep":                "nextStep",
+							"opportunityType":         "opportunityType",
+							"probability":             "probability",
+							"prospectingRepId":        "prospectingRepId",
+							"sharingTeamId":           "sharingTeamId",
+							"tags":                    "tags",
+							"territoryId":             "territoryId",
+							"touchedAt":               "touchedAt",
+							"trashedAt":               "trashedAt",
+							"updatedAt":               "updatedAt",
+						},
+					},
+				},
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		// nolint:varnamelen
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.ObjectMetadataConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
+
+func constructTestConnector(serverURL string) (*Connector, error) {
+	connector, err := NewConnector(
+		WithAuthenticatedClient(http.DefaultClient),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// for testing we want to redirect calls to our mock server
+	connector.setBaseURL(serverURL)
+
+	return connector, nil
+}
