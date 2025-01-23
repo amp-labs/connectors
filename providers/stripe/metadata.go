3ebfe49f05f2198ff5a48d8bf9cd102d2c2b6554
Cobalt0s <constantin.y.koval@gmail.com>
2025-01-23 03:13:43 +0200

    [CON-883] feat(Stripe): ListObjectMetadata

---
+++ providers/stripe/metadata.go
@@ -0,0 +1,14 @@
+package stripe
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/providers/stripe/metadata"
+)
+
+func (c *Connector) ListObjectMetadata(
+	ctx context.Context, objectNames []string,
+) (*common.ListObjectMetadataResult, error) {
+	return metadata.Schemas.Select(c.Module.ID, objectNames)
+}
---
+++ test/stripe/metadata/customers/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	connTest "github.com/amp-labs/connectors/test/stripe"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+var objectName = "customers"
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := connTest.GetStripeConnector(ctx)
+
+	metadata, err := conn.ListObjectMetadata(ctx, []string{
+		objectName,
+	})
+	if err != nil {
+		utils.Fail("error listing metadata for Stripe", "error", err)
+	}
+
+	slog.Info("Metadata " + objectName + "..")
+	utils.DumpJSON(metadata, os.Stdout)
+}
