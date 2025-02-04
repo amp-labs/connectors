2447eac6627bc7d6cd89731e772a71affcbcea98
Sanjay <sanjaykanth.a@mitrahsoft.com>
2025-02-04 11:06:55 +0530

    fix: Removed unsupported datacenter configurations and changed display to human-readable format

--- providers/solarWindsServiceDesk.go
+++ providers/solarWindsServiceDesk.go
@@ -2,15 +2,13 @@ package providers
 
 const (
 	SolarWindsServiceDeskUS Provider = "solarWindsServiceDeskUS"
-	SolarWindsServiceDeskEU Provider = "solarWindsServiceDeskEU"
-	SolarWindsServiceDeskAU Provider = "solarWindsServiceDeskAU"
 )
 
-//nolint:funlen
+// SolarWinds Service Desk has data centers in three regions: US, EU, and APJ, each with a different base URL.
 func init() {
-	// SolarWindsServiceDesk US(region) configuration
+	// SolarWindsServiceDesk US configuration
 	SetInfo(SolarWindsServiceDeskUS, ProviderInfo{
-		DisplayName: "solarWindsServiceDeskUS",
+		DisplayName: "Solar Winds Service Desk US",
 		AuthType:    ApiKey,
 		BaseURL:     "https://api.samanage.com",
 		ApiKeyOpts: &ApiKeyOpts{
@@ -19,81 +17,7 @@ func init() {
 				Name:        "X-Samanage-Authorization",
 				ValuePrefix: "Bearer ",
 			},
-			DocsURL: "https://apidoc.samanage.com/",
-		}, Support: Support{
-			BulkWrite: BulkWriteSupport{
-				Insert: false,
-				Update: false,
-				Upsert: false,
-				Delete: false,
-			},
-			Proxy:     false,
-			Read:      false,
-			Subscribe: false,
-			Write:     false,
-		},
-		PostAuthInfoNeeded: false,
-		Media: &Media{
-			DarkMode: &MediaTypeDarkMode{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322100/media/solarwinds.com_1738322099.svg",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322158/media/solarwinds.com_1738322159.svg",
-			},
-			Regular: &MediaTypeRegular{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322100/media/solarwinds.com_1738322099.svg",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322158/media/solarwinds.com_1738322159.svg",
-			},
-		},
-	})
-
-	// SolarWindsServiceDesk EU(region) configuration
-	SetInfo(SolarWindsServiceDeskEU, ProviderInfo{
-		DisplayName: "solarWindsServiceDeskEU",
-		AuthType:    ApiKey,
-		BaseURL:     "https://apieu.samanage.com",
-		ApiKeyOpts: &ApiKeyOpts{
-			AttachmentType: Header,
-			Header: &ApiKeyOptsHeader{
-				Name:        "X-Samanage-Authorization",
-				ValuePrefix: "Bearer ",
-			},
-			DocsURL: "https://apidoc.samanage.com/",
-		}, Support: Support{
-			BulkWrite: BulkWriteSupport{
-				Insert: false,
-				Update: false,
-				Upsert: false,
-				Delete: false,
-			},
-			Proxy:     false,
-			Read:      false,
-			Subscribe: false,
-			Write:     false,
-		},
-		PostAuthInfoNeeded: false,
-		Media: &Media{
-			DarkMode: &MediaTypeDarkMode{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322100/media/solarwinds.com_1738322099.svg",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322158/media/solarwinds.com_1738322159.svg",
-			},
-			Regular: &MediaTypeRegular{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322100/media/solarwinds.com_1738322099.svg",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322158/media/solarwinds.com_1738322159.svg",
-			},
-		},
-	})
-
-	// SolarWindsServiceDesk APJ(region) configuration
-	SetInfo(SolarWindsServiceDeskAU, ProviderInfo{
-		DisplayName: "solarWindsServiceDeskAU",
-		AuthType:    ApiKey,
-		BaseURL:     "https://apiau.samanage.com",
-		ApiKeyOpts: &ApiKeyOpts{
-			AttachmentType: Header,
-			Header: &ApiKeyOptsHeader{
-				Name:        "X-Samanage-Authorization",
-				ValuePrefix: "Bearer ",
-			},
-			DocsURL: "https://apidoc.samanage.com/",
+			DocsURL: "https://documentation.solarwinds.com/en/success_center/swsd/content/completeguidetoswsd/token-authentication-for-api-integration.htm#link4",
 		}, Support: Support{
 			BulkWrite: BulkWriteSupport{
 				Insert: false,
