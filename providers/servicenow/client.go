3917ebe04c9816d26aa0f80663a1cb592f2a136f
jkarage <josephbkarage@gmail.com>
2025-02-11 00:28:18 +0300

    feta: Add Metadata Connector

---
+++ providers/servicenow/client.go
@@ -0,0 +1,12 @@
+package servicenow
+
+import "github.com/amp-labs/connectors/common"
+
+// JSONHTTPClient returns the underlying JSON HTTP client.
+func (c *Connector) JSONHTTPClient() *common.JSONHTTPClient {
+	return c.Client
+}
+
+func (c *Connector) HTTPClient() *common.HTTPClient {
+	return c.Client.HTTPClient
+}
---
+++ test/servicenow/metadata/metadata.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/amp-labs/connectors/test/servicenow"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error(err.Error())
+		os.Exit(-1)
+	}
+}
+
+func run() error {
+	ctx := context.Background()
+
+	// construct connector instance conn
+	conn := servicenow.GetServiceNowConnector(ctx)
+
+	m, err := conn.ListObjectMetadata(ctx, []string{"incident"})
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	fmt.Println("Results: ", m.Result)
+	fmt.Println("Errors: ", m.Errors)
+
+	return nil
+}
