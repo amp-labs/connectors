af6fd815eccdd39dbd5977c7f30c461a4f898d7c
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-02-11 20:32:02 +0200

    tidy(metadata): Separate Schema definition from tools (#1313)

---
+++ internal/metadatadef/definition.go
@@ -0,0 +1,51 @@
+package metadatadef
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/internal/datautils"
+)
+
+// Schema is a model that describes a REST API object.
+// This is usually created when metadata is coming not from API but alternative sources, example: OpenAPI.
+// This model holds more information than common.ObjectMetadata.
+type Schema struct {
+	ObjectName  string
+	DisplayName string
+	Fields      Fields
+	QueryParams []string
+	URLPath     string
+	ResponseKey string
+	Problem     error
+}
+
+type Schemas []Schema
+
+type Field struct {
+	Name string
+	Type string
+}
+
+type Fields = datautils.Map[string, Field]
+
+func (s Schemas) Combine(others Schemas) Schemas {
+	registry := datautils.Map[string, Schema]{}
+	for _, schema := range append(s, others...) {
+		_, found := registry[schema.ObjectName]
+
+		if !found || len(schema.Fields) != 0 {
+			registry[schema.ObjectName] = schema
+		}
+	}
+
+	return registry.Values()
+}
+
+func (s Schema) String() string {
+	if s.Problem != nil {
+		return fmt.Sprintf("    {%v}    ", s.ObjectName)
+	}
+
+	return fmt.Sprintf("%v=[%v]", s.ObjectName, strings.Join(s.Fields.Keys(), ","))
+}
