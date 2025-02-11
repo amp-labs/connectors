cd15513ca69caff7e811e70e3cc410df4bad683b
Joseph Karage <josephbkarage@gmail.com>
2025-02-11 13:03:56 +0300

    [CON-989] feat: Add ChiliPiper Read Connector (#1309)
    * [CON-989] feat: Add ChiliPiper Read Connector
    
    * update read
    
    * update tests
    
    * Add more tests
    
    * feat: Add Mock Tests
    
    * Address RP Review
    
    * revert read page size

--- providers/chilipiper/url.go
+++ providers/chilipiper/url.go
@@ -1,9 +1,23 @@
 package chilipiper
 
-import "github.com/amp-labs/connectors/common/urlbuilder"
+import (
+	"github.com/amp-labs/connectors/common/urlbuilder"
+)
 
 const restAPIVersionPrefix = "api/fire-edge/v1/org"
 
-func (conn *Connector) buildReadURL(object string) (*urlbuilder.URL, error) {
-	return urlbuilder.New(conn.BaseURL, restAPIVersionPrefix, object)
+func (conn *Connector) buildURL(objectName string, pageSize string) (string, error) {
+	path, err := supportsRead(objectName)
+	if err != nil {
+		return "", err
+	}
+
+	url, err := urlbuilder.New(conn.BaseURL, restAPIVersionPrefix, path)
+	if err != nil {
+		return "", err
+	}
+
+	url.WithQueryParam(pageSizeKey, pageSize)
+
+	return url.String(), nil
 }
