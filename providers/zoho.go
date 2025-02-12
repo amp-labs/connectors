781f7b8cacde4ced93feefd76c00191f8e702926
Dipu <dipuchaurasiya91@gmail.com>
2025-02-12 12:26:56 +0545

    feat(zoho): Add Zoho Projects and Zoho BugTracker connector

--- providers/zoho.go
+++ providers/zoho.go
@@ -1,7 +1,12 @@
 package providers
 
-const Zoho Provider = "zoho"
+const (
+	Zoho           Provider = "zoho"
+	ZohoProjects   Provider = "zohoProjects"
+	ZohoBugTracker Provider = "zohoBugTracker"
+)
 
+//nolint:funlen
 func init() {
 	// Zoho configuration
 	SetInfo(Zoho, ProviderInfo{
@@ -41,6 +46,86 @@ func init() {
 			Regular: &MediaTypeRegular{
 				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722471890/media/zoho_1722471890.svg",
 				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722471890/media/zoho_1722471890.svg",
+			},
+		},
+	})
+
+	SetInfo(ZohoProjects, ProviderInfo{
+		DisplayName: "Zoho Projects",
+		AuthType:    Oauth2,
+		BaseURL:     "https://projectsapi.zoho.com",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType:                 AuthorizationCode,
+			AuthURL:                   "https://accounts.zoho.com/oauth/v2/auth",
+			AuthURLParams:             map[string]string{"access_type": "offline"},
+			TokenURL:                  "https://accounts.zoho.com/oauth/v2/token",
+			ExplicitScopesRequired:    true,
+			ExplicitWorkspaceRequired: false,
+			TokenMetadataFields: TokenMetadataFields{
+				WorkspaceRefField: "api_domain",
+				ScopesField:       "scope",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     true,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1739333796/oda23pivlucm71ef7vu_bbv5sl.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1739333472/projects_lw3z3y.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1739333796/oda23pivlucm71ef7vu_bbv5sl.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1739333472/projects_lw3z3y.svg",
+			},
+		},
+	})
+
+	SetInfo(ZohoBugTracker, ProviderInfo{
+		DisplayName: "Zoho BugTracker",
+		AuthType:    Oauth2,
+		BaseURL:     " https://bugtracker.zoho.com",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType:                 AuthorizationCode,
+			AuthURL:                   "https://accounts.zoho.com/oauth/v2/auth",
+			AuthURLParams:             map[string]string{"access_type": "offline"},
+			TokenURL:                  "https://accounts.zoho.com/oauth/v2/token",
+			ExplicitScopesRequired:    true,
+			ExplicitWorkspaceRequired: false,
+			TokenMetadataFields: TokenMetadataFields{
+				WorkspaceRefField: "api_domain",
+				ScopesField:       "scope",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     true,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1739342042/r25of246tym71jbt61_kuiugm.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1739342060/bugtracker_qtjkcz.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1739342042/r25of246tym71jbt61_kuiugm.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1739342060/bugtracker_qtjkcz.svg",
 			},
 		},
 	})
