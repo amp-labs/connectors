98fe1fc83c6def488f866217adeb980f4c5329a0
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-02-13 01:54:55 +0200

    tidy(proxy): Split file, utils package (#1357)

---
+++ scripts/utils/proxyserv/oauthBasic.go
@@ -0,0 +1,67 @@
+// nolint:ireturn
+package proxyserv
+
+import (
+	"context"
+	"log"
+	"log/slog"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/scanning"
+	"github.com/amp-labs/connectors/common/scanning/credscanning"
+	"github.com/amp-labs/connectors/connector"
+	"github.com/amp-labs/connectors/providers"
+)
+
+func (f Factory) CreateProxyBasic(ctx context.Context) *Proxy {
+	params := createBasicParams(f.Registry)
+	providerInfo := getProviderConfig(f.Provider, f.CatalogVariables)
+	httpClient := setupBasicAuthHTTPClient(ctx, providerInfo, params.User, params.Pass, f.Debug)
+	baseURL := getBaseURL(providerInfo)
+
+	return newProxy(baseURL, httpClient)
+}
+
+func createBasicParams(registry scanning.Registry) *providers.BasicParams {
+	user := registry.MustString(credscanning.Fields.Username.Name)
+	pass := registry.MustString(credscanning.Fields.Password.Name)
+
+	if len(user)+len(pass) == 0 {
+		log.Fatalf("Missing username or password")
+	}
+
+	if len(user) == 0 {
+		slog.Warn("no username for basic authentication, ensure that it is not required")
+	}
+
+	if len(pass) == 0 {
+		slog.Warn("no password for basic authentication, ensure that it is not required")
+	}
+
+	return &providers.BasicParams{
+		User: user,
+		Pass: pass,
+	}
+}
+
+func setupBasicAuthHTTPClient(
+	ctx context.Context, prov *providers.ProviderInfo, user, pass string, debug bool,
+) common.AuthenticatedHTTPClient {
+	client, err := prov.NewClient(ctx, &providers.NewClientParams{
+		Debug: debug,
+		BasicCreds: &providers.BasicParams{
+			User: user,
+			Pass: pass,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	cc, err := connector.NewConnector(prov.Name, connector.WithAuthenticatedClient(client))
+	if err != nil {
+		panic(err)
+	}
+
+	return cc.HTTPClient().Client
+}
---
+++ scripts/utils/proxyserv/proxy.go
@@ -0,0 +1,92 @@
+// nolint:forbidigo,ireturn
+package proxyserv
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+
+	"github.com/amp-labs/connectors/common"
+)
+
+type Proxy struct {
+	*httputil.ReverseProxy
+	target *url.URL
+}
+
+func newProxy(target *url.URL, httpClient common.AuthenticatedHTTPClient) *Proxy {
+	reverseProxy := httputil.NewSingleHostReverseProxy(target)
+	reverseProxy.Transport = &customTransport{httpClient}
+
+	return &Proxy{
+		ReverseProxy: reverseProxy,
+		target:       target,
+	}
+}
+
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.URL.Host = p.target.Host
+	r.Host = p.target.Host
+	r.RequestURI = "" // Must be cleared
+
+	fmt.Printf("Proxying request: %s %s%s\n", r.Method, r.URL.Host, r.URL.Path)
+	p.ReverseProxy.ServeHTTP(w, r)
+}
+
+func (p *Proxy) Start(ctx context.Context, port int) {
+	http.Handle("/", p)
+
+	fmt.Printf("\nProxy server listening on :%d\n", port)
+
+	if err := listen(ctx, port); err != nil {
+		panic(err)
+	}
+}
+
+type customTransport struct {
+	httpClient common.AuthenticatedHTTPClient
+}
+
+func (t *customTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	return t.httpClient.Do(req)
+}
+
+// listen will start a server on the given port and block until it is closed.
+// This is used as opposed to http.ListenAndServe because it respects the context
+// and has a cleaner shutdown sequence.
+func listen(ctx context.Context, port int) error {
+	var lc net.ListenConfig
+
+	listener, err := lc.Listen(ctx, "tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", port),
+		ReadHeaderTimeout: 5 * time.Second, // nolint:mnd
+	}
+
+	go func() {
+		<-ctx.Done()
+
+		_ = server.Shutdown(context.Background()) // nolint:contextcheck
+	}()
+
+	if err := server.Serve(listener); err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Println("HTTP server stopped")
+
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
+}
