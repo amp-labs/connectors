7fde7f47b1ccc8e4a63181a4761ad33242aff324
Cobalt0s <constantin.y.koval@gmail.com>
2025-02-14 01:13:55 +0200

    change gong to use OpenAPI from FrontPlatform

--- scripts/openapi/gong/metadata/main.go
+++ scripts/openapi/gong/metadata/main.go
@@ -10,43 +10,10 @@ import (
 	"github.com/amp-labs/connectors/providers/gong/openapi"
 	"github.com/amp-labs/connectors/tools/fileconv/api3"
 	"github.com/amp-labs/connectors/tools/scrapper"
-	"github.com/getkin/kin-openapi/openapi3"
 )
 
 var (
-	// Even though OpenAPI docs and official documentation say that some query parameters are required
-	// in practice you still can make an API call without any specified.
-	// Must include "calls" object.
-	queryParamFilterExceptions = datautils.NewSet("calls") // nolint:gochecknoglobals
-
 	ignoreEndpoints = []string{ // nolint:gochecknoglobals
-		"/v2/settings/scorecards",
-		"/v2/settings/trackers",
-		"/v2/stats/activity/scorecards",
-		"/v2/data-privacy/data-for-email-address",
-		"/v2/data-privacy/data-for-phone-number",
-		"/v2/crm/entities",
-		"/v2/crm/entity-schema",
-		"/v2/crm/request-status",
-		"/v2/library/folder-content",
-		"/v2/calls/manual-crm-associations",
-		"/v2/crm/integrations",
-		"/v2/permission-profile", // GET, single item
-		"/v2/permission-profile/users",
-		"/v2/users/extensive",
-		"/v2/library/folders",
-		//
-		// Endpoints that require query parameters.
-		//
-		// Requires query parameter: workspaceId.
-		// Response field is `profiles`.
-		"/v2/all-permission-profiles",
-		// Requires query parameters: workspace-id, manager-id, from, to.
-		"/v2/coaching",
-		// Requires query parameters: logType, fromDateTime, toDateTime, cursor.
-		"/v2/logs",
-		// Requires query parameters: flowOwnerEmail.
-		"/v2/flows",
 	}
 )
 
@@ -56,16 +23,15 @@ func main() {
 			api3.CamelCaseToSpaceSeparated,
 			api3.CapitalizeFirstLetterEveryWord,
 		),
-		api3.WithParameterFilterGetMethod(func(objectName string, operation *openapi3.Operation) bool {
-			return queryParamFilterExceptions.Has(objectName) ||
-				api3.OnlyOptionalQueryParameters(objectName, operation)
-		}),
+		api3.WithParameterFilterGetMethod(api3.OnlyOptionalQueryParameters),
 	)
 	goutils.MustBeNil(err)
 
 	objects, err := explorer.ReadObjectsGet(
 		api3.NewDenyPathStrategy(ignoreEndpoints),
-		nil, nil, api3.IdenticalObjectLocator,
+		nil, nil, func(objectName, fieldName string) bool {
+			return fieldName == ""
+		},
 	)
 	goutils.MustBeNil(err)
 
