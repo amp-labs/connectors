475a8944b842421fc5acce96e389ff0cc0dc10a8
jkarage <josephbkarage@gmail.com>
2025-02-17 11:06:47 +0300

    feat: Add Freshdesk Metadata Connector

---
+++ providers/freshdesk/metadata.go
@@ -0,0 +1,76 @@
+package freshdesk
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/naming"
+)
+
+const restAPIPrefix = "api/v2"
+
+func (conn *Connector) ListObjectMetadata(ctx context.Context,
+	objectNames []string,
+) (*common.ListObjectMetadataResult, error) {
+	// Ensure that objectNames is not empty
+	if len(objectNames) == 0 {
+		return nil, common.ErrMissingObjects
+	}
+
+	metadataResults := common.ListObjectMetadataResult{
+		Result: make(map[string]common.ObjectMetadata),
+		Errors: make(map[string]error),
+	}
+
+	for _, object := range objectNames {
+		if !objectReadSupported(object) {
+			metadataResults.Errors[object] = common.ErrObjectNotSupported
+
+			continue
+		}
+
+		url, err := conn.getAPIURL(object)
+		if err != nil {
+			return nil, err
+		}
+
+		url.WithQueryParam("page", "1")
+		url.WithQueryParam("per_page", "1")
+
+		response, err := conn.Client.Get(ctx, url.String())
+		if err != nil {
+			return nil, err
+		}
+
+		if err := buildMetadataFields(object, response, &metadataResults); err != nil {
+			return nil, err
+		}
+	}
+
+	return &metadataResults, nil
+}
+
+func buildMetadataFields(object string, response *common.JSONHTTPResponse, res *common.ListObjectMetadataResult) error {
+	objectMetadata := common.ObjectMetadata{
+		FieldsMap:   make(map[string]string),
+		DisplayName: naming.CapitalizeFirstLetterEveryWord(object),
+	}
+
+	// We're unmarshaling the data to []map[string]any, all supported objects returns this data type.
+	data, err := common.UnmarshalJSON[[]map[string]any](response)
+	if err != nil {
+		return common.ErrFailedToUnmarshalBody
+	}
+
+	if len(*data) == 0 {
+		return err
+	}
+
+	for fld := range (*data)[0] {
+		objectMetadata.FieldsMap[fld] = fld
+	}
+
+	res.Result[object] = objectMetadata
+
+	return nil
+}
---
+++ test/freshdesk/connector.go
@@ -0,0 +1,26 @@
+package freshdesk
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/amp-labs/connectors/common/scanning/credscanning"
+	"github.com/amp-labs/connectors/providers"
+	"github.com/amp-labs/connectors/providers/freshdesk"
+	testUtils "github.com/amp-labs/connectors/test/utils"
+)
+
+func GetFreshdeskConnector(ctx context.Context) *freshdesk.Connector {
+	filePath := credscanning.LoadPath(providers.Freshdesk)
+	reader := testUtils.MustCreateProvCredJSON(filePath, false, true)
+
+	conn, err := freshdesk.NewConnector(
+		freshdesk.WithClient(ctx, http.DefaultClient, reader.Get(credscanning.Fields.Username), reader.Get(credscanning.Fields.Password)),
+		freshdesk.WithWorkspace("pepkarage"),
+	)
+	if err != nil {
+		testUtils.Fail("error creating Freshdesk connector", "error", err)
+	}
+
+	return conn
+}
