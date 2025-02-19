72a26fceba45e27fee38b71db7a9f25af61f691e
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-02-19 21:11:29 +0200

    tidy(Zendesk): Read response key from schemas (#1375)

--- providers/zendesksupport/objectNames.go
+++ providers/zendesksupport/objectNames.go
@@ -1,30 +1,6 @@
 package zendesksupport
 
-import (
-	"github.com/amp-labs/connectors/common"
-	"github.com/amp-labs/connectors/internal/datautils"
-	"github.com/amp-labs/connectors/providers/zendesksupport/metadata"
-)
+import "github.com/amp-labs/connectors/providers/zendesksupport/metadata"
 
 // Supported object names can be found under schemas.json.
 var supportedObjectsByRead = metadata.Schemas.ObjectNames() //nolint:gochecknoglobals
-
-// ObjectNameToResponseField maps ObjectName to the response field name which contains that object.
-var ObjectNameToResponseField = common.ModuleObjectNameToFieldName{ //nolint:gochecknoglobals
-	ModuleTicketing: datautils.NewDefaultMap(map[string]string{
-		"ticket_audits":        "audits",
-		"search":               "results", // This is "/api/v2/search"
-		"satisfaction_reasons": "reasons",
-	},
-		func(objectName string) (fieldName string) {
-			return objectName
-		},
-	),
-	ModuleHelpCenter: datautils.NewDefaultMap(map[string]string{
-		"articles":        "results",
-		"article_labels":  "labels",
-		"community_posts": "results",
-	}, func(objectName string) (fieldName string) {
-		return objectName
-	}),
-}
