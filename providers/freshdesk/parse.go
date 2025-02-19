e0080b1827d20a651918fa85bdd70e2a91d81262
jkarage <josephbkarage@gmail.com>
2025-02-20 02:00:20 +0300

    Address JLim Review

--- providers/freshdesk/parse.go
+++ providers/freshdesk/parse.go
@@ -1,6 +1,8 @@
 package freshdesk
 
 import (
+	"strings"
+
 	"github.com/amp-labs/connectors/common"
 	"github.com/spyzhov/ajson"
 )
@@ -9,6 +11,27 @@ func nextRecordsURL(link string) common.NextPageFunc {
 	return func(n *ajson.Node) (string, error) {
 		// if link is available, this indicates there's a next page url else there isn't.
 		// link format `<https://ampersand.freshdesk.com/api/v2/contacts?per_page=1&page=2>; rel="next"`
-		return common.ParseNexPageLinkHeader(link), nil
+		return ParseNexPageLinkHeader(link), nil
 	}
+}
+
+// ParseLinkHeader extracts the next page URL from the Link Header response.
+func ParseNexPageLinkHeader(linkHeader string) string {
+	var url string
+
+	if linkHeader == "" {
+		return "" // this indicates we're done.
+	}
+
+	links := strings.Split(linkHeader, ",")
+	// [<https://dev269415.service-now.com/api/now/v2/table/incident?sysparm_limit=1&sysparm_offset=0>;rel="next" ...]
+	for _, link := range links {
+		if strings.Contains(link, `rel="next"`) {
+			urls := strings.Split(link, ";")
+			url = strings.TrimPrefix(urls[0], "<")
+			url = strings.TrimRight(url, ">")
+		}
+	}
+
+	return url
 }
