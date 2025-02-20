e992d53caa04a055850ffb0ea51788d30df8e60d
Joseph Karage <josephbkarage@gmail.com>
2025-02-20 09:49:16 +0300

    [CON-1062] feat: Add Freshdesk Read Connector (#1371)
    * feat: Add Freshdesk Metadata Connector
    
    * Add mock tests, return fieldErr
    
    * feat: Add Freshdesk Read Connector
    
    * Address JLim Review
    
    * Address JL review
    
    * remove rebaseerr

---
+++ providers/freshdesk/read.go
@@ -0,0 +1,34 @@
+package freshdesk
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+)
+
+func (conn *Connector) Read(ctx context.Context, config common.ReadParams) (*common.ReadResult, error) {
+	if err := config.ValidateParams(true); err != nil {
+		return nil, err
+	}
+
+	url, err := conn.buildReadURL(config)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := conn.Client.Get(ctx, url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	// we're using the link response header to construct the next page url.
+	linkHeader := resp.Headers.Get("link")
+
+	return common.ParseResult(
+		resp,
+		common.GetRecordsUnderJSONPath(""), // we're reading the current node.
+		nextRecordsURL(linkHeader),
+		common.GetMarshaledData,
+		config.Fields,
+	)
+}
---
+++ providers/freshdesk/read_test.go
@@ -0,0 +1,100 @@
+package freshdesk
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/datautils"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+	"github.com/amp-labs/connectors/test/utils/testutils"
+)
+
+func TestRead(t *testing.T) { // nolint:funlen,gocognit,cyclop
+	t.Parallel()
+
+	zeroRecords := testutils.DataFromFile(t, "empty.json")
+	mailboxes := testutils.DataFromFile(t, "mailboxes.json")
+
+	tests := []testroutines.Read{
+		{
+			Name:         "Object Name is required",
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:         "At least one field is required",
+			Input:        common.ReadParams{ObjectName: "deals"},
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingFields},
+		},
+		{
+			Name:  "Unsupported object",
+			Input: common.ReadParams{ObjectName: "meme", Fields: datautils.NewStringSet("name")},
+			Server: mockserver.Fixed{
+				Setup:  mockserver.ContentJSON(),
+				Always: mockserver.Response(http.StatusNotFound, nil),
+			}.Server(),
+			ExpectedErrs: []error{common.ErrObjectNotSupported},
+		},
+		{
+			Name:  "Zero records response",
+			Input: common.ReadParams{ObjectName: "companies", Fields: connectors.Fields("id")},
+			Server: mockserver.Fixed{
+				Setup:  mockserver.ContentJSON(),
+				Always: mockserver.Response(http.StatusOK, zeroRecords),
+			}.Server(),
+			Expected:     &common.ReadResult{Rows: 0, Data: []common.ReadResultRow{}, Done: true},
+			ExpectedErrs: nil,
+		},
+		{
+			Name: "Successfully Read Mailboxes",
+			Input: common.ReadParams{
+				ObjectName: "mailboxes",
+				Fields:     connectors.Fields("id", "name"),
+			},
+			Server: mockserver.Fixed{
+				Setup:  mockserver.ContentJSON(),
+				Always: mockserver.Response(http.StatusOK, mailboxes),
+			}.Server(),
+			Expected: &common.ReadResult{
+				Rows: 1,
+				Data: []common.ReadResultRow{{
+					Fields: map[string]any{
+						"id":   float64(1),
+						"name": "Test Account",
+					},
+					Raw: map[string]any{
+						"id":                  float64(1),
+						"name":                "Test Account",
+						"support_email":       "testemail@gmail.com",
+						"group_id":            float64(1),
+						"default_reply_email": true,
+						"active":              true,
+						"mailbox_type":        "freshdesk_mailbox",
+						"created_at":          "2019-08-20T05:55:38Z",
+						"updated_at":          "2019-08-20T05:55:38Z",
+						"product_id":          float64(2),
+						"freshdesk_mailbox": map[string]any{
+							"forward_email": "gmailcomtestemail@freshdesk.com",
+						},
+					},
+				}},
+				Done: true,
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.ReadConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
