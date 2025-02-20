c3b9d53ac58487876852b5bf32dd8128d6a57213
RajatPawar <18614743+RajatPawar@users.noreply.github.com>
2025-02-20 19:24:24 +0530

    extend test

--- providers/zendesksupport/schema_test.go
+++ providers/zendesksupport/schema_test.go
@@ -3,6 +3,7 @@ package zendesksupport
 import (
 	"testing"
 
+	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/providers/zendesksupport/metadata"
 )
 
@@ -10,24 +11,41 @@ func TestLookupPaginationType(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		name   string
+		module common.ModuleID
 		object string
 		want   string
 	}{
 		{
+			module: ModuleTicketing,
 			object: "tickets",
 			want:   "cursor",
 		},
 		{
+			module: ModuleTicketing,
 			object: "workspaces",
+			want:   "offset",
+		},
+		{
+			module: ModuleHelpCenter,
+			object: "articles",
+			want:   "offset",
+		},
+		{
+			module: ModuleTicketing,
+			object: "macros",
+			want:   "cursor",
+		},
+		{
+			module: ModuleHelpCenter,
+			object: "community_posts",
 			want:   "offset",
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			got, ok := metadata.Schemas.LookupPaginationType(ModuleTicketing, test.object)
-			if !ok {
+		t.Run(test.object, func(t *testing.T) {
+			got, ok := metadata.Schemas.LookupPaginationType(test.module, test.object)
+			if test.want != got || !ok {
 				t.Errorf("LookupPaginationType(%s) = %v, want %v", test.object, got, test.want)
 			}
 		})
