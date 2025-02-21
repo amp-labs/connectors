0fe65f71abcef76d40fb0e2e88af35956830bcee
Joseph Karage <josephbkarage@gmail.com>
2025-02-21 12:37:46 +0300

    [CON-990] feat: Add ChiliPiper Write Connector (#1311)
    * feat: Add ChiliPiper Write Connector
    
    * Add tests
    
    * Make linter happy
    
    * Address RP Review
    
    * use setsto store supported objects

--- providers/chilipiper/url.go
+++ providers/chilipiper/url.go
@@ -1,18 +1,18 @@
 package chilipiper
 
 import (
+	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/urlbuilder"
 )
 
 const restAPIVersionPrefix = "api/fire-edge/v1/org"
 
 func (conn *Connector) buildURL(objectName string, pageSize string) (string, error) {
-	path, err := supportsRead(objectName)
-	if err != nil {
-		return "", err
+	if !supportedReadObjects.Has(objectName) {
+		return "", common.ErrObjectNotSupported
 	}
 
-	url, err := urlbuilder.New(conn.BaseURL, restAPIVersionPrefix, path)
+	url, err := urlbuilder.New(conn.BaseURL, restAPIVersionPrefix, objectName)
 	if err != nil {
 		return "", err
 	}
@@ -20,4 +20,17 @@ func (conn *Connector) buildURL(objectName string, pageSize string) (string, err
 	url.WithQueryParam(pageSizeKey, pageSize)
 
 	return url.String(), nil
+}
+
+func (conn *Connector) buildWriteURL(object string) (*urlbuilder.URL, error) {
+	if !supportedWriteObjects.Has(object) {
+		return nil, common.ErrObjectNotSupported
+	}
+
+	writeURL, err := urlbuilder.New(conn.BaseURL, restAPIVersionPrefix, object)
+	if err != nil {
+		return nil, err
+	}
+
+	return writeURL, nil
 }
---
+++ test/chilipiper/write/write.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/amp-labs/connectors/common"
+	ch "github.com/amp-labs/connectors/providers/chilipiper"
+	"github.com/amp-labs/connectors/test/chilipiper"
+)
+
+func main() {
+	os.Exit(MainFn())
+}
+
+func MainFn() int {
+	ctx := context.Background()
+	conn := chilipiper.GetChiliPiperConnector(ctx)
+
+	if err := removeUsersTeam(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := updateDistribution(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := inviteUsers(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+	return 0
+}
+
+func removeUsersTeam(ctx context.Context, conn *ch.Connector) error {
+	prms := common.WriteParams{
+		ObjectName: "team/users/remove",
+		RecordData: map[string]any{
+			"teamId": "4edf8761-e5ee-48b2-81c8-c5e4849481fc",
+			"userIds": []string{
+				"67929af0725ce43853fd2b8c",
+			},
+		},
+	}
+
+	result, err := conn.Write(ctx, prms)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(jsonStr))
+
+	return nil
+}
+
+func updateDistribution(ctx context.Context, conn *ch.Connector) error {
+	prms := common.WriteParams{
+		ObjectName: "distribution",
+		RecordId:   "3b3baa8f-ee2c-40ed-92a0-a08aae954925",
+		RecordData: map[string]any{
+			"resetDistribution": false,
+		},
+	}
+
+	result, err := conn.Write(ctx, prms)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(jsonStr))
+
+	return nil
+}
+
+func inviteUsers(ctx context.Context, conn *ch.Connector) error {
+	prms := common.WriteParams{
+		ObjectName: "user/invite",
+		RecordData: map[string]any{
+			"email": "pepkarage@gmail.com",
+		},
+	}
+
+	result, err := conn.Write(ctx, prms)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(jsonStr))
+
+	return nil
+}
