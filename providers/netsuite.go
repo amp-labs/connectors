16104f373b700083b3dc3a84e57680797c439bd0
RajatPawar <18614743+RajatPawar@users.noreply.github.com>
2025-02-24 15:51:28 +0530

    add netsuite config

---
+++ providers/netsuite.go
@@ -0,0 +1,40 @@
+package providers
+
+const Netsuite Provider = "netsuite"
+
+func init() {
+	SetInfo(Netsuite, ProviderInfo{
+		DisplayName: "Netsuite",
+		AuthType:    Oauth2,
+		BaseURL:     "https://{{.workspace}}.suitetalk.api.netsuite.com",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType:                 AuthorizationCode,
+			AuthURL:                   "https://{{.workspace}}.app.netsuite.com/app/login/oauth2/authorize.nl.",
+			TokenURL:                  "https://{{.workspace}}.suitetalk.api.netsuite.com/services/rest/auth/oauth2/v1/token",
+			ExplicitScopesRequired:    false,
+			ExplicitWorkspaceRequired: true,
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     true,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "",
+				LogoURL: "",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "",
+				LogoURL: "",
+			},
+		},
+	})
+}
