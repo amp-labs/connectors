c8415f529d4255445808eb0a345947e049b3f874
jkarage <josephbkarage@gmail.com>
2025-02-25 15:59:11 +0300

    remove the first implementation

--- providers/front/connector.go
+++ providers/front/connector.go
@@ -1,49 +1,126 @@
-package front
+package smartleadv2
 
 import (
+	_ "embed"
+
 	"github.com/amp-labs/connectors/common"
-	"github.com/amp-labs/connectors/common/paramsbuilder"
-	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/common/interpreter"
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/deleter"
+	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/reader"
+	"github.com/amp-labs/connectors/internal/components/schema"
+	"github.com/amp-labs/connectors/internal/components/writer"
+	"github.com/amp-labs/connectors/internal/staticschema"
 	"github.com/amp-labs/connectors/providers"
+	"github.com/amp-labs/connectors/tools/fileconv"
+	"github.com/amp-labs/connectors/tools/scrapper"
+)
+
+// nolint:gochecknoglobals
+var (
+	//go:embed schemas.json
+	schemaContent []byte
+
+	fileManager = scrapper.NewMetadataFileManager[staticschema.FieldMetadataMapV1](
+		schemaContent, fileconv.NewSiblingFileLocator())
+
+	schemas = fileManager.MustLoadSchemas()
 )
 
 type Connector struct {
-	BaseURL string
-	Client  *common.JSONHTTPClient
+	// Basic connector
+	*components.Connector
+
+	// Require authenticated client
+	common.RequireAuthenticatedClient
+
+	// Supported operations
+	components.SchemaProvider
+	components.Reader
+	components.Writer
+	components.Deleter
 }
 
-func NewConnector(opts ...Option) (*Connector, error) {
-	params, err := paramsbuilder.Apply(parameters{}, opts)
+func NewConnector(params common.Parameters) (*Connector, error) {
+	// Create base connector with provider info
+	return components.Initialize(providers.Smartlead, params, constructor)
+}
+
+// nolint:funlen
+func constructor(base *components.Connector) (*Connector, error) {
+	connector := &Connector{Connector: base}
+
+	registry, err := components.NewEndpointRegistry(supportedOperations())
 	if err != nil {
 		return nil, err
 	}
 
-	jsonClient := common.JSONHTTPClient{
-		HTTPClient: params.Caller,
-	}
+	// Set the metadata provider for the connector
+	connector.SchemaProvider = schema.NewOpenAPISchemaProvider(connector.ProviderContext.Module(), schemas)
+	// Other examples:
+	// 	schema.NewAggregateSchemaProvider(
+	// 		connector.HTTPClient().Client,
+	// 		operations.ListObjectMetadataHandlers{
+	// 			BuildRequest:  connector.buildListObjectMetadataRequest,
+	// 			ParseResponse: connector.parseListObjectMetadataResponse,
+	// 		},
+	// 	),
 
-	connector := Connector{
-		Client: &jsonClient,
-	}
+	//  schema.NewObjectSchemaProvider(
+	// 		connector.HTTPClient().Client,
+	// 		schema.FetchModeParallel,
+	// 		operations.SingleObjectMetadataHandlers{
+	// 			BuildRequest:  connector.buildSingleObjectMetadataRequest,
+	// 			ParseResponse: connector.parseSingleObjectMetadataResponse,
+	// 		},
+	//  ),
 
-	providerInfo, err := providers.ReadInfo(connector.Provider())
-	if err != nil {
-		return nil, err
-	}
+	// Set the reader for the connector
+	// TODO: Refactor to simplify clients
+	connector.Reader = reader.NewHTTPReader(
+		connector.HTTPClient().Client,
+		registry,
+		connector.ProviderContext.Module(),
+		operations.ReadHandlers{
+			BuildRequest:  connector.buildReadRequest,
+			ParseResponse: connector.parseReadResponse,
+			ErrorHandler: interpreter.ErrorHandler{
+				JSON: interpreter.NewFaultyResponder(errorFormats, nil),
+				HTML: &interpreter.DirectFaultyResponder{Callback: interpretHTMLError},
+			}.Handle,
+		},
+	)
 
-	connector.BaseURL = providerInfo.BaseURL
+	// Set the writer for the connector
+	connector.Writer = writer.NewHTTPWriter(
+		connector.HTTPClient().Client,
+		registry,
+		connector.ProviderContext.Module(),
+		operations.WriteHandlers{
+			BuildRequest:  connector.buildWriteRequest,
+			ParseResponse: connector.parseWriteResponse,
+			ErrorHandler: interpreter.ErrorHandler{
+				JSON: interpreter.NewFaultyResponder(errorFormats, nil),
+				HTML: &interpreter.DirectFaultyResponder{Callback: interpretHTMLError},
+			}.Handle,
+		},
+	)
 
-	return &connector, nil
-}
+	// Set the deleter for the connector
+	connector.Deleter = deleter.NewHTTPDeleter(
+		connector.HTTPClient().Client,
+		registry,
+		connector.ProviderContext.Module(),
+		operations.DeleteHandlers{
+			BuildRequest:  connector.buildDeleteRequest,
+			ParseResponse: connector.parseDeleteResponse,
+			ErrorHandler: interpreter.ErrorHandler{
+				JSON: interpreter.NewFaultyResponder(errorFormats, nil),
+				HTML: &interpreter.DirectFaultyResponder{Callback: interpretHTMLError},
+			}.Handle,
+		},
+	)
 
-func (conn *Connector) Provider() providers.Provider {
-	return providers.Front
-}
-
-func (conn *Connector) String() string {
-	return conn.Provider() + ".Connector"
-}
-
-func (conn *Connector) getAPIURL(object string) (*urlbuilder.URL, error) {
-	return urlbuilder.New(conn.BaseURL, object)
+	return connector, nil
 }
