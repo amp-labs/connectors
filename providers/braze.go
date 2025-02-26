dd384b0c7962da806b9f8e6b7aa4efad931cf1c7
jkarage <josephbkarage@gmail.com>
2025-02-26 13:33:27 +0300

    feat: Add Braze Connector

--- providers/braze.go
+++ providers/braze.go
@@ -13,7 +13,17 @@ func init() {
 				Name:        "Authorization",
 				ValuePrefix: "Bearer ",
 			},
-			DocsURL: "https://www.braze.com/docs/developer_guide/home",
+			DocsURL: "https://www.braze.com/docs/api/basics/#creating-rest-api-keys",
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740565804/media/braze.com_1740565802.png",
+				LogoURL: " https://res.cloudinary.com/dycvts6vp/image/upload/v1740565904/media/braze.com_1740565903.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740565804/media/braze.com_1740565802.png",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740565849/media/braze.com_1740565848.svg",
+			},
 		},
 		Support: Support{
 			BulkWrite: BulkWriteSupport{
