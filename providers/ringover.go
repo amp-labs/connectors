dbe1fa1200b90eb47bc39d42c7a3f9146412cf12
Dipu <dipuchaurasiya91@gmail.com>
2025-02-27 23:46:16 +0545

    feat: Add Ringover proxy connector

---
+++ providers/ringover.go
@@ -0,0 +1,40 @@
+package providers
+
+const Ringover Provider = "ringover"
+
+func init() {
+	SetInfo(Ringover, ProviderInfo{
+		DisplayName: "Ringover",
+		AuthType:    ApiKey,
+		BaseURL:     "https://public-api-us.ringover.com",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Header,
+			Header: &ApiKeyOptsHeader{
+				Name: "Authorization",
+			},
+			DocsURL: "https://developers.ringover.com/docs/api/authentication",
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: " https://res.cloudinary.com/dycvts6vp/image/upload/v1740678708/media/app.ringover.com_1740678707.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740678624/media/app.ringover.com_1740678624.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740678502/ringOver_vkuk42.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740678649/media/app.ringover.com_1740678648.svg",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+}
