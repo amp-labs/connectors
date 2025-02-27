f64a17f4c605096af4c0941efb75f1729df1ea1b
Joseph Karage <josephbkarage@gmail.com>
2025-02-27 09:16:05 +0300

    [CON-1070] feat: Add Front Read Connector (#1405)
    * Initial front connector commit
    
    * [CON-1069] feat: Add Front Metadata Connector
    
    * remove the first implementation
    
    * use internal components
    
    * Add Front Read Connector
    
    * add test
    
    * Address RP Review

---
+++ providers/front/handlers.go
@@ -0,0 +1,46 @@
+package front
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+)
+
+func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
+	var (
+		url *urlbuilder.URL
+		err error
+	)
+
+	url, err = urlbuilder.New(c.ProviderInfo().BaseURL, params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	url.WithQueryParam(pageSizeKey, pageSize)
+
+	if params.NextPage != "" {
+		url, err = urlbuilder.New(params.NextPage.String())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+}
+
+func (c *Connector) parseReadResponse(
+	ctx context.Context,
+	params common.ReadParams,
+	response *common.JSONHTTPResponse,
+) (*common.ReadResult, error) {
+	return common.ParseResult(
+		response,
+		common.GetRecordsUnderJSONPath(dataResponseKey),
+		nextRecordsURL(),
+		common.GetMarshaledData,
+		params.Fields,
+	)
+}
