71678ee8918bdeed54f0fa4ae92a6d24fdf14b61
jkarage <josephbkarage@gmail.com>
2025-02-27 10:49:21 +0300

    Add MetadataConnector using Internal

--- providers/gorgias/connector.go
+++ providers/gorgias/connector.go
@@ -1,6 +1,8 @@
 package gorgias
 
 import (
+	_ "embed"
+
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/components"
 	"github.com/amp-labs/connectors/internal/components/operations"
