2b5b2ae14e16c93484784ea0cd2bd7af755797f9
Joseph Karage <josephbkarage@gmail.com>
2025-02-27 09:16:05 +0300

    [CON-1070] feat: Add Front Read Connector (#1405)
    * Initial front connector commit
    
    * [CON-1069] feat: Add Front Metadata Connector
    
    * remove the first implementation
    
    * use internal components
    
    * Add Front Read Connector
    
    * add test
    
    * Address RP Review

---
+++ providers/front/supports.go
@@ -0,0 +1,27 @@
+package front
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/staticschema"
+)
+
+func supportedOperations() components.EndpointRegistryInput {
+	readSupport := []string{
+		"accounts", "channels", "contacts", "contact_lists",
+		"conversations", "events", "inboxes", "knowledge_bases",
+		"links", "rules", "message_templates", "message_template_folders",
+		"shifts", "tags", "teammates", "teammate_groups", "teams",
+	}
+
+	return components.EndpointRegistryInput{
+		staticschema.RootModuleID: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
+	}
+}
