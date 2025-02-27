4b68e8d7e33b164bb2424fe6927d88cf9e1a2971
Joseph Karage <josephbkarage@gmail.com>
2025-02-27 09:52:55 +0300

    CON-1071 feat: Add Front Write Connector (#1406)
    * Initial front connector commit
    
    * [CON-1069] feat: Add Front Metadata Connector
    
    * remove the first implementation
    
    * use internal components
    
    * Add Front Read Connector
    
    * add test
    
    * feat: Add Front Write Connector

--- providers/front/supports.go
+++ providers/front/supports.go
@@ -16,11 +16,23 @@ func supportedOperations() components.EndpointRegistryInput {
 		"shifts", "tags", "teammates", "teammate_groups", "teams",
 	}
 
+	writeSupports := []string{
+		"accounts", "contacts", "contact_lists", "conversations",
+		"inboxes", // Create an inbox in the default team (workspace)
+		"channels", "comments", "signatures",
+		"knowledge_bases", "links", "message_templates", "message_template_folders",
+		"shifts", "tags", "teammate_groups", "teams",
+	}
+
 	return components.EndpointRegistryInput{
 		staticschema.RootModuleID: {
 			{
 				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
 				Support:  components.ReadSupport,
+			},
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(writeSupports, ",")),
+				Support:  components.WriteSupport,
 			},
 		},
 	}
