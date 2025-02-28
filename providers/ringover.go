a895b5d7dec0b02f9eb7fee5f7298120a119ff78
Dipu <dipuchaurasiya91@gmail.com>
2025-02-28 09:04:52 +0545

    feat: Add support for RingOver EU and US providers

--- providers/ringover.go
+++ providers/ringover.go
@@ -1,10 +1,13 @@
 package providers
 
-const Ringover Provider = "ringover"
+const (
+	RingOverEU Provider = "ringOverEU"
+	RingOverUS Provider = "ringOverUS"
+)
 
 func init() {
-	SetInfo(Ringover, ProviderInfo{
-		DisplayName: "Ringover",
+	SetInfo(RingOverUS, ProviderInfo{
+		DisplayName: "RingOver US",
 		AuthType:    ApiKey,
 		BaseURL:     "https://public-api-us.ringover.com",
 		ApiKeyOpts: &ApiKeyOpts{
@@ -37,4 +40,40 @@ func init() {
 			Write:     false,
 		},
 	})
+
+	SetInfo(RingOverEU, ProviderInfo{
+		DisplayName: "RingOver EU",
+		AuthType:    ApiKey,
+		BaseURL:     "https://public-api.ringover.com",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Header,
+			Header: &ApiKeyOptsHeader{
+				Name: "Authorization",
+			},
+			DocsURL: "https://developer.ringover.com",
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: " https://res.cloudinary.com/dycvts6vp/image/upload/v1740678708/media/app.ringover.com_1740678707.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740678624/media/app.ringover.com_1740678624.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740678502/ringOver_vkuk42.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740678649/media/app.ringover.com_1740678648.svg",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+
 }
