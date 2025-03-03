131a91981bbdcf3f186a20ce77474920447fdd1c
jkarage <josephbkarage@gmail.com>
2025-03-03 14:58:11 +0300

    feat: Add Groove Read Connector

--- providers/groove/supports.go
+++ providers/groove/supports.go
@@ -1,5 +1,13 @@
 package groove
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/staticschema"
+)
+
 var responseField = map[string]string{ //nolint:gochecknoglobals
 	"tickets":       "tickets",
 	"customers":     "customers",
@@ -11,4 +19,21 @@ var responseField = map[string]string{ //nolint:gochecknoglobals
 	"widgets":       "widgets",
 	"kb":            "knowledge_bases",
 	"tickets/count": "",
+}
+
+func supportedOperations() components.EndpointRegistryInput {
+	readSupport := []string{
+		"tickets", "customers", "mailboxes", "folders",
+		"agents", "groups", "kb/themes", "widgets",
+		"kb", "tickets/count",
+	}
+
+	return components.EndpointRegistryInput{
+		staticschema.RootModuleID: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
+	}
 }
