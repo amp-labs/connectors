379eacb9cf3dd402ece1ca055f33af759d71ef9f
Dipu <dipuchaurasiya91@gmail.com>
2025-03-03 22:37:01 +0545

    feat: Implement read support

---
+++ providers/brevo/handlers.go
@@ -0,0 +1,59 @@
+package brevo
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/providers/brevo/metadata"
+)
+
+var apiVersion = "v3" //nolint:gochecknoglobals
+
+func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
+	var (
+		url *urlbuilder.URL
+		err error
+	)
+
+	if params.NextPage != "" {
+		url, err = urlbuilder.New(params.NextPage.String())
+		if err != nil {
+			return nil, err
+		}
+
+		return http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+	}
+
+	url, err = urlbuilder.New(c.ProviderInfo().BaseURL, apiVersion, params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	// first page pagination
+	if supportLimitAndOffset.Has(params.ObjectName) {
+		url.WithQueryParam("limit", strconv.Itoa(pageSize))
+		url.WithQueryParam("offset", "0")
+	}
+
+	return http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+}
+
+func (c *Connector) parseReadResponse(
+	ctx context.Context,
+	params common.ReadParams,
+	response *common.JSONHTTPResponse,
+) (*common.ReadResult, error) {
+	responseFieldName := metadata.Schemas.LookupArrayFieldName(c.Module(), params.ObjectName)
+	requestURL := response.Request.URL
+
+	return common.ParseResult(
+		response,
+		common.GetRecordsUnderJSONPath(responseFieldName),
+		nextRecordsURL(requestURL),
+		common.GetMarshaledData,
+		params.Fields,
+	)
+}
