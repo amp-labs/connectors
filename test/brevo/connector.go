f207778f6774c442d47dfacb610711351f774186
Dipu <103568666+immdipu@users.noreply.github.com>
2025-03-04 09:23:39 +0545

    [CON-1436] feat: Add Brevo ListMetadata (#1415)

---
+++ test/brevo/connector.go
@@ -0,0 +1,34 @@
+package brevo
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/scanning/credscanning"
+	"github.com/amp-labs/connectors/providers"
+	"github.com/amp-labs/connectors/providers/brevo"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func GetBrevoConnector(ctx context.Context) *brevo.Connector {
+	filePath := credscanning.LoadPath(providers.Brevo)
+
+	reader := utils.MustCreateProvCredJSON(filePath, false, false)
+
+	client, err := common.NewApiKeyHeaderAuthHTTPClient(ctx, "api-key", reader.Get(credscanning.Fields.ApiKey))
+
+	if err != nil {
+		utils.Fail(err.Error())
+	}
+
+	conn, err := brevo.NewConnector(
+		common.Parameters{
+			AuthenticatedClient: client,
+		},
+	)
+	if err != nil {
+		utils.Fail("error creating brevo connector", "error", err)
+	}
+
+	return conn
+}
