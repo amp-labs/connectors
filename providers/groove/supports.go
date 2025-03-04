2c3894bb33f140bdc974d02d816af8c8329f8a08
Joseph Karage <josephbkarage@gmail.com>
2025-03-04 08:49:42 +0300

    feat: Add Groove Metadata Connector (#1424)

---
+++ providers/groove/supports.go
@@ -0,0 +1,14 @@
+package groove
+
+var responseField = map[string]string{ //nolint:gochecknoglobals
+	"tickets":       "tickets",
+	"customers":     "customers",
+	"mailboxes":     "mailboxes",
+	"folders":       "folders",
+	"agents":        "agents",
+	"groups":        "groups",
+	"kb/themes":     "themes",
+	"widgets":       "widgets",
+	"kb":            "knowledge_bases",
+	"tickets/count": "",
+}
