cdb127d825e22857a0050c05ae3b4c4ea48c4114
Dipu <dipuchaurasiya91@gmail.com>
2025-03-05 18:36:51 +0545

    feat(github): add read

--- providers/github/supports.go
+++ providers/github/supports.go
@@ -1,1 +1,46 @@
 package github
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/datautils"
+	"github.com/amp-labs/connectors/internal/staticschema"
+	"github.com/amp-labs/connectors/providers/asana/metadata"
+)
+
+const (
+	objectNameCampaigns       = "campaigns"
+	objectNameEmailTemplates  = "email_templates"
+	objectNameExternalFetches = "external_fetches"
+	objectNameOnsiteSlots     = "onsite_slots"
+	objectNameSmsTemplates    = "sms_templates"
+	objectNamePushTemplates   = "push_templates"
+	objectNameCatalogs        = "catalogs"
+	objectNameCustomers       = "customers"
+	objectNameCustomUserLists = "custom_user_lists/create"
+	objectNameEvent           = "event"
+)
+
+var supportPagination = datautils.NewSet( //nolint:gochecknoglobals
+	objectNameCampaigns,
+	objectNameEmailTemplates,
+	objectNameExternalFetches,
+	objectNameOnsiteSlots,
+	objectNameSmsTemplates,
+	objectNamePushTemplates,
+)
+
+func supportedOperations() components.EndpointRegistryInput {
+	readSupport := metadata.Schemas.ObjectNames().GetList(staticschema.RootModuleID)
+
+	return components.EndpointRegistryInput{
+		staticschema.RootModuleID: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
+	}
+}
