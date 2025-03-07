841fd256bc2019f9abaf9b9eaa3ed6c459f9fefb
Dipu <dipuchaurasiya91@gmail.com>
2025-03-07 20:51:30 +0545

    feat(ashby): implement read operations with HTTP request handling and pagination support

--- providers/ashby/handlers.go
+++ providers/ashby/handlers.go
@@ -1,1 +1,66 @@
 package ashby
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/providers/ashby/metadata"
+)
+
+func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
+	var (
+		url *urlbuilder.URL
+		err error
+	)
+
+	path, err := metadata.Schemas.LookupURLPath(c.Module(), params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	url, err = urlbuilder.New(c.ProviderInfo().BaseURL, path)
+	if err != nil {
+		return nil, err
+	}
+
+	// if supportPagination.Has(params.ObjectName) {
+	// 	url.WithQueryParam(pageSizeKey, pageSize)
+	// 	url.WithQueryParam(pageKey, pageNumber)
+	// }
+
+	if params.NextPage != "" {
+		url, err = urlbuilder.New(params.NextPage.String())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+}
+
+func (c *Connector) parseReadResponse(
+	ctx context.Context,
+	params common.ReadParams,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.ReadResult, error) {
+	path, err := metadata.Schemas.LookupURLPath(c.Module(), params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	baseURL, err := urlbuilder.New(c.ProviderInfo().BaseURL, path)
+	if err != nil {
+		return nil, err
+	}
+
+	return common.ParseResult(
+		response,
+		getRecords(params.ObjectName, c.Module()),
+		makeNextRecordsURL(baseURL.String()),
+		common.GetMarshaledData,
+		params.Fields,
+	)
+}
