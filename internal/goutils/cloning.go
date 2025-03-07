629faca936521f69e6c6d0dfface8304abe882ba
Joseph Karage <josephbkarage@gmail.com>
2025-03-07 11:27:12 +0300

    [CON-1592] fix(outreach): panic when there is no response (#1450)
    * fix: panic when there is no response
    
    * Add tests, avoid total failing, lint
    
    * Address Review 1

--- internal/goutils/cloning.go
+++ internal/goutils/cloning.go
@@ -5,13 +5,16 @@ import (
 	"encoding/gob"
 )
 
+//nolint:ireturn
 func init() {
 	gob.Register(map[string]any{})
 	gob.Register([]any{})
 }
 
 // Clone uses gob to deep copy objects.
-func Clone[T any](input T) (T, error) { // nolint:ireturn
+//
+// nolint:ireturn
+func Clone[T any](input T) (T, error) {
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
 	dec := gob.NewDecoder(&buf)
