5f6c96089ea65ec519677c8c1c9cd06eb420400f
jkarage <josephbkarage@gmail.com>
2025-03-11 13:20:59 +0300

    Initial schema commit

---
+++ providers/mixmax/supports.go
@@ -0,0 +1,10 @@
+package mixmax
+
+func responseField(objectName string) string {
+	// --> appointmentlinks/me --> object
+	// --> codesnippets --> object with `result` array, "hasNext": false
+	// --> contacts api deprecated
+	// --> filerequests --> object with `result` array, "hasNext": false
+	// --> insightsreports --> object with
+	return ""
+}
