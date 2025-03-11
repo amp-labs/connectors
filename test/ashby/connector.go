daa52b280d2bd7953a54aaf5fc4fddf0b4cfef6b
Dipu <103568666+immdipu@users.noreply.github.com>
2025-03-11 07:40:48 +0545

    [CON-1594] feat : Add Ashby ListMetadata (#1460)
    * feat(ashby): add metadata schemas and OpenAPI
    
    * fix(ashby): update package name from blueshift to ashby
    
    * feat(ashby): implement supported operations and add connector tests
    
    * fix(ashby):  import path
    
    * fix(ashby): update metadata file manager to use FieldMetadataMapV1
    
    * fix(ashby): update assessment list path in schemas and test metadata retrieval
    
    * refactor(ashby): remove unused OpenAPI metadata files and main script
    
    * refactor(ashby): remove unused embed import from connector.go

---
+++ test/ashby/connector.go
@@ -0,0 +1,33 @@
+package ashby
+
+import (
+	"context"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/scanning/credscanning"
+	"github.com/amp-labs/connectors/providers"
+	"github.com/amp-labs/connectors/providers/ashby"
+	testUtils "github.com/amp-labs/connectors/test/utils"
+)
+
+func GetAshbyConnector(ctx context.Context) *ashby.Connector {
+	filePath := credscanning.LoadPath(providers.Ashby)
+	reader := testUtils.MustCreateProvCredJSON(filePath, false, false)
+
+	client, err := common.NewBasicAuthHTTPClient(ctx, reader.Get(credscanning.Fields.Username), reader.Get(credscanning.Fields.Password))
+
+	if err != nil {
+		testUtils.Fail(err.Error())
+	}
+
+	conn, err := ashby.NewConnector(
+		common.Parameters{
+			AuthenticatedClient: client,
+		},
+	)
+	if err != nil {
+		testUtils.Fail("error creating ashby connector", "error", err)
+	}
+
+	return conn
+}
