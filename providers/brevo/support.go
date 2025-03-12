0f5c5137637e6f8498d1530731e6bb1b121dc835
Dipu <103568666+immdipu@users.noreply.github.com>
2025-03-12 06:24:27 +0545

    [CON-1438] feat: Add Brevo Write Connector  (#1466)

--- providers/brevo/support.go
+++ providers/brevo/support.go
@@ -38,11 +38,38 @@ func supportedOperations() components.EndpointRegistryInput {
 	// We support reading everything under schema.json, so we get all the objects and join it into a pattern.
 	readSupport := metadata.Schemas.ObjectNames().GetList(staticschema.RootModuleID)
 
+	writeSupport := []string{
+		"smtp/email", "smtp/templates", "smtp/blockedDomains",
+		"smtp/deleteHardbounces", "transactionalSMS/sms", "whatsapp/sendMessage",
+		"emailCampaigns", "emailCampaigns/images", "smsCampaigns", "whatsappCampaigns",
+		"whatsappCampaigns/template", "contacts", "contacts/doubleOptinConfirmation", "contacts/folders",
+		"contacts/export", "contacts/import", "events", "senders", "senders/domains", "webhooks", "webhooks/export",
+		"corporate/subAccount", "corporate/ssoToken", "corporate/subAccount/ssoToken", "corporate/subAccount/key",
+
+		"corporate/group", "corporate/user/invitation/send",
+		"organization/user/invitation/send", "organization/user/update/permissions",
+
+		"feeds", "companies", "crm/attributes", "companies/import",
+		"crm/deals", "crm/deals/import", "crm/tasks", "crm/notes",
+		"conversations/messages",
+
+		"conversations/pushedMessages", "conversations/agentOnlinePing",
+		"ecommerce/activate", "ecommerce/config/displayCurrency",
+
+		"orders/status", "orders/status/batch", "categories", "categories/batch", "products", "couponCollections",
+		"coupons", "payments/requests", "loyalty/config/programs",
+	}
+
 	return components.EndpointRegistryInput{
 		staticschema.RootModuleID: {
 			{
 				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
 				Support:  components.ReadSupport,
+			},
+
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(writeSupport, ",")),
+				Support:  components.WriteSupport,
 			},
 		},
 	}
