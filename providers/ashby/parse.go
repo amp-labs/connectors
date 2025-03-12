82710e34307ab67ac3a4f4d7fc86ded274c4cd14
Dipu <dipuchaurasiya91@gmail.com>
2025-03-12 07:27:25 +0545

    refactor(ashby): replace getRecords with GetRecordsUnderJSONPath

--- providers/ashby/parse.go
+++ providers/ashby/parse.go
@@ -1,24 +1,9 @@
 package ashby
 
 import (
-	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/jsonquery"
-	"github.com/amp-labs/connectors/providers/ashby/metadata"
 	"github.com/spyzhov/ajson"
 )
-
-func getRecords(objectName string, moduleID common.ModuleID) common.RecordsFunc {
-	return func(node *ajson.Node) ([]map[string]any, error) {
-		responseKey := metadata.Schemas.LookupArrayFieldName(moduleID, objectName)
-
-		rcds, err := jsonquery.New(node).ArrayOptional(responseKey)
-		if err != nil {
-			return nil, err
-		}
-
-		return jsonquery.Convertor.ArrayToMap(rcds)
-	}
-}
 
 func makeNextRecordsURL(node *ajson.Node) (string, error) {
 	moreDataAvailable, err := jsonquery.New(node).BoolWithDefault("moreDataAvailable", true)
