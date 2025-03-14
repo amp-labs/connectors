c683acacaac0ae2949ef27ebfc3c2e2404b0935c
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-03-14 19:45:26 +0530

    [CON-1610] chore: added support to standard & custom object

--- providers/attio/metadata.go
+++ providers/attio/metadata.go
@@ -11,6 +11,11 @@ type responseObject struct {
 	Data []map[string]any `json:"data"`
 }
 
+// This struct is used for when the response data having single data.
+type singleResponseObject struct {
+	Data map[string]any `json:"data"`
+}
+
 // ListObjectMetadata creates metadata of object via reading objects using Attio API.
 func (c *Connector) ListObjectMetadata(ctx context.Context,
 	objectNames []string,
@@ -24,14 +29,24 @@ func (c *Connector) ListObjectMetadata(ctx context.Context,
 		Errors: make(map[string]error),
 	}
 
+	var (
+		objName                    string
+		isAttioStandardOrCustomObj bool
+	)
+
 	for _, obj := range objectNames {
+		isAttioStandardOrCustomObj = false
+
 		// Constructing the request url.
-		url, err := c.getApiURL(obj)
-		if err != nil {
-			return nil, err
+		if supportAttioApiObj.Has(obj) {
+			objName = obj
+		} else {
+			isAttioStandardOrCustomObj = true
+			objName = c.getObjectsURL(obj)
 		}
 
-		resp, err := c.Client.Get(ctx, url.String())
+		resp, err := getResponse(c, ctx, objName)
+
 		if err != nil {
 			metadataResult.Errors[obj] = err
 
@@ -40,22 +55,57 @@ func (c *Connector) ListObjectMetadata(ctx context.Context,
 
 		var metadata *common.ObjectMetadata
 
-		metadata, err = parseMetadataFromResponse(resp)
+		metadata, err = parseMetadataFromResponse(resp, isAttioStandardOrCustomObj)
 		if err != nil {
 			metadataResult.Errors[obj] = err
 
 			continue
 		}
 
-		metadata.DisplayName = obj
+		if isAttioStandardOrCustomObj {
+			objName = c.getObjects(obj)
+			res, err := getResponse(c, ctx, objName)
+			if err != nil {
+				metadataResult.Errors[obj] = err
+
+				continue
+			}
+
+			displayName, err := getDisplayName(res)
+			if err != nil {
+				metadataResult.Errors[obj] = err
+
+				continue
+			}
+
+			metadata.DisplayName = displayName
+
+		} else {
+			metadata.DisplayName = obj
+		}
+
 		metadataResult.Result[obj] = *metadata
 	}
 
 	return &metadataResult, nil
 }
 
-// This function is used for slice of objects.
-func parseMetadataFromResponse(resp *common.JSONHTTPResponse) (*common.ObjectMetadata, error) {
+// Getting the response
+func getResponse(c *Connector, ctx context.Context, objName string) (*common.JSONHTTPResponse, error) {
+	url, err := c.getApiURL(objName)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.Client.Get(ctx, url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func parseMetadataFromResponse(resp *common.JSONHTTPResponse, isAttioStandardOrCustomObj bool) (*common.ObjectMetadata, error) {
 	response, err := common.UnmarshalJSON[responseObject](resp)
 	if err != nil {
 		return nil, err
@@ -69,10 +119,43 @@ func parseMetadataFromResponse(resp *common.JSONHTTPResponse) (*common.ObjectMet
 		FieldsMap: make(map[string]string),
 	}
 
-	// Using the first result data to generate the metadata.
-	for k := range response.Data[0] {
-		metadata.FieldsMap[k] = k
+	// Retrieving metadata for standard and custom objects in Attio using the api_slug field.
+	if isAttioStandardOrCustomObj {
+		for _, value := range response.Data {
+			for k, objData := range value {
+				if k == "api_slug" {
+					if strValue, ok := objData.(string); ok {
+						metadata.FieldsMap[strValue] = strValue
+					}
+				}
+			}
+		}
+	} else {
+		// Using the first result data to generate the metadata.
+		for k := range response.Data[0] {
+			metadata.FieldsMap[k] = k
+		}
 	}
 
 	return metadata, nil
+}
+
+// Getting display name for both standard and custom objects.
+func getDisplayName(resp *common.JSONHTTPResponse) (string, error) {
+	response, err := common.UnmarshalJSON[singleResponseObject](resp)
+	if err != nil {
+		return "", err
+	}
+
+	if len(response.Data) == 0 {
+		return "", common.ErrMissingExpectedValues
+	}
+
+	for key, value := range response.Data {
+		if key == "plural_noun" {
+			return value.(string), nil
+		}
+	}
+
+	return "", common.ErrNotFound
 }
