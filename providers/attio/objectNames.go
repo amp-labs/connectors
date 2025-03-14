12b866ee50b8dbb938aa72d4794472d1dbbb4453
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-03-14 19:45:26 +0530

    [CON-1610] chore: added support to standard & custom object

--- providers/attio/objectNames.go
+++ providers/attio/objectNames.go
@@ -3,33 +3,26 @@ package attio
 import "github.com/amp-labs/connectors/internal/datautils"
 
 const (
-	objectNameObjects          = "objects"
 	objectNameLists            = "lists"
 	objectNameWorkspacemembers = "workspace_members"
-	objectNameWebhooks         = "webhooks"
 	objectNameTasks            = "tasks"
 	objectNameNotes            = "notes"
 )
 
-var supportedObjectsByRead = datautils.NewSet( //nolint:gochecknoglobals
-	objectNameObjects,
-	objectNameLists,
-	objectNameWorkspacemembers,
-	objectNameWebhooks,
-	objectNameTasks,
-	objectNameNotes,
-)
-
 var supportedObjectsByWrite = datautils.NewSet( //nolint:gochecknoglobals
-	objectNameObjects,
 	objectNameLists,
 	objectNameTasks,
 	objectNameNotes,
-	objectNameWebhooks,
 )
 
 var supportLimitAndOffset = datautils.NewSet( //nolint:gochecknoglobals
 	objectNameTasks,
 	objectNameNotes,
-	objectNameWebhooks,
+)
+
+var supportAttioApiObj = datautils.NewSet( //nolint:gochecknoglobals
+	objectNameLists,
+	objectNameWorkspacemembers,
+	objectNameTasks,
+	objectNameNotes,
 )
