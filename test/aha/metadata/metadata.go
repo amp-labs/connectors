b4f58d3c2f86e00741bd34a5701b0781939dac92
Dipu <103568666+immdipu@users.noreply.github.com>
2025-03-19 15:16:05 +0545

    [CON-1539] feat(Aha): add ListMetadata (#1493)

---
+++ test/aha/metadata/metadata.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/amp-labs/connectors/test/aha"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	ctx := context.Background()
+
+	conn := aha.GetAhaConnector(ctx)
+
+	m, err := conn.ListObjectMetadata(ctx, []string{"audits", "idea_organizations", "me/assigned"})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	utils.DumpJSON(m, os.Stdout)
+}
