b3adbea25f61d69529c9f0ce2446594dfe0f8c89
Joseph Karage <josephbkarage@gmail.com>
2025-03-21 11:07:57 +0300

    feat: Add Hunter Read Connector (#1506)

---
+++ providers/hunter/supports.go
@@ -0,0 +1,24 @@
+package hunter
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/staticschema"
+)
+
+func supportedOperations() components.EndpointRegistryInput {
+	readSupport := []string{
+		"leads", "leads_custom_attributes", "leads_lists", "campaigns",
+	}
+
+	return components.EndpointRegistryInput{
+		staticschema.RootModuleID: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
+	}
+}
