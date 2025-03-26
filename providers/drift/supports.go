7cac2ba19a811ee6ca0976af3cea6641a81837cd
jkarage <josephbkarage@gmail.com>
2025-03-26 12:26:36 +0300

    Address Review 1

--- providers/drift/supports.go
+++ providers/drift/supports.go
@@ -1,18 +1,1 @@
 package drift
-
-const (
-	list   = "list"
-	object = "object"
-	data   = "data"
-)
-
-func responseSchema(objectName string) (string, string) {
-	switch objectName {
-	case "users/list", "conversations/list", "teams/org", "users/meetings/org":
-		return object, data
-	case "playbooks/list", "playbooks/clp":
-		return list, ""
-	default:
-		return object, ""
-	}
-}
