d6d68cf0b40474b504d300704cee1391222d7a2e
jkarage <josephbkarage@gmail.com>
2025-03-26 13:13:49 +0300

    Address Review 1

--- providers/seismic/parse.go
+++ providers/seismic/parse.go
@@ -8,6 +8,7 @@ import (
 func nextRecordsURL() common.NextPageFunc {
 	return func(n *ajson.Node) (string, error) {
 		// Reporting API do not support pagination.
+		// https://developer.seismic.com/seismicsoftware/reference/h1-reporting-api-overview
 		return "", nil
 	}
 }
