920959905021c9087997c3ef0200e43345fc226d
jkarage <josephbkarage@gmail.com>
2025-03-27 10:33:10 +0300

    Initial metadata commit

---
+++ providers/gitlab/connector.go
@@ -0,0 +1,50 @@
+package gitlab
+
+import (
+	_ "embed"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/schema"
+	"github.com/amp-labs/connectors/internal/staticschema"
+	"github.com/amp-labs/connectors/providers"
+	"github.com/amp-labs/connectors/tools/fileconv"
+	"github.com/amp-labs/connectors/tools/scrapper"
+)
+
+// nolint:gochecknoglobals
+var (
+	//go:embed schemas.json
+	schemaContent []byte
+
+	fileManager = scrapper.NewMetadataFileManager[staticschema.FieldMetadataMapV1](
+		schemaContent, fileconv.NewSiblingFileLocator())
+
+	schemas = fileManager.MustLoadSchemas()
+)
+
+type Connector struct {
+	// Basic connector
+	*components.Connector
+
+	// Require authenticated client
+	common.RequireAuthenticatedClient
+
+	// Supported operations
+	components.SchemaProvider
+	components.Reader
+	components.Writer
+}
+
+func NewConnector(params common.Parameters) (*Connector, error) {
+	return components.Initialize(providers.GitLab, params, constructor)
+}
+
+func constructor(base *components.Connector) (*Connector, error) {
+	connector := &Connector{Connector: base}
+
+	// Set the metadata provider for the connector
+	connector.SchemaProvider = schema.NewOpenAPISchemaProvider(connector.ProviderContext.Module(), schemas)
+
+	return connector, nil
+}
---
+++ scripts/openapi/gitlab/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	_ "embed"
+	"log"
+	"log/slog"
+
+	"github.com/amp-labs/connectors/internal/datautils"
+	"github.com/amp-labs/connectors/internal/goutils"
+	"github.com/amp-labs/connectors/internal/staticschema"
+	"github.com/amp-labs/connectors/tools/fileconv"
+	"github.com/amp-labs/connectors/tools/fileconv/api2"
+	"github.com/amp-labs/connectors/tools/fileconv/api3"
+	"github.com/amp-labs/connectors/tools/scrapper"
+)
+
+// nolint:gochecknoglobals
+var (
+	ignoreEndpoints = []string{
+		"/account/summary",                  // returns single customer
+		"/account/summary/physical_address", // returns single address
+		"/contacts/counts",                  // object describing statistics on contacts
+		// Requires query parameters
+		"/emails/campaign_id_xrefs",
+		"/contacts/contact_id_xrefs",
+		"/contact_lists/list_id_xrefs",
+		// Partner accounts need auth token to be in 2 headers, usual auth and custom x-api-key.
+		"/partner/*",
+	}
+	objectEndpoints = map[string]string{
+		"/account/emails": "account_emails",
+		"/emails":         "email_campaigns",
+	}
+	displayNameOverride = map[string]string{
+		// "campaign_id_xrefs": "Email Campaign Identifiers",
+		// "contact_id_xrefs":  "Contact Identifiers",
+		// "list_id_xrefs":     "List Identifiers",
+	}
+	objectNameToResponseField = datautils.NewDefaultMap(map[string]string{
+		// "campaign_id_xrefs":        "xrefs",
+		// "contact_id_xrefs":         "xrefs",
+		// "list_id_xrefs":            "xrefs",
+		"accounts":                 "site_owner_list",
+		"email_campaign_summaries": "bulk_email_campaign_summaries",
+		"contact_tags":             "tags",
+		"contact_lists":            "lists",
+		"contact_custom_fields":    "custom_fields",
+		"email_campaigns":          "campaigns",
+		"account_emails":           "", // response is already an array, empty refers to current
+		"privileges":               "",
+		"subscriptions":            "",
+	},
+		func(objectName string) (fieldName string) {
+			return objectName
+		},
+	)
+
+	// Static file containing openapi spec.
+	//
+	//go:embed openapi_v2.yaml
+	apiFile     []byte
+	FileManager = api2.NewOpenapiFileManager[any](apiFile) // nolint:gochecknoglobals
+
+	// Static file containing a list of object metadata is embedded and can be served.
+	//
+	//go:embed schemas.json
+	schemas       []byte
+	schemaManager = scrapper.NewMetadataFileManager[staticschema.FieldMetadataMapV1]( // nolint:gochecknoglobals
+		schemas, fileconv.NewSiblingFileLocator())
+
+	// Schemas is cached Object schemas.
+	Schemas = schemaManager.MustLoadSchemas() // nolint:gochecknoglobals
+)
+
+func main() {
+	explorer, err := FileManager.GetExplorer(
+		api3.WithDisplayNamePostProcessors(
+			api3.CamelCaseToSpaceSeparated,
+			api3.CapitalizeFirstLetterEveryWord,
+		))
+	goutils.MustBeNil(err)
+
+	objects, err := explorer.ReadObjectsGet(
+		api3.NewDenyPathStrategy(ignoreEndpoints),
+		objectEndpoints, displayNameOverride,
+		api3.CustomMappingObjectCheck(objectNameToResponseField),
+	)
+	goutils.MustBeNil(err)
+
+	schemas := staticschema.NewMetadata[staticschema.FieldMetadataMapV1]()
+	registry := datautils.NamedLists[string]{}
+
+	for _, object := range objects {
+		if object.Problem != nil {
+			slog.Error("schema not extracted",
+				"objectName", object.ObjectName,
+				"error", object.Problem,
+			)
+		}
+
+		for _, field := range object.Fields {
+			schemas.Add("", object.ObjectName, object.DisplayName, object.URLPath, object.ResponseKey,
+				staticschema.FieldMetadataMapV1{
+					field.Name: field.Name,
+				}, nil, object.Custom)
+		}
+
+		for _, queryParam := range object.QueryParams {
+			registry.Add(queryParam, object.ObjectName)
+		}
+	}
+
+	goutils.MustBeNil(schemaManager.SaveSchemas(schemas))
+	goutils.MustBeNil(schemaManager.SaveQueryParamStats(scrapper.CalculateQueryParamStats(registry)))
+
+	log.Println("Completed.")
+}
