b7c6dfad610d8baff554a3dc564f7f47c9199d70
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-01 00:51:59 +0300

    instantly

--- providers/instantly/connector.go
+++ providers/instantly/connector.go
@@ -3,65 +3,43 @@ package instantly
 import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/interpreter"
-	"github.com/amp-labs/connectors/common/paramsbuilder"
 	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/components"
 	"github.com/amp-labs/connectors/providers"
 )
 
 const apiVersion = "v1"
 
 type Connector struct {
-	BaseURL string
-	Module  common.Module
-	Client  *common.JSONHTTPClient
+	// Basic connector
+	*components.Connector
 }
 
-func NewConnector(opts ...Option) (conn *Connector, outErr error) {
-	params, err := paramsbuilder.Apply(parameters{}, opts)
+// NewConnector is an old constructor, use NewConnectorV2.
+// Deprecated.
+func NewConnector(opts ...Option) (*Connector, error) {
+	params, err := newParams(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	httpClient := params.Client.Caller
-	conn = &Connector{
-		Client: &common.JSONHTTPClient{
-			HTTPClient: httpClient,
-		},
-	}
+	return NewConnectorV2(*params)
+}
 
-	providerInfo, err := providers.ReadInfo(conn.Provider())
-	if err != nil {
-		return nil, err
-	}
+func NewConnectorV2(params common.Parameters) (*Connector, error) {
+	return components.Initialize(providers.Instantly, params, constructor)
+}
 
-	if err != nil {
-		return nil, err
-	}
-
-	// connector and its client must mirror base url and provide its own error parser
-	conn.setBaseURL(providerInfo.BaseURL)
-	conn.Client.HTTPClient.ErrorHandler = interpreter.ErrorHandler{
+func constructor(base *components.Connector) (*Connector, error) {
+	base.SetErrorHandler(interpreter.ErrorHandler{
 		JSON: interpreter.NewFaultyResponder(errorFormats, nil),
-	}.Handle
+	}.Handle)
 
-	return conn, nil
-}
-
-func (c *Connector) Provider() providers.Provider {
-	return providers.Instantly
-}
-
-func (c *Connector) String() string {
-	return c.Provider() + ".Connector"
+	return &Connector{Connector: base}, nil
 }
 
 func (c *Connector) getURL(parts ...string) (*urlbuilder.URL, error) {
-	return urlbuilder.New(c.BaseURL, append([]string{
+	return urlbuilder.New(c.ProviderInfo().BaseURL, append([]string{
 		apiVersion,
 	}, parts...)...)
-}
-
-func (c *Connector) setBaseURL(newURL string) {
-	c.BaseURL = newURL
-	c.Client.HTTPClient.Base = newURL
 }
