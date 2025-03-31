10348bfce991738f717c0b448089d03c3273aa97
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-01 00:17:40 +0300

    chilipiper

--- providers/chilipiper/metadata_test.go
+++ providers/chilipiper/metadata_test.go
@@ -83,7 +83,7 @@ func constructTestConnector(serverURL string) (*Connector, error) {
 		return nil, err
 	}
 
-	connector.BaseURL = serverURL
+	connector.SetBaseURL(serverURL)
 
 	return connector, nil
 }
