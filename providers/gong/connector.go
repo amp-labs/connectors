9f5973de3380d8f71649370e06b352ecda4927f6
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-01 00:36:24 +0300

    gong

--- providers/gong/connector.go
+++ providers/gong/connector.go
@@ -3,8 +3,8 @@ package gong
 import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/interpreter"
-	"github.com/amp-labs/connectors/common/paramsbuilder"
 	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/components"
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/providers/gong/metadata"
 )
@@ -12,53 +12,42 @@ import (
 const ApiVersion = "v2"
 
 type Connector struct {
-	BaseURL string
-	Module  common.Module
-	Client  *common.JSONHTTPClient
+	// Basic connector
+	*components.Connector
 }
 
-func NewConnector(opts ...Option) (conn *Connector, outErr error) {
-	params, err := paramsbuilder.Apply(parameters{}, opts)
+// NewConnector is an old constructor, use NewConnectorV2.
+// Deprecated.
+func NewConnector(opts ...Option) (*Connector, error) {
+	params, err := newParams(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	httpClient := params.Client.Caller
-	conn = &Connector{
-		Client: &common.JSONHTTPClient{
-			HTTPClient: httpClient,
-		},
-	}
+	return NewConnectorV2(*params)
+}
 
-	// Read provider info
-	providerInfo, err := providers.ReadInfo(conn.Provider())
-	if err != nil {
-		return nil, err
-	}
+func NewConnectorV2(params common.Parameters) (*Connector, error) {
+	return components.Initialize(providers.Gong, params, constructor)
+}
 
-	// connector and its client must mirror base url and provide its own error parser
-	conn.setBaseURL(providerInfo.BaseURL)
-	conn.Client.HTTPClient.ErrorHandler = interpreter.ErrorHandler{
+func constructor(base *components.Connector) (*Connector, error) {
+	base.SetErrorHandler(interpreter.ErrorHandler{
 		JSON: interpreter.NewFaultyResponder(errorFormats, nil),
-	}.Handle
+	}.Handle)
 
-	return conn, nil
+	return &Connector{Connector: base}, nil
 }
 
 func (c *Connector) getURL(arg string) (*urlbuilder.URL, error) {
-	return urlbuilder.New(c.BaseURL, ApiVersion, arg)
+	return urlbuilder.New(c.ProviderInfo().BaseURL, ApiVersion, arg)
 }
 
 func (c *Connector) getReadURL(objectName string) (*urlbuilder.URL, error) {
-	path, err := metadata.Schemas.LookupURLPath(c.Module.ID, objectName)
+	path, err := metadata.Schemas.LookupURLPath(c.Module(), objectName)
 	if err != nil {
 		return nil, err
 	}
 
-	return urlbuilder.New(c.BaseURL, path)
-}
-
-func (c *Connector) setBaseURL(newURL string) {
-	c.BaseURL = newURL
-	c.Client.HTTPClient.Base = newURL
+	return urlbuilder.New(c.ProviderInfo().BaseURL, path)
 }
