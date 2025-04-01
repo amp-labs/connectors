6fcb3f26701d1b294e258f701da3182de54b1071
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-01 21:39:15 +0300

    marketo

--- providers/marketo/read.go
+++ providers/marketo/read.go
@@ -17,7 +17,7 @@ func (c *Connector) Read(ctx context.Context, config common.ReadParams) (*common
 		return nil, err
 	}
 
-	res, err := c.Client.Get(ctx, url.String())
+	res, err := c.JSONHTTPClient().Get(ctx, url.String())
 	if err != nil {
 		return nil, err
 	}
