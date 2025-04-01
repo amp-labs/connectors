733aac6269b2c29c86de9861a159de286e3772e8
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-01 03:56:41 +0300

    salesloft

--- providers/salesloft/connector.go
+++ providers/salesloft/connector.go
@@ -3,8 +3,8 @@ package salesloft
 import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/interpreter"
-	"github.com/amp-labs/connectors/common/paramsbuilder"
 	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/components"
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/providers/salesloft/metadata"
 )
@@ -12,56 +12,39 @@ import (
 const ApiVersion = "v2"
 
 type Connector struct {
-	BaseURL string
-	Module  common.Module
-	Client  *common.JSONHTTPClient
+	// Basic connector
+	*components.Connector
 }
 
-func NewConnector(opts ...Option) (conn *Connector, outErr error) {
-	params, err := paramsbuilder.Apply(parameters{}, opts)
+// NewConnector is an old constructor, use NewConnectorV2.
+// Deprecated.
+func NewConnector(opts ...Option) (*Connector, error) {
+	params, err := newParams(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	providerInfo, err := providers.ReadInfo(providers.Salesloft)
-	if err != nil {
-		return nil, err
-	}
+	return NewConnectorV2(*params)
+}
 
-	httpClient := params.Client.Caller
-	conn = &Connector{
-		Client: &common.JSONHTTPClient{
-			HTTPClient: httpClient,
-		},
-	}
-	// connector and its client must mirror base url and provide its own error parser
-	conn.setBaseURL(providerInfo.BaseURL)
-	conn.Client.HTTPClient.ErrorHandler = interpreter.ErrorHandler{
+func NewConnectorV2(params common.Parameters) (*Connector, error) {
+	return components.Initialize(providers.Salesloft, params, constructor)
+}
+
+func constructor(base *components.Connector) (*Connector, error) {
+	base.SetErrorHandler(interpreter.ErrorHandler{
 		JSON: interpreter.NewFaultyResponder(errorFormats, statusCodeMapping),
-	}.Handle
+	}.Handle)
 
-	return conn, nil
-}
-
-func (c *Connector) Provider() providers.Provider {
-	return providers.Salesloft
-}
-
-func (c *Connector) String() string {
-	return c.Provider() + ".Connector"
+	return &Connector{Connector: base}, nil
 }
 
 func (c *Connector) getURL(objectName string) (*urlbuilder.URL, error) {
-	path, _ := metadata.Schemas.LookupURLPath(c.Module.ID, objectName)
+	path, _ := metadata.Schemas.LookupURLPath(c.Module(), objectName)
 	if len(path) == 0 {
 		// Fallback, try objectName as a URL.
 		path = objectName
 	}
 
-	return urlbuilder.New(c.BaseURL, ApiVersion, path)
-}
-
-func (c *Connector) setBaseURL(newURL string) {
-	c.BaseURL = newURL
-	c.Client.HTTPClient.Base = newURL
+	return urlbuilder.New(c.ProviderInfo().BaseURL, ApiVersion, path)
 }
