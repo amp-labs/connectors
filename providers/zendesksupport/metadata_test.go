c31b78abd54db7fe92a3cff620355cf6dfac0fb0
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-01 06:02:41 +0300

    zendesk

--- providers/zendesksupport/metadata_test.go
+++ providers/zendesksupport/metadata_test.go
@@ -162,7 +162,7 @@ func BenchmarkListObjectMetadata(b *testing.B) {
 
 	dummyServer := mockserver.Dummy()
 
-	connector.setBaseURL(dummyServer.URL)
+	connector.SetBaseURL(dummyServer.URL)
 
 	// start of benchmark
 	for i := 0; i < b.N; i++ {
