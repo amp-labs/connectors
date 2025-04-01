2d51322a5473498c279fa5d12860b76ac0c2f950
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-01 06:26:42 +0300

    dynamicscrm

--- providers/dynamicscrm/connector.go
+++ providers/dynamicscrm/connector.go
@@ -3,63 +3,50 @@ package dynamicscrm
 import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/interpreter"
-	"github.com/amp-labs/connectors/common/paramsbuilder"
 	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/components"
 	"github.com/amp-labs/connectors/providers"
 )
 
 const apiVersion = "v9.2"
 
 type Connector struct {
-	BaseURL                     string
-	Client                      *common.JSONHTTPClient
+	// Basic connector
+	*components.Connector
+
 	metadataDiscoveryRepository metadataDiscoveryRepository
 }
 
-func NewConnector(opts ...Option) (conn *Connector, outErr error) {
-	params, err := paramsbuilder.Apply(parameters{}, opts)
+// NewConnector is an old constructor, use NewConnectorV2.
+// Deprecated.
+func NewConnector(opts ...Option) (*Connector, error) {
+	params, err := newParams(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	httpClient := params.Client.Caller
-	conn = &Connector{
-		Client: &common.JSONHTTPClient{
-			HTTPClient: httpClient,
-		},
-	}
+	return NewConnectorV2(*params)
+}
 
-	providerInfo, err := providers.ReadInfo(conn.Provider(), &params.Workspace)
-	if err != nil {
-		return nil, err
-	}
+func NewConnectorV2(params common.Parameters) (*Connector, error) {
+	return components.Initialize(providers.DynamicsCRM, params, constructor)
+}
 
-	// connector and its client must mirror base url and provide its own error parser
-	conn.setBaseURL(providerInfo.BaseURL)
-	conn.Client.HTTPClient.ErrorHandler = interpreter.ErrorHandler{
+func constructor(base *components.Connector) (*Connector, error) {
+	base.SetErrorHandler(interpreter.ErrorHandler{
 		JSON: interpreter.NewFaultyResponder(errorFormats, nil),
-	}.Handle
+	}.Handle)
+
+	conn := &Connector{Connector: base}
+
 	conn.metadataDiscoveryRepository = metadataDiscoveryRepository{
-		client:   conn.Client,
+		client:   conn.JSONHTTPClient(),
 		buildURL: conn.getURL,
 	}
 
 	return conn, nil
 }
 
-func (c *Connector) Provider() providers.Provider {
-	return providers.DynamicsCRM
-}
-
-func (c *Connector) String() string {
-	return c.Provider() + ".Connector"
-}
-
 func (c *Connector) getURL(arg string) (*urlbuilder.URL, error) {
-	return constructURL(c.BaseURL, apiVersion, arg)
-}
-
-func (c *Connector) setBaseURL(newURL string) {
-	c.BaseURL = newURL
-	c.Client.HTTPClient.Base = newURL
+	return constructURL(c.ProviderInfo().BaseURL, apiVersion, arg)
 }
