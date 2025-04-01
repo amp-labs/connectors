ff68cdaf67502a4fe69926172353ed3a3f3b3c4e
Joseph Karage <josephbkarage@gmail.com>
2025-04-01 10:31:32 +0300

    [CON-1585] feat: Add GitLab Write Connector (#1532)
    * Initial metadata commit
    
    * feat: Add GitLab Metadata Connector
    
    * feat: Add GitLab Write Connector
    
    * Address Review

---
+++ test/gitlab/write/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/amp-labs/connectors/common"
+	gl "github.com/amp-labs/connectors/providers/gitlab"
+	"github.com/amp-labs/connectors/test/gitlab"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error(err.Error())
+	}
+}
+
+func run() error {
+	ctx := context.Background()
+	conn := gitlab.GetConnector(ctx)
+
+	err := testCreatingProjects(ctx, conn)
+	if err != nil {
+		return err
+	}
+
+	err = testCreatingSnippets(ctx, conn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func testCreatingProjects(ctx context.Context, conn *gl.Connector) error {
+	params := common.WriteParams{
+		ObjectName: "projects",
+		RecordData: map[string]any{
+			"name": "Test Project",
+		},
+	}
+
+	res, err := conn.Write(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
+
+func testCreatingSnippets(ctx context.Context, conn *gl.Connector) error {
+	params := common.WriteParams{
+		ObjectName: "snippets",
+		RecordData: map[string]any{
+			"title":       "This is a snippet",
+			"description": "Hello World snippet",
+			"visibility":  "public",
+			"files": []map[string]string{
+				{
+					"content":   "Hello world",
+					"file_path": "test.txt",
+				},
+			},
+		},
+	}
+
+	res, err := conn.Write(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
