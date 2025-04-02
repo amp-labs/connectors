75717d002bbb99d5b3f4fde32d684195a94b85fd
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-02 04:08:38 +0300

    [CON-1619]

--- internal/components/provider.go
+++ internal/components/provider.go
@@ -13,7 +13,8 @@ import (
 type ProviderContext struct {
 	provider     providers.Provider
 	providerInfo *providers.ProviderInfo
-	module       common.ModuleID
+	moduleInfo   providers.ModuleInfo
+	moduleID     common.ModuleID
 }
 
 func NewProviderContext(
@@ -30,20 +31,24 @@ func NewProviderContext(
 
 	metadata[catalogreplacer.VariableWorkspace] = workspace
 
-	// TODO: Use module to get provider info
 	providerInfo, err := providers.ReadInfo(provider, paramsbuilder.NewCatalogVariables(metadata)...)
 	if err != nil {
 		return nil, err
 	}
 
+	pctx.moduleID = module
 	pctx.providerInfo = providerInfo
-	pctx.module = module
+
+	pctx.moduleInfo, err = providerInfo.ReadModuleInfo(module)
+	if err != nil {
+		return nil, err
+	}
 
 	return pctx, nil
 }
 
 func (p *ProviderContext) String() string {
-	return fmt.Sprintf("%v.Connector[%v]", p.provider, p.module)
+	return fmt.Sprintf("%v.Connector[%v]", p.provider, p.moduleID)
 }
 
 func (p *ProviderContext) Provider() providers.Provider {
@@ -55,5 +60,5 @@ func (p *ProviderContext) ProviderInfo() *providers.ProviderInfo {
 }
 
 func (p *ProviderContext) Module() common.ModuleID {
-	return p.module
+	return p.moduleID
 }
