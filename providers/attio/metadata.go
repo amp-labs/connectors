cf5815cc9426aebb1117be7750c93d003c14f8ce
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-04-02 11:51:05 +0530

    [CON-1610] feat(attio): added support to standard & custom object (#1482)
    * [CON-1610] chore: added support to standard & custom object
    
    * fix: Resolved the lint issue
    
    * fix: linter issue
    
    * fix: solved linter issue
    
    * fix: solved linter issue
    
    * fix: modified the functionality based on the PR comments and added concrete type struct to get response
    
    * fix: linter issue
    
    * fix: solved linter issue
    
    * fix: Modified the metadata parsing logic and reduced the input size for the metadata test cases.
    
    * fix: Resolved the llint issue
    
    * fix: Added the commentsa and changed the variable name
    
    * chore: Updated the read.me file and added the comments
    
    * [CON-1610] feat(attio): read : To support standard & custom objects (#1504)
    
    * [CON-1610] feat(attio): Added support to standard & custom objects for read functionality.
    
    * fix: Resolved the linter issue
    
    * fix: solved linter issue
    
    * fix: solved the linter issue
    
    * chore: updated the read.me file
    
    * chore: Modified the Pagination logic and flattenning the standard/custom object response
    
    * chore: Modified the pagination value in the test cases
    
    * chore: Modified the Pagination logic and implemented the new methods for handle raw and field vlaues
    
    * fix: solved the linter issue
    
    * chore: modified the code on the handle next record logic and flattener method.
    
    * [CON-1613] chore(attio): updated the field values single/Multi select. (#1526)
    
    * [CON-1613] chore(attio): updated the field values single/Multi select.
    
    * chore: updated the condition
    
    * chore: Added the test case for the getting option value for type single/multiselect
    
    * fix: Resolved the linter issue

--- providers/attio/metadata.go
+++ providers/attio/metadata.go
@@ -2,8 +2,10 @@ package attio
 
 import (
 	"context"
+	"time"
 
 	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
 )
 
 // This struct is used for when the response data having slice of data.
@@ -11,7 +13,42 @@ type responseObject struct {
 	Data []map[string]any `json:"data"`
 }
 
+// This struct is used for when the response data having single data.
+type objectAttribute struct {
+	Data []Data `json:"data"`
+}
+
+type Data struct {
+	ID struct {
+		WorkspaceID string `json:"workspace_id"` //nolint:tagliatelle
+		ObjectID    string `json:"object_id"`    //nolint:tagliatelle
+		AttributeID string `json:"attribute_id"` //nolint:tagliatelle
+	} `json:"id"`
+	Title                 string    `json:"title"`
+	APISlug               string    `json:"api_slug"` //nolint:tagliatelle
+	Type                  string    `json:"type"`
+	IsWritable            bool      `json:"is_writable"`              //nolint:tagliatelle
+	IsMultiselect         bool      `json:"is_multiselect"`           //nolint:tagliatelle
+	IsDefaultValueEnabled bool      `json:"is_default_value_enabled"` //nolint:tagliatelle
+	CreatedAt             time.Time `json:"created_at"`               //nolint:tagliatelle
+}
+
+type objectResponse struct {
+	Data struct {
+		Id struct {
+			WorkspaceId string `json:"workspace_id"` //nolint:tagliatelle
+			ObjectId    string `json:"object_id"`    //nolint:tagliatelle
+		} `json:"id"`
+		ApiSlug      string    `json:"api_slug"`      //nolint:tagliatelle
+		SingularNoun string    `json:"singular_noun"` //nolint:tagliatelle
+		PluralNoun   string    `json:"plural_noun"`   //nolint:tagliatelle
+		CreatedAt    time.Time `json:"created_at"`    //nolint:tagliatelle
+	} `json:"data"`
+}
+
 // ListObjectMetadata creates metadata of object via reading objects using Attio API.
+//
+//nolint:funlen
 func (c *Connector) ListObjectMetadata(ctx context.Context,
 	objectNames []string,
 ) (*common.ListObjectMetadataResult, error) {
@@ -19,44 +56,110 @@ func (c *Connector) ListObjectMetadata(ctx context.Context,
 		return nil, common.ErrMissingObjects
 	}
 
-	metadataResult := common.ListObjectMetadataResult{
-		Result: make(map[string]common.ObjectMetadata),
-		Errors: make(map[string]error),
-	}
+	metadataResult := common.NewListObjectMetadataResult()
 
 	for _, obj := range objectNames {
-		// Constructing the request url.
-		url, err := c.getApiURL(obj)
-		if err != nil {
-			return nil, err
-		}
-
-		resp, err := c.Client.Get(ctx, url.String())
+		metadata, isCustom, err := c.getObjectAttributes(ctx, obj)
 		if err != nil {
 			metadataResult.Errors[obj] = err
 
 			continue
 		}
 
-		var metadata *common.ObjectMetadata
+		displayName := obj
 
-		metadata, err = parseMetadataFromResponse(resp)
-		if err != nil {
-			metadataResult.Errors[obj] = err
+		if isCustom {
+			name, err := c.getObjectDisplayName(ctx, obj)
+			if err != nil {
+				metadataResult.Errors[obj] = err
 
-			continue
+				continue
+			}
+
+			if name != "" {
+				displayName = name
+			}
 		}
 
-		metadata.DisplayName = obj
-		metadataResult.Result[obj] = *metadata
+		metadataResult.Result[obj] = *common.NewObjectMetadata(
+			displayName, metadata,
+		)
 	}
 
-	return &metadataResult, nil
+	return metadataResult, nil
 }
 
-// This function is used for slice of objects.
-func parseMetadataFromResponse(resp *common.JSONHTTPResponse) (*common.ObjectMetadata, error) {
-	response, err := common.UnmarshalJSON[responseObject](resp)
+func (c *Connector) getObjectAttributes(
+	ctx context.Context, obj string,
+) (map[string]common.FieldMetadata, bool, error) {
+	// Standard isn't a term we commonly use, but rather a concept defined by Attio itself.
+	// supportAttioApi represents the APIs listed under the Attio API section in the docs
+	// (this does not cover the entire Attio API). Reference: https://developers.attio.com/reference.
+	isAttioStandardOrCustomObj := !supportAttioApi.Has(obj)
+
+	var (
+		url *urlbuilder.URL
+		err error
+	)
+
+	if isAttioStandardOrCustomObj {
+		url, err = c.getObjectAttributesURL(obj)
+		if err != nil {
+			return nil, false, err
+		}
+	} else {
+		url, err = c.getApiURL(obj)
+		if err != nil {
+			return nil, false, err
+		}
+	}
+
+	resp, err := c.Client.Get(ctx, url.String())
+	if err != nil {
+		return nil, false, err
+	}
+
+	metadata, err := c.parseMetadataFromResponse(ctx, resp, isAttioStandardOrCustomObj)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return metadata, isAttioStandardOrCustomObj, nil
+}
+
+// getObjectDisplayName fetches the display name for custom objects.
+func (c *Connector) getObjectDisplayName(ctx context.Context, obj string) (string, error) {
+	url, err := c.getObjectsURL(obj)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := c.Client.Get(ctx, url.String())
+	if err != nil {
+		return "", err
+	}
+
+	return getDisplayName(resp)
+}
+
+func (c *Connector) parseMetadataFromResponse(ctx context.Context, resp *common.JSONHTTPResponse,
+	isAttioStandardOrCustomObj bool,
+) (map[string]common.FieldMetadata, error) {
+	// Retrieving metadata for standard and custom objects in Attio using the api_slug field.
+	if isAttioStandardOrCustomObj {
+		return c.parseStandardOrCustomMetadata(ctx, resp)
+	}
+
+	return c.parseMetadata(resp)
+}
+
+// Parsing the metadata response for standard or custom objects.
+func (c *Connector) parseStandardOrCustomMetadata(
+	ctx context.Context, resp *common.JSONHTTPResponse,
+) (map[string]common.FieldMetadata, error) {
+	metadata := make(map[string]common.FieldMetadata)
+
+	response, err := common.UnmarshalJSON[objectAttribute](resp)
 	if err != nil {
 		return nil, err
 	}
@@ -65,14 +168,123 @@ func parseMetadataFromResponse(resp *common.JSONHTTPResponse) (*common.ObjectMet
 		return nil, common.ErrMissingExpectedValues
 	}
 
-	metadata := &common.ObjectMetadata{
-		FieldsMap: make(map[string]string),
-	}
+	for _, value := range response.Data {
+		apiSlug := value.APISlug
 
-	// Using the first result data to generate the metadata.
-	for k := range response.Data[0] {
-		metadata.FieldsMap[k] = k
+		var defaultValues []common.FieldValue
+
+		if value.Type == "select" || value.Type == "record-reference" || value.Type == "domain" {
+			defaultValues, err = c.getDefaultValues(ctx, value)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		metadata[apiSlug] = common.FieldMetadata{
+			DisplayName:  apiSlug,
+			ValueType:    getFieldValueType(value.Type, value.IsMultiselect),
+			ProviderType: value.Type,
+			ReadOnly:     !value.IsWritable,
+			Values:       defaultValues,
+		}
 	}
 
 	return metadata, nil
+}
+
+// Parsing the metadata response for non-standard or custom objects.
+func (c *Connector) parseMetadata(resp *common.JSONHTTPResponse) (map[string]common.FieldMetadata, error) {
+	metadata := make(map[string]common.FieldMetadata)
+
+	response, err := common.UnmarshalJSON[responseObject](resp)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(response.Data) == 0 {
+		return nil, common.ErrMissingExpectedValues
+	}
+	// Using the first result data to generate the metadata.
+	for k := range response.Data[0] {
+		metadata[k] = common.FieldMetadata{
+			DisplayName:  k,
+			ValueType:    common.ValueTypeOther,
+			ProviderType: "", // not available
+			ReadOnly:     false,
+			Values:       nil,
+		}
+	}
+
+	return metadata, nil
+}
+
+// Getting display name for both standard and custom objects.
+func getDisplayName(resp *common.JSONHTTPResponse) (string, error) {
+	response, err := common.UnmarshalJSON[objectResponse](resp)
+	if err != nil {
+		return "", err
+	}
+
+	if response == nil {
+		return "", common.ErrMissingExpectedValues
+	}
+
+	res := response.Data.PluralNoun
+	if res == "" {
+		return "", common.ErrNotFound
+	}
+
+	return res, nil
+}
+
+func getFieldValueType(field string, ismultiselect bool) common.ValueType {
+	switch field {
+	case "number":
+		return common.ValueTypeInt
+	case "text":
+		return common.ValueTypeString
+	case "select", "record-reference", "domain":
+		if ismultiselect {
+			return common.ValueTypeMultiSelect
+		}
+
+		return common.ValueTypeSingleSelect
+	case "date":
+		return common.ValueTypeDate
+	case "timestamp":
+		return common.ValueTypeDateTime
+	default:
+		// location, currency, interaction, actor-reference
+		return common.ValueTypeOther
+	}
+}
+
+func (c *Connector) getDefaultValues(ctx context.Context, o Data) (fields []common.FieldValue, err error) {
+	if !o.IsDefaultValueEnabled {
+		return nil, nil
+	}
+
+	url, err := c.getOptionsURL(o.ID.ObjectID, o.ID.AttributeID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.Client.Get(ctx, url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	objectAttribute, err := common.UnmarshalJSON[objectAttribute](resp)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, title := range objectAttribute.Data {
+		fields = append(fields, common.FieldValue{
+			Value:        title.Title,
+			DisplayValue: title.Title,
+		})
+	}
+
+	return
 }
---
+++ providers/attio/url.go
@@ -0,0 +1,27 @@
+package attio
+
+import (
+	"github.com/amp-labs/connectors/common/urlbuilder"
+)
+
+// A trailing slash is added to the relative URL to ensure proper concatenation of dynamic values.
+//
+// Relative URL for retrieving metadata for standard and custom objects in Attio.
+func (c *Connector) getObjectAttributesURL(objName string) (*urlbuilder.URL, error) {
+	return urlbuilder.New(c.BaseURL, apiVersion, "objects", objName, "attributes")
+}
+
+// Relative URL for retrieving display Name for standard and custom objects in Attio.
+func (c *Connector) getObjectsURL(objName string) (*urlbuilder.URL, error) {
+	return urlbuilder.New(c.BaseURL, apiVersion, "objects", objName)
+}
+
+// Relative URL for retrieving the options in the attributes.
+func (c *Connector) getOptionsURL(objName, attributeID string) (*urlbuilder.URL, error) {
+	return urlbuilder.New(c.BaseURL, apiVersion, "objects", objName, "attributes", attributeID, "options")
+}
+
+// Relative URL for retrieving standard and custom object read URL.
+func (c *Connector) getObjectReadURL(objName string) (*urlbuilder.URL, error) {
+	return urlbuilder.New(c.BaseURL, apiVersion, "objects", objName, "records", "query")
+}
