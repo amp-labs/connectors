e6b28ab2d57ff781660109076a99ce00a788acaf
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-02 04:08:13 +0300

    atlassian

--- providers/atlassian/authmetadata.go
+++ providers/atlassian/authmetadata.go
@@ -59,7 +59,7 @@ func (c *Connector) retrieveCloudId(ctx context.Context) (string, error) {
 		return "", err
 	}
 
-	res, err := c.Client.Get(ctx, url.String())
+	res, err := c.JSONHTTPClient().Get(ctx, url.String())
 	if err != nil {
 		return "", err
 	}
