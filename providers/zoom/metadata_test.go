b4ef918e085d85edff3ce5101bb19e1d437b5524
Cobalt0s <constantin.y.koval@gmail.com>
2025-04-04 05:51:28 +0300

    zoom

--- providers/zoom/metadata_test.go
+++ providers/zoom/metadata_test.go
@@ -122,7 +122,7 @@ func constructTestConnector(serverURL string, moduleID common.ModuleID) (*Connec
 		return nil, err
 	}
 	// for testing we want to redirect calls to our mock server.
-	connector.SetURL(serverURL)
+	testroutines.OverrideURLOrigin(connector.Transport, serverURL)
 
 	return connector, nil
 }
