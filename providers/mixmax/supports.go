1faedd00811c80d289259829d6bc8f6f41bfbcf6
jkarage <josephbkarage@gmail.com>
2025-04-07 10:15:20 +0300

    Address Review

--- providers/mixmax/supports.go
+++ providers/mixmax/supports.go
@@ -3,7 +3,7 @@ package mixmax
 func responseField(objectName string) string {
 	switch objectName {
 	case "appointmentlinks/me", "userpreferences/me", "users/me":
-		return ""
+		return "" // indicates we're reading data fields from the root level.
 	default:
 		return "results"
 	}
