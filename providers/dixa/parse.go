225ca5b855af0cce84d8e714c48a5d69f0cb574f
Joseph Karage <josephbkarage@gmail.com>
2025-04-07 15:09:20 +0300

    [CON-1095] feat: Add Dixa Read Connector (#1453)
    * feat: Add Dixa Metadata Connector
    
    * feat: Add Dixa Read Connector
    
    * fix gci formatting issue
    
    * Address Review
    
    * fix records func
    
    * fix rebasing issue

---
+++ providers/dixa/parse.go
@@ -0,0 +1,43 @@
+package dixa
+
+import (
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/jsonquery"
+	"github.com/spyzhov/ajson"
+)
+
+func constructRecords(objectName string) common.RecordsFunc {
+	switch objectName {
+	case businessHours:
+		return func(node *ajson.Node) ([]map[string]any, error) {
+			schedules, err := jsonquery.New(node, data).ArrayOptional("schedules")
+			if err != nil {
+				return nil, err
+			}
+
+			return jsonquery.Convertor.ArrayToMap(schedules)
+		}
+	default:
+		return common.ExtractRecordsFromPath(data)
+	}
+}
+
+func nextRecordsURL() common.NextPageFunc {
+	return func(node *ajson.Node) (string, error) {
+		meta, err := jsonquery.New(node).ObjectOptional("meta")
+		if err != nil {
+			return "", err
+		}
+
+		nextCrs, err := jsonquery.New(meta).StringOptional("next")
+		if err != nil {
+			return "", err
+		}
+
+		if nextCrs == nil {
+			return "", err
+		}
+
+		return *nextCrs, nil
+	}
+}
---
+++ test/dixa/read/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	dx "github.com/amp-labs/connectors/providers/dixa"
+	"github.com/amp-labs/connectors/test/dixa"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := dixa.GetConnector(ctx)
+
+	if err := testRead(ctx, conn, "agents", []string{"id", "createdAt", "displayName"}); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := testRead(ctx, conn, "business-hours/schedules", []string{"id", "name"}); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := testRead(ctx, conn, "queues", []string{"name", "slaCalculationMethod", "id"}); err != nil {
+		slog.Error(err.Error())
+	}
+}
+
+func testRead(ctx context.Context, conn *dx.Connector, objectName string, fields []string) error {
+	params := common.ReadParams{
+		ObjectName: objectName,
+		Fields:     connectors.Fields(fields...),
+		// NextPage:   "/v1/agents?pageKey=ZnJvbT1leUpwWkNJNklqaGhNVEpqTkdFM0xURTVZVEl0TkRKak9TMDVaV00yTFRKaU5UazVOamt6WXpZNU5pSXNJbTl5WjE5cFpDSTZJamMxT1dJM056WXhMV1U1TVRJdE5Ea3hZeTFpWmpZd0xURTBNR1V6WWpFelpqSXlaaUlzSW5WelpYSmZkSGx3WlNJNkltMWxiV0psY2lKOSZwYWdlTGltaXQ9MSZkaXI9Zm9yd2FyZA%3D%3D",
+	}
+
+	res, err := conn.Read(ctx, params)
+	if err != nil {
+		return fmt.Errorf("failed to read %s: %w", objectName, err)
+	}
+
+	jsonStr, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	if _, err := os.Stdout.Write(jsonStr); err != nil {
+		return fmt.Errorf("error writing to stdout: %w", err)
+	}
+
+	if _, err := os.Stdout.WriteString("\n"); err != nil {
+		return fmt.Errorf("error writing to stdout: %w", err)
+	}
+
+	return nil
+}
