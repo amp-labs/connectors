d1a60132f37a9d5a892c9caddfbf8b55b4231301
Joseph Karage <josephbkarage@gmail.com>
2025-04-10 15:02:54 +0300

    [CON-1453] feat: Add MixMax Read Connector (#1472)
    * feat: Add MixMax Read Connnector
    
    * Address Review
    
    * update the records function

--- providers/mixmax/supports.go
+++ providers/mixmax/supports.go
@@ -1,10 +1,39 @@
 package mixmax
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/staticschema"
+)
+
 func responseField(objectName string) string {
 	switch objectName {
 	case "appointmentlinks/me", "userpreferences/me", "users/me":
 		return "" // indicates we're reading data fields from the root level.
 	default:
 		return "results"
+	}
+}
+
+func supportedOperations() components.EndpointRegistryInput {
+	// The objecst salesforce/* are not supported as they have not been tested.
+	readSupport := []string{
+		"appointmentlinks/me", "userpreferences/me", "users/me",
+		"codesnippets", "filerequests", "insightsreports", "integrations/commands",
+		"integrations/enhancements", "integrations/linkresolvers", "integrations/sidebars",
+		"livefeed", // livefeed has another field object with stats field
+		"meetinginvites", "meetingtypes", "messages", "polls", "qa", "rules", "sequences",
+		"sequencefolders", "snippets", "snippettags", "teams", "unsubscribes", "yesno",
+	}
+
+	return components.EndpointRegistryInput{
+		staticschema.RootModuleID: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
 	}
 }
