1aa7d502a73865f4a4028be6fb286ac28b33da76
Joseph Karage <josephbkarage@gmail.com>
2025-04-11 15:07:32 +0300

    [1470] feat: Add Drift Write Connector (#1502)
    * feat: Add Drift Read Connector
    
    * feat: Add Drift Write Connector
    
    * update test func name
    
    * Address Review

--- providers/drift/connector.go
+++ providers/drift/connector.go
@@ -8,6 +8,7 @@ import (
 	"github.com/amp-labs/connectors/internal/components/operations"
 	"github.com/amp-labs/connectors/internal/components/reader"
 	"github.com/amp-labs/connectors/internal/components/schema"
+	"github.com/amp-labs/connectors/internal/components/writer"
 	"github.com/amp-labs/connectors/internal/staticschema"
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/tools/fileconv"
@@ -35,6 +36,7 @@ type Connector struct {
 	// Supported operations
 	components.SchemaProvider
 	components.Reader
+	components.Writer
 }
 
 func NewConnector(params common.Parameters) (*Connector, error) {
@@ -60,6 +62,17 @@ func constructor(base *components.Connector) (*Connector, error) {
 		operations.ReadHandlers{
 			BuildRequest:  connector.buildReadRequest,
 			ParseResponse: connector.parseReadResponse,
+			ErrorHandler:  common.InterpretError,
+		},
+	)
+
+	connector.Writer = writer.NewHTTPWriter(
+		connector.HTTPClient().Client,
+		registry,
+		common.ModuleRoot,
+		operations.WriteHandlers{
+			BuildRequest:  connector.buildWriteRequest,
+			ParseResponse: connector.parseWriteResponse,
 			ErrorHandler:  common.InterpretError,
 		},
 	)
---
+++ test/drift/write/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/amp-labs/connectors/common"
+	dr "github.com/amp-labs/connectors/providers/drift"
+	"github.com/amp-labs/connectors/test/drift"
+	"github.com/brianvoe/gofakeit/v6"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error(err.Error())
+	}
+}
+
+func run() error {
+	ctx := context.Background()
+	conn := drift.GetConnector(ctx)
+
+	err := testCreatingContacts(ctx, conn)
+	if err != nil {
+		return err
+	}
+
+	err = testUpdateContacts(ctx, conn)
+	if err != nil {
+		return err
+	}
+
+	err = testCreatingNewConversation(ctx, conn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func testCreatingContacts(ctx context.Context, conn *dr.Connector) error {
+	params := common.WriteParams{
+		ObjectName: "contacts",
+		RecordData: map[string]any{
+			"attributes": map[string]any{
+				"email": gofakeit.Email(),
+			},
+		},
+	}
+
+	res, err := conn.Write(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
+
+func testUpdateContacts(ctx context.Context, conn *dr.Connector) error {
+	params := common.WriteParams{
+		ObjectName: "contacts",
+		RecordId:   "24691647847",
+		RecordData: map[string]any{
+			"attributes": map[string]any{
+				"externalId": gofakeit.RandomString([]string{"abcdeghijklmno"}),
+			},
+		},
+	}
+
+	res, err := conn.Write(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
+
+func testCreatingNewConversation(ctx context.Context, conn *dr.Connector) error {
+	params := common.WriteParams{
+		ObjectName: "conversations/new",
+		RecordData: map[string]any{
+			"email": "josephkarage@email.com",
+			"message": map[string]any{
+				"body": "A conversation was started <a href='www.withampersand.com'>here</a>, let's resume from drift!",
+				"attributes": map[string]any{
+					"integrationSource": "Message from facebook",
+				},
+			},
+		},
+	}
+
+	res, err := conn.Write(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
