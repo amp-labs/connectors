c8d92eb7bdfca7e927d2a2d3a92de3bcd9aacf86
Joseph Karage <josephbkarage@gmail.com>
2025-04-11 14:37:36 +0300

    [CON-1469] feat: Add Drift Read Connector (#1498)
    * feat: Add Drift Metadata Connector
    
    * feat: Add Drift Read Connector
    
    * clean, updates

---
+++ providers/drift/handlers.go
@@ -0,0 +1,45 @@
+package drift
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+)
+
+func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
+	var (
+		url *urlbuilder.URL
+		err error
+	)
+
+	url, err = urlbuilder.New(c.ProviderInfo().BaseURL, params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	if params.NextPage != "" {
+		url, err = urlbuilder.New(params.NextPage.String())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+}
+
+func (c *Connector) parseReadResponse(
+	ctx context.Context,
+	params common.ReadParams,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.ReadResult, error) {
+	return common.ParseResult(
+		response,
+		records(params.ObjectName),
+		nextRecordsURL(),
+		common.GetMarshaledData,
+		params.Fields,
+	)
+}
---
+++ test/drift/read/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	dr "github.com/amp-labs/connectors/providers/drift"
+	"github.com/amp-labs/connectors/test/drift"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := drift.GetConnector(ctx)
+
+	if err := testRead(ctx, conn, "users/list", []string{"id", "name", "email"}); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := testRead(ctx, conn, "conversations/stats", []string{"conversationCount"}); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := testRead(ctx, conn, "scim/Users", []string{"Resources", "schemas", "id"}); err != nil {
+		slog.Error(err.Error())
+	}
+}
+
+func testRead(ctx context.Context, conn *dr.Connector, objectName string, fields []string) error {
+	params := common.ReadParams{
+		ObjectName: objectName,
+		Fields:     connectors.Fields(fields...),
+	}
+
+	res, err := conn.Read(ctx, params)
+	if err != nil {
+		return fmt.Errorf("failed to read %s: %w", objectName, err)
+	}
+
+	jsonStr, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	if _, err := os.Stdout.Write(jsonStr); err != nil {
+		return fmt.Errorf("error writing to stdout: %w", err)
+	}
+
+	if _, err := os.Stdout.WriteString("\n"); err != nil {
+		return fmt.Errorf("error writing to stdout: %w", err)
+	}
+
+	return nil
+}
