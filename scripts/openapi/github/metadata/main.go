e5971a5d08adb1800c230414a666ab7840072374
Dipu <dipuchaurasiya91@gmail.com>
2025-04-15 16:28:27 +0545

    feat: add new user-related endpoints to OpenAPI metadata

--- scripts/openapi/github/metadata/main.go
+++ scripts/openapi/github/metadata/main.go
@@ -49,6 +49,12 @@ var (
 		"/gists/starred":                     "gists/starred",
 		"/user/starred":                      "user/starred",
 		"/gists/public":                      "gists/public",
+		"/user/keys":                         "user/keys",
+		"/user/gpg_keys":                     "user/gpg_keys",
+		"/user/emails":                       "user/emails",
+		"/user/ssh_signing_keys":             "user/ssh_signing_keys",
+		"/user/social_accounts":              "user/social_accounts",
+		"/user/codespaces":                   "user/codespaces",
 	}
 
 	overrideDisplayName = map[string]string{ //nolint:gochecknoglobals
