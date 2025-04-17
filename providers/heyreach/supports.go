91ebcc186d42cd4454e7b38ea34a879ffa8a5b96
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-04-17 15:39:24 +0530

    fix: Changed the variable

--- providers/heyreach/supports.go
+++ providers/heyreach/supports.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/components"
-	"github.com/amp-labs/connectors/internal/staticschema"
 	"github.com/amp-labs/connectors/providers/heyreach/metadata"
 )
 
 func supportedOperations() components.EndpointRegistryInput {
-	readSupport := metadata.Schemas.ObjectNames().GetList(staticschema.RootModuleID)
+	readSupport := metadata.Schemas.ObjectNames().GetList(common.ModuleRoot)
 
 	return components.EndpointRegistryInput{
-		staticschema.RootModuleID: {
+		common.ModuleRoot: {
 			{
 				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
 				Support:  components.ReadSupport,
