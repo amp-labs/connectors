049020512082289263f8af4d97198a92726dc96d
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-04-17 16:03:14 +0530

    [CON-1414] feat: Added write functionality in the heyreach connector. (#1557)
    * [CON-1412] feat: Added heyreach connector metadata
    
    * fix: use internal/components for heyreach metadata functionality.
    
    * chore: changed the testfile into test folder and modified the testcases
    
    * [CON-1413] feat: Added read functionality in the heyreach connector.
    
    * fix: resolved the linter issue
    
    * fix: resolved the linter issue
    
    * fix: solved lint issue and worked on the PR comments
    
    * chore: updated Readme file, trimmed some data in schema file and changed the object names
    
    * chore: Modified the script, test cases and updated the readme file.
    
    * fix: resolved linter issue
    
    * [CON-1414] feat: Added write functionality in the heyreach connector.
    
    * fix: Resolved the PR comments
    
    * fix: solved the PR comments
    
    * fix: corrected the space issue

--- providers/heyreach/supports.go
+++ providers/heyreach/supports.go
@@ -12,11 +12,22 @@ import (
 func supportedOperations() components.EndpointRegistryInput {
 	readSupport := metadata.Schemas.ObjectNames().GetList(common.ModuleRoot)
 
+	supportWrite := []string{
+		"list/CreateEmptyList",
+		"campaign/AddLeadsToCampaignV2",
+		"list/AddLeadsToListV2",
+		"inbox/SendMessage",
+	}
+
 	return components.EndpointRegistryInput{
 		common.ModuleRoot: {
 			{
 				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
 				Support:  components.ReadSupport,
+			},
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(supportWrite, ",")),
+				Support:  components.WriteSupport,
 			},
 		},
 	}
