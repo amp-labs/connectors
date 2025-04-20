8499d50d095d1d14d71b00f09c67dc0b6dfb7868
Dipu <dipuchaurasiya91@gmail.com>
2025-04-20 11:30:13 +0545

    feat(ashby): implement candidate creation and update application function to use candidate ID

--- test/ashby/write/main.go
+++ test/ashby/write/main.go
@@ -24,19 +24,27 @@ func main() {
 
 	conn := hs.GetAshbyConnector(ctx)
 
+	slog.Info("> TEST Create Candidate")
+
+	recordId, err := createCandidate(ctx, conn)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
 	slog.Info("> TEST Create Application")
 
-	if err := createApplication(ctx, conn); err != nil {
+	if err := createApplication(ctx, conn, recordId); err != nil {
 		slog.Error(err.Error())
 	}
 	slog.Info("Done")
+
 }
 
-func createApplication(ctx context.Context, conn *ashby.Connector) error {
+func createApplication(ctx context.Context, conn *ashby.Connector, candidateId string) error {
 	config := common.WriteParams{
 		ObjectName: "application",
 		RecordData: map[string]any{
-			"candidateId": "bd7229a1-be3e-4e30-a538-0b95a41602d7",
+			"candidateId": candidateId,
 			"jobId":       "783338ea-e6ac-406a-853b-964fa75a5d62",
 		},
 	}
@@ -55,4 +63,27 @@ func createApplication(ctx context.Context, conn *ashby.Connector) error {
 	fmt.Println(string(jsonStr))
 
 	return nil
+}
+
+func createCandidate(ctx context.Context, conn *ashby.Connector) (string, error) {
+	config := common.WriteParams{
+		ObjectName: "candidate",
+		RecordData: map[string]any{
+			"name": "Deepu",
+		},
+	}
+	result, err := conn.Write(ctx, config)
+	if err != nil {
+		return "", err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println(string(jsonStr))
+
+	return result.RecordId, nil
 }
