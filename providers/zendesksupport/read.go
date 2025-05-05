6210a2f82d75b98290a50f2498df75f1ed5ae2e2
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-05-05 17:57:52 +0300

    [CON-1619] feat(modules): Connectors read moduleInfo (#1523)
    * Atlassian
    * Hubspot
    * Keap
    * Klaviyo
    * Marketo
    * Zendesk
    * Zoom

--- providers/zendesksupport/read.go
+++ providers/zendesksupport/read.go
@@ -15,7 +15,7 @@ func (c *Connector) Read(ctx context.Context, config common.ReadParams) (*common
 		return nil, err
 	}
 
-	if !supportedObjectsByRead[c.Module.ID].Has(config.ObjectName) {
+	if !supportedObjectsByRead[c.moduleID].Has(config.ObjectName) {
 		return nil, common.ErrOperationNotSupportedForObject
 	}
 
@@ -36,7 +36,7 @@ func (c *Connector) Read(ctx context.Context, config common.ReadParams) (*common
 
 	return common.ParseResult(
 		rsp,
-		getRecords(c.Module.ID, config.ObjectName),
+		getRecords(c.moduleID, config.ObjectName),
 		getNextRecordsURL,
 		common.MakeMarshaledDataFunc(c.attachReadCustomFields(customFields)),
 		config.Fields,
@@ -55,8 +55,8 @@ func (c *Connector) buildReadURL(config common.ReadParams) (*urlbuilder.URL, err
 		return nil, err
 	}
 
-	pageSizeStr := metadata.Schemas.PageSize(c.Module.ID, config.ObjectName)
-	isIncremental := metadata.Schemas.IsIncrementalRead(c.Module.ID, config.ObjectName)
+	pageSizeStr := metadata.Schemas.PageSize(c.moduleID, config.ObjectName)
+	isIncremental := metadata.Schemas.IsIncrementalRead(c.moduleID, config.ObjectName)
 
 	if isIncremental {
 		// Incremental endpoints requires start query parameter.
@@ -68,7 +68,7 @@ func (c *Connector) buildReadURL(config common.ReadParams) (*urlbuilder.URL, err
 	} else {
 		// Different objects have different pagination types.
 		// https://developer.zendesk.com/api-reference/introduction/pagination/#using-offset-pagination
-		ptype := metadata.Schemas.LookupPaginationType(c.Module.ID, config.ObjectName)
+		ptype := metadata.Schemas.LookupPaginationType(c.moduleID, config.ObjectName)
 		if ptype == "cursor" {
 			url.WithQueryParam("page[size]", pageSizeStr)
 		}
