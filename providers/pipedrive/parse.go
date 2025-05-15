0df46e9a18a229e09b1699caf4fb7038ecf9e57a
Jatin Sandilya <7681067+jatinsandilya@users.noreply.github.com>
2025-05-15 15:52:18 -0600

    fix: nil pointer de-reference (#1657)
    * fix: nil pointer de-reference

--- providers/pipedrive/parse.go
+++ providers/pipedrive/parse.go
@@ -40,7 +40,7 @@ func nextRecordsURL(url *urlbuilder.URL) common.NextPageFunc {
 			return "", err
 		}
 
-		if *more {
+		if more != nil && *more && startValue != nil {
 			url.WithQueryParam("start", strconv.FormatInt(*startValue, 10))
 
 			return url.String(), nil
