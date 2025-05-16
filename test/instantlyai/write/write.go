d33683779455884e822c1f503015fb4c0962e1f0
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-05-16 11:51:49 +0530

    [CON-1604] feat: Added write functionality for the instantly V2 connector (#1643)
    * [CON-1602] feat: Instantly V2 connector metadata
    
    * fix: resolved the linter issue
    
    * fix: resolved the linter issue
    
    * [CON-1603] feat: Added read functionality for instantly v2 connector
    
    * [CON-1604] feat: Added write functionality for the instantly V2 connector
    
    * fix: Resolved he linter issue
    
    * fix: Removed the nolint comment
    
    * fix: solved the linter issue
    
    * chore: added the delete functionality for instantly V2 connector
    
    * chore: updated the read script in the test folder
    
    * chore: supported some object for the instantly v2 connector
    
    * chore: changed the lead objectname
    
    * chore: implemented the script to handle direct response endpoint
    
    * fix: Resolved the linter issue
    
    * fix: solved the linter issue
    
    * chore: Supports some write object in the instantly V2 connector
    
    * chore: Remove api-keys object and updated the readme and testcases
    
    * chore: updated the supported objects
    
    * chore: Supported incremental read, remove api-keys object and modfiied the code.
    
    * chore: corrected the test case
    
    * chore: worked on the PR comments

---
+++ test/instantlyai/write/write.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/amp-labs/connectors/common"
+	ap "github.com/amp-labs/connectors/providers/instantlyai"
+	"github.com/amp-labs/connectors/test/instantlyai"
+)
+
+func main() {
+	os.Exit(MainFn())
+}
+
+func MainFn() int {
+	ctx := context.Background()
+
+	err := testLeadLabel(ctx)
+	if err != nil {
+		return 1
+	}
+
+	err = testCustomTags(ctx)
+	if err != nil {
+		return 1
+	}
+
+	err = testLeadLists(ctx)
+	if err != nil {
+		return 1
+	}
+
+	return 0
+}
+
+func testLeadLabel(ctx context.Context) error {
+	conn := instantlyai.GetInstantlyAIConnector(ctx)
+
+	slog.Info("Creating the lead labels")
+
+	writeParams := common.WriteParams{
+		ObjectName: "lead-labels",
+		RecordData: map[string]any{
+			"label":                 "Hot Lead",
+			"interest_status_label": "positive",
+		},
+		RecordId: "",
+	}
+
+	writeRes, err := Write(ctx, conn, writeParams)
+	if err != nil {
+		fmt.Println("ERR: ", err)
+
+		return err
+	}
+
+	if err := constructResponse(writeRes); err != nil {
+		return err
+	}
+
+	slog.Info("Updating the lead labels")
+
+	updateParams := common.WriteParams{
+		ObjectName: "lead-labels",
+		RecordData: map[string]any{
+			"label": "Lead",
+		},
+		RecordId: writeRes.RecordId,
+	}
+
+	res, err := Write(ctx, conn, updateParams)
+	if err != nil {
+		fmt.Println("ERR: ", err)
+
+		return err
+	}
+
+	if err := constructResponse(res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func testCustomTags(ctx context.Context) error {
+	conn := instantlyai.GetInstantlyAIConnector(ctx)
+
+	slog.Info("Creating the custom tags")
+
+	writeParams := common.WriteParams{
+		ObjectName: "custom-tags",
+		RecordData: map[string]any{
+			"label": "Important",
+		},
+		RecordId: "",
+	}
+
+	writeRes, err := Write(ctx, conn, writeParams)
+	if err != nil {
+		fmt.Println("ERR: ", err)
+
+		return err
+	}
+
+	if err := constructResponse(writeRes); err != nil {
+		return err
+	}
+
+	slog.Info("Updating the custom tags")
+
+	updateParams := common.WriteParams{
+		ObjectName: "custom-tags",
+		RecordData: map[string]any{
+			"label": "Demo",
+		},
+		RecordId: writeRes.RecordId,
+	}
+
+	res, err := Write(ctx, conn, updateParams)
+	if err != nil {
+		fmt.Println("ERR: ", err)
+
+		return err
+	}
+
+	if err := constructResponse(res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func testLeadLists(ctx context.Context) error {
+	conn := instantlyai.GetInstantlyAIConnector(ctx)
+
+	slog.Info("Creating the lead lists")
+
+	writeParams := common.WriteParams{
+		ObjectName: "lead-lists",
+		RecordData: map[string]any{
+			"name": "Lead List",
+		},
+		RecordId: "",
+	}
+
+	writeRes, err := Write(ctx, conn, writeParams)
+	if err != nil {
+		fmt.Println("ERR: ", err)
+
+		return err
+	}
+
+	if err := constructResponse(writeRes); err != nil {
+		return err
+	}
+
+	slog.Info("Updating the lead lists")
+
+	updateParams := common.WriteParams{
+		ObjectName: "lead-lists",
+		RecordData: map[string]any{
+			"name": "Demo",
+		},
+		RecordId: writeRes.RecordId,
+	}
+
+	res, err := Write(ctx, conn, updateParams)
+	if err != nil {
+		fmt.Println("ERR: ", err)
+
+		return err
+	}
+
+	if err := constructResponse(res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Write(ctx context.Context, conn *ap.Connector, payload common.WriteParams) (*common.WriteResult, error) {
+	res, err := conn.Write(ctx, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// unmarshal the write response.
+func constructResponse(res *common.WriteResult) error {
+	jsonStr, err := json.MarshalIndent(res, "", " ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
