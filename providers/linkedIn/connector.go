716e87ba5b7e4e8c5d49bd829b2536a7a6ad9b34
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-05-16 20:01:17 +0530

    [CON-1390] feat: Added write functionality in the LinkedIn Connector

---
+++ providers/linkedIn/connector.go
@@ -0,0 +1,52 @@
+package linkedIn
+
+import (
+	_ "embed"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/interpreter"
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/writer"
+	"github.com/amp-labs/connectors/providers"
+)
+
+type Connector struct {
+	// Basic connector
+	*components.Connector
+
+	// Require authenticated client
+	common.RequireAuthenticatedClient
+
+	// Supported operations
+	components.Writer
+}
+
+func NewConnector(params common.Parameters) (*Connector, error) {
+	// Create base connector with provider info
+	return components.Initialize(providers.LinkedIn, params, constructor)
+}
+
+func constructor(base *components.Connector) (*Connector, error) {
+	connector := &Connector{Connector: base}
+
+	registry, err := components.NewEndpointRegistry(supportedOperations())
+	if err != nil {
+		return nil, err
+	}
+
+	connector.Writer = writer.NewHTTPWriter(
+		connector.HTTPClient().Client,
+		registry,
+		connector.ProviderContext.Module(),
+		operations.WriteHandlers{
+			BuildRequest:  connector.buildWriteRequest,
+			ParseResponse: connector.parseWriteResponse,
+			ErrorHandler: interpreter.ErrorHandler{
+				JSON: interpreter.NewFaultyResponder(errorFormats, nil),
+			}.Handle,
+		},
+	)
+
+	return connector, nil
+}
---
+++ providers/linkedIn/write_test.go
@@ -0,0 +1,93 @@
+package linkedIn
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockcond"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+)
+
+func TestWrite(t *testing.T) { // nolint:funlen,gocognit,cyclop,maintidx
+	t.Parallel()
+
+	tests := []testroutines.Write{
+		{
+			Name:         "Write object must be included",
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:  "creating the adAccounts",
+			Input: common.WriteParams{ObjectName: "adAccounts", RecordData: "dummy"},
+			Server: mockserver.Conditional{
+				Setup: mockserver.ContentJSON(),
+				If: mockcond.And{
+					mockcond.PathSuffix("/rest/adAccounts"),
+					mockcond.Header(http.Header{
+						"LinkedIn-Version":          []string{"202504"},
+						"X-Restli-Protocol-Version": []string{"2.0.0"},
+					}),
+					mockcond.MethodPOST(),
+				},
+				Then: mockserver.Response(http.StatusOK, nil),
+			}.Server(),
+			Expected: &common.WriteResult{
+				Success: true,
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name: "Update adAccounts as POST",
+			Input: common.WriteParams{
+				ObjectName: "adAccounts",
+				RecordId:   "514674276",
+				RecordData: "dummy",
+			},
+			Server: mockserver.Conditional{
+				Setup: mockserver.ContentJSON(),
+				If: mockcond.And{
+					mockcond.PathSuffix("/rest/adAccounts/514674276"),
+					mockcond.Header(http.Header{
+						"LinkedIn-Version":          []string{"202504"},
+						"X-Restli-Protocol-Version": []string{"2.0.0"},
+						"X-Restli-Method":           []string{"PARTIAL_UPDATE"},
+					}),
+					mockcond.MethodPOST(),
+				},
+				Then: mockserver.Response(http.StatusOK, nil),
+			}.Server(),
+			Expected: &common.WriteResult{
+				Success: true,
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.WriteConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
+
+func constructTestConnector(serverURL string) (*Connector, error) {
+	connector, err := NewConnector(common.Parameters{
+		AuthenticatedClient: http.DefaultClient,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// for testing we want to redirect calls to our mock server
+	connector.SetBaseURL(serverURL)
+
+	return connector, nil
+}
