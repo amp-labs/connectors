7be5c33faaf4b14dd23baa87bac3a2fd0ad73f91
Cobalt0s <constantin.y.koval@gmail.com>
2025-05-17 03:27:59 +0300

    [CON-1682] feat(salesforce): AccountEngagement - Read

--- providers/salesforce/internal/pardot/adapter.go
+++ providers/salesforce/internal/pardot/adapter.go
@@ -1,20 +1,37 @@
 package pardot
 
 import (
+	"errors"
+
 	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
 	"github.com/amp-labs/connectors/providers"
 )
 
 type Adapter struct {
-	Client  *common.JSONHTTPClient
-	BaseURL string
+	Client         *common.JSONHTTPClient
+	BaseURL        string
+	BusinessUnitID string
 }
+
+var ErrMissingBusinessUnitID = errors.New("missing metadata variable: business unit id")
 
 func NewAdapter(
 	client *common.JSONHTTPClient, info *providers.ModuleInfo, metadata map[string]string,
 ) (*Adapter, error) {
+
+	businessUnitID, ok := metadata["businessUnitId"]
+	if !ok || businessUnitID == "" {
+		return nil, ErrMissingBusinessUnitID
+	}
+
 	return &Adapter{
-		Client:  client,
-		BaseURL: info.BaseURL,
+		Client:         client,
+		BaseURL:        info.BaseURL,
+		BusinessUnitID: businessUnitID,
 	}, nil
+}
+
+func (a *Adapter) getURL(objectName string) (*urlbuilder.URL, error) {
+	return urlbuilder.New(a.BaseURL, "api/v5/objects", objectName)
 }
