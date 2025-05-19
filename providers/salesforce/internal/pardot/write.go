7f51000186e36b5cdf11de17461d98ba79b5c627
Cobalt0s <constantin.y.koval@gmail.com>
2025-05-19 23:21:58 +0300

    [CON-1682] feat(salesforce): AccountEngagement - Write

--- providers/salesforce/internal/pardot/write.go
+++ providers/salesforce/internal/pardot/write.go
@@ -1,12 +1,70 @@
 package pardot
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
 
 	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/jsonquery"
 )
 
-func (a *Adapter) Write(ctx context.Context, params common.WriteParams) (*common.WriteResult, error) {
-	// TODO needs implementation.
-	return nil, common.ErrNotImplemented
+func (a *Adapter) buildWriteRequest(ctx context.Context, params common.WriteParams) (*http.Request, error) {
+	objectNameLower := strings.ToLower(params.ObjectName)
+
+	url, err := a.getURL(objectNameLower)
+	if err != nil {
+		return nil, err
+	}
+
+	method := http.MethodPost
+	if params.IsUpdate() {
+		method = http.MethodPatch
+		url.AddPath(params.RecordId)
+	}
+
+	jsonData, err := json.Marshal(params.RecordData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal record data: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, url.String(), bytes.NewReader(jsonData))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.Header.Add("Pardot-Business-Unit-Id", a.businessUnitID)
+
+	return req, nil
+}
+
+func (a *Adapter) parseWriteResponse(ctx context.Context, params common.WriteParams,
+	request *http.Request, response *common.JSONHTTPResponse,
+) (*common.WriteResult, error) {
+	body, ok := response.Body()
+	if !ok {
+		return &common.WriteResult{
+			Success: true,
+		}, nil
+	}
+
+	recordID, err := jsonquery.New(body).TextWithDefault("id", "")
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := jsonquery.Convertor.ObjectToMap(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.WriteResult{
+		Success:  true,
+		RecordId: recordID,
+		Errors:   nil,
+		Data:     data,
+	}, nil
 }
