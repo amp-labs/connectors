936e2f711efb7a433aed25e4d4db7f1ad5f99f56
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-05-23 11:04:26 +0530

    fix: changed the variable name

--- providers/campaignmonitor/utils.go
+++ providers/campaignmonitor/utils.go
@@ -2,7 +2,7 @@ package campaignmonitor
 
 import "github.com/amp-labs/connectors/internal/datautils"
 
-var JsonExtensionEndpoint = datautils.NewSet( //nolint:gochecknoglobals
+var JSONExtensionEndpoint = datautils.NewSet( //nolint:gochecknoglobals
 	"clients",
 	"admins",
 )
