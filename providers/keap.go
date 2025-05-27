59337fae2102f8c10ebaa5f241cfdf19b1ce5606
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-05-28 00:23:33 +0300

    [CON-190] Enable read+write: Keap (#1220)

--- providers/keap.go
+++ providers/keap.go
@@ -34,9 +34,9 @@ func init() {
 				Delete: false,
 			},
 			Proxy:     true,
-			Read:      false,
+			Read:      true,
 			Subscribe: false,
-			Write:     false,
+			Write:     true,
 		},
 		DefaultModule: ModuleKeapV1,
 		Modules: &Modules{
