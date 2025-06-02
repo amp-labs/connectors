5e5ca46b966b4a569f30723cbe4f34617422d726
Cobalt0s <constantin.y.koval@gmail.com>
2025-06-02 17:51:27 +0300

    whitelist business unit id

--- providers/salesforce.go
+++ providers/salesforce.go
@@ -123,6 +123,15 @@ func init() { // nolint:funlen
 					DisplayName: "Subdomain",
 					DocsURL:     "https://help.salesforce.com/s/articleView?language=en_US&id=sf.faq_domain_name_what.htm&type=5",
 				},
+				{
+					Name:        "businessUnitId",
+					DisplayName: "Business Unit ID",
+					DocsURL:     "https://help.salesforce.com/s/articleView?id=000381973&type=1",
+					ModuleDependencies: &ModuleDependencies{
+						ModuleSalesforceAccountEngagement:     {},
+						ModuleSalesforceAccountEngagementDemo: {},
+					},
+				},
 			},
 		},
 	})
