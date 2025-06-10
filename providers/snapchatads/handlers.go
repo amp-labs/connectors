5ae3f65c7a15af163274564d450ef8f32ca7750a
sanjay kanth a <sanjay.kanth@roundcircle.tech>
2025-06-10 17:53:30 +0530

    [CON-1404] feat: Added the metadata functionality for snapchatads connector.

---
+++ providers/snapchatads/handlers.go
@@ -0,0 +1,61 @@
+package snapchatads
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/naming"
+	"github.com/amp-labs/connectors/internal/jsonquery"
+)
+
+func (c *Connector) buildSingleObjectMetadataRequest(ctx context.Context, objectName string) (*http.Request, error) {
+	url, err := c.constructURL(objectName)
+	if err != nil {
+		return nil, err
+	}
+
+	return http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+}
+
+func (c *Connector) parseSingleObjectMetadataResponse(
+	ctx context.Context,
+	objectName string,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.ObjectMetadata, error) {
+	objectMetadata := common.ObjectMetadata{
+		FieldsMap:   make(map[string]string),
+		DisplayName: naming.CapitalizeFirstLetterEveryWord(objectName),
+	}
+
+	node, ok := response.Body()
+	if !ok {
+		return nil, common.ErrEmptyJSONHTTPResponse
+	}
+
+	objectResponse, err := jsonquery.New(node).ArrayRequired(objectName)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := jsonquery.Convertor.ArrayToMap(objectResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	objKey := naming.NewSingularString(objectName).String()
+
+	// Extract and assert the inner map
+	innerData, ok := data[0][objKey].(map[string]any)
+	if !ok {
+		return nil, fmt.Errorf("expected map structure under key %q in response", objKey)
+	}
+
+	for field := range innerData {
+		objectMetadata.FieldsMap[field] = field
+	}
+
+	return &objectMetadata, nil
+}
