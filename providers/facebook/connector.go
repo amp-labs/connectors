c708bdcf4219b06479a43e72801d74781f1d3d89
sanjay kanth a <sanjay.kanth@roundcircle.tech>
2025-06-11 20:18:46 +0530

    [CON-1380] feat: Added the metadata functionality for the facebook connector.

---
+++ providers/facebook/connector.go
@@ -0,0 +1,59 @@
+package facebook
+
+import (
+	_ "embed"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/schema"
+	"github.com/amp-labs/connectors/providers"
+)
+
+type Connector struct {
+	// Basic connector
+	*components.Connector
+
+	// Require authenticated client
+	common.RequireAuthenticatedClient
+	common.RequireMetadata
+	// Supported operations
+	components.SchemaProvider
+
+	adAccountId string
+	businessId  string
+}
+
+const (
+	metadataKeyAdAccountID = "adAccountId"
+	metadataKeyBusinessID  = "businessId"
+)
+
+func NewConnector(params common.ConnectorParams) (*Connector, error) {
+	// Create base connector with provider info
+	conn, err := components.Initialize(providers.Facebook, params, constructor)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.adAccountId = params.Metadata[metadataKeyAdAccountID]
+	conn.businessId = params.Metadata[metadataKeyBusinessID]
+
+	return conn, nil
+}
+
+func constructor(base *components.Connector) (*Connector, error) {
+	connector := &Connector{Connector: base}
+
+	// Set the metadata provider for the connector
+	connector.SchemaProvider = schema.NewObjectSchemaProvider(
+		connector.HTTPClient().Client,
+		schema.FetchModeParallel,
+		operations.SingleObjectMetadataHandlers{
+			BuildRequest:  connector.buildSingleObjectMetadataRequest,
+			ParseResponse: connector.parseSingleObjectMetadataResponse,
+		},
+	)
+
+	return connector, nil
+}
