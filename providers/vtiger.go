b612c21406b6190f29e17f01c3420e234ebf3e98
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-06-12 15:57:20 +0530

    fix: Changed the dcos url

--- providers/vtiger.go
+++ providers/vtiger.go
@@ -8,9 +8,8 @@ func init() {
 		AuthType:    Basic,
 		// Every vtiger customer can have a full custom domain.
 		BaseURL: "https://{{.workspace}}/restapi",
-		//nolint:lll
 		BasicOpts: &BasicAuthOpts{
-			DocsURL: "https://help.vtiger.com/faq/140159403-What-is-Access-Key#:~:text=Access%20Key%20is%20a%20unique,key%20under%20Settings%20%3E%20My%20Preferences.",
+			DocsURL: "https://help.vtiger.com/faq/140159403-What-is-Access-Key",
 		},
 		Media: &Media{
 			DarkMode: &MediaTypeDarkMode{
