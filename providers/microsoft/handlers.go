597f58e8e80f0deec381180be0daf45f1a57a122
Cobalt0s <constantin.y.koval@gmail.com>
2025-06-16 23:54:30 +0300

    [CON-1803] feat(microsoft): Read

---
+++ providers/microsoft/handlers.go
@@ -0,0 +1,58 @@
+package microsoft
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/jsonquery"
+	"github.com/spyzhov/ajson"
+)
+
+const DefaultPageSize = "100"
+
+func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
+	url, err := c.buildReadURL(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+}
+
+func (c *Connector) buildReadURL(params common.ReadParams) (*urlbuilder.URL, error) {
+	if len(params.NextPage) != 0 {
+		// Next page
+		return urlbuilder.New(params.NextPage.String())
+	}
+
+	// First page
+	url, err := c.getURL(params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	url.WithQueryParam("$top", DefaultPageSize)
+
+	return url, nil
+}
+
+func (c *Connector) parseReadResponse(
+	ctx context.Context,
+	params common.ReadParams,
+	request *http.Request,
+	resp *common.JSONHTTPResponse,
+) (*common.ReadResult, error) {
+	return common.ParseResult(
+		resp,
+		common.ExtractOptionalRecordsFromPath("value"),
+		getNextRecordsURL,
+		common.GetMarshaledData,
+		params.Fields,
+	)
+}
+
+func getNextRecordsURL(node *ajson.Node) (string, error) {
+	return jsonquery.New(node).StrWithDefault("@odata.nextLink", "")
+}
