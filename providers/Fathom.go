6373e679e8ecbc1600fa92ba95bc12390070322f
Dipu <103568666+immdipu@users.noreply.github.com>
2025-06-18 11:58:35 +0545

    feat: add Fathom connector (#1757)

---
+++ providers/Fathom.go
@@ -0,0 +1,40 @@
+package providers
+
+const Fathom Provider = "fathom"
+
+func init() {
+	SetInfo(Fathom, ProviderInfo{
+		DisplayName: "Fathom",
+		AuthType:    ApiKey,
+		BaseURL:     "https://api.fathom.ai",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Header,
+			Header: &ApiKeyOptsHeader{
+				Name: "X-Api-Key",
+			},
+			DocsURL: "https://docs.fathom.ai",
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1749723319/media/fathom.video_1749723318.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1749723372/media/fathom.video_1749723371.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1749723319/media/fathom.video_1749723318.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1749723350/media/fathom.video_1749723350.svg",
+			},
+		},
+	})
+}
