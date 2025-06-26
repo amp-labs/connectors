6558647f329b070dc40c688994d0a5b069bbbfb4
Cobalt0s <constantin.y.koval@gmail.com>
2025-06-26 21:43:24 +0300

    [CON-1950] feat(confluence): ListObjectMetadata

---
+++ providers/atlassian/internal/confluence/adapter.go
@@ -0,0 +1,32 @@
+package confluence
+
+import (
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/schema"
+	"github.com/amp-labs/connectors/providers"
+)
+
+// nolint:unused
+const apiVersion = "/wiki/api/v2"
+
+type Adapter struct {
+	*components.Connector
+	common.RequireAuthenticatedClient
+
+	components.SchemaProvider
+}
+
+func NewAdapter(params common.ConnectorParams) (*Adapter, error) {
+	return components.Initialize(providers.Atlassian, params, constructor)
+}
+
+func constructor(base *components.Connector) (*Adapter, error) {
+	adapter := &Adapter{
+		Connector: base,
+	}
+
+	adapter.SchemaProvider = schema.NewOpenAPISchemaProvider(adapter.ProviderContext.Module(), Schemas)
+
+	return adapter, nil
+}
