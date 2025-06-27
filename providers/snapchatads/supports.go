f26ad881e6ca7c8919bbad1d3bdb95a8582bb798
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-06-27 19:38:11 +0530

    [CON-1405] feat: Added snapchatads connector read functionality

---
+++ providers/snapchatads/supports.go
@@ -0,0 +1,29 @@
+package snapchatads
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/components"
+)
+
+func supportedOperations() components.EndpointRegistryInput {
+	readSupport := []string{
+		"fundingsources",
+		"billingcenters",
+		"transactions",
+		"adaccounts",
+		"members",
+		"roles",
+	}
+
+	return components.EndpointRegistryInput{
+		common.ModuleRoot: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
+	}
+}
