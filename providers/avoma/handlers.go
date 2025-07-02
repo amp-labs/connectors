7c220fae5f44c9348adec8dbd815c89f34a4f683
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-07-02 16:52:30 +0530

    [CON-1710] feat: Added the write functionality of the avoma connector. (#1772)
    * [CON-1707] feat: Added the metadata functionality for avoma connector
    
    * fix: Resolved the linter issue
    
    * chore: updated supported objects in readme file.
    
    * feat: Added the test cases
    
    * fixL linter issue
    
    * [CON-1709] feat: Added the read functionality for the avoma connector.
    
    * fix: Resolved the linter issue
    
    * feat: Added the test cases
    
    * fix: linter issue
    
    * fix: linter issue
    
    * fix: Modified the test cases.
    
    * fix: Modified the content in the users.json file
    
    * fix: modified the next page logic
    
    * fix: Modified the pagination logic
    
    * [CON-1710] feat: Added the write functionality of the avoma connector.
    
    * chore: updated the supported object in the readme file
    
    * fix: Added the trailing slash in the helper file
    
    * fix: linter issue
    
    * fix: Added comments and modified the read logic.
    
    * fix: linter issue
    
    * feat: Added the write test cases
    
    * feat: Added support for the update endpoints and devloped the test cases.
    
    * fix: solved the linter issue

--- providers/avoma/handlers.go
+++ providers/avoma/handlers.go
@@ -1,33 +1,39 @@
 package avoma
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"net/http"
 
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/urlbuilder"
 	"github.com/amp-labs/connectors/internal/datautils"
+	"github.com/amp-labs/connectors/internal/jsonquery"
 )
 
 func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
-	url, err := urlbuilder.New(c.ProviderInfo().BaseURL, apiVersion, params.ObjectName, "/")
+	if params.NextPage != "" {
+		url, err := urlbuilder.New(params.NextPage.String())
+		if err != nil {
+			return nil, err
+		}
+
+		return http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+	}
+
+	url, err := urlbuilder.New(c.ProviderInfo().BaseURL, apiVersion, params.ObjectName, common.WithTrailingSlash)
 	if err != nil {
 		return nil, err
 	}
 
-	if EndpointsWithResultsPath.Has(params.ObjectName) {
+	if endpointsWithResultsPath.Has(params.ObjectName) {
 		url.WithQueryParam("page_size", pageSize)
 
+		// Query parameters `from_date` and `to_date` are required for paginated objects in Avoma.
 		if !params.Since.IsZero() && !params.Until.IsZero() {
 			url.WithQueryParam("from_date", datautils.Time.FormatRFC3339inUTC(params.Since))
 			url.WithQueryParam("to_date", datautils.Time.FormatRFC3339inUTC(params.Until))
-		}
-	}
-
-	if params.NextPage != "" {
-		url, err = urlbuilder.New(params.NextPage.String())
-		if err != nil {
-			return nil, err
 		}
 	}
 
@@ -42,7 +48,7 @@ func (c *Connector) parseReadResponse(
 ) (*common.ReadResult, error) {
 	nodePath := ""
 
-	if EndpointsWithResultsPath.Has(params.ObjectName) {
+	if endpointsWithResultsPath.Has(params.ObjectName) {
 		nodePath = "results"
 	}
 
@@ -53,4 +59,78 @@ func (c *Connector) parseReadResponse(
 		common.GetMarshaledData,
 		params.Fields,
 	)
+}
+
+func (c *Connector) buildWriteRequest(ctx context.Context, params common.WriteParams) (*http.Request, error) {
+	url, err := urlbuilder.New(c.ProviderInfo().BaseURL, apiVersion, params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	method := http.MethodPost
+
+	if params.RecordId != "" {
+		url.AddPath(params.RecordId)
+
+		// The connector supports two endpoints for update the object one is template with PUT method
+		// another one is smart_categories with PATCH method.
+		switch params.ObjectName {
+		case "template":
+			method = http.MethodPut
+		default:
+			method = http.MethodPatch
+		}
+	}
+
+	jsonData, err := json.Marshal(params.RecordData)
+	if err != nil {
+		return nil, err
+	}
+
+	return http.NewRequestWithContext(
+		ctx,
+		method,
+		url.String()+common.WithTrailingSlash,
+		bytes.NewReader(jsonData),
+	)
+}
+
+func (c *Connector) parseWriteResponse(
+	ctx context.Context,
+	params common.WriteParams,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.WriteResult, error) {
+	body, ok := response.Body()
+	if !ok {
+		return &common.WriteResult{ // nolint:nilerr
+			Success: true,
+		}, nil
+	}
+
+	var searchValue string
+
+	switch params.ObjectName {
+	case "smart_categories", "template":
+		searchValue = "uuid"
+	case "calls":
+		searchValue = "external_id"
+	}
+
+	recordID, err := jsonquery.New(body).StrWithDefault(searchValue, "")
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := jsonquery.Convertor.ObjectToMap(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.WriteResult{
+		Success:  true,
+		RecordId: recordID,
+		Errors:   nil,
+		Data:     resp,
+	}, nil
 }
--- providers/avoma/supports.go
+++ providers/avoma/supports.go
@@ -12,11 +12,21 @@ import (
 func supportedOperations() components.EndpointRegistryInput {
 	readSupport := metadata.Schemas.ObjectNames().GetList(common.ModuleRoot)
 
+	writeSupport := []string{
+		"smart_categories",
+		"template",
+		"calls",
+	}
+
 	return components.EndpointRegistryInput{
 		common.ModuleRoot: {
 			{
 				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
 				Support:  components.ReadSupport,
+			},
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(writeSupport, ",")),
+				Support:  components.WriteSupport,
 			},
 		},
 	}
