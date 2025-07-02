c961369c5848dc76c456c68981cbcc10ae233b5e
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-07-02 16:40:12 +0530

    [CON-1709] feat: Added the read functionality for the avoma connector (#1764)
    * [CON-1707] feat: Added the metadata functionality for avoma connector
    
    * fix: Resolved the linter issue
    
    * chore: updated supported objects in readme file.
    
    * feat: Added the test cases
    
    * fixL linter issue
    
    * [CON-1709] feat: Added the read functionality for the avoma connector.
    
    * fix: Resolved the linter issue
    
    * feat: Added the test cases
    
    * fix: linter issue
    
    * fix: linter issue
    
    * fix: Modified the test cases.
    
    * fix: Modified the content in the users.json file
    
    * fix: modified the next page logic
    
    * fix: Modified the pagination logic

---
+++ providers/avoma/utils.go
@@ -0,0 +1,50 @@
+package avoma
+
+import (
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/datautils"
+	"github.com/amp-labs/connectors/internal/jsonquery"
+	"github.com/spyzhov/ajson"
+)
+
+const (
+	apiVersion = "v1"
+	pageSize   = "100"
+)
+
+// avoma pagination cursor sometimes ends with `=`.
+var avomaQueryEncodingExceptions = map[string]string{ //nolint:gochecknoglobals
+	"%3A": ":",
+}
+
+var EndpointsWithResultsPath = datautils.NewSet( //nolint:gochecknoglobals
+	"meetings",
+	"calls",
+	"custom_categories",
+	"notes",
+	"scorecard_evaluations",
+	"smart_categories",
+)
+
+func makeNextRecordsURL() common.NextPageFunc {
+	return func(node *ajson.Node) (string, error) {
+		nextPage, err := jsonquery.New(node).StringOptional("next")
+		if err != nil {
+			return "", err
+		}
+
+		if nextPage == nil {
+			return "", nil
+		}
+
+		url, err := urlbuilder.New(*nextPage)
+		if err != nil {
+			return "", err
+		}
+
+		url.AddEncodingExceptions(avomaQueryEncodingExceptions)
+
+		return url.String(), nil
+	}
+}
