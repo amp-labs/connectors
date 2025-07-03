9fb6b3c722bf147e944db24cbf45b904474e8f26
Dipu <dipuchaurasiya91@gmail.com>
2025-07-03 18:47:18 +0545

    feat: Implement read functionality for flatfile connector with HTTP reader and response parsing

---
+++ providers/flatfile/parse.go
@@ -0,0 +1,46 @@
+package flatfile
+
+import (
+	"net/url"
+	"strconv"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/jsonquery"
+	"github.com/spyzhov/ajson"
+)
+
+func records() common.RecordsFunc {
+	return func(node *ajson.Node) ([]map[string]any, error) {
+		records, err := jsonquery.New(node).ArrayRequired("data")
+		if err != nil {
+			return nil, err
+		}
+
+		return jsonquery.Convertor.ArrayToMap(records)
+	}
+}
+
+func nextRecordsURL(url *url.URL) common.NextPageFunc {
+	return func(n *ajson.Node) (string, error) {
+		nexURL := *url
+
+		query := nexURL.Query()
+		currentPage := 1
+		currentPageStr := query.Get(pageQuery)
+
+		if currentPageStr != "" {
+			var err error
+
+			currentPage, err = strconv.Atoi(currentPageStr)
+			if err != nil {
+				return "", err
+			}
+		}
+
+		nextPage := currentPage + 1
+		query.Set(pageQuery, strconv.Itoa(nextPage))
+		nexURL.RawQuery = query.Encode()
+
+		return nexURL.String(), nil
+	}
+}
