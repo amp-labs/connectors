39f48d03b47c438f5efdc562010c89bc842cd76e
Dipu <dipuchaurasiya91@gmail.com>
2025-07-03 18:47:18 +0545

    feat: Implement read functionality for flatfile connector with HTTP reader and response parsing

---
+++ test/flatfile/read/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/test/flatfile"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := flatfile.GetConnector(ctx)
+
+	res, err := conn.Read(ctx, common.ReadParams{
+		ObjectName: "events",
+		Fields:     connectors.Fields("id", "subject", "mode"),
+		Since:      time.Date(2025, 07, 0, 0, 0, 0, 0, time.UTC),
+	})
+
+	if err != nil {
+		utils.Fail("error reading from flatfile", "error", err)
+	}
+
+	slog.Info("Reading jobs..")
+	utils.DumpJSON(res, os.Stdout)
+
+	res, err = conn.Read(ctx, common.ReadParams{
+		ObjectName: "Workbooks",
+		Fields:     connectors.Fields("id", "name"),
+		Since:      time.Date(2025, 07, 0, 0, 0, 0, 0, time.UTC),
+	})
+
+	if err != nil {
+		utils.Fail("error reading from flatfile", "error", err)
+	}
+
+	slog.Info("Reading jobs..")
+	utils.DumpJSON(res, os.Stdout)
+
+	slog.Info("Read operation completed successfully.")
+}
