cd4fc164aeb949732e451dd5da9a7bce38f993cc
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-07-07 19:56:56 +0530

    [CON-1406] feat: Added snapchat write functionality

--- providers/snapchatads/handlers.go
+++ providers/snapchatads/handlers.go
@@ -1,7 +1,10 @@
 package snapchatads
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -97,4 +100,111 @@ func (c *Connector) parseReadResponse(
 		DataMarshall(response, params.ObjectName),
 		params.Fields,
 	)
+}
+
+func (c *Connector) buildWriteRequest(ctx context.Context, params common.WriteParams) (*http.Request, error) {
+	url, err := c.constructURL(params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	method := http.MethodPost
+
+	// When updating an object, the record ID is not included in the URL path; instead,
+	// it must be provided in the body parameters.
+	// EX. Refer https://developers.snap.com/api/marketing-api/Ads-API/billing-centers#update-a-billing-center.
+	if params.RecordId != "" {
+		method = http.MethodPut
+	}
+
+	jsonData, err := json.Marshal(params.RecordData)
+	if err != nil {
+		return nil, err
+	}
+
+	return http.NewRequestWithContext(ctx, method, url.String(), bytes.NewReader(jsonData))
+}
+
+func (c *Connector) parseWriteResponse(
+	ctx context.Context,
+	params common.WriteParams,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.WriteResult, error) {
+	body, ok := response.Body()
+	if !ok {
+		return &common.WriteResult{ // nolint:nilerr
+			Success: true,
+		}, nil
+	}
+
+	objectResponse, err := jsonquery.New(body).ArrayRequired(params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	objKey := naming.NewSingularString(params.ObjectName).String()
+
+	recordID, err := jsonquery.New(objectResponse[0], objKey).StringRequired("id")
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := jsonquery.Convertor.ObjectToMap(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.WriteResult{
+		Success:  true,
+		RecordId: recordID,
+		Errors:   nil,
+		Data:     resp,
+	}, nil
+}
+
+func (c *Connector) buildDeleteRequest(ctx context.Context, params common.DeleteParams) (*http.Request, error) {
+	url, err := urlbuilder.New(c.ProviderInfo().BaseURL, apiVersion, params.ObjectName, params.RecordId)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func (c *Connector) parseDeleteResponse(
+	ctx context.Context,
+	params common.DeleteParams,
+	request *http.Request,
+	resp *common.JSONHTTPResponse,
+) (*common.DeleteResult, error) {
+	body, ok := resp.Body()
+	if !ok {
+		return &common.DeleteResult{ // nolint:nilerr
+			Success: true,
+		}, nil
+	}
+
+	objectResponse, err := jsonquery.New(body).ArrayRequired(params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := jsonquery.Convertor.ArrayToMap(objectResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(res) != 0 {
+		return nil, fmt.Errorf("%v", res[0]["sub_request_error_reason"])
+	}
+
+	return &common.DeleteResult{
+		Success: true,
+	}, nil
 }
