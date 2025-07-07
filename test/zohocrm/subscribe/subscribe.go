8e3fc853b492ae9297951e65b400cdc7f7dfc169
jlimatampersand <135054647+jlimatampersand@users.noreply.github.com>
2025-07-07 16:44:13 -0700

    Feat/zohocrm subscribe (#1768)
    * Zoho CRM subscribe and maintenance

--- test/zohocrm/subscribe/subscribe.go
+++ test/zohocrm/subscribe/subscribe.go
@@ -51,9 +51,9 @@ func main() {
 			},
 		},
 		Request: &zohocrm.SubscriptionRequest{
-			UniqueRef:       uniqueRef,
+			UniqueRef:       "amp_" + uniqueRef,
 			WebhookEndPoint: "https://play.svix.com/in/e_BVbta2ttNmjqeA1md230npV13f5/",
-			// Duration:        &dur,
+			Duration:        &dur,
 		},
 	}
 
@@ -92,7 +92,7 @@ func main() {
 			},
 		},
 		Request: &zohocrm.SubscriptionRequest{
-			UniqueRef:       uniqueRef,
+			UniqueRef:       "amp_" + uniqueRef,
 			WebhookEndPoint: "https://play.svix.com/in/e_BVbta2ttNmjqeA1md230npV13f5/",
 			Duration:        &dur,
 		},
@@ -105,8 +105,6 @@ func main() {
 		return
 	}
 
-	fmt.Println("Update subscription results:", prettyPrint(updateResult))
-
 	err = conn.DeleteSubscription(ctx, *updateResult)
 	if err != nil {
 		logging.Logger(ctx).Error("Error deleting subscription", "error", err)
@@ -115,15 +113,6 @@ func main() {
 	}
 
 	fmt.Println("Delete subscription successful")
-
-	records, err := conn.GetRecordsByIds(ctx, "Leads", []string{"6756839000000575405", "6756839000000575402"}, []string{"phone", "company"}, []string{})
-	if err != nil {
-		logging.Logger(ctx).Error("Error getting records", "error", err)
-
-		return
-	}
-
-	fmt.Println("Records:", prettyPrint(records))
 }
 
 func prettyPrint(v any) string {
