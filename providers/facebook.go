05e58cf2a86c13535485c9c738c38bc84c1a756d
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-07-09 19:49:45 +0530

    [CON-1813] feat: Added whatsApp connector

--- providers/facebook.go
+++ providers/facebook.go
@@ -1,7 +1,9 @@
 package providers
 
 const Facebook Provider = "facebook"
+const whatsApp Provider = "whatsApp"
 
+//nolint:funlen
 func init() {
 	// Facebook Ads Manager Configuration
 	SetInfo(Facebook, ProviderInfo{
@@ -35,6 +37,42 @@ func init() {
 			Regular: &MediaTypeRegular{
 				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722478709/media/facebook_1722478708.svg",
 				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722478689/media/facebook_1722478688.svg",
+			},
+		},
+	})
+
+	// WhatsApp configuration
+	SetInfo(whatsApp, ProviderInfo{
+		DisplayName: "whatsApp",
+		AuthType:    Oauth2,
+		BaseURL:     "https://graph.facebook.com",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType:                 AuthorizationCode,
+			AuthURL:                   "https://www.facebook.com/v23.0/dialog/oauth",
+			TokenURL:                  "https://graph.facebook.com/v23.0/oauth/access_token",
+			ExplicitScopesRequired:    true,
+			ExplicitWorkspaceRequired: false,
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1752068006/media/whatsApp.com_1752068015.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1752068046/media/whatsApp.com_1752068056.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1752068006/media/whatsApp.com_1752068015.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1752068046/media/whatsApp.com_1752068056.svg",
 			},
 		},
 	})
