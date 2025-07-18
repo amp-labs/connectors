388001ed8278860cc2e4de50e2f457f9269f2993
Dipu <103568666+immdipu@users.noreply.github.com>
2025-07-18 15:02:48 +0545

    [CON-1349] feat(teamleader): add Read (#1833)

---
+++ providers/teamleader/supports.go
@@ -0,0 +1,63 @@
+package teamleader
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/components"
+)
+
+func supportedOperations() components.EndpointRegistryInput {
+	//nolint:lll
+	readSupport := []string{
+		"departments",
+		"contacts",
+		"users",
+		"teams",
+		"customFieldDefinitions",
+		"workTypes",
+		"closingDays",
+		"companies",
+		"tags",
+		"deals",
+		"dealPipelines",
+		"dealPhases",
+		"dealSources",
+		"quotations",
+		"orders",
+		"meetings",
+		"calls",
+		"callOutcomes",
+		"events",
+		"activityTypes",
+		"invoices",
+		"creditNotes",
+		"subscriptions",
+		"taxRates",
+		"withholdingTaxRates",
+		"commercialDiscounts",
+		"paymentMethods",
+		"productCategories",
+		"products",
+		"unitsOfMeasure",
+		"priceLists",
+		"projects",
+		"milestones",
+		"tasks",
+		"timeTracking",
+		"tickets",
+		"ticketStatus",
+		"files",
+		"mailTemplates",
+	}
+
+	return components.EndpointRegistryInput{
+		common.ModuleRoot: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
+	}
+}
