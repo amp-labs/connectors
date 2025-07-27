30cc259a40560329134e02af432dd58a9ba60d8d
Lauren Long <laurenzlong@users.noreply.github.com>
2025-07-26 18:19:15 -0700

    fix: ListObjectMetadata shouldn't mutate original object and fix gitlab casing

--- internal/components/schema/composite.go
+++ internal/components/schema/composite.go
@@ -37,7 +37,8 @@ func (c *CompositeSchemaProvider) ListObjectMetadata(
 
 	// Track objects that haven't been successfully processed yet
 	// Initialized with all objects.
-	remainingObjects := objects
+	remainingObjects := make([]string, len(objects))
+	copy(remainingObjects, objects)
 
 	for _, schemaProvider := range c.schemaProviders {
 		if len(remainingObjects) == 0 {
