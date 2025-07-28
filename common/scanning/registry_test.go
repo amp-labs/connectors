d44c110e6d2038586259e4e24847fc25beb3f338
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-07-29 01:02:44 +0300

    [CON-1666] feat(insightly): Metadata custom objects & fields (#1589)
    * [CON-1666] feat(insightly): Metadata custom objects & fields
    * tenv linters

--- common/scanning/registry_test.go
+++ common/scanning/registry_test.go
@@ -2,7 +2,6 @@
 package scanning
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -46,11 +45,7 @@ var testPreset = []Reader{
 }
 
 func TestCredentialOptions(t *testing.T) {
-	t.Parallel()
-
-	if os.Setenv("TEST_ENV_CLIENT_ID", "clientId") != nil {
-		t.Fatal("Error setting environment variable")
-	}
+	t.Setenv("TEST_ENV_CLIENT_ID", "clientId")
 
 	opts := NewRegistry()
 
