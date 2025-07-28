29c1cd2f47caaec9fbf1d5ce563a127889f707b3
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-07-29 01:39:03 +0300

    [CON-1309] feat(dynamicsbusiness): Read (#1651)

--- providers/dynamicsbusiness/connector.go
+++ providers/dynamicsbusiness/connector.go
@@ -2,9 +2,12 @@ package dynamicsbusiness
 
 import (
 	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/interpreter"
 	"github.com/amp-labs/connectors/internal/components"
 	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/reader"
 	"github.com/amp-labs/connectors/internal/components/schema"
+	"github.com/amp-labs/connectors/internal/datautils"
 	"github.com/amp-labs/connectors/providers"
 )
 
@@ -18,10 +21,13 @@ type Connector struct {
 	common.RequireMetadata
 
 	components.SchemaProvider
+	components.Reader
 
 	environmentName string
 	tenantID        string
 	companyID       string
+
+	incrementalRegistry *datautils.Cache[string, bool]
 }
 
 const (
@@ -48,6 +54,7 @@ func constructor(base *components.Connector) (*Connector, error) {
 		RequireMetadata: common.RequireMetadata{
 			ExpectedMetadataKeys: []string{metadataKeyCompanyID, metadataKeyEnvironmentName},
 		},
+		incrementalRegistry: datautils.NewCache[string, bool](),
 	}
 
 	connector.SchemaProvider = schema.NewObjectSchemaProvider(
@@ -56,6 +63,21 @@ func constructor(base *components.Connector) (*Connector, error) {
 		operations.SingleObjectMetadataHandlers{
 			BuildRequest:  connector.buildSingleObjectMetadataRequest,
 			ParseResponse: connector.parseSingleObjectMetadataResponse,
+		},
+	)
+
+	errorHandler := interpreter.ErrorHandler{
+		JSON: interpreter.NewFaultyResponder(errorFormats, nil),
+	}.Handle
+
+	connector.Reader = reader.NewHTTPReader(
+		connector.HTTPClient().Client,
+		components.NewEmptyEndpointRegistry(),
+		connector.ProviderContext.Module(),
+		operations.ReadHandlers{
+			BuildRequest:  connector.buildReadRequest,
+			ParseResponse: connector.parseReadResponse,
+			ErrorHandler:  errorHandler,
 		},
 	)
 
