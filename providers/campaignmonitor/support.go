cb70512b0c4649450f701dcd3151bd9c8ad7f0cb
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-07-29 19:07:56 +0530

    [CON-1374] feat: Added campaign monitor write functionality. (#1816)
    * [CON-1372] feat: Added the metadata for campaign monitor connector
    
    * fix: Resolved the linter issue
    
    * fix: changed the variable name
    
    * chore: Supports endpoints with clientId in the url path.
    
    * fix: solved linter issue
    
    * fix: linter issue
    
    * fix: linter issue
    
    * chore: Changed the name of clientid
    
    * fix: linter issue
    
    * fix: Changed the data in the campaigns json file
    
    * [CON-1373] feat: Added campaign monitor read functionality
    
    * fix: linter issue
    
    * chore: Added the error handler
    
    * [CON-1374] feat: Added campaign monitor write functionality.
    
    * chore: Added the test case for write functionality
    
    * feat: Added campaign monitor delete functionality
    
    * fix: linter issue
    
    * fix: Solved linter issue
    
    * chore: updated the comments
    
    * fix: Added comments and switch statement to handle construct url.
    
    * fix: linter issue
    
    * chore: Removed the client Id input from providerInfo and refactored the logic.
    
    * chore: Removed the clientid requirement in the test connector file.
    
    * chore: Updated the readme file
    
    * fix: solved linter issue,
    
    * fix: solved the linter issue
    
    * fix: Resolved linter issue, unsupported the enpdoints with sharedID in the URL path.
    
    * fix: Removed unsupported endpoints testing script in the test folder.
    
    * chore: Renamed the file

--- providers/campaignmonitor/support.go
+++ providers/campaignmonitor/support.go
@@ -14,11 +14,31 @@ func supportedOperations() components.EndpointRegistryInput {
 		"admins",
 	}
 
+	// List of supported write endpoints based on official Campaign Monitor API documentation:
+	// - https://www.campaignmonitor.com/api/v3-3/account/#adding-an-administrator.
+	// - https://www.campaignmonitor.com/api/v3-3/clients/#creating-a-client.
+	writeSupport := []string{
+		"admins",
+		"clients",
+	}
+
+	deleteSupport := []string{
+		"clients",
+	}
+
 	return components.EndpointRegistryInput{
 		common.ModuleRoot: {
 			{
 				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
 				Support:  components.ReadSupport,
+			},
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(writeSupport, ",")),
+				Support:  components.WriteSupport,
+			},
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(deleteSupport, ",")),
+				Support:  components.DeleteSupport,
 			},
 		},
 	}
