3b6420950bc4583b4951172a4cff5d629ccc9231
Joseph Karage <josephbkarage@gmail.com>
2025-07-29 20:30:35 +0300

    fix(Braze): support reading next page for campaigns, canvas objects (#1926)
    * fix: Reading Campaigns, Canvas + default next page
    
    * update comments

--- test/braze/write/main.go
+++ test/braze/write/main.go
@@ -37,9 +37,9 @@ func MainFn() int {
 
 func createEmailTemplate(ctx context.Context, conn *br.Connector) error {
 	prms := common.WriteParams{
-		ObjectName: "templates/email/create",
+		ObjectName: "templates/email",
 		RecordData: map[string]any{
-			"template_name":     "welcome_email",
+			"template_name":     "welcome_email___",
 			"subject":           "Welcome to [Company Name]!",
 			"body":              "<p>Hi {{first_name}},</p><p>Thank you for joining us! We're excited to have you.</p><p>Get started by exploring your account <a href='{{dashboard_url}}'>here</a>.</p><p>Cheers,<br>The {{company_name}} Team</p>",
 			"plaintext_body":    "Hi {{first_name}},\n\nThank you for joining us! Get started here: {{dashboard_url}}\n\nCheers,\nThe {{company_name}} Team",
