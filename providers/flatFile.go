ade88ba0a41289ab7f593cc60af776cd49a3971a
Dipu <103568666+immdipu@users.noreply.github.com>
2025-07-31 23:36:09 +0545

    fix: flatfile name casing (#1938)

--- providers/flatFile.go
+++ providers/flatFile.go
@@ -1,9 +1,9 @@
 package providers
 
-const FlatFile Provider = "flatFile"
+const Flatfile Provider = "flatfile"
 
 func init() {
-	SetInfo(FlatFile, ProviderInfo{
+	SetInfo(Flatfile, ProviderInfo{
 		DisplayName: "Flatfile",
 		AuthType:    ApiKey,
 		BaseURL:     "https://api.x.flatfile.com",
