dc14716f6f2982e122cfb5ad53685808bd91f156
sanjay-a01 <sanjaykanth.a@mitrahsoft.com>
2025-08-12 20:51:37 +0530

    [CON-2059] feat: [breakcold] Metadata functionality

---
+++ providers/breakcold/utils.go
@@ -0,0 +1,21 @@
+package breakcold
+
+import "github.com/amp-labs/connectors/internal/datautils"
+
+// The endpoints below use the POST method instead of the GET method.
+// https://developer.breakcold.com/v3/api-reference/leads/list-leads-with-pagination-and-filters.
+// https://developer.breakcold.com/v3/api-reference/notes/list-notes.
+// https://developer.breakcold.com/v3/api-reference/reminders/list-reminders-with-filters-and-pagination.
+var getEndpointsPostMethod = datautils.NewSet(
+	"leads/list",
+	"notes/list",
+	"reminders/list",
+)
+
+// The endpoints has data nodePath in the response.
+// https://developer.breakcold.com/v3/api-reference/notes/list-notes.
+// https://developer.breakcold.com/v3/api-reference/campaigns/list-campaigns.
+var objectWithDataNodePath = datautils.NewSet(
+	"campaigns",
+	"notes/list",
+)
