cd3e1f6e5f45c189ed1acc43b4b71e5d46d34123
Cobalt0s <constantin.y.koval@gmail.com>
2025-08-15 19:45:54 +0300

    [CON-1318] feat(nutshell): Write

---
+++ providers/nutshell/internal/metadata/README.md
@@ -0,0 +1,40 @@
+# Write
+
+### accounts
+POST  https://app.nutshell.com/rest/accounts
+PATCH https://app.nutshell.com/rest/accounts/{id}
+https://developers.nutshell.com/reference/post_accounts
+https://developers.nutshell.com/reference/patch_accounts-id
+
+### activities
+POST https://app.nutshell.com/rest/activities
+https://developers.nutshell.com/reference/post_activities-1
+
+### audiences
+POST https://app.nutshell.com/rest/audiences
+https://developers.nutshell.com/reference/post_audiences
+
+### leads
+POST  https://app.nutshell.com/rest/leads
+PATCH https://app.nutshell.com/rest/leads/{id}
+https://developers.nutshell.com/reference/post_leads-2
+https://developers.nutshell.com/reference/patch_leads-id-2
+
+### contacts
+POST  https://app.nutshell.com/rest/contacts
+PATCH https://app.nutshell.com/rest/contacts/{id}
+https://developers.nutshell.com/reference/post_contacts
+https://developers.nutshell.com/reference/patch_contacts-id
+
+### notes
+POST https://app.nutshell.com/rest/notes
+https://developers.nutshell.com/reference/post_notes
+
+### sources
+POST https://app.nutshell.com/rest/sources
+https://developers.nutshell.com/reference/post_sources
+
+### tags
+POST https://app.nutshell.com/rest/tags
+https://developers.nutshell.com/reference/post_tags
+
---
+++ providers/nutshell/write_test.go
@@ -0,0 +1,150 @@
+package nutshell
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockcond"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+	"github.com/amp-labs/connectors/test/utils/testutils"
+)
+
+func TestWrite(t *testing.T) { // nolint:funlen,gocognit,cyclop
+	t.Parallel()
+
+	errorCreateAccount := testutils.DataFromFile(t, "write/err-create-account.json")
+	errorMediaType := testutils.DataFromFile(t, "write/err-media-type.txt")
+	responseAccounts := testutils.DataFromFile(t, "write/accounts/new.json")
+
+	tests := []testroutines.Write{
+		{
+			Name:         "Write object must be included",
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:  "Error invalid payload",
+			Input: common.WriteParams{ObjectName: "accounts", RecordData: map[string]any{}},
+			Server: mockserver.Fixed{
+				Setup:  mockserver.ContentJSON(),
+				Always: mockserver.Response(http.StatusBadRequest, errorCreateAccount),
+			}.Server(),
+			ExpectedErrs: []error{
+				common.ErrBadRequest,
+				errors.New("You may only create one accounts resource per request."), // nolint:goerr113
+			},
+		},
+		{
+			Name:  "Error missing update header",
+			Input: common.WriteParams{ObjectName: "accounts", RecordData: map[string]any{}},
+			Server: mockserver.Fixed{
+				Setup:  mockserver.ContentJSON(),
+				Always: mockserver.Response(http.StatusBadRequest, errorMediaType),
+			}.Server(),
+			ExpectedErrs: []error{
+				common.ErrBadRequest,
+				errors.New(`"application/json-patch+json is required, see http://jsonapi.org"`), // nolint:goerr113
+			},
+		},
+		{
+			Name:  "Create company via POST",
+			Input: common.WriteParams{ObjectName: "accounts", RecordData: map[string]any{"name": "Strawberry"}},
+			Server: mockserver.Conditional{
+				Setup: mockserver.ContentJSON(),
+				If: mockcond.And{
+					mockcond.MethodPOST(),
+					mockcond.Path("/rest/accounts"),
+					mockcond.Body(`{"accounts": [{"name": "Strawberry"}]}`),
+				},
+				Then: mockserver.Response(http.StatusOK, responseAccounts),
+			}.Server(),
+			Comparator: testroutines.ComparatorSubsetWrite,
+			Expected: &common.WriteResult{
+				Success:  true,
+				RecordId: "23-accounts",
+				Errors:   nil,
+				Data: map[string]any{
+					"type":        "accounts",
+					"name":        "Blueberry",
+					"href":        "https://app.nutshell.com/rest/accounts/23-accounts",
+					"htmlUrlPath": "/company/23-blueberry",
+				},
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name: "Update company via PUT",
+			Input: common.WriteParams{
+				ObjectName: "accounts",
+				RecordId:   "23-accounts",
+				RecordData: map[string]any{"op": "replace", "path": "/accounts/0/name", "value": "Strawberry"},
+			},
+			Server: mockserver.Conditional{
+				Setup: mockserver.ContentMIME("application/json-patch+json"),
+				If: mockcond.And{
+					mockcond.MethodPATCH(),
+					mockcond.Path("/rest/accounts/23-accounts"),
+					mockcond.Body(`[{
+						"op":		"replace",
+						"path":		"/accounts/0/name",
+						"value":	"Strawberry"
+					}]`),
+					mockcond.Header(http.Header{"Content-Type": []string{"application/json-patch+json"}}),
+				},
+				Then: mockserver.Response(http.StatusOK, responseAccounts),
+			}.Server(),
+			Comparator: testroutines.ComparatorSubsetWrite,
+			Expected: &common.WriteResult{
+				Success:  true,
+				RecordId: "23-accounts",
+				Errors:   nil,
+				Data: map[string]any{
+					"type":        "accounts",
+					"name":        "Blueberry",
+					"href":        "https://app.nutshell.com/rest/accounts/23-accounts",
+					"htmlUrlPath": "/company/23-blueberry",
+				},
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name:  "Create company via POST",
+			Input: common.WriteParams{ObjectName: "accounts", RecordData: map[string]any{}},
+			Server: mockserver.Conditional{
+				Setup: mockserver.ContentJSON(),
+				If: mockcond.And{
+					mockcond.MethodPOST(),
+					mockcond.Path("/rest/accounts"),
+				},
+				Then: mockserver.Response(http.StatusOK, responseAccounts),
+			}.Server(),
+			Comparator: testroutines.ComparatorSubsetWrite,
+			Expected: &common.WriteResult{
+				Success:  true,
+				RecordId: "23-accounts",
+				Errors:   nil,
+				Data: map[string]any{
+					"type":        "accounts",
+					"name":        "Blueberry",
+					"href":        "https://app.nutshell.com/rest/accounts/23-accounts",
+					"htmlUrlPath": "/company/23-blueberry",
+				},
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.WriteConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
