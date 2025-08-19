b13e3edb358803b9eb9bbf82cb1e127bf01d6b21
Joseph Karage <josephbkarage@gmail.com>
2025-08-19 14:30:50 +0300

    fix: update object naming drift connector (#1968)
    * fix: object naming drift connector
    
    * Address review

--- providers/drift/handlers.go
+++ providers/drift/handlers.go
@@ -5,19 +5,27 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/datautils"
 	"github.com/amp-labs/connectors/internal/jsonquery"
 )
 
-func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
-	var (
-		url *urlbuilder.URL
-		err error
-	)
+const (
+	users         = "users"
+	conversations = "conversations"
+	playbooks     = "playbooks"
 
-	url, err = urlbuilder.New(c.ProviderInfo().BaseURL, params.ObjectName)
+	ListSuffix = "/list"
+)
+
+// Create a set of endpoints that require the list suffix.
+var endpointsRequiringListSuffix = datautils.NewSet(users, conversations, playbooks) //nolint: gochecknoglobals
+
+func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
+	url, err := c.constructReadURL(params.ObjectName)
 	if err != nil {
 		return nil, err
 	}
@@ -45,6 +53,17 @@ func (c *Connector) parseReadResponse(
 		common.GetMarshaledData,
 		params.Fields,
 	)
+}
+
+func (c *Connector) constructReadURL(objectName string) (*urlbuilder.URL, error) {
+	lowerCaseObject := strings.ToLower(objectName)
+
+	// Check if this endpoint requires the list suffix
+	if endpointsRequiringListSuffix.Has(lowerCaseObject) {
+		objectName += ListSuffix
+	}
+
+	return urlbuilder.New(c.ProviderInfo().BaseURL, objectName)
 }
 
 func (c *Connector) buildWriteRequest(ctx context.Context, params common.WriteParams) (*http.Request, error) {
