5957ba9f69a34ae42c3117f1d14f0d59125a45f2
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-08-26 10:54:16 +0530

    [CON-2059] feat: [breakcold] Metadata functionality (#1965)
    * [CON-2059] feat: [breakcold] Metadata functionality
    
    * fix: linter issue
    
    * feat: Added the readme file.
    
    * fix: Removed some object and modified the code.
    
    * chore: Changed some objectname as appropriate and modified the code.

---
+++ test/breakcold/metadata/metadata.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/amp-labs/connectors/test/breakcold"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	ctx := context.Background()
+
+	conn := breakcold.GetBreakcoldConnector(ctx)
+
+	m, err := conn.ListObjectMetadata(ctx, []string{"status", "workspaces", "members", "tags", "lists", "leads"})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	utils.DumpJSON(m, os.Stdout)
+}
