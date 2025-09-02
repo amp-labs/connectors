1a81ab6638a9d39116f00c6981c1d8599e59e38d
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-09-02 04:10:20 +0300

    [CON-2155] feat(salesforce): UpsertMetadata (#2026)

---
+++ providers/salesforce/internal/crm/custom/adapter.go
@@ -0,0 +1,35 @@
+package custom
+
+import (
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/providers"
+)
+
+const apiVersion = "60.0"
+
+type Adapter struct {
+	XMLClient  *common.XMLHTTPClient
+	moduleInfo *providers.ModuleInfo
+}
+
+func NewAdapter(httpClient *common.HTTPClient, moduleInfo *providers.ModuleInfo) *Adapter {
+	return &Adapter{
+		XMLClient: &common.XMLHTTPClient{
+			HTTPClient: httpClient,
+		},
+		moduleInfo: moduleInfo,
+	}
+}
+
+func APIVersionSOAP() string {
+	return apiVersion
+}
+
+func (a *Adapter) getModuleURL() string {
+	return a.moduleInfo.BaseURL
+}
+
+func (a *Adapter) getSoapURL() (*urlbuilder.URL, error) {
+	return urlbuilder.New(a.getModuleURL(), "services/Soap/m", APIVersionSOAP())
+}
