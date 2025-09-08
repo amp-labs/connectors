0aef17437cf72b0636104300b1077f5100ce57ed
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-09-08 11:35:47 +0530

    [CON-2081] feat: [blackbaud] write functionality (#1986)
    * [CON-2079] feat: [blackbaud] Metadata functionality
    
    * fix: Resolved linter issue
    
    * [CON-2080] feat: [blackbaud] Read functionality
    
    * fix: Modified the Error response structure
    
    * fix: Resolved the linter issue
    
    * fix: Modified the error handling logic
    
    * chore: Modified the error handling logic.
    
    * chore: Updated the readme file.
    
    * [CON-2081] feat: [blackbaud] write functionality
    
    * feat: Added the comments
    
    * feat: Added the comments and order the endpoints in the alphabet
    
    * feat: Added some comments on the support file.
    
    * feat: Added the comments for error response schema
    
    * feat:  populated ObjectMetadata.Fields with all the necessary data for metadata.

---
+++ test/blackbaud/delete/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/amp-labs/connectors/common"
+	ap "github.com/amp-labs/connectors/providers/blackbaud"
+	"github.com/amp-labs/connectors/test/blackbaud"
+)
+
+func main() {
+	os.Exit(MainFn())
+}
+
+func MainFn() int {
+	ctx := context.Background()
+
+	err := testEvents(ctx)
+	if err != nil {
+		return 1
+	}
+
+	err = testVolunteers(ctx)
+	if err != nil {
+		return 1
+	}
+
+	return 0
+}
+
+func testEvents(ctx context.Context) error {
+	conn := blackbaud.GetBlackbaudConnector(ctx)
+
+	slog.Info("Deleting the Crm event")
+
+	deleteParams := common.DeleteParams{
+		ObjectName: "crm-evtmg/events",
+		RecordId:   "48a81a28-c61d-4bce-8272-ada71d061c5e",
+	}
+
+	res, err := Delete(ctx, conn, deleteParams)
+	if err != nil {
+		fmt.Println("ERR: ", err)
+
+		return err
+	}
+
+	if err := constructResponse(res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func testVolunteers(ctx context.Context) error {
+	conn := blackbaud.GetBlackbaudConnector(ctx)
+
+	slog.Info("Deleting the Crm volunteers")
+
+	deleteParams := common.DeleteParams{
+		ObjectName: "crm-volmg/volunteers",
+		RecordId:   "4C6F50CE-701E-4453-A4AC-097A3FEB2364",
+	}
+
+	res, err := Delete(ctx, conn, deleteParams)
+	if err != nil {
+		fmt.Println("ERR: ", err)
+
+		return err
+	}
+
+	if err := constructResponse(res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Delete(ctx context.Context, conn *ap.Connector, payload common.DeleteParams) (*common.DeleteResult, error) {
+	res, err := conn.Delete(ctx, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// unmarshal the delte response.
+func constructResponse(res *common.DeleteResult) error {
+	jsonStr, err := json.MarshalIndent(res, "", " ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: %w", err)
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
