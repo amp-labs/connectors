dc6ef8d0d5913b5b9a2df75b77dbc09861b92b6b
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-09-08 11:24:29 +0530

    [CON-2079] feat: [blackbaud] Metadata functionality (#1980)
    * [CON-2079] feat: [blackbaud] Metadata functionality
    
    * fix: Resolved linter issue
    
    * chore: Updated the readme file.
    
    * feat:  populated ObjectMetadata.Fields with all the necessary data for metadata.
    
    * [CON-2080] feat: [blackbaud] Read functionality (#1985)
    
    * [CON-2080] feat: [blackbaud] Read functionality
    
    * fix: Modified the Error response structure
    
    * fix: Resolved the linter issue
    
    * chore: Modified the error handling logic.
    
    * feat: Added the comments
    
    * feat: Added some comments on the support file.
    
    * feat: Added the comments for error response schema

---
+++ providers/blackbaud/handlers.go
@@ -0,0 +1,118 @@
+package blackbaud
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/naming"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/jsonquery"
+)
+
+func (c *Connector) buildSingleObjectMetadataRequest(ctx context.Context, objectName string) (*http.Request, error) {
+	url, err := urlbuilder.New(c.ProviderInfo().BaseURL, objectName)
+	if err != nil {
+		return nil, err
+	}
+
+	if objectNameWithSearchResource.Has(objectName) {
+		url.AddPath("search")
+	}
+
+	if objectNameWithListResource.Has(objectName) {
+		url.AddPath("list")
+	}
+
+	url.WithQueryParam("limit", "1")
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Bb-Api-Subscription-Key", c.SubscriptionKey)
+
+	return req, nil
+}
+
+func (c *Connector) parseSingleObjectMetadataResponse(
+	ctx context.Context,
+	objectName string,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.ObjectMetadata, error) {
+	objectMetadata := common.ObjectMetadata{
+		Fields:      make(map[string]common.FieldMetadata),
+		DisplayName: naming.CapitalizeFirstLetterEveryWord(objectName),
+	}
+
+	body, ok := response.Body()
+	if !ok {
+		return nil, common.ErrEmptyJSONHTTPResponse
+	}
+
+	res, err := jsonquery.New(body).ArrayOptional("value")
+	if err != nil {
+		return nil, err
+	}
+
+	record, err := jsonquery.Convertor.ArrayToMap(res)
+	if err != nil {
+		return nil, err
+	}
+
+	for field := range record[0] {
+		objectMetadata.Fields[field] = common.FieldMetadata{
+			DisplayName:  field,
+			ValueType:    common.ValueTypeOther,
+			ProviderType: "", // not available
+			ReadOnly:     false,
+			Values:       nil,
+		}
+	}
+
+	return &objectMetadata, nil
+}
+
+func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
+	url, err := urlbuilder.New(c.ProviderInfo().BaseURL, params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	if objectNameWithSearchResource.Has(params.ObjectName) {
+		url.AddPath("search")
+	}
+
+	if objectNameWithListResource.Has(params.ObjectName) {
+		url.AddPath("list")
+	}
+
+	url.WithQueryParam("limit", strconv.Itoa(defaultPageSize))
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Bb-Api-Subscription-Key", c.SubscriptionKey)
+
+	return req, nil
+}
+
+func (c *Connector) parseReadResponse(
+	ctx context.Context,
+	params common.ReadParams,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.ReadResult, error) {
+	return common.ParseResult(
+		response,
+		common.ExtractRecordsFromPath("value"),
+		makeNextRecord(),
+		common.GetMarshaledData,
+		params.Fields,
+	)
+}
