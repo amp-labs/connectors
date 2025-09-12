684c0a66c86ebede36857790a703bbfbf068d01b
Dipu <dipuchaurasiya91@gmail.com>
2025-09-12 18:42:04 +0545

    feat: enhance account read functionality to include contact and employee data

--- test/sageintacct/read/main.go
+++ test/sageintacct/read/main.go
@@ -31,7 +31,26 @@ func main() {
 	if err != nil {
 		utils.Fail("error reading accounts from Sage Intacct", "error", err)
 	}
-
 	slog.Info("Reading accounts..")
+	utils.DumpJSON(res, os.Stdout)
+
+	res, err = conn.Read(ctx, common.ReadParams{
+		ObjectName: "contact",
+		Fields:     connectors.Fields("id", "key", "href"),
+	})
+	if err != nil {
+		utils.Fail("error reading contacts from Sage Intacct", "error", err)
+	}
+	slog.Info("Reading contacts..")
+	utils.DumpJSON(res, os.Stdout)
+
+	res, err = conn.Read(ctx, common.ReadParams{
+		ObjectName: "employee",
+		Fields:     connectors.Fields("id", "key", "href"),
+	})
+	if err != nil {
+		utils.Fail("error reading employees from Sage Intacct", "error", err)
+	}
+	slog.Info("Reading employees..")
 	utils.DumpJSON(res, os.Stdout)
 }
