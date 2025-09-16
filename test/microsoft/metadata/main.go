5fb84b16a859d07cae1e69c23ff05ae14c0eb359
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-09-16 21:16:34 +0300

    [CON-1802] feat(microsoft): ListObjectMetadata (#1780)

---
+++ test/microsoft/metadata/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	connTest "github.com/amp-labs/connectors/test/microsoft"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := connTest.GetMicrosoftGraphConnector(ctx)
+
+	metadata, err := conn.ListObjectMetadata(ctx, []string{
+		"users",
+	})
+	if err != nil {
+		utils.Fail("error listing metadata for microsoft CRM", "error", err)
+	}
+
+	utils.DumpJSON(metadata, os.Stdout)
+}
