ab7454a3d2a7d1eb710eb6b9b7dafea8e94ee1a2
Dipu <103568666+immdipu@users.noreply.github.com>
2025-09-19 12:54:46 +0545

    feat(xero): enable read and write support (#2070)

--- providers/xero.go
+++ providers/xero.go
@@ -28,9 +28,9 @@ func init() {
 				Delete: false,
 			},
 			Proxy:     false,
-			Read:      false,
+			Read:      true,
 			Subscribe: false,
-			Write:     false,
+			Write:     true,
 		},
 		Media: &Media{
 			DarkMode: &MediaTypeDarkMode{
