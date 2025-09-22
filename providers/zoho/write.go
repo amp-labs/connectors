95b5ef64d1356ac9cdc90a7c83e530b037b8c41b
jkarage <josephbkarage@gmail.com>
2025-09-22 17:52:13 +0300

    Add zoho desk write connector

--- providers/zoho/write.go
+++ providers/zoho/write.go
@@ -7,6 +7,7 @@ import (
 	"github.com/amp-labs/connectors/common/logging"
 	"github.com/amp-labs/connectors/common/naming"
 	"github.com/amp-labs/connectors/internal/jsonquery"
+	"github.com/amp-labs/connectors/providers"
 	"github.com/spyzhov/ajson"
 )
 
@@ -49,14 +50,69 @@ const dataKey = "data"
 func (c *Connector) Write(ctx context.Context, config common.WriteParams) (*common.WriteResult, error) {
 	ctx = logging.With(ctx, "connector", "zoho CRM")
 
+	if err := config.ValidateParams(); err != nil {
+		return nil, err
+	}
+
+	switch c.moduleID { // nolint: exhaustive
+	case providers.ZohoDeskV2:
+		return c.writeDesk(ctx, config)
+	default:
+		return c.writeCRM(ctx, config)
+	}
+}
+
+func (c *Connector) writeDesk(ctx context.Context, config common.WriteParams) (*common.WriteResult, error) {
+	var write common.WriteMethod
+
+	url, err := c.getAPIURL(deskAPIVersion, config.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(config.RecordId) != 0 {
+		url.AddPath(config.RecordId)
+
+		write = c.Client.Patch
+	} else {
+		write = c.Client.Post
+	}
+
+	resp, err := write(ctx, url.String(), config.RecordData)
+	if err != nil {
+		return nil, err
+	}
+
+	node, ok := resp.Body()
+	if !ok {
+		logging.Logger(ctx).Error("failed to retrieve the created/updated response data", "object", config.ObjectName)
+
+		return &common.WriteResult{Success: true}, nil
+	}
+
+	recordId, err := jsonquery.New(node).StringOptional("id")
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := jsonquery.Convertor.ObjectToMap(node)
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.WriteResult{
+		Success:  true,
+		Errors:   nil,
+		RecordId: *recordId,
+		Data:     data,
+	}, nil
+}
+
+func (c *Connector) writeCRM(ctx context.Context, config common.WriteParams) (*common.WriteResult, error) {
 	var (
 		errs  []any
 		write common.WriteMethod
 	)
-
-	if err := config.ValidateParams(); err != nil {
-		return nil, err
-	}
 
 	// Object names in ZohoCRM API are case sensitive.
 	// Capitalizing the first character of object names to form correct URL.
