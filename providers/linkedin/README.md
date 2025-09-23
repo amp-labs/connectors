5a7300a3ed49043486c1928971ccdbf53e83989f
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-09-24 04:47:57 +0530

    [CON-1388] feat: [linkedIn] Metadata functionality (#2078)
    * [CON-1390] feat: Added write functionality in the LinkedIn Connector
    
    * fix: Resolved the linter issue
    
    * fix: solved linter issue
    
    * Delete providers/linkedIn directory
    
    * Delete test/linkedIn directory
    
    * fix: Resolved the linter issue
    
    * fix: Solved linter issue
    
    * [CON-1387] feat: [linkedIn] Metadata functionality.
    
    * fix: Resolved linter issue
    
    * feat: Supports the endpoints with query param and shared Id in the url path.
    
    * fix: linter issue
    
    * fix: Resolved linter issue
    
    * chore: Modified the parsing write response logic.
    
    * fix: Resolved linter issue
    
    * chore: Added the docs URL for getting adAccountId

---
+++ providers/linkedin/README.md
@@ -0,0 +1,41 @@
+# LinkedIn connector
+
+
+## Supported Objects 
+Below is an exhaustive list of objects & methods supported on the objects
+
+Note: 
+- In this connector, there is no GetAll endpoint; it only has search endpoints.
+- This connector supports write operation for some other objects. 
+- For creating the objects, it return a 201 Created HTTP status code and return shared ID in the x-restli-id response header instead of response body.
+- For updating the objects, it return a 204 No Content HTTP status code instead of response body.
+
+linkedIn API Environment: rest
+
+LinkedIn API version: v2
+
+---------------------------------------------------------------------
+| Object                  | Resource                | Method        |
+| adTargetingFacets       | adTargetingFacets       | read          |
+| dmpEngagementSourceTypes| dmpEngagementSourceTypes| read          |
+| adCampaignGroups        | adCampaignGroups        | read          |
+| adCampaigns             | adCampaigns             | read          |
+| adAccounts              | adAccounts              | read, write   |
+| adTargetTemplates       | adTargetTemplates       | write         |
+| adPublisherRestrictions | adPublisherRestrictions | write         |
+| inmailContents          | inMailContents          | write         |
+| conversationAds         | conversationAds         | write         |
+| adLiftTests             | adLiftTests             | write         |
+| adExperiments           | adExperiments           | write         |
+| conversions             | conversions             | write         |
+| thirdPartyTrackingTags  | thirdPartyTrackingTags  | write         |
+| events                  | events                  | write         |
+| insightTags             | insightTags             | write         |
+| adPageSets              | adPageSets              | write         |
+| dmpSegments             | dmpSegments             | write         |
+| leadForms               | leadForms               | write         |
+| posts                   | posts                   | write         |
+| creatives               | creatives               | write         |
+---------------------------------------------------------------------
+
+Note: In this connector, lot of endpoints having same name but differ in the body parameter which are Events, posts, creatives. Go through the Advertising & Targeting section in the link https://learn.microsoft.com/ru-ru/linkedin/marketing/overview?view=li-lms-2025-04.
---
+++ providers/linkedin/handlers.go
@@ -0,0 +1,134 @@
+package linkedin
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/naming"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+)
+
+const LinkedInVersion = "202504"
+
+type responseObject struct {
+	Elements []map[string]any `json:"elements"`
+}
+
+func (c *Connector) buildSingleObjectMetadataRequest(ctx context.Context, objectName string) (*http.Request, error) {
+	var (
+		url *urlbuilder.URL
+		err error
+	)
+
+	switch {
+	case ObjectWithAccountId.Has(objectName):
+		url, err = urlbuilder.New(c.ProviderInfo().BaseURL, "rest", "adAccounts", c.AdAccountId, objectName)
+	default:
+		url, err = urlbuilder.New(c.ProviderInfo().BaseURL, "rest", objectName)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if ObjectsWithSearchQueryParam.Has(objectName) {
+		url.WithQueryParam("q", "search")
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("LinkedIn-Version", LinkedInVersion) // nolint:canonicalheader
+	req.Header.Add("X-Restli-Protocol-Version", "2.0.0")
+
+	return req, nil
+}
+
+func (c *Connector) parseSingleObjectMetadataResponse(
+	ctx context.Context,
+	objectName string,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.ObjectMetadata, error) {
+	objectMetadata := common.ObjectMetadata{
+		Fields:      make(map[string]common.FieldMetadata),
+		DisplayName: naming.CapitalizeFirstLetter(objectName),
+	}
+
+	data, err := common.UnmarshalJSON[responseObject](response)
+	if err != nil {
+		return nil, common.ErrFailedToUnmarshalBody
+	}
+
+	if len(data.Elements) == 0 {
+		return nil, ErrMetadataNotFound
+	}
+
+	for field := range data.Elements[0] {
+		objectMetadata.Fields[field] = common.FieldMetadata{
+			DisplayName:  field,
+			ValueType:    common.ValueTypeOther,
+			ProviderType: "",
+			ReadOnly:     false,
+			Values:       nil,
+		}
+	}
+
+	return &objectMetadata, nil
+}
+
+func (c *Connector) buildWriteRequest(ctx context.Context, params common.WriteParams) (*http.Request, error) {
+	var (
+		url *urlbuilder.URL
+		err error
+	)
+
+	url, err = urlbuilder.New(c.ProviderInfo().BaseURL, "rest", params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	if params.RecordId != "" {
+		url.AddPath(params.RecordId)
+	}
+
+	jsonData, err := json.Marshal(params.RecordData)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url.String(), bytes.NewReader(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	if params.RecordId != "" {
+		req.Header.Add("X-Restli-Method", "PARTIAL_UPDATE")
+	}
+
+	req.Header.Add("LinkedIn-Version", LinkedInVersion) // nolint:canonicalheader
+	req.Header.Add("X-Restli-Protocol-Version", "2.0.0")
+
+	return req, nil
+}
+
+func (c *Connector) parseWriteResponse(
+	ctx context.Context,
+	params common.WriteParams,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.WriteResult, error) {
+	RecordId := response.Headers.Get("X-Restli-Id")
+
+	return &common.WriteResult{
+		Success:  true,
+		RecordId: RecordId,
+		Errors:   nil,
+		Data:     nil,
+	}, nil
+}
