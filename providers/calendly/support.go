e6da736d43ca1cae67d5a13366b086572049d95d
Joseph Karage <josephbkarage@gmail.com>
2025-10-06 09:53:57 +0300

    [CON-1477] feat: Add calendly read connector (#1981)
    * add calendly metadata
    
    * remove test changes
    
    * remove unwanted test changes
    
    * add calendly metadata
    
    * undo zoom change
    
    * retrieve userURI and orgURI details
    
    * Add read connector
    
    * Add commets
    
    * Address review

---
+++ providers/calendly/support.go
@@ -0,0 +1,37 @@
+package calendly
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/datautils"
+)
+
+var (
+	requiresOrgURIQueryParam = datautils.NewSet("activity_log_entries", //nolint: gochecknoglobals
+		"event_types", "groups", "organization_memberships", "outgoing_communications",
+		"routing_forms", "scheduled_events", "group_relationships")
+
+	requiresUserURIQueryParam = datautils.NewSet("user_busy_times", //nolint: gochecknoglobals
+		"user_availability_schedules",
+		"locations", "scheduled_events")
+
+	EndpointWithUpdatedAtParam = datautils.NewSet("event_types") //nolint: gochecknoglobals
+)
+
+func supportedOperations() components.EndpointRegistryInput {
+	readSupport := []string{
+		"*",
+	}
+
+	return components.EndpointRegistryInput{
+		common.ModuleRoot: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
+	}
+}
