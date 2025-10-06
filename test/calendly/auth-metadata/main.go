85a44ccdb42d4785acc877589de197ff8c5c0ad6
Joseph Karage <josephbkarage@gmail.com>
2025-10-06 09:33:10 +0300

    feat: Add UserURI and OrgURI metadata in calendly connector (#1979)
    * add calendly metadata
    
    * remove test changes
    
    * remove unwanted test changes
    
    * add calendly metadata
    
    * undo zoom change
    
    * retrieve userURI and orgURI details

---
+++ test/calendly/auth-metadata/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os/signal"
+	"syscall"
+
+	"github.com/amp-labs/connectors/providers/calendly"
+	connTest "github.com/amp-labs/connectors/test/calendly"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := connTest.GetCalendlyConnector(ctx)
+
+	info, err := conn.GetPostAuthInfo(ctx)
+	if err != nil || info.CatalogVars == nil {
+		utils.Fail("error obtaining auth info", "error", err)
+	}
+
+	userURI := calendly.NewAuthMetadataVars(*info.CatalogVars).UserURI
+	orgURI := calendly.NewAuthMetadataVars(*info.CatalogVars).OrganizationURI
+
+	// Log the retrieved tenant ID.
+	slog.Info("retrieved auth metadata", "userURI", userURI)
+	slog.Info("retrieved auth metadata", "orgURI", orgURI)
+}
