9a0880794dbff498712bcdd2bcfc926c5ad582a7
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-10-16 12:29:52 +0530

    [CON-2215] feat: [jobber] Initialize the connector (#2184)

--- connector/new.go
+++ connector/new.go
@@ -59,6 +59,7 @@ import (
 	"github.com/amp-labs/connectors/providers/instantlyai"
 	"github.com/amp-labs/connectors/providers/intercom"
 	"github.com/amp-labs/connectors/providers/iterable"
+	"github.com/amp-labs/connectors/providers/jobber"
 	"github.com/amp-labs/connectors/providers/keap"
 	"github.com/amp-labs/connectors/providers/kit"
 	"github.com/amp-labs/connectors/providers/klaviyo"
@@ -160,6 +161,7 @@ var connectorConstructors = map[providers.Provider]outputConstructorFunc{ // nol
 	providers.InstantlyAI:             wrapper(newInstantlyAIConnector),
 	providers.Intercom:                wrapper(newIntercomConnector),
 	providers.Iterable:                wrapper(newIterableConnector),
+	providers.Jobber:                  wrapper(newJobberConnector),
 	providers.Keap:                    wrapper(newKeapConnector),
 	providers.Kit:                     wrapper(newKitConnector),
 	providers.Klaviyo:                 wrapper(newKlaviyoConnector),
@@ -813,4 +815,9 @@ func newCalendlyConnector(params common.ConnectorParams,
 func newPaddleConnector(params common.ConnectorParams,
 ) (*paddle.Connector, error) {
 	return paddle.NewConnector(params)
+}
+
+func newJobberConnector(params common.ConnectorParams,
+) (*jobber.Connector, error) {
+	return jobber.NewConnector(params)
 }
