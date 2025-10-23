4ef10993eec93efc4229f73cebb9350a3093df85
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-10-23 21:32:17 +0300

    [ENG-2553] update(pipeliner): Workspace,Region,OpenAPI (#2061)

--- providers/pipeliner/connector.go
+++ providers/pipeliner/connector.go
@@ -3,61 +3,97 @@ package pipeliner
 import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/interpreter"
-	"github.com/amp-labs/connectors/common/paramsbuilder"
 	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/deleter"
+	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/reader"
+	"github.com/amp-labs/connectors/internal/components/schema"
+	"github.com/amp-labs/connectors/internal/components/writer"
 	"github.com/amp-labs/connectors/providers"
+	"github.com/amp-labs/connectors/providers/pipeliner/internal/metadata"
 )
 
 type Connector struct {
-	BaseURL   string
-	Workspace string
-	Module    common.Module
-	Client    *common.JSONHTTPClient
+	*components.Connector
+	common.RequireAuthenticatedClient
+
+	components.SchemaProvider
+	components.Reader
+	components.Writer
+	components.Deleter
+
+	workspace string
 }
 
-func NewConnector(opts ...Option) (conn *Connector, outErr error) {
-	params, err := paramsbuilder.Apply(parameters{}, opts)
+func NewConnector(params common.ConnectorParams) (*Connector, error) {
+	return components.Initialize(providers.Pipeliner, params, makeConstructor(params))
+}
+
+func makeConstructor(params common.ConnectorParams) components.ConnectorConstructor[Connector] {
+	return func(base *components.Connector) (*Connector, error) {
+		connector := &Connector{
+			Connector: base,
+			workspace: params.Workspace,
+		}
+
+		connector.SchemaProvider = schema.NewOpenAPISchemaProvider(connector.ProviderContext.Module(), metadata.Schemas)
+
+		errorHandler := interpreter.ErrorHandler{
+			JSON: interpreter.NewFaultyResponder(errorFormats, statusCodeMapping),
+		}.Handle
+
+		connector.Reader = reader.NewHTTPReader(
+			connector.HTTPClient().Client,
+			components.NewEmptyEndpointRegistry(),
+			connector.ProviderContext.Module(),
+			operations.ReadHandlers{
+				BuildRequest:  connector.buildReadRequest,
+				ParseResponse: connector.parseReadResponse,
+				ErrorHandler:  errorHandler,
+			},
+		)
+
+		connector.Writer = writer.NewHTTPWriter(
+			connector.HTTPClient().Client,
+			components.NewEmptyEndpointRegistry(),
+			connector.ProviderContext.Module(),
+			operations.WriteHandlers{
+				BuildRequest:  connector.buildWriteRequest,
+				ParseResponse: connector.parseWriteResponse,
+				ErrorHandler:  errorHandler,
+			},
+		)
+
+		// Set the deleter for the connector
+		connector.Deleter = deleter.NewHTTPDeleter(
+			connector.HTTPClient().Client,
+			components.NewEmptyEndpointRegistry(),
+			connector.ProviderContext.Module(),
+			operations.DeleteHandlers{
+				BuildRequest:  connector.buildDeleteRequest,
+				ParseResponse: connector.parseDeleteResponse,
+				ErrorHandler:  errorHandler,
+			},
+		)
+
+		return connector, nil
+	}
+}
+
+func (c *Connector) getURL(objectName, recordID string) (*urlbuilder.URL, error) {
+	path, err := metadata.Schemas.FindURLPath(common.ModuleRoot, objectName)
 	if err != nil {
 		return nil, err
 	}
 
-	httpClient := params.Client.Caller
-	conn = &Connector{
-		Client: &common.JSONHTTPClient{
-			HTTPClient: httpClient,
-		},
-		Workspace: params.Workspace.Name,
-	}
-
-	providerInfo, err := providers.ReadInfo(conn.Provider())
-	if err != nil {
-		return nil, err
-	}
-
-	// connector and its client must mirror base url and provide its own error parser
-	conn.setBaseURL(providerInfo.BaseURL)
-	conn.Client.HTTPClient.ErrorHandler = interpreter.ErrorHandler{
-		JSON: interpreter.NewFaultyResponder(errorFormats, statusCodeMapping),
-	}.Handle
-
-	return conn, nil
+	return urlbuilder.New(c.ProviderInfo().BaseURL, "api/v100/rest/spaces/", c.workspace, path, recordID)
 }
 
-func (c *Connector) Provider() providers.Provider {
-	return providers.Pipeliner
+func (c *Connector) getReadURL(objectName string) (*urlbuilder.URL, error) {
+	return c.getURL(objectName, "")
 }
 
-func (c *Connector) String() string {
-	return c.Provider() + ".Connector"
-}
-
-func (c *Connector) getURL(parts ...string) (*urlbuilder.URL, error) {
-	return urlbuilder.New(c.BaseURL, append([]string{
-		"api/v100/rest/spaces/", c.Workspace, "/entities",
-	}, parts...)...)
-}
-
-func (c *Connector) setBaseURL(newURL string) {
-	c.BaseURL = newURL
-	c.Client.HTTPClient.Base = newURL
+func (c *Connector) getWriteURL(objectName, recordID string) (*urlbuilder.URL, error) {
+	return c.getURL(objectName, recordID)
 }
