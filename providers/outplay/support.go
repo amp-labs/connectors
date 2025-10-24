b01d6a8dc66da392ecd7675008832c0aa86b4f8e
Dipu <103568666+immdipu@users.noreply.github.com>
2025-10-24 17:56:38 +0545

    [CON-2569]: feat(Outplay): add ListMetadata (#2198)

---
+++ providers/outplay/support.go
@@ -0,0 +1,16 @@
+package outplay
+
+import (
+	"github.com/amp-labs/connectors/internal/datautils"
+)
+
+var objectAPIPath = datautils.NewDefaultMap(datautils.Map[string, string]{ //nolint:gochecknoglobals
+	"prospect":        "prospect/search",
+	"prospectaccount": "prospectaccount/search",
+	"sequence":        "sequence/search",
+	"call":            "call/search",
+	"task":            "task/list",
+	"callanalysis":    "callanalysis/list",
+}, func(objectName string) string {
+	return objectName
+})
