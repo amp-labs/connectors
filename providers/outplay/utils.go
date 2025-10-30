e930c58de7de08b4a4a432f7dedb8078adf68f95
Dipu <103568666+immdipu@users.noreply.github.com>
2025-10-30 22:26:53 +0545

    [CON-2570] feat(Outplay): add read (#2204)

--- providers/outplay/utils.go
+++ providers/outplay/utils.go
@@ -3,9 +3,11 @@ package outplay
 import (
 	"bytes"
 	"encoding/json"
+	"strconv"
 	"time"
 
 	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
 )
 
 func inferValueTypeFromData(value any) common.ValueType {
@@ -30,7 +32,7 @@ func buildMetadataBody(objectName string) (*bytes.Reader, error) {
 		"pageindex": 1,
 	}
 
-	if objectName == "call" {
+	if objectName == ObjectNameCall {
 		now := time.Now()
 
 		// Call object requires fromdate and todate parameters.
@@ -49,4 +51,61 @@ func buildMetadataBody(objectName string) (*bytes.Reader, error) {
 	bodyReader := bytes.NewReader(bodyJSON)
 
 	return bodyReader, nil
+}
+
+func buildReadBody(params common.ReadParams) (*bytes.Reader, error) {
+	body := map[string]any{}
+
+	if params.NextPage != "" {
+		pageIndex, err := strconv.Atoi(params.NextPage.String())
+		if err != nil {
+			return nil, err
+		}
+
+		body["pageindex"] = pageIndex
+	} else {
+		body["pageindex"] = 1
+	}
+
+	// call object requires date filters
+	if params.ObjectName == ObjectNameCall {
+		// There is no limit on date range but it is mandatory to provide fromdate and todate
+		// we set default date range as last 30 days
+		startDate := time.Now().AddDate(0, 0, -30)
+		endDate := time.Now()
+
+		if !params.Since.IsZero() {
+			startDate = params.Since
+		}
+
+		if !params.Until.IsZero() {
+			endDate = params.Until
+		}
+
+		body["fromdate"] = startDate.Format(timeLayout)
+		body["todate"] = endDate.Format(timeLayout)
+	}
+
+	bodyJSON, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewReader(bodyJSON), nil
+}
+
+func buildReadQueryParams(url *urlbuilder.URL, params common.ReadParams) {
+	if !params.Since.IsZero() {
+		url.WithQueryParam("fromdate", params.Since.Format(timeLayout))
+	}
+
+	if !params.Until.IsZero() {
+		url.WithQueryParam("todate", params.Until.Format(timeLayout))
+	}
+
+	if params.NextPage != "" {
+		url.WithQueryParam("pageindex", params.NextPage.String())
+	} else {
+		url.WithQueryParam("pageindex", "1")
+	}
 }
