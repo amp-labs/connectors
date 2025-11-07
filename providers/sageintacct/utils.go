615e1a4d97c4c98f01402e3590d2bc4468c6e92a
Dipu <dipuchaurasiya91@gmail.com>
2025-11-07 16:06:49 +0545

    feat(sageintacct): enhance read functionality with date filters and unsupported object list

--- providers/sageintacct/utils.go
+++ providers/sageintacct/utils.go
@@ -2,6 +2,7 @@ package sageintacct
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/naming"
@@ -44,6 +45,30 @@ func buildReadBody(params common.ReadParams) (map[string]interface{}, error) {
 		"fields":      fieldNames,
 		pageSizeParam: defaultPageSize,
 		pageParam:     1,
+	}
+
+	if !objectNameNotSupportIncremental.Has(params.ObjectName) {
+		dateFilters := make([]map[string]any, 0, 2) //nolint:mnd
+
+		if !params.Since.IsZero() {
+			dateFilters = append(dateFilters, map[string]any{
+				"$gte": map[string]any{
+					"audit.modifiedDateTime": params.Since.Format(time.RFC3339),
+				},
+			})
+		}
+
+		if !params.Until.IsZero() {
+			dateFilters = append(dateFilters, map[string]any{
+				"$lte": map[string]any{
+					"audit.modifiedDateTime": params.Until.Format(time.RFC3339),
+				},
+			})
+		}
+
+		if len(dateFilters) > 0 {
+			payload["filters"] = dateFilters
+		}
 	}
 
 	if params.NextPage != "" {
