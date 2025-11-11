a5586cbc4d50f6cf12ed5ee71bdb55833b3edf8b
jkarage <josephbkarage@gmail.com>
2025-11-11 12:00:55 +0300

    adds general solarwinds connector

--- providers/solarWindsServiceDesk.go
+++ providers/solarWindsServiceDesk.go
@@ -1,12 +1,15 @@
 package providers
 
-const SolarWindsServiceDeskUS Provider = "solarWindsServiceDeskUS"
+const (
+	SolarWindsServiceDeskUS Provider = "solarWindsServiceDeskUS"
+	SolarWindsServiceDesk   Provider = "solarWindsServiceDesk"
+)
 
 // SolarWinds Service Desk has data centers in three regions: US, EU, and APJ, each with a different base URL.
 //
-//nolint:lll
+//nolint:lll,funlen
 func init() {
-	// SolarWindsServiceDesk US configuration
+	// SolarWindsServiceDesk configuration
 	SetInfo(SolarWindsServiceDeskUS, ProviderInfo{
 		DisplayName: "SolarWinds Service Desk US",
 		AuthType:    ApiKey,
@@ -38,6 +41,51 @@ func init() {
 			Regular: &MediaTypeRegular{
 				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322100/media/solarwinds.com_1738322099.svg",
 				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322158/media/solarwinds.com_1738322159.svg",
+			},
+		},
+	})
+
+	// SolarWindsServiceDesk configuration
+	SetInfo(SolarWindsServiceDesk, ProviderInfo{
+		DisplayName: "SolarWinds Service Desk",
+		AuthType:    ApiKey,
+		BaseURL:     "https://{{.subdomain}}.samanage.com",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Header,
+			Header: &ApiKeyOptsHeader{
+				Name:        "X-Samanage-Authorization",
+				ValuePrefix: "Bearer ",
+			},
+			DocsURL: "https://documentation.solarwinds.com/en/success_center/swsd/content/completeguidetoswsd/token-authentication-for-api-integration.htm#link4",
+		}, Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     true,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322100/media/solarwinds.com_1738322099.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322158/media/solarwinds.com_1738322159.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322100/media/solarwinds.com_1738322099.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1738322158/media/solarwinds.com_1738322159.svg",
+			},
+		},
+		Metadata: &ProviderMetadata{
+			Input: []MetadataItemInput{
+				{
+					Name:         "subdomain",
+					DisplayName:  "API Subdomain",
+					DefaultValue: "api",
+				},
 			},
 		},
 	})
