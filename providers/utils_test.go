8f7d5aae3b8d36be2acdb9f2527002cebc67c381
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2025-11-21 03:17:39 +0200

    [ENG-2914] feat(hubspot/crm): Enable batch Create,Update (#2272)

--- providers/utils_test.go
+++ providers/utils_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/substitutions/catalogreplacer"
+	"github.com/amp-labs/connectors/internal/goutils"
 	"github.com/amp-labs/connectors/test/utils/testutils"
 )
 
@@ -346,6 +347,18 @@ func TestReadModuleInfo(t *testing.T) { // nolint:funlen,maintidx
 				BaseURL:     "https://api.hubapi.com/crm",
 				DisplayName: "HubSpot CRM",
 				Support: Support{
+					BatchWrite: &BatchWriteSupport{
+						Create: BatchWriteSupportConfig{
+							DefaultRecordLimit: goutils.Pointer(100), // nolint:mnd
+							ObjectRecordLimits: nil,
+							Supported:          true,
+						},
+						Update: BatchWriteSupportConfig{
+							DefaultRecordLimit: goutils.Pointer(100), // nolint:mnd
+							ObjectRecordLimits: nil,
+							Supported:          true,
+						},
+					},
 					Read:  true,
 					Write: true,
 				},
@@ -401,6 +414,18 @@ func TestReadModuleInfo(t *testing.T) { // nolint:funlen,maintidx
 				BaseURL:     "https://api.hubapi.com/crm",
 				DisplayName: "HubSpot CRM",
 				Support: Support{
+					BatchWrite: &BatchWriteSupport{
+						Create: BatchWriteSupportConfig{
+							DefaultRecordLimit: goutils.Pointer(100), // nolint:mnd
+							ObjectRecordLimits: nil,
+							Supported:          true,
+						},
+						Update: BatchWriteSupportConfig{
+							DefaultRecordLimit: goutils.Pointer(100), // nolint:mnd
+							ObjectRecordLimits: nil,
+							Supported:          true,
+						},
+					},
 					Read:  true,
 					Write: true,
 				},
