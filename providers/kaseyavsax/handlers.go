ea343090de447140e4789cfeeb52d18d5cc644e1
jkarage <josephbkarage@gmail.com>
2025-12-01 13:03:40 +0300

    add kaseyavsax read connector

--- providers/kaseyavsax/handlers.go
+++ providers/kaseyavsax/handlers.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/common/naming"
@@ -12,7 +14,12 @@ import (
 
 const (
 	limitQuery       = "$top"
+	filterQuery      = "$filter"
 	metadataPageSize = "1"
+	metaField        = "Meta"
+	dataField        = "Data"
+	NextRecordsField = "NextQueryLink"
+	pageSize         = 100
 )
 
 type readResponse struct {
@@ -65,4 +72,47 @@ func (c *Connector) parseSingleObjectMetadataResponse(
 	}
 
 	return &objectMetadata, nil
+}
+
+func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
+	var (
+		url *urlbuilder.URL
+		err error
+	)
+
+	url, err = urlbuilder.New(c.ProviderInfo().BaseURL, restAPIVersion, params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	if !params.Since.IsZero() && supportsFiltering(params.ObjectName) {
+		f := fmt.Sprintf("UpdatedAt ge %v", params.Since.Format(time.RFC3339))
+		url.WithQueryParam(filterQuery, f)
+	}
+
+	url.WithQueryParam(limitQuery, strconv.Itoa(pageSize))
+
+	if params.NextPage != "" {
+		url, err = urlbuilder.New(params.NextPage.String())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
+}
+
+func (c *Connector) parseReadResponse(
+	ctx context.Context,
+	params common.ReadParams,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.ReadResult, error) {
+	return common.ParseResult(
+		response,
+		records(),
+		nextRecordsURL(),
+		common.GetMarshaledData,
+		params.Fields,
+	)
 }
