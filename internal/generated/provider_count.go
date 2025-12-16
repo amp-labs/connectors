f7205f0e1538f676bbd739aa799b08edf0cf69cd
Ampersand Ops <devops@withampersand.com>
2025-12-16 21:27:10 +0000

    [ampersand-ops] auto: update catalog

--- internal/generated/provider_count.go
+++ internal/generated/provider_count.go
@@ -2,4 +2,4 @@ package generated
 
 // This file will be updated automatically, do not edit it manually.
 
-const ProviderCount = 196
+const ProviderCount = 197
--- internal/generated/timestamp.go
+++ internal/generated/timestamp.go
@@ -2,4 +2,4 @@ package generated
 
 // This file will be updated automatically, do not edit it manually.
 
-const Timestamp = "2025-12-13T01:07:45Z"
+const Timestamp = "2025-12-16T21:27:10Z"
