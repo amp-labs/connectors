b9d2261b10f8ff16bcf74611a97e276c94eb4278
ejoo <ejazkarimhunzai@gmail.com>
2025-12-20 00:02:52 +0500

    feat(braintree): enable Read and Write support

--- providers/braintree.go
+++ providers/braintree.go
@@ -16,9 +16,9 @@ func init() {
 				Delete: false,
 			},
 			Proxy:     true,
-			Read:      false,
+			Read:      true,
 			Subscribe: false,
-			Write:     false,
+			Write:     true,
 		},
 		Media: &Media{
 			DarkMode: &MediaTypeDarkMode{
