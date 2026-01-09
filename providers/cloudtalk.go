ba383f79d90978786481552e9d5e152744577ee0
sajan jaiswal <sjnjaiswal@gmail.com>
2026-01-10 04:32:57 +0530

    enabled read and write for cloudtalk

--- providers/cloudtalk.go
+++ providers/cloudtalk.go
@@ -19,9 +19,9 @@ func init() {
 				Delete: false,
 			},
 			Proxy:     false,
-			Read:      false,
+			Read:      true,
 			Subscribe: false,
-			Write:     false,
+			Write:     true,
 		},
 		Media: &Media{
 			DarkMode: &MediaTypeDarkMode{
