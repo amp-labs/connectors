9b6f7e71a6a8b365ca653b711082980c937bace3
jkarage <josephbkarage@gmail.com>
2026-01-11 11:35:17 +0300

    add g2 read connector

--- providers/g2/connector.go
+++ providers/g2/connector.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/reader"
 	"github.com/amp-labs/connectors/internal/components/schema"
 	"github.com/amp-labs/connectors/internal/staticschema"
 	"github.com/amp-labs/connectors/providers"
@@ -32,6 +34,7 @@ type Connector struct {
 	common.RequireMetadata
 
 	components.SchemaProvider
+	components.Reader
 
 	// subjectProductId represents either subject_product_id or product_id.
 	subjectProductId string
@@ -58,6 +61,23 @@ func constructor(base *components.Connector) (*Connector, error) {
 
 	// Set the metadata provider for the connector
 	connector.SchemaProvider = schema.NewOpenAPISchemaProvider(connector.ProviderContext.Module(), schemas)
+
+	registry, err := components.NewEndpointRegistry(supportedOperations())
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the read provider for the connector
+	connector.Reader = reader.NewHTTPReader(
+		connector.HTTPClient().Client,
+		registry,
+		common.ModuleRoot,
+		operations.ReadHandlers{
+			BuildRequest:  connector.buildReadRequest,
+			ParseResponse: connector.parseReadResponse,
+			ErrorHandler:  common.InterpretError,
+		},
+	)
 
 	return connector, nil
 }
---
+++ providers/g2/utils.go
@@ -0,0 +1,276 @@
+package g2
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/jsonquery"
+	"github.com/spyzhov/ajson"
+)
+
+const (
+	PathBuyerIntent        string = "buyer_intent"
+	PathBuyerIntentSandBox        = "sandbox/buyer_intent"
+	PathCompetitors        string = "competitors"
+	PathDiscussions        string = "discussions"
+	PathDownloads          string = "downloads"
+	PathReviews            string = "reviews"
+	PathProductScreenshots string = "product/screenshots"
+	PathSnippets           string = "snippets"
+	PathIntegrationReviews string = "integration_reviews"
+	PathFeatures           string = "features"
+	PathVideos             string = "videos"
+	PathVideoReviews       string = "video_reviews"
+
+	PathCategories         string = "categories"
+	PathProducts           string = "products"
+	PathCategoriesFeatures string = "categories/features"
+	PathProductsFeatures   string = "products/features"
+	PathProductMappings    string = "product_mappings"
+	PathScreenshots        string = "screenshots"
+	PathQuestions          string = "questions"
+	PathVendors            string = "vendors"
+
+	maxPageSize = "100"
+)
+
+type ObjectConfig struct {
+	fieldsQuery      string
+	sinceQuery       string
+	untilQuery       string
+	pageSizeQuery    string
+	maximumPerPage   string
+	sinceValueFormat string
+}
+
+var readObjCfg = map[string]ObjectConfig{ // nolint: gochecknoglobals
+	PathBuyerIntent: {
+		fieldsQuery:      "dimensions",
+		sinceQuery:       "dimension_filters[time_gteq]",
+		pageSizeQuery:    "page[size]",
+		maximumPerPage:   "100",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathBuyerIntentSandBox: {
+		fieldsQuery:      "dimensions",
+		sinceQuery:       "dimension_filters[time_gteq]",
+		pageSizeQuery:    "page[size]",
+		maximumPerPage:   "50",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathCategories: {
+		fieldsQuery:      "fields[categories]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathCompetitors: {
+		fieldsQuery:    "fields[products]",
+		pageSizeQuery:  "per",
+		maximumPerPage: "50",
+	},
+	PathDiscussions: {
+		fieldsQuery: "fields[discussions]",
+	},
+	PathDownloads: {
+		fieldsQuery:      "fields[downloads]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathIntegrationReviews: { // needs incremental read live test
+		fieldsQuery: "fields[integration_reviews]",
+	},
+	PathCategoriesFeatures: {
+		fieldsQuery:      "fields[product_features]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathFeatures: {
+		fieldsQuery:      "fields[product_features]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathProductsFeatures: {
+		fieldsQuery:      "fields[product_features]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathProductMappings: {
+		fieldsQuery:      "fields[product_mappings]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathVideos: {
+		fieldsQuery:      "fields[product_videos]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathProducts: {
+		fieldsQuery:   "fields[products]",
+		pageSizeQuery: "page[size]",
+	},
+	PathQuestions: {
+		fieldsQuery:      "fields[questions]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathReviews: {
+		fieldsQuery:      "fields[reviews]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathScreenshots: {
+		fieldsQuery:      "fields[screenshots]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathProductScreenshots: {
+		fieldsQuery:      "fields[screenshots]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathSnippets: {
+		fieldsQuery: "fields[snippets]",
+	},
+	PathVendors: {
+		fieldsQuery:      "fields[vendors]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+	PathVideoReviews: {
+		fieldsQuery:      "fields[video_reviews]",
+		sinceQuery:       "filter[updated_at_gt]",
+		untilQuery:       "filter[updated_at_lt]",
+		sinceValueFormat: time.RFC3339,
+	},
+}
+
+// PathsConfig returns the appropriate path string based on the object name.
+// For product-specific paths, it requires a productID and returns a path in the
+// format "products/{productID}/{objectName}".
+func PathsConfig(productID, objectName string) (string, error) {
+	switch objectName {
+	// Product-specific paths - require productID
+	case PathBuyerIntent, PathCompetitors, PathDiscussions, PathReviews,
+		PathProductScreenshots, PathSnippets, PathIntegrationReviews,
+		PathFeatures, PathVideos, PathVideoReviews:
+		return fmt.Sprintf("products/%s/%s", productID, objectName), nil
+
+	// Paths - don't require productID
+	case PathCategories, PathProducts, PathCategoriesFeatures,
+		PathProductsFeatures, PathProductMappings, PathScreenshots,
+		PathQuestions, PathVendors:
+		return objectName, nil
+	// for testing only
+	case PathBuyerIntentSandBox:
+		return fmt.Sprintf("products/%s/buyer_intent", productID), nil
+	default:
+		return "", common.ErrObjectNotSupported
+	}
+}
+
+func (c *Connector) buildReadURL(params common.ReadParams) (*urlbuilder.URL, error) { // nolint: cyclop
+	var (
+		url            *urlbuilder.URL
+		err            error
+		restAPIVersion = "api/v2"
+	)
+
+	if params.NextPage != "" {
+		url, err = urlbuilder.New(params.NextPage.String())
+		if err != nil {
+			return nil, err
+		}
+
+		return url, nil
+	}
+
+	path, err := PathsConfig(c.subjectProductId, params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, exists := readObjCfg[params.ObjectName]
+	if !exists {
+		return nil, common.ErrObjectNotSupported
+	}
+
+	if params.ObjectName == PathBuyerIntentSandBox {
+		restAPIVersion = "api/sandbox"
+	}
+
+	url, err = urlbuilder.New(c.ProviderInfo().BaseURL, restAPIVersion, path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add fields query values
+	if cfg.fieldsQuery != "" {
+		url.WithQueryParam(cfg.fieldsQuery, strings.Join(params.Fields.List(), ","))
+	}
+
+	// Add page size query values
+	if cfg.pageSizeQuery != "" {
+		pageSize := maxPageSize
+
+		if cfg.maximumPerPage != "" {
+			pageSize = cfg.maximumPerPage
+		}
+
+		url.WithQueryParam(cfg.pageSizeQuery, pageSize)
+	}
+
+	if !params.Since.IsZero() && cfg.sinceQuery != "" {
+		url.WithQueryParam(cfg.sinceQuery, params.Since.Format(cfg.sinceValueFormat))
+	}
+
+	return url, nil
+}
+
+func nextRecordsURL() common.NextPageFunc {
+	return func(node *ajson.Node) (string, error) {
+		links, err := jsonquery.New(node).ObjectOptional("links")
+		if err != nil {
+			return "", err
+		}
+
+		nextURL, err := jsonquery.New(links).StringOptional("next")
+		if err != nil {
+			return "", err
+		}
+
+		if nextURL == nil {
+			return "", err
+		}
+
+		return *nextURL, nil
+	}
+}
+
+func supportedOperations() components.EndpointRegistryInput {
+	readSupport := []string{"*"}
+
+	return components.EndpointRegistryInput{
+		common.ModuleRoot: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
+	}
+}
