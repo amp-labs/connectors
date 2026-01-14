4a6d243504d1b0c4d7f863da27ae978b66146bdf
Ali Iqbal <ali.dev01@invo.email>
2026-01-14 16:39:54 +0500

    Use new NewOauth2ClientForProvider for Shopify connector

--- test/shopify/connector.go
+++ test/shopify/connector.go
@@ -8,14 +8,17 @@ import (
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/providers/shopify"
 	"github.com/amp-labs/connectors/test/utils"
+	"golang.org/x/oauth2"
 )
 
 func GetShopifyConnector(ctx context.Context) *shopify.Connector {
 	filePath := credscanning.LoadPath(providers.Shopify)
-	reader := utils.MustCreateProvCredJSON(filePath, false)
+	reader := utils.MustCreateProvCredJSON(filePath, true)
 
 	workspace := reader.Get(credscanning.Fields.Workspace)
-	client := utils.NewAPIKeyClient(ctx, reader, providers.Shopify)
+
+	// Shopify uses OAuth2 with a custom header (X-Shopify-Access-Token)
+	client := utils.NewOauth2ClientForProvider(ctx, providers.Shopify, reader, getOAuthConfig(workspace))
 
 	conn, err := shopify.NewConnector(common.ConnectorParams{
 		AuthenticatedClient: client,
@@ -26,4 +29,24 @@ func GetShopifyConnector(ctx context.Context) *shopify.Connector {
 	}
 
 	return conn
+}
+
+func getOAuthConfig(workspace string) func(*credscanning.ProviderCredentials) *oauth2.Config {
+	return func(reader *credscanning.ProviderCredentials) *oauth2.Config {
+		return &oauth2.Config{
+			ClientID:     reader.Get(credscanning.Fields.ClientId),
+			ClientSecret: reader.Get(credscanning.Fields.ClientSecret),
+			RedirectURL:  "http://localhost:8080/callbacks/v1/oauth",
+			Endpoint: oauth2.Endpoint{
+				AuthURL:   "https://" + workspace + ".myshopify.com/admin/oauth/authorize",
+				TokenURL:  "https://" + workspace + ".myshopify.com/admin/oauth/access_token",
+				AuthStyle: oauth2.AuthStyleInParams,
+			},
+			Scopes: []string{
+				"read_customers", "write_customers",
+				"read_products", "write_products",
+				"read_orders", "write_orders",
+			},
+		}
+	}
 }
