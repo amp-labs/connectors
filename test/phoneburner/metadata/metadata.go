2af1462bfd18c99537e343cceb689a54dbb1ff5b
Ali Iqbal <ali.dev01@invo.email>
2026-01-23 16:26:03 +0500

    Metadata update

---
+++ test/phoneburner/metadata/metadata.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/amp-labs/connectors/test/phoneburner"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	ctx := context.Background()
+
+	conn := phoneburner.GetPhoneBurnerConnector(ctx)
+
+	m, err := conn.ListObjectMetadata(ctx, []string{"contacts", "tags", "folders", "members", "voicemails", "phonenumber"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	utils.DumpJSON(m, os.Stdout)
+}
+
