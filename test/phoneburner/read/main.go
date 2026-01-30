12d1b4f33708cf729009c9d1032e1b23f0219b9c
Ali Iqbal <ali.dev01@invo.email>
2026-01-30 15:47:47 +0500

    Added integration tests

---
+++ test/phoneburner/read/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os/signal"
+	"syscall"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	connTest "github.com/amp-labs/connectors/test/phoneburner"
+	"github.com/amp-labs/connectors/test/utils"
+	"github.com/amp-labs/connectors/test/utils/testscenario"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := connTest.GetPhoneBurnerConnector(ctx)
+
+	slog.Info("=== Reading contacts ===")
+	testscenario.ReadThroughPages(ctx, conn, common.ReadParams{
+		ObjectName: "contacts",
+		Fields:     connectors.Fields("contact_user_id", "first_name", "last_name", "raw_phone"),
+	})
+
+	slog.Info("=== Reading tags ===")
+	testscenario.ReadThroughPages(ctx, conn, common.ReadParams{
+		ObjectName: "tags",
+		Fields:     connectors.Fields("tag_id", "name"),
+	})
+
+	slog.Info("=== Reading members ===")
+	testscenario.ReadThroughPages(ctx, conn, common.ReadParams{
+		ObjectName: "members",
+		Fields:     connectors.Fields("user_id", "username", "email_address"),
+	})
+
+	slog.Info("=== Reading voicemails ===")
+	testscenario.ReadThroughPages(ctx, conn, common.ReadParams{
+		ObjectName: "voicemails",
+		Fields:     connectors.Fields("recording_id", "name", "created_when"),
+	})
+
+	// Folders is not paginated and returns a map shape, but the connector normalizes it to rows.
+	slog.Info("=== Reading folders ===")
+	testscenario.ReadThroughPages(ctx, conn, common.ReadParams{
+		ObjectName: "folders",
+		Fields:     connectors.Fields("folder_id", "folder_name"),
+	})
+}
+
