f5d868aa633a4fd59762edffba205d8a704e31f5
Ali Iqbal <ali.dev01@invo.email>
2026-01-30 14:40:47 +0500

    Build read request

--- providers/phoneburner/connector.go
+++ providers/phoneburner/connector.go
@@ -2,8 +2,11 @@ package phoneburner
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/reader"
 	"github.com/amp-labs/connectors/internal/components"
 	"github.com/amp-labs/connectors/internal/components/schema"
 	"github.com/amp-labs/connectors/providers"
@@ -19,6 +22,7 @@ type Connector struct {
 
 	// supported operations
 	components.SchemaProvider
+	components.Reader
 }
 
 func NewConnector(params common.ConnectorParams) (*Connector, error) {
@@ -33,6 +37,22 @@ func constructor(base *components.Connector) (*Connector, error) {
 		metadata.Schemas,
 	)
 
+	registry, err := components.NewEndpointRegistry(supportedOperations())
+	if err != nil {
+		return nil, err
+	}
+
+	connector.Reader = reader.NewHTTPReader(
+		connector.HTTPClient().Client,
+		registry,
+		connector.ProviderContext.Module(),
+		operations.ReadHandlers{
+			BuildRequest:  connector.buildReadRequest,
+			ParseResponse: connector.parseReadResponse,
+			ErrorHandler:  common.InterpretError,
+		},
+	)
+
 	return connector, nil
 }
 
@@ -45,4 +65,17 @@ func (c *Connector) ListObjectMetadata(
 	}
 
 	return c.SchemaProvider.ListObjectMetadata(ctx, objectNames)
+}
+
+func (c *Connector) buildReadRequest(ctx context.Context, params common.ReadParams) (*http.Request, error) {
+	return buildReadRequest(ctx, c.ProviderInfo().BaseURL, params)
+}
+
+func (c *Connector) parseReadResponse(
+	ctx context.Context,
+	params common.ReadParams,
+	request *http.Request,
+	response *common.JSONHTTPResponse,
+) (*common.ReadResult, error) {
+	return parseReadResponse(ctx, params, request, response)
 }
