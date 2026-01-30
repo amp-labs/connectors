4004182af5a05cee2f6116ee469346add9b0d367
Ali Iqbal <ali.dev01@invo.email>
2026-01-30 14:35:10 +0500

    Added read support

---
+++ providers/phoneburner/support.go
@@ -0,0 +1,29 @@
+package phoneburner
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/components"
+)
+
+func supportedOperations() components.EndpointRegistryInput {
+	readSupport := []string{
+		"contacts",
+		"folders",
+		"members",
+		"tags",
+		"voicemails",
+	}
+
+	return components.EndpointRegistryInput{
+		common.ModuleRoot: {
+			{
+				Endpoint: fmt.Sprintf("{%s}", strings.Join(readSupport, ",")),
+				Support:  components.ReadSupport,
+			},
+		},
+	}
+}
+
