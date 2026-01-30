06349b627ca7b2d7dd799ee54928bf4ad0b11bdf
Ali Iqbal <ali.dev01@invo.email>
2026-01-30 16:10:22 +0500

    Added 33 more objects

--- test/phoneburner/metadata/main.go
+++ test/phoneburner/metadata/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	conn := phoneburner.GetPhoneBurnerConnector(ctx)
 
-	m, err := conn.ListObjectMetadata(ctx, []string{"contacts", "tags", "folders", "members", "voicemails", "phonenumber"})
+	m, err := conn.ListObjectMetadata(ctx, []string{"contacts", "tags", "folders", "members", "voicemails", "phonenumber", "content", "dialsession", "customfields"})
 	if err != nil {
 		log.Fatal(err)
 	}
