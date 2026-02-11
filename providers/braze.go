5e677433762283b1756b1e6a681d31f5e480e614
jkarage <josephbkarage@gmail.com>
2026-02-11 10:07:10 +0300

    add brazeEU provider

--- providers/braze.go
+++ providers/braze.go
@@ -2,6 +2,9 @@ package providers
 
 const Braze Provider = "braze"
 
+// BrazeEU serves customers using braze's france data-center only, the rest should use Braze.
+const BrazeEU Provider = "brazeEU"
+
 func init() {
 	SetInfo(Braze, ProviderInfo{
 		DisplayName: "Braze",
@@ -46,4 +49,49 @@ func init() {
 			},
 		},
 	})
+
+	SetInfo(BrazeEU, ProviderInfo{
+		DisplayName: "BrazeEU",
+		AuthType:    ApiKey,
+		BaseURL:     "https://rest.{{.workspace}}.braze.eu",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Header,
+			Header: &ApiKeyOptsHeader{
+				Name:        "Authorization",
+				ValuePrefix: "Bearer ",
+			},
+			DocsURL: "https://www.braze.com/docs/api/basics/#creating-rest-api-keys",
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740565804/media/braze.com_1740565802.png",
+				LogoURL: " https://res.cloudinary.com/dycvts6vp/image/upload/v1740565904/media/braze.com_1740565903.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740565804/media/braze.com_1740565802.png",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740565849/media/braze.com_1740565848.svg",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     true,
+			Read:      true,
+			Subscribe: false,
+			Write:     true,
+		},
+		Metadata: &ProviderMetadata{
+			Input: []MetadataItemInput{
+				{
+					Name:        "workspace",
+					DisplayName: "Instance ID",
+				},
+			},
+		},
+	})
+
 }
