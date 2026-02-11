41a55c58c08a1bb0754e564d75e762d0ec209a7b
jkarage <josephbkarage@gmail.com>
2026-02-11 10:37:43 +0300

    request whole domain

--- providers/braze.go
+++ providers/braze.go
@@ -1,18 +1,13 @@
 package providers
 
-const (
-	Braze Provider = "braze"
-
-	// BrazeEU serves customers using braze's france data-center only, the rest should use Braze.
-	BrazeEU Provider = "brazeEU"
-)
+const Braze Provider = "braze"
 
 // nolint: funlen
 func init() {
 	SetInfo(Braze, ProviderInfo{
 		DisplayName: "Braze",
 		AuthType:    ApiKey,
-		BaseURL:     "https://rest.{{.workspace}}.braze.com",
+		BaseURL:     "https://{{.workspace}}",
 		ApiKeyOpts: &ApiKeyOpts{
 			AttachmentType: Header,
 			Header: &ApiKeyOptsHeader{
@@ -47,56 +42,10 @@ func init() {
 			Input: []MetadataItemInput{
 				{
 					Name:        "workspace",
-					DisplayName: "Instance ID",
-					Prompt:      "Instance ID where your data is being hosted (ex: iad-01, au-01)",
+					DisplayName: "Rest Instance Endpoint",
+					Prompt:      "Rest Instance ID where your data is being hosted (ex: rest.iad-01.braze.com)",
 				},
 			},
 		},
 	})
-
-	SetInfo(BrazeEU, ProviderInfo{
-		DisplayName: "BrazeEU",
-		AuthType:    ApiKey,
-		BaseURL:     "https://rest.{{.workspace}}.braze.eu",
-		ApiKeyOpts: &ApiKeyOpts{
-			AttachmentType: Header,
-			Header: &ApiKeyOptsHeader{
-				Name:        "Authorization",
-				ValuePrefix: "Bearer ",
-			},
-			DocsURL: "https://www.braze.com/docs/api/basics/#creating-rest-api-keys",
-		},
-		Media: &Media{
-			DarkMode: &MediaTypeDarkMode{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740565804/media/braze.com_1740565802.png",
-				LogoURL: " https://res.cloudinary.com/dycvts6vp/image/upload/v1740565904/media/braze.com_1740565903.svg",
-			},
-			Regular: &MediaTypeRegular{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740565804/media/braze.com_1740565802.png",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1740565849/media/braze.com_1740565848.svg",
-			},
-		},
-		Support: Support{
-			BulkWrite: BulkWriteSupport{
-				Insert: false,
-				Update: false,
-				Upsert: false,
-				Delete: false,
-			},
-			Proxy:     true,
-			Read:      true,
-			Subscribe: false,
-			Write:     true,
-		},
-		Metadata: &ProviderMetadata{
-			Input: []MetadataItemInput{
-				{
-					Name:        "workspace",
-					DisplayName: "Instance ID",
-					Prompt:      "Instance ID where your data is being hosted (ex: fra-01, fra-02)",
-				},
-			},
-		},
-	})
-
 }
