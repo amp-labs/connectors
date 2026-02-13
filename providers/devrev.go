e64a7f1952349a42c6e99d1e22efb26b43b49097
lafetz <yafetyohaness@gmail.com>
2026-02-13 16:24:57 +0300

    wip: devrev-proxy

---
+++ providers/devrev.go
@@ -0,0 +1,50 @@
+package providers
+
+const DevRev = "devrev"
+
+func init() {
+	SetInfo(DevRev, ProviderInfo{
+		DisplayName: "DevRev",
+		AuthType:    Custom,
+		BaseURL:     "https://api.devrev.ai",
+
+		CustomOpts: &CustomAuthOpts{
+			Headers: []CustomAuthHeader{
+				{
+					Name:          "Authorization",
+					ValueTemplate: "Bearer {{ .pat }}",
+				},
+			},
+			Inputs: []CustomAuthInput{
+				{
+					Name:        "pat",
+					DisplayName: "Personal Access Token",
+					Prompt:      "Create a PAT in DevRev: Settings > Account > Personal Access Token.",
+					DocsURL:     "https://developer.devrev.ai/about/authentication",
+				},
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1770988610/media/devrev.ai_1770988609.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1770988771/media/devrev.ai_1770988771.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1770988685/media/devrev.ai_1770988685.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1770988737/media/devrev.ai_1770988737.svg",
+			},
+		},
+	})
+}
