75dbdd0100b8361fad0a318676068b41525eed39
jkarage <josephbkarage@gmail.com>
2026-02-17 08:13:00 +0300

    add talkdesk read connector

---
+++ providers/talkdesk/params.go
@@ -0,0 +1,46 @@
+package talkdesk
+
+import "github.com/amp-labs/connectors/internal/datautils"
+
+var (
+	filtersByUpdates  = datautils.NewSet("do-not-call-lists", "record-lists", "prompts") // nolint: gochecknoglobals
+	filtersByCreation = datautils.NewSet("campaigns", "")                                // nolint: gochecknoglobals
+
+	// "cm/core/va/cases" updates by update_at.
+
+	responseKeys = map[string][]string{ // nolint: gochecknoglobals
+		"apps":                             {"_embedded", "apps"},
+		"contacts":                         {"_embedded", "contacts"},
+		"ring-groups":                      {"_embedded", "ring_groups"},
+		"do-not-call-lists":                {"_embedded", "do_not_call_lists"},
+		"bulk-imports/users":               {"_embedded", "imports"},
+		"express/accounts":                 {"_embedded", "accounts"},
+		"record-lists":                     {"_embedded", "record_lists"},
+		"identity/activities":              {"_embedded"},
+		"routing/attributes":               {"_embedded", "attributes"},
+		"routing/attribute-categories":     {"_embedded", "attribute_categories"},
+		"contacts/fetch":                   {"_embedded", "contacts"},
+		"prompts":                          {"_embedded", "prompts"},
+		"insights/available-queries":       {"_embedded", "available_queries"},
+		"guardian/users":                   {"_embedded", "users"},
+		"guardian/cases":                   {"_embedded", "cases"},
+		"calls-quality":                    {"_embedded", "calls_quality"},
+		"users":                            {"_embedded", "users"},
+		"campaigns":                        {"_embedded", "campaigns"},
+		"campaigns/scripts":                {"_embedded", "scripts"},
+		"campaigns/results":                {"_embedded", "results"},
+		"service-providers-resource-types": {"_embedded", "resource_types"},
+		"users/resource-types":             {"_embedded", "resource_types"},
+		"v2/users":                         {"_embedded", "users"},
+		"account-wallet":                   {"_embedded", "wallets"},
+		"cases":                            {"_embedded", "cases"},
+		"cases/fields":                     {"_embedded", "fields"},
+		"teams":                            {"_embedded", "teams"},
+		"express/products":                 {"_embedded", "products"},
+		"express/subscriptions":            {"_embedded", "subscriptions"},
+		"express/invoices":                 {"_embedded", "invoices"},
+		"queues":                           {"_embedded", "queues"},
+		"webhooks":                         {"_embedded", "webhooks"},
+		"call-recordings":                  {"_embedded", "call_recordings"},
+	}
+)
---
+++ test/talkdesk/read/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/providers/talkdesk"
+	testTalkdesk "github.com/amp-labs/connectors/test/talkdesk"
+	"github.com/amp-labs/connectors/test/utils"
+)
+
+func main() {
+	// Handle Ctrl-C gracefully.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	// Set up slog logging.
+	utils.SetupLogging()
+
+	conn := testTalkdesk.NewConnector(ctx)
+
+	if err := readContacts(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := readDNCL(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+
+	if err := readRecordLists(ctx, conn); err != nil {
+		slog.Error(err.Error())
+	}
+}
+
+func readContacts(ctx context.Context, conn *talkdesk.Connector) error {
+	config := connectors.ReadParams{
+		ObjectName: "contacts",
+		Since:      time.Now().Add(-1000 * time.Hour),
+		Until:      time.Now().Add(-1 * time.Hour),
+		Fields:     connectors.Fields("phones", "name", "id"),
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
+
+func readDNCL(ctx context.Context, conn *talkdesk.Connector) error {
+	config := connectors.ReadParams{
+		ObjectName: "do-not-call-lists",
+		Since:      time.Now().Add(-7020 * time.Hour),
+		Fields:     connectors.Fields("description", "id"),
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
+
+func readRecordLists(ctx context.Context, conn *talkdesk.Connector) error {
+	config := connectors.ReadParams{
+		ObjectName: "record-lists",
+		Since:      time.Now().Add(-7020 * time.Hour),
+		Fields:     connectors.Fields("status", "name", "category", "id"),
+	}
+
+	result, err := conn.Read(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	// Print the results
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, _ = os.Stdout.Write(jsonStr)
+	_, _ = os.Stdout.WriteString("\n")
+
+	return nil
+}
