f7f0dc671797fa8d18e4b2c7f6c88dd35402e40a
Ali Iqbal <ali.dev01@invo.email>
2026-02-18 19:11:44 +0500

    Added metadata

--- providers/revenueCat.go
+++ providers/revenueCat.go
@@ -39,5 +39,15 @@ func init() {
 			Subscribe: false,
 			Write:     false,
 		},
+		Metadata: &ProviderMetadata{
+			Input: []MetadataItemInput{
+				{
+					Name:        "project_id",
+					DisplayName: "Project ID",
+					DocsURL:     "https://www.revenuecat.com/docs/api-v2#tag/Project",
+					Prompt:      "Copy the Project ID from RevenueCat dashboard (Project settings → General). It typically starts with `proj`.",
+				},
+			},
+		},
 	})
 }
