243c88d3ccb21b69e741e908aa63a5f8e2792a6a
Ali Iqbal <ali.dev01@invo.email>
2026-02-20 11:28:03 +0500

    Revenuecat changes

--- providers/revenuecat/connector.go
+++ providers/revenuecat/connector.go
@@ -1,14 +1,9 @@
 package revenuecat
 
 import (
-	"strings"
-
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/components"
-	"github.com/amp-labs/connectors/internal/components/deleter"
-	"github.com/amp-labs/connectors/internal/components/operations"
 	"github.com/amp-labs/connectors/internal/components/schema"
-	"github.com/amp-labs/connectors/internal/components/writer"
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/providers/revenuecat/metadata"
 )
@@ -16,64 +11,20 @@ import (
 type Connector struct {
 	*components.Connector
 	components.SchemaProvider
-	components.Writer
-	components.Deleter
 
 	// Require authenticated client
 	common.RequireAuthenticatedClient
-	common.RequireMetadata
-
-	ProjectID string
 }
 
 func NewConnector(params common.ConnectorParams) (*Connector, error) {
-	conn, err := components.Initialize(providers.RevenueCat, params, constructor)
-	if err != nil {
-		return nil, err
-	}
-
-	for k, v := range params.Metadata {
-		if strings.EqualFold(k, "project_id") {
-			conn.ProjectID = v
-			break
-		}
-	}
-
-	return conn, nil
+	return components.Initialize(providers.RevenueCat, params, constructor)
 }
 
 func constructor(base *components.Connector) (*Connector, error) {
-	connector := &Connector{
-		Connector: base,
-		RequireMetadata: common.RequireMetadata{
-			ExpectedMetadataKeys: []string{"project_id"},
-		},
-	}
+	connector := &Connector{Connector: base}
 
 	connector.SchemaProvider = schema.NewCompositeSchemaProvider(
 		schema.NewOpenAPISchemaProvider(connector.ProviderContext.Module(), metadata.Schemas),
-	)
-
-	connector.Writer = writer.NewHTTPWriter(
-		connector.HTTPClient().Client,
-		components.NewEmptyEndpointRegistry(),
-		connector.ProviderContext.Module(),
-		operations.WriteHandlers{
-			BuildRequest:  connector.buildWriteRequest,
-			ParseResponse: connector.parseWriteResponse,
-			ErrorHandler:  common.InterpretError,
-		},
-	)
-
-	connector.Deleter = deleter.NewHTTPDeleter(
-		connector.HTTPClient().Client,
-		components.NewEmptyEndpointRegistry(),
-		connector.ProviderContext.Module(),
-		operations.DeleteHandlers{
-			BuildRequest:  connector.buildDeleteRequest,
-			ParseResponse: connector.parseDeleteResponse,
-			ErrorHandler:  common.InterpretError,
-		},
 	)
 
 	return connector, nil
