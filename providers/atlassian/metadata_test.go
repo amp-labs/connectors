048d2730e34d860ca049666469da3bf6ea00303f
Cobalt0s <constantin.y.koval@gmail.com>
2026-02-21 03:24:16 +0200

    [CON-1950] feat(confluence): ListObjectMetadata

--- providers/atlassian/metadata_test.go
+++ providers/atlassian/metadata_test.go
@@ -105,3 +105,80 @@ func TestListObjectMetadata(t *testing.T) { // nolint:funlen,gocognit,cyclop
 		})
 	}
 }
+
+func TestListObjectMetadataConfluence(t *testing.T) { // nolint:funlen,gocognit,cyclop
+	t.Parallel()
+
+	tests := []testroutines.Metadata{
+		{
+			Name:         "At least one object name must be queried",
+			Input:        nil,
+			Server:       mockserver.Dummy(),
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:       "Successfully describe one object with metadata",
+			Input:      []string{"pages", "blogposts"},
+			Server:     mockserver.Dummy(),
+			Comparator: testroutines.ComparatorSubsetMetadata,
+			Expected: &common.ListObjectMetadataResult{
+				Result: map[string]common.ObjectMetadata{
+					"pages": {
+						DisplayName: "Pages",
+						Fields: map[string]common.FieldMetadata{
+							"parentType": {
+								DisplayName:  "parentType",
+								ValueType:    "singleSelect",
+								ProviderType: "string",
+								Values: []common.FieldValue{{
+									Value:        "page",
+									DisplayValue: "page",
+								}, {
+									Value:        "whiteboard",
+									DisplayValue: "whiteboard",
+								}, {
+									Value:        "database",
+									DisplayValue: "database",
+								}, {
+									Value:        "embed",
+									DisplayValue: "embed",
+								}, {
+									Value:        "folder",
+									DisplayValue: "folder",
+								}},
+							},
+						},
+					},
+					"blogposts": {
+						DisplayName: "Blog Posts",
+						Fields: map[string]common.FieldMetadata{
+							"title": {
+								DisplayName:  "title",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+							"version": {
+								DisplayName:  "version",
+								ValueType:    "other",
+								ProviderType: "object",
+							},
+						},
+					},
+				},
+				Errors: map[string]error{},
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		// nolint:varnamelen
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.ObjectMetadataConnector, error) {
+				return constructTestConnectorConfluence(tt.Server.URL)
+			})
+		})
+	}
+}
