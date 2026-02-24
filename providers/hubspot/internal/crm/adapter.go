5f95eeeff2571ec2e838bb4221b4fe237e370523
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2026-02-24 23:36:29 +0200

    tidy(hubspot): Create package associations (#2665)

--- providers/hubspot/internal/crm/adapter.go
+++ providers/hubspot/internal/crm/adapter.go
@@ -9,6 +9,7 @@ import (
 	"github.com/amp-labs/connectors/internal/components/deleter"
 	"github.com/amp-labs/connectors/internal/components/operations"
 	"github.com/amp-labs/connectors/providers"
+	"github.com/amp-labs/connectors/providers/hubspot/internal/crm/associations"
 	"github.com/amp-labs/connectors/providers/hubspot/internal/crm/batch"
 	"github.com/amp-labs/connectors/providers/hubspot/internal/crm/core"
 	"github.com/amp-labs/connectors/providers/hubspot/internal/crm/custom"
@@ -29,8 +30,9 @@ type Adapter struct {
 
 	// CRM module sub-adapters
 	// These delegate specialized subsets of CRM functionality to keep Connector modular and prevent code bloat.
-	customAdapter *custom.Adapter // used for connectors.UpsertMetadataConnector capabilities.
-	batchAdapter  *batch.Adapter  // used for connectors.BatchWriteConnector capabilities.
+	customAdapter        *custom.Adapter // used for connectors.UpsertMetadataConnector capabilities.
+	batchAdapter         *batch.Adapter  // used for connectors.BatchWriteConnector capabilities.
+	AssociationsStrategy *associations.Strategy
 }
 
 // NewAdapter creates a new crm Adapter configured to work with Hubspot's APIs.
@@ -55,6 +57,7 @@ func constructor(base *components.Connector) (*Adapter, error) {
 	adapter.SetErrorHandler(core.InterpretJSONError)
 	adapter.customAdapter = custom.NewAdapter(adapter.JSONHTTPClient(), adapter.ModuleInfo())
 	adapter.batchAdapter = batch.NewAdapter(adapter.HTTPClient(), adapter.ModuleInfo())
+	adapter.AssociationsStrategy = associations.NewStrategy(adapter.JSONHTTPClient(), adapter.ModuleInfo())
 
 	return adapter, nil
 }
