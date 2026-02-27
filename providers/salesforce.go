f5509cb5e541bf4c56b12722a63cf1e4d8f2477e
AmandaYao00 <amanda.yao@berkeley.edu>
2026-02-26 16:17:22 -0800

    "business unit id"

--- providers/salesforce.go
+++ providers/salesforce.go
@@ -155,16 +155,16 @@ func init() { // nolint:funlen
 						ModuleSalesforceAccountEngagementDemo: {},
 					},
 				},
-				// {
-				// 	DisplayName: "Business Unit ID",
-				// 	DocsURL:     "https://help.salesforce.com/s/articleView?id=000381973&type=1",
-				// 	ModuleDependencies: &ModuleDependencies{
-				// 		ModuleSalesforceAccountEngagement:     {},
-				// 		ModuleSalesforceAccountEngagementDemo: {},
-				// 	},
-				// 	Name:   "businessUnitId",
-				// 	Prompt: "Business Unit ID is found in Business Unit Setup within Salesforce Setup or Marketing Setup.",
-				// },
+				{
+					DisplayName: "Business Unit ID",
+					DocsURL:     "https://developer.salesforce.com/docs/marketing/pardot/guide/authentication.html",
+					ModuleDependencies: &ModuleDependencies{
+						ModuleSalesforceAccountEngagement:     {},
+						ModuleSalesforceAccountEngagementDemo: {},
+					},
+					Name:   "businessUnitId",
+					Prompt: "Business Unit ID is the 18-character ID that starts with 0Uv, found in Business Unit Setup within Salesforce Setup or Marketing Setup.",
+				},
 			},
 		},
 	})
