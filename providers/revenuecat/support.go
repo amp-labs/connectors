3ec3f32759aa036364a8e1bd106a945579046cef
Ali Iqbal <ali.dev01@invo.email>
2026-03-09 15:25:03 +0500

    Fix lint

--- providers/revenuecat/support.go
+++ providers/revenuecat/support.go
@@ -6,6 +6,8 @@ import (
 
 // writeAndDeleteSupportedObjects are object names that support write and delete.
 // Subscriptions and other read-only objects are excluded. Validated in buildWriteRequest and buildDeleteRequest.
+//
+//nolint:gochecknoglobals
 var writeAndDeleteSupportedObjects = datautils.NewStringSet(
 	"apps",
 	"customers",
