5732c7b3952cad007694abdf5f3f16e447538f40
Cobalt0s <constantin.y.koval@gmail.com>
2026-03-12 05:12:38 +0200

    chore(pardot): Add more increment objects

--- providers/salesforce/internal/pardot/incremental.go
+++ providers/salesforce/internal/pardot/incremental.go
@@ -32,47 +32,118 @@ func nextPageFunc(node *ajson.Node) (string, error) {
 // Used for connector side filtering.
 var objectsFilterParam = map[string]string{ // nolint:gochecknoglobals
 	// Objects that have "updatedAt" field:
-	"campaigns":                  "updatedAt",
 	"custom-redirects":           "updatedAt",
-	"dynamic-contents":           "updatedAt",
-	"email-templates":            "updatedAt",
 	"engagement-studio-programs": "updatedAt",
 	"files":                      "updatedAt",
 	"folder-contents":            "updatedAt",
 	"folders":                    "updatedAt",
-	"form-fields":                "updatedAt",
 	"forms":                      "updatedAt",
-	"landing-pages":              "updatedAt",
 	"layout-templates":           "updatedAt",
-	"lifecycle-stages":           "updatedAt",
-	"list-emails":                "updatedAt",
-	"list-memberships":           "updatedAt",
-	"lists":                      "updatedAt",
-	"opportunities":              "updatedAt",
 	"prospect-accounts":          "updatedAt",
-	"prospects":                  "updatedAt",
-	"tags":                       "updatedAt",
 	"tracker-domains":            "updatedAt",
-	"users":                      "updatedAt",
-	"visitor-activities":         "updatedAt",
 	"visitors":                   "updatedAt",
-	"visits":                     "updatedAt",
-	// Objects that don't have "updatedAt" but have "createdAt":
-	"form-handler-fields": "createdAt",
-	"form-handlers":       "createdAt",
-	"lifecycle-histories": "createdAt",
-	"tagged-objects":      "createdAt",
-	"visitor-page-views":  "createdAt",
 }
 
 // Used for provider side filtering.
 var incrementalSinceQuery = map[string]string{ // nolint:gochecknoglobals
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/campaign-v5.html#query
+	"campaigns": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/dynamic-content-v5.html#dynamic-content-query
+	"dynamic-contents": "updatedAtAfterOrEqualTo",
 	// https://developer.salesforce.com/docs/marketing/pardot/guide/email-v5.html
 	"emails": "sentAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/email-template-v5.html#email-template-query
+	"email-templates": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/export-v5.html
+	"exports": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/external-activities-v5.html
+	"external-activities": "createdAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/form-field-v5.html
+	"form-fields": "createdAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/form-handler-v5.html
+	"form-handlers": "createdAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/form-handler-field-v5.html
+	"form-handler-fields": "createdAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/import-v5.html
+	"imports": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/landing-page-v5.html
+	"landing-pages": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/lifecycle-stage-v5.html
+	"lifecycle-stages": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/lifecycle-history-v5.html
+	"lifecycle-histories": "createdAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/list-v5.html
+	"lists": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/list-email-v5.html
+	"list-emails": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/list-membership-v5.html
+	"list-memberships": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/opportunity-v5.html
+	"opportunities": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/prospect-v5.html
+	"prospects": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/tag-v5.html
+	"tags": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/tagged-object-v5.html
+	"tagged-objects": "createdAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/user-v5.html
+	"users": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/visitor-activity-v5.html
+	"visitor-activities": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/visit-v5.html
+	"visits": "updatedAtAfterOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/visitor-page-view-v5.html
+	"visitor-page-views": "createdAtAfterOrEqualTo",
 }
 
 // Used for provider side filtering.
 var incrementalUntilQuery = map[string]string{ // nolint:gochecknoglobals
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/campaign-v5.html#query
+	"campaigns": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/dynamic-content-v5.html#dynamic-content-query
+	"dynamic-contents": "updatedAtBeforeOrEqualTo",
 	// https://developer.salesforce.com/docs/marketing/pardot/guide/email-v5.html
 	"emails": "sentAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/email-template-v5.html#email-template-query
+	"email-templates": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/export-v5.html
+	"exports": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/external-activities-v5.html
+	"external-activities": "createdAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/form-field-v5.html
+	"form-fields": "createdAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/form-handler-v5.html
+	"form-handlers": "createdAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/form-handler-field-v5.html
+	"form-handler-fields": "createdAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/import-v5.html
+	"imports": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/landing-page-v5.html
+	"landing-pages": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/lifecycle-stage-v5.html
+	"lifecycle-stages": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/lifecycle-history-v5.html
+	"lifecycle-histories": "createdAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/list-v5.html
+	"lists": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/list-email-v5.html
+	"list-emails": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/list-membership-v5.html
+	"list-memberships": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/opportunity-v5.html
+	"opportunities": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/prospect-v5.html
+	"prospects": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/tag-v5.html
+	"tags": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/tagged-object-v5.html
+	"tagged-objects": "createdAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/user-v5.html
+	"users": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/visitor-activity-v5.html
+	"visitor-activities": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/visit-v5.html
+	"visits": "updatedAtBeforeOrEqualTo",
+	// https://developer.salesforce.com/docs/marketing/pardot/guide/visitor-page-view-v5.html
+	"visitor-page-views": "createdAtBeforeOrEqualTo",
 }
