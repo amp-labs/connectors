b8ce4b6d1b44eda7e09ca71048271ed86bdadb10
Cobalt0s <constantin.y.koval@gmail.com>
2026-03-19 04:07:19 +0200

    [CON-3029] feat(connectWise): Write

---
+++ providers/connectwise/delete.go
@@ -0,0 +1,36 @@
+package connectwise
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/internal/httpkit"
+)
+
+func (c *Connector) buildDeleteRequest(ctx context.Context, params common.DeleteParams) (*http.Request, error) {
+	url, err := c.getReadURL(params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	url.AddPath(params.RecordId)
+
+	return http.NewRequestWithContext(ctx, http.MethodDelete, url.String(), nil)
+}
+
+func (c *Connector) parseDeleteResponse(
+	ctx context.Context,
+	params common.DeleteParams,
+	request *http.Request,
+	resp *common.JSONHTTPResponse,
+) (*common.DeleteResult, error) {
+	if httpkit.Status2xx(resp.Code) {
+		return nil, fmt.Errorf("%w: failed to delete record: %d", common.ErrRequestFailed, resp.Code)
+	}
+
+	return &common.DeleteResult{
+		Success: true,
+	}, nil
+}
