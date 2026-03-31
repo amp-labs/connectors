af8241d4da904348a166e81c5a01631c95d529c7
Dipu <dipuchaurasiya91@gmail.com>
2026-03-31 23:12:21 +0545

    feat(deel): add Deel provider

---
+++ providers/deel.go
@@ -0,0 +1,41 @@
+package providers
+
+const Deel Provider = "deel"
+
+func init() {
+	// Deel configuration
+	SetInfo(Deel, ProviderInfo{
+		DisplayName: "Deel",
+		AuthType:    Oauth2,
+		BaseURL:     "https://api.letsdeel.com",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType:                 AuthorizationCode,
+			AuthURL:                   "https://app.deel.com/oauth2/authorize",
+			TokenURL:                  "https://app.deel.com/oauth2/tokens",
+			ExplicitScopesRequired:    false,
+			ExplicitWorkspaceRequired: false,
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1774977960/media/deel.com_1774977958.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1774977989/media/deel.com_1774977988.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1774977960/media/deel.com_1774977958.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1774978008/media/deel.com_1774978007.svg",
+			},
+		},
+	})
+}
