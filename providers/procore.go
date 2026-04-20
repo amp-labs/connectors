8569c73cf922f6a9f7b4401d8a7c886fd7683b9c
Dipu <dipuchaurasiya91@gmail.com>
2026-04-20 15:14:47 +0545

    fix lint

--- providers/procore.go
+++ providers/procore.go
@@ -5,6 +5,7 @@ const (
 	ProcoreSandbox Provider = "procoreSandbox"
 )
 
+//nolint:funlen
 func init() {
 	SetInfo(Procore, ProviderInfo{
 		DisplayName: "Procore",
@@ -31,12 +32,12 @@ func init() {
 		},
 		Media: &Media{
 			DarkMode: &MediaTypeDarkMode{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428797/Image_1776428788_0_hanjjk.png",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428963/Procore-Logo-Signature-Design-PNG_vk5wro.png",
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428797/Image_1776428788_0_hanjjk.png",                //nolint: lll
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428963/Procore-Logo-Signature-Design-PNG_vk5wro.png", //nolint: lll
 			},
 			Regular: &MediaTypeRegular{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428739/Procore-Logo-Signage-Design-PNG_ftf2os.png",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1776428922/media/procore.com_1776428921.svg",
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428739/Procore-Logo-Signage-Design-PNG_ftf2os.png", //nolint: lll
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1776428922/media/procore.com_1776428921.svg",                         //nolint: lll
 			},
 		},
 	})
@@ -66,12 +67,12 @@ func init() {
 		},
 		Media: &Media{
 			DarkMode: &MediaTypeDarkMode{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428797/Image_1776428788_0_hanjjk.png",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428963/Procore-Logo-Signature-Design-PNG_vk5wro.png",
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428797/Image_1776428788_0_hanjjk.png",                //nolint: lll
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428963/Procore-Logo-Signature-Design-PNG_vk5wro.png", //nolint: lll
 			},
 			Regular: &MediaTypeRegular{
-				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428739/Procore-Logo-Signage-Design-PNG_ftf2os.png",
-				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1776428922/media/procore.com_1776428921.svg",
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/q_auto/f_auto/v1776428739/Procore-Logo-Signage-Design-PNG_ftf2os.png", //nolint: lll
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1776428922/media/procore.com_1776428921.svg",                         //nolint: lll
 			},
 		},
 	})
