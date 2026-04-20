20f8f24f10f25d8541fa1907b6ca25d6b6e54792
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2026-04-20 20:26:40 +0300

    feat(tests): Improve difference reporting (#1644)

--- test/utils/mockutils/readResult.go
+++ test/utils/mockutils/readResult.go
@@ -1,7 +1,7 @@
 package mockutils
 
 import (
-	"reflect"
+	"fmt"
 
 	"github.com/amp-labs/connectors/common"
 )
@@ -11,9 +11,11 @@ var ReadResultComparator = readResultComparator{}
 type readResultComparator struct{}
 
 // SubsetRaw checks that expected ReadResult.Raw is a subset of actual ReadResult.Raw
-func (readResultComparator) SubsetRaw(actual, expected *common.ReadResult) bool {
+func (readResultComparator) SubsetRaw(actual, expected *common.ReadResult) *CompareResult {
+	result := NewCompareResult()
 	if len(actual.Data) < len(expected.Data) {
-		return false
+		result.AddDiff(fmt.Sprintf("expected at least %d data entries, got %d", len(expected.Data), len(actual.Data)))
+		return result
 	}
 
 	if len(expected.Data) == 0 || len(expected.Data[0].Raw) == 0 {
@@ -22,19 +24,21 @@ func (readResultComparator) SubsetRaw(actual, expected *common.ReadResult) bool 
 
 	for i := range expected.Data {
 		for field := range expected.Data[i].Raw {
-			if !reflect.DeepEqual(actual.Data[i].Raw[field], expected.Data[i].Raw[field]) {
-				return false
-			}
+			got := actual.Data[i].Raw[field]
+			exp := expected.Data[i].Raw[field]
+			result.Assert(fmt.Sprintf("Data[%d].Raw[%s]", i, field), exp, got)
 		}
 	}
 
-	return true
+	return result
 }
 
 // SubsetFields checks that expected ReadResult.Fields is a subset of actual ReadResult.Fields
-func (readResultComparator) SubsetFields(actual, expected *common.ReadResult) bool {
+func (readResultComparator) SubsetFields(actual, expected *common.ReadResult) *CompareResult {
+	result := NewCompareResult()
 	if len(actual.Data) < len(expected.Data) {
-		return false
+		result.AddDiff(fmt.Sprintf("expected at least %d data entries, got %d", len(expected.Data), len(actual.Data)))
+		return result
 	}
 
 	if len(expected.Data) == 0 || len(expected.Data[0].Fields) == 0 {
@@ -43,71 +47,75 @@ func (readResultComparator) SubsetFields(actual, expected *common.ReadResult) bo
 
 	for i := range expected.Data {
 		for field := range expected.Data[i].Fields {
-			if !reflect.DeepEqual(actual.Data[i].Fields[field], expected.Data[i].Fields[field]) {
-				return false
-			}
+			got := actual.Data[i].Fields[field]
+			exp := expected.Data[i].Fields[field]
+			result.Assert(fmt.Sprintf("Data[%d].Fields[%s]", i, field), exp, got)
 		}
 	}
 
-	return true
+	return result
 }
 
 // SubsetAssociationsRaw checks that expected ReadResult.Associations are matching exactly,
 // but for each Association.Raw it only checks if every mentioned expected field is present in actual raw.
-func (readResultComparator) SubsetAssociationsRaw(actual, expected *common.ReadResult) bool {
+func (readResultComparator) SubsetAssociationsRaw(actual, expected *common.ReadResult) *CompareResult {
+	result := NewCompareResult()
 	if len(actual.Data) < len(expected.Data) {
-		return false
+		result.AddDiff(fmt.Sprintf("expected at least %d data entries, got %d", len(expected.Data), len(actual.Data)))
+		return result
 	}
 
 	for i := range expected.Data {
-		if len(actual.Data[i].Associations) != len(expected.Data[i].Associations) {
-			return false
+		message := fmt.Sprintf("Data[%d].Associations length", i)
+		if result.Assert(message, len(expected.Data[i].Associations), len(actual.Data[i].Associations)) {
+			continue
 		}
 
 		for key, expectedAssociations := range expected.Data[i].Associations {
 			actualAssociations, ok := actual.Data[i].Associations[key]
 			if !ok {
-				return false
+				result.AddDiff(fmt.Sprintf("Data[%d].Associations[%s] missing", i, key))
+				continue
 			}
 
-			if len(actualAssociations) != len(expectedAssociations) {
-				return false
+			message = fmt.Sprintf("Data[%d].Associations[%s] length", i, key)
+			if result.Assert(message, len(expectedAssociations), len(actualAssociations)) {
+				continue
 			}
 
 			for j := range expectedAssociations {
-				if actualAssociations[j].ObjectId != expectedAssociations[j].ObjectId {
-					return false
-				}
-
-				if actualAssociations[j].AssociationType != expectedAssociations[j].AssociationType {
-					return false
-				}
+				result.Assert(fmt.Sprintf("Data[%d].Associations[%s][%d].ObjectId", i, key, j),
+					expectedAssociations[j].ObjectId, actualAssociations[j].ObjectId)
+				result.Assert(fmt.Sprintf("Data[%d].Associations[%s][%d].ObjectId", i, key, j),
+					expectedAssociations[j].AssociationType, actualAssociations[j].AssociationType)
 
 				// Check if expected Raw is a subset of actual Raw
 				for field := range expectedAssociations[j].Raw {
-					if !reflect.DeepEqual(actualAssociations[j].Raw[field], expectedAssociations[j].Raw[field]) {
-						return false
-					}
+					exp := expectedAssociations[j].Raw[field]
+					got := actualAssociations[j].Raw[field]
+					message = fmt.Sprintf("Data[%d].Associations[%s][%d].Raw[%s]", i, key, j, field)
+					result.Assert(message, exp, got)
 				}
 			}
 		}
 	}
 
-	return true
+	return result
 }
 
 // Identifiers checks that actual rows have identifiers matching with expected.
-// Empty strings signify nothing should be compared.
-func (c readResultComparator) Identifiers(actual *common.ReadResult, expected *common.ReadResult) bool {
-	for key, datum := range expected.Data {
-		if datum.Id != "" {
-			if actual.Data[key].Id != datum.Id {
-				return false
-			}
+// NOTE: Empty strings signify nothing should be compared.
+func (c readResultComparator) Identifiers(actual *common.ReadResult, expected *common.ReadResult) *CompareResult {
+	result := NewCompareResult()
+	for index, datum := range expected.Data {
+		expectedID := datum.Id
+		if expectedID != "" {
+			actualID := actual.Data[index].Id
+			result.Assert(fmt.Sprintf("Data[%v].Id", index), expectedID, actualID)
 		}
 	}
 
-	return true
+	return result
 }
 
 func invalidTest(message string) {
