fd971fd2f2ddc3c9681ed982fbf7d7e34d9b32c3
Cobalt0s <constantin.y.koval@gmail.com>
2026-05-07 19:39:06 +0300

    (4) reorder

--- providers/hubspot/url.go
+++ providers/hubspot/url.go
@@ -5,12 +5,33 @@ import (
 	"github.com/amp-labs/connectors/providers/hubspot/internal/core"
 )
 
-func (c *Connector) getBatchReadURL(objectName string) (*urlbuilder.URL, error) {
-	return c.crmURL(core.APIVersion3, "objects", objectName, "batch", "read")
+// https://developers.hubspot.com/docs/api-reference/latest/account/account-information/get-account-details
+func (c *Connector) getAccountDetailsURL() (*urlbuilder.URL, error) {
+	return urlbuilder.New(c.ProviderInfo().BaseURL, "account-info", core.APIVersion2026March, "details")
+}
+
+// https://developers.hubspot.com/docs/api-reference/latest/crm/objects/schemas/get-schema
+func (c *Connector) getObjectSchemaURL(objectName string) (*urlbuilder.URL, error) {
+	return urlbuilder.New(c.ProviderInfo().BaseURL, "crm-object-schemas", core.APIVersion2026March, "schemas", objectName)
+}
+
+// https://developers.hubspot.com/docs/api-reference/latest/crm/properties/get-properties
+// Note: Version APIVersion2026March is NOT FOUND at the moment for this endpoint. Using older V3.
+func (c *Connector) getPropertiesURL(objectName string) (*urlbuilder.URL, error) {
+	return c.crmURL(core.APIVersion3, "properties", objectName, "/")
 }
 
 func (c *Connector) getCRMObjectsReadURL(objectName string) (*urlbuilder.URL, error) {
 	return c.crmURL(core.APIVersion3, "objects", objectName)
+}
+
+// https://developers.hubspot.com/docs/api-reference/latest/crm/objects/contacts/delete-contact
+func (c *Connector) getDeleteURL(objectName, recordID string) (*urlbuilder.URL, error) {
+	return c.crmURL("objects", core.APIVersion2026March, objectName, recordID)
+}
+
+func (c *Connector) getBatchReadURL(objectName string) (*urlbuilder.URL, error) {
+	return c.crmURL(core.APIVersion3, "objects", objectName, "batch", "read")
 }
 
 func (c *Connector) getCRMObjectsSearchURL(objectName string) (*urlbuilder.URL, error) {
@@ -21,34 +42,13 @@ func (c *Connector) getCRMSearchURL(objectName string) (*urlbuilder.URL, error) 
 	return c.crmURL(core.APIVersion3, objectName, "search")
 }
 
-// https://developers.hubspot.com/docs/api-reference/latest/crm/properties/get-properties
-// Note: Version APIVersion2026March is NOT FOUND at the moment for this endpoint. Using older V3.
-func (c *Connector) getPropertiesURL(objectName string) (*urlbuilder.URL, error) {
-	return c.crmURL(core.APIVersion3, "properties", objectName, "/")
-}
-
-// https://developers.hubspot.com/docs/api-reference/latest/crm/objects/schemas/get-schema
-func (c *Connector) getObjectSchemaURL(objectName string) (*urlbuilder.URL, error) {
-	return urlbuilder.New(c.ProviderInfo().BaseURL, "crm-object-schemas", core.APIVersion2026March, "schemas", objectName)
-}
-
-// https://developers.hubspot.com/docs/api-reference/latest/account/account-information/get-account-details
-func (c *Connector) getAccountDetailsURL() (*urlbuilder.URL, error) {
-	return urlbuilder.New(c.ProviderInfo().BaseURL, "account-info", core.APIVersion2026March, "details")
-}
-
-func (c *Connector) getURLFromRoot(relativePath string) string {
-	return c.ProviderInfo().BaseURL + relativePath
-}
-
-// https://developers.hubspot.com/docs/api-reference/latest/crm/objects/contacts/delete-contact
-func (c *Connector) getDeleteURL(objectName, recordID string) (*urlbuilder.URL, error) {
-	return c.crmURL("objects", core.APIVersion2026March, objectName, recordID)
-}
-
 func (c *Connector) crmURL(paths ...string) (*urlbuilder.URL, error) {
 	parts := append([]string{"crm"}, paths...)
 
 	// URL: "https://api.hubapi.com/crm"
 	return urlbuilder.New(c.ProviderInfo().BaseURL, parts...)
+}
+
+func (c *Connector) getURLFromRoot(relativePath string) string {
+	return c.ProviderInfo().BaseURL + relativePath
 }
