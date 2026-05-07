e5f2c751ef53a44ca78fda5679dc510f5560d4da
Ali Iqbal <ali.dev01@invo.email>
2026-05-07 12:36:02 +0500

    Keep only metadata

--- providers/livestorm/connector.go
+++ providers/livestorm/connector.go
@@ -3,11 +3,7 @@ package livestorm
 import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/components"
-	"github.com/amp-labs/connectors/internal/components/deleter"
-	"github.com/amp-labs/connectors/internal/components/operations"
-	"github.com/amp-labs/connectors/internal/components/reader"
 	"github.com/amp-labs/connectors/internal/components/schema"
-	"github.com/amp-labs/connectors/internal/components/writer"
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/providers/livestorm/metadata"
 )
@@ -17,9 +13,6 @@ type Connector struct {
 
 	common.RequireAuthenticatedClient
 	components.SchemaProvider
-	components.Reader
-	components.Writer
-	components.Deleter
 }
 
 func NewConnector(params common.ConnectorParams) (*Connector, error) {
@@ -32,39 +25,6 @@ func constructor(base *components.Connector) (*Connector, error) {
 	connector.SchemaProvider = schema.NewOpenAPISchemaProvider(
 		connector.ProviderContext.Module(),
 		metadata.Schemas,
-	)
-
-	connector.Reader = reader.NewHTTPReader(
-		connector.HTTPClient().Client,
-		components.NewEmptyEndpointRegistry(),
-		connector.ProviderContext.Module(),
-		operations.ReadHandlers{
-			BuildRequest:  connector.buildReadRequest,
-			ParseResponse: connector.parseReadResponse,
-			ErrorHandler:  common.InterpretError,
-		},
-	)
-
-	connector.Writer = writer.NewHTTPWriter(
-		connector.HTTPClient().Client,
-		components.NewEmptyEndpointRegistry(),
-		connector.ProviderContext.Module(),
-		operations.WriteHandlers{
-			BuildRequest:  connector.buildWriteRequest,
-			ParseResponse: connector.parseWriteResponse,
-			ErrorHandler:  common.InterpretError,
-		},
-	)
-
-	connector.Deleter = deleter.NewHTTPDeleter(
-		connector.HTTPClient().Client,
-		components.NewEmptyEndpointRegistry(),
-		connector.ProviderContext.Module(),
-		operations.DeleteHandlers{
-			BuildRequest:  connector.buildDeleteRequest,
-			ParseResponse: connector.parseDeleteResponse,
-			ErrorHandler:  common.InterpretError,
-		},
 	)
 
 	return connector, nil
