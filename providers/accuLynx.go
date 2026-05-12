4a08691b00e7e5a95cd8b9c11e41bb677b66d672
ejoo <ejazkarimhunzai@gmail.com>
2026-05-12 16:59:15 +0500

    feat(acculynx): add AccuLynx proxy connector

---
+++ providers/accuLynx.go
@@ -0,0 +1,41 @@
+package providers
+
+const AccuLynx Provider = "accuLynx"
+
+func init() {
+	SetInfo(AccuLynx, ProviderInfo{
+		DisplayName: "AccuLynx",
+		AuthType:    ApiKey,
+		BaseURL:     "https://api.acculynx.com",
+		ApiKeyOpts: &ApiKeyOpts{
+			AttachmentType: Header,
+			Header: &ApiKeyOptsHeader{
+				Name:        "Authorization",
+				ValuePrefix: "Bearer ",
+			},
+			DocsURL: "https://apidocs.acculynx.com/docs/authentication",
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1777290175/media/acculynx.com_1777290175.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1777290119/media/acculynx.com_1777290118.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1777290175/media/acculynx.com_1777290175.jpg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1777290119/media/acculynx.com_1777290118.svg",
+			},
+		},
+	})
+}
