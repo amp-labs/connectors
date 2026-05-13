b1cdcaa47249e3a48732e0fb290f26d12adb24a0
Ali Iqbal <ali.dev01@invo.email>
2026-05-13 13:51:33 +0500

    Comment instead of hard delete

--- providers/livestorm.go
+++ providers/livestorm.go
@@ -5,9 +5,49 @@ const (
 )
 
 func init() {
-	// API token auth is used for development and integration tests until OAuth2 client
-	// credentials are available from Livestorm support; then restore AuthType Oauth2 and
-	// Oauth2Opts. https://developers.livestorm.co/docs/api-token-authentication
+	// OAuth2 (restore when client id/secret are available from Livestorm support):
+	/*
+		SetInfo(Livestorm, ProviderInfo{
+			DisplayName: "Livestorm",
+			AuthType:    Oauth2,
+			BaseURL:     "https://api.livestorm.co",
+			Oauth2Opts: &Oauth2Opts{
+				GrantType:                 AuthorizationCode,
+				AuthURL:                   "https://app.livestorm.co/oauth/authorize",
+				TokenURL:                  "https://app.livestorm.co/oauth/token",
+				ExplicitScopesRequired:    true,
+				ExplicitWorkspaceRequired: false,
+				TokenMetadataFields: TokenMetadataFields{
+					ScopesField: "scope",
+				},
+			},
+			Support: Support{
+				BulkWrite: BulkWriteSupport{
+					Insert: false,
+					Update: false,
+					Upsert: false,
+					Delete: false,
+				},
+				Proxy:     true,
+				Read:      false,
+				Subscribe: false,
+				Write:     false,
+			},
+			Media: &Media{
+				DarkMode: &MediaTypeDarkMode{
+					IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1741158143/media/api.livestorm.co_1741158142.svg",
+					LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1741158176/media/api.livestorm.co_1741158174.svg",
+				},
+				Regular: &MediaTypeRegular{
+					IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1741158111/media/api.livestorm.co_1741158108.jpg",
+					LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1741158160/media/api.livestorm.co_1741158158.svg",
+				},
+			},
+		})
+	*/
+
+	// API token auth for development and integration tests until OAuth2 is restored above.
+	// https://developers.livestorm.co/docs/api-token-authentication
 	SetInfo(Livestorm, ProviderInfo{
 		DisplayName: "Livestorm",
 		AuthType:    ApiKey,
