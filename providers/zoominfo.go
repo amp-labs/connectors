eb377adce767e3a1c3ea7a9e28c35238c6bb2856
jkarage <josephbkarage@gmail.com>
2026-05-20 16:46:18 +0300

    add zoominfo provider

---
+++ providers/zoominfo.go
@@ -0,0 +1,44 @@
+package providers
+
+const ZoomInfo Provider = "zoominfo"
+
+func init() {
+	// ZoomInfo configuration
+	SetInfo(ZoomInfo, ProviderInfo{
+		DisplayName: "ZoomInfo",
+		AuthType:    Oauth2,
+		BaseURL:     "https://api.zoominfo.com",
+		Oauth2Opts: &Oauth2Opts{
+			AuthURL:                   "https://api.zoominfo.com/gtm/oauth/v1/authorize",
+			TokenURL:                  "https://api.zoominfo.com/gtm/oauth/v1/token",
+			ExplicitScopesRequired:    false,
+			ExplicitWorkspaceRequired: false,
+			GrantType:                 AuthorizationCodePKCE,
+			TokenMetadataFields: TokenMetadataFields{
+				ScopesField: "scope",
+			},
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1758621074/media/zoominfo.com_1758621078.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1758621183/media/zoominfo.com_1758621188.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1758621074/media/zoominfo.com_1758621078.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1758621167/media/zoominfo.com_1758621172.svg",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+}
