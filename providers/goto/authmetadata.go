af6598850e7faa4cf875d7e036defa6f68011647
Dipu <103568666+immdipu@users.noreply.github.com>
2026-05-22 14:07:36 +0545

    feat(goTo): add postAuth and modules (#2947)

---
+++ providers/goto/authmetadata.go
@@ -0,0 +1,68 @@
+package gotoconn
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/common/urlbuilder"
+)
+
+func (c *Connector) GetPostAuthInfo(ctx context.Context) (*common.PostAuthInfo, error) {
+	accountKey, err := c.retrieveAccountKey(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if accountKey == "" {
+		return nil, common.ErrMissingExpectedValues
+	}
+
+	c.accountKey = accountKey
+
+	return &common.PostAuthInfo{
+		CatalogVars: AuthMetadataVars{
+			AccountKey: accountKey,
+		}.AsMap(),
+		RawResponse: nil,
+	}, nil
+}
+
+func (c *Connector) retrieveAccountKey(ctx context.Context) (string, error) {
+	url, err := c.getMeURL()
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := c.JSONHTTPClient().Get(ctx, url.String())
+	if err != nil {
+		return "", err
+	}
+
+	if _, ok := resp.Body(); !ok {
+		return "", common.ErrEmptyJSONHTTPResponse
+	}
+
+	data, err := common.UnmarshalJSON[map[string]any](resp)
+	if err != nil {
+		return "", common.ErrFailedToUnmarshalBody
+	}
+
+	rawAccountKey, ok := (*data)["accountKey"]
+	if !ok {
+		return "", common.ErrMissingExpectedValues
+	}
+
+	switch v := rawAccountKey.(type) {
+	case string:
+		return v, nil
+	case float64:
+		return strconv.FormatInt(int64(v), 10), nil
+	default:
+		return "", common.ErrMissingExpectedValues
+	}
+}
+
+func (c *Connector) getMeURL() (*urlbuilder.URL, error) {
+	return urlbuilder.New(c.ProviderInfo().BaseURL, "/admin/rest/v1/me")
+}
