3b7d4e61b0f4c0cd22650be88b9a2497105d7789
Ali Iqbal <ali.dev01@invo.email>
2026-06-10 16:29:12 +0500

    Metadata test case

---
+++ providers/breezy/metadata_test.go
@@ -0,0 +1,150 @@
+package breezy
+
+import (
+	"testing"
+
+	"github.com/amp-labs/connectors"
+	"github.com/amp-labs/connectors/common"
+	"github.com/amp-labs/connectors/test/utils/mockutils"
+	"github.com/amp-labs/connectors/test/utils/mockutils/mockserver"
+	"github.com/amp-labs/connectors/test/utils/testroutines"
+)
+
+func TestListObjectMetadata(t *testing.T) {
+	t.Parallel()
+
+	tests := []testroutines.Metadata{
+		{
+			Name:         "Empty objects returns missing objects error",
+			Input:        nil,
+			Server:       mockserver.Dummy(),
+			Expected:     nil,
+			ExpectedErrs: []error{common.ErrMissingObjects},
+		},
+		{
+			Name:       "Successful metadata for recruiting objects",
+			Input:      []string{"companies", "positions", "candidates", "webhook_endpoints"},
+			Server:     mockserver.Dummy(),
+			Comparator: testroutines.ComparatorSubsetMetadata,
+			Expected: &common.ListObjectMetadataResult{
+				Result: map[string]common.ObjectMetadata{
+					"companies": {
+						DisplayName: "Companies",
+						Fields: map[string]common.FieldMetadata{
+							"_id": {
+								DisplayName:  "Company Id",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+							"name": {
+								DisplayName:  "Name",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+						},
+					},
+					"positions": {
+						DisplayName: "Positions",
+						Fields: map[string]common.FieldMetadata{
+							"_id": {
+								DisplayName:  "Position Id",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+							"name": {
+								DisplayName:  "Name",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+						},
+					},
+					"candidates": {
+						DisplayName: "Candidates",
+						Fields: map[string]common.FieldMetadata{
+							"_id": {
+								DisplayName:  "Candidate Id",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+							"email_address": {
+								DisplayName:  "Email Address",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+						},
+					},
+					"webhook_endpoints": {
+						DisplayName: "Webhook Endpoints",
+						Fields: map[string]common.FieldMetadata{
+							"id": {
+								DisplayName:  "Id",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+							"url": {
+								DisplayName:  "Url",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+						},
+					},
+				},
+				Errors: map[string]error{},
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name:       "Unsupported object returns object not supported error",
+			Input:      []string{"companies", "unknown_object"},
+			Server:     mockserver.Dummy(),
+			Comparator: testroutines.ComparatorSubsetMetadata,
+			Expected: &common.ListObjectMetadataResult{
+				Result: map[string]common.ObjectMetadata{
+					"companies": {
+						DisplayName: "Companies",
+						Fields: map[string]common.FieldMetadata{
+							"_id": {
+								DisplayName:  "Company Id",
+								ValueType:    "string",
+								ProviderType: "string",
+							},
+						},
+					},
+				},
+				Errors: map[string]error{
+					"unknown_object": mockutils.ExpectedSubsetErrors{common.ErrObjectNotSupported},
+				},
+			},
+			ExpectedErrs: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.Run(t, func() (connectors.ObjectMetadataConnector, error) {
+				return constructTestConnector(tt.Server.URL)
+			})
+		})
+	}
+}
+
+func constructTestConnector(serverURL string) (*Connector, error) {
+	connector, err := NewConnector(
+		common.ConnectorParams{
+			Module:              common.ModuleRoot,
+			AuthenticatedClient: mockutils.NewClient(),
+			Metadata: map[string]string{
+				"company_id": "abc123def456",
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	connector.SetUnitTestBaseURL(serverURL)
+
+	return connector, nil
+}
