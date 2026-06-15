b8e36ee2529526d6e60784830fa85587935f8f6a
Joseph Karage <josephbkarage@gmail.com>
2026-06-15 19:33:24 +0300

    [CON-3259] feat: update object name in servicenow connector (#3041)
    * update object naming in servicenow connector
    
    * lint
    
    * revert test search incident
    
    * [CON-3259] feat: explicitly add read write supported objects  (#3042)
    
    * per operation read write support
    
    * lint
    
    * [CON-3259] feat: add metadata v2 support servicenow connector  (#3043)
    
    * update reads, V2 metadata, empty-404 handling
    
    * lint
    
    * [CON-3259] feat: populate recordId in write connector response (#3044)
    
    * write response parsing with RecordId
    
    * lint
    
    * [CON-3259, CON-3260] feat: add incremental read & pagination support in servicenow connector (#3045)
    
    * support incremental read and pagination
    
    * remove unused binary
    
    * update tests
    
    * lint
    
    * resolve written recordId parsing

--- providers/servicenow/search.go
+++ providers/servicenow/search.go
@@ -28,8 +28,8 @@ func (c *Connector) Search(ctx context.Context, params *common.SearchParams) (*c
 	}
 
 	return common.ParseResult(resp,
-		common.ExtractRecordsFromPath("result"),
-		getNextRecordsURL(resp),
+		recordsFunc(params.ObjectName),
+		getNextRecordsURL(resp, c.ProviderInfo().BaseURL),
 		common.GetMarshaledData,
 		params.Fields,
 	)
@@ -40,7 +40,12 @@ func (c *Connector) constructSearchURL(params *common.SearchParams) (*urlbuilder
 		return urlbuilder.New(params.NextPage.String())
 	}
 
-	url, err := urlbuilder.New(c.ProviderInfo().BaseURL, restAPIPrefix, params.ObjectName)
+	path, err := objectPath(params.ObjectName)
+	if err != nil {
+		return nil, err
+	}
+
+	url, err := urlbuilder.New(c.ProviderInfo().BaseURL, restAPIPrefix, path)
 	if err != nil {
 		return nil, err
 	}
