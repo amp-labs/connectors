0e69caa6328a74e6d91aba62d5c6221f74894023
jkarage <josephbkarage@gmail.com>
2026-06-18 18:37:39 +0300

    add serviceNowPKCE

--- providers/serviceNow.go
+++ providers/serviceNow.go
@@ -1,6 +1,9 @@
 package providers
 
-const ServiceNow Provider = "serviceNow"
+const (
+	ServiceNow     Provider = "serviceNow"
+	ServiceNowPKCE Provider = "serviceNowPKCE"
+)
 
 func init() {
 	// ServiceNow configuration
@@ -43,6 +46,59 @@ func init() {
 			Search: SearchSupport{
 				Operators: SearchOperators{
 					Equals: true,
+				},
+			},
+		},
+		Metadata: &ProviderMetadata{
+			Input: []MetadataItemInput{
+				{
+					Name:        "workspace",
+					DisplayName: "Instance",
+				},
+			},
+		},
+	})
+
+	// ServiceNow configuration
+	SetInfo(ServiceNowPKCE, ProviderInfo{
+		DisplayName: "ServiceNow",
+		AuthType:    Oauth2,
+		BaseURL:     "https://{{.workspace}}.service-now.com",
+		Oauth2Opts: &Oauth2Opts{
+			AuthURL:                   "https://{{.workspace}}.service-now.com/oauth_auth.do",
+			TokenURL:                  "https://{{.workspace}}.service-now.com/oauth_token.do",
+			ExplicitScopesRequired:    false,
+			ExplicitWorkspaceRequired: true,
+			GrantType:                 AuthorizationCodePKCE,
+			TokenMetadataFields: TokenMetadataFields{
+				ScopesField: "scope",
+			},
+		},
+		//nolint:lll
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1724169123/media/tn5f6xh2nbb3bops7bsn.png",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1724169123/media/tn5f6xh2nbb3bops7bsn.png",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722405162/media/const%20ServiceNow%20Provider%20%3D%20%22serviceNow%22_1722405162.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722405162/media/const%20ServiceNow%20Provider%20%3D%20%22serviceNow%22_1722405162.svg",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+			Search: SearchSupport{
+				Operators: SearchOperators{
+					Equals: false,
 				},
 			},
 		},
