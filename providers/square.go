1d17399f6ee13fd203c1b847b15a428f0e7d7fad
Dipu <103568666+immdipu@users.noreply.github.com>
2026-06-25 00:52:29 +0545

    [CON-3317] feat(square): add proxy  (#3110)

---
+++ providers/square.go
@@ -0,0 +1,98 @@
+package providers
+
+const (
+	Square        Provider = "square"
+	SquareSandbox Provider = "squareSandbox"
+)
+
+//nolint:funlen
+func init() {
+	SetInfo(Square, ProviderInfo{
+		DisplayName: "Square",
+		AuthType:    Oauth2,
+		BaseURL:     "https://connect.squareup.com",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType: AuthorizationCode,
+			AuthURL:   "https://connect.squareup.com/oauth2/authorize",
+			// Square requires the `session` query param to be set to `true`
+			// to help ensure that sellers with multiple Square accounts use the correct account to authorize.
+			/// Ref: https://developer.squareup.com/docs/oauth-api/create-urls-for-square-authorization
+			AuthURLParams: map[string]string{
+				"session": "true",
+			},
+			TokenURL:                  "https://connect.squareup.com/oauth2/token",
+			ExplicitScopesRequired:    true,
+			ExplicitWorkspaceRequired: false,
+			DocsURL:                   "https://developer.squareup.com/docs/oauth-api/overview",
+			TokenMetadataFields: TokenMetadataFields{
+				ConsumerRefField: "merchant_id",
+			},
+		},
+		//nolint:lll
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1782270338/media/squareup.com_1782270337.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1782270357/media/squareup.com_1782270356.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1782270287/media/squareup.com_1782270286.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1782270322/media/squareup.com_1782270321.svg",
+			},
+		},
+
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+
+	SetInfo(SquareSandbox, ProviderInfo{
+		DisplayName: "Square Sandbox",
+		AuthType:    Oauth2,
+		BaseURL:     "https://connect.squareupsandbox.com",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType:                 AuthorizationCode,
+			AuthURL:                   "https://connect.squareupsandbox.com/oauth2/authorize",
+			TokenURL:                  "https://connect.squareupsandbox.com/oauth2/token",
+			ExplicitScopesRequired:    true,
+			ExplicitWorkspaceRequired: false,
+			DocsURL:                   "https://developer.squareup.com/docs/devtools/sandbox/overview",
+			TokenMetadataFields: TokenMetadataFields{
+				ConsumerRefField: "merchant_id",
+			},
+		},
+
+		//nolint:lll
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1782270338/media/squareup.com_1782270337.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1782270357/media/squareup.com_1782270356.svg",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1782270287/media/squareup.com_1782270286.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1782270322/media/squareup.com_1782270321.svg",
+			},
+		},
+
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
+		},
+	})
+}
