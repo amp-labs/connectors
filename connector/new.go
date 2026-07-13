2fbe23c50cc8da8be0aaa7cde484cc6a57b9d9f5
Ali Iqbal <ali.dev01@invo.email>
2026-07-13 13:29:36 +0500

    Added client and init connector

--- connector/new.go
+++ connector/new.go
@@ -126,6 +126,7 @@ import (
 	"github.com/amp-labs/connectors/providers/salesloft"
 	"github.com/amp-labs/connectors/providers/seismic"
 	"github.com/amp-labs/connectors/providers/sellsy"
+	"github.com/amp-labs/connectors/providers/sendgrid"
 	"github.com/amp-labs/connectors/providers/servicenow"
 	"github.com/amp-labs/connectors/providers/shopify"
 	"github.com/amp-labs/connectors/providers/slack"
@@ -993,6 +994,12 @@ func newSeismicConnector(
 	params common.ConnectorParams,
 ) (*seismic.Connector, error) {
 	return seismic.NewConnector(params)
+}
+
+func newSendGridConnector(
+	params common.ConnectorParams,
+) (*sendgrid.Connector, error) {
+	return sendgrid.NewConnector(params)
 }
 
 func newXeroConnector(
