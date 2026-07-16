0c15f0e87099d18e4f172b5bb274d2f985adf83d
Ali Iqbal <ali.dev01@invo.email>
2026-07-16 17:33:02 +0500

    Added read objects

---
+++ providers/sendgrid/objects.go
@@ -0,0 +1,23 @@
+package sendgrid
+
+// Object names for read routing (match schemas.json keys).
+const (
+	objectContacts              = "contacts"
+	objectLists                 = "lists"
+	objectSegments              = "segments"
+	objectSinglesends           = "singlesends"
+	objectTemplates             = "templates"
+	objectFieldDefinitions      = "field_definitions"
+	objectVerifiedSenders       = "verified_senders"
+	objectSenders               = "senders"
+	objectBounces               = "bounces"
+	objectBlocks                = "blocks"
+	objectSpamReports           = "spam_reports"
+	objectUnsubscribes          = "unsubscribes"
+	objectInvalidEmails         = "invalid_emails"
+	objectASMGroups             = "asm_groups"
+	objectCategories            = "categories"
+	objectSubusers              = "subusers"
+	objectEventWebhookSettings  = "event_webhook_settings"
+	objectParseWebhookSettings  = "parse_webhook_settings"
+)
