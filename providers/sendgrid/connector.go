20586f39bbe098f2363c7de70ffb9b6eafb46350
Ali Iqbal <ali.dev01@invo.email>
2026-07-16 17:21:02 +0500

    Enable read in connectors

--- providers/sendgrid/connector.go
+++ providers/sendgrid/connector.go
@@ -3,6 +3,8 @@ package sendgrid
 import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/reader"
 	"github.com/amp-labs/connectors/internal/components/schema"
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/providers/sendgrid/metadata"
@@ -13,6 +15,7 @@ type Connector struct {
 
 	common.RequireAuthenticatedClient
 	components.SchemaProvider
+	components.Reader
 }
 
 func NewConnector(params common.ConnectorParams) (*Connector, error) {
@@ -25,6 +28,17 @@ func constructor(params common.ConnectorParams, base *components.Connector) (*Co
 	connector.SchemaProvider = schema.NewOpenAPISchemaProvider(
 		connector.ProviderContext.Module(),
 		metadata.Schemas,
+	)
+
+	connector.Reader = reader.NewHTTPReader(
+		connector.HTTPClient().Client,
+		components.NewEmptyEndpointRegistry(),
+		connector.ProviderContext.Module(),
+		operations.ReadHandlers{
+			BuildRequest:  connector.buildReadRequest,
+			ParseResponse: connector.parseReadResponse,
+			ErrorHandler:  common.InterpretError,
+		},
 	)
 
 	return connector, nil
