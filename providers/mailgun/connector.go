28b33612ae737fe14b3a64eb6041e5a94ae37f5d
lafetz <yafetyohaness@gmail.com>
2026-07-19 22:53:42 +0300

    [CON-3399] feat(mailgun): Read functionality

--- providers/mailgun/connector.go
+++ providers/mailgun/connector.go
@@ -3,6 +3,8 @@ package mailgun
 import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/reader"
 	"github.com/amp-labs/connectors/internal/components/schema"
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/providers/mailgun/metadata"
@@ -13,6 +15,7 @@ type Connector struct {
 
 	common.RequireAuthenticatedClient
 	components.SchemaProvider
+	components.Reader
 }
 
 func NewConnector(params common.ConnectorParams) (*Connector, error) {
@@ -22,9 +25,25 @@ func NewConnector(params common.ConnectorParams) (*Connector, error) {
 func constructor(_ common.ConnectorParams, base *components.Connector) (*Connector, error) {
 	connector := &Connector{Connector: base}
 
+	registry, err := components.NewEndpointRegistry(supportedOperations())
+	if err != nil {
+		return nil, err
+	}
+
 	connector.SchemaProvider = schema.NewOpenAPISchemaProvider(
 		connector.ProviderContext.Module(),
 		metadata.Schemas,
+	)
+
+	connector.Reader = reader.NewHTTPReader(
+		connector.HTTPClient().Client,
+		registry,
+		connector.ProviderContext.Module(),
+		operations.ReadHandlers{
+			BuildRequest:  connector.buildReadRequest,
+			ParseResponse: connector.parseReadResponse,
+			ErrorHandler:  common.InterpretError,
+		},
 	)
 
 	return connector, nil
