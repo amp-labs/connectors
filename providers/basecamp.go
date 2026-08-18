c352e9cacbaeccbd50459098569abaa4a6edb8cb
Joseph Karage <josephbkarage@gmail.com>
2026-08-18 15:42:55 +0300

    [CON-39] feat: update basecamp OAuth requirements (#3328)
    * update basecamp OAuth requirements
    
    * Address review
    
    * lint

--- providers/basecamp.go
+++ providers/basecamp.go
@@ -3,8 +3,11 @@ package providers
 const Basecamp Provider = "basecamp"
 
 func init() {
-	// Basecamp Configuration
-	// The wokspace in baseURL should be mapped to accounts.id
+	// Basecamp Configuration.
+	// The workspace in baseURL maps to accounts[].id from the authorization document.
+	// Every Basecamp resource endpoint is prefixed with the account ID; only
+	// https://launchpad.37signals.com/authorization.json can be reached without it,
+	// which is where a user can look the account ID up if the URL is not to hand.
 	SetInfo(Basecamp, ProviderInfo{
 		DisplayName: "Basecamp",
 		AuthType:    Oauth2,
@@ -21,8 +24,8 @@ func init() {
 		},
 		Oauth2Opts: &Oauth2Opts{
 			GrantType:                 AuthorizationCode,
-			AuthURL:                   "https://launchpad.37signals.com/authorization/new?type=web_server",
-			TokenURL:                  "https://launchpad.37signals.com/authorization/token?type=refresh",
+			AuthURL:                   "https://launchpad.37signals.com/authorization/new",
+			TokenURL:                  "https://launchpad.37signals.com/authorization/token",
 			ExplicitScopesRequired:    false,
 			ExplicitWorkspaceRequired: false,
 		},
@@ -43,6 +46,7 @@ func init() {
 				{
 					Name:        "workspace",
 					DisplayName: "Account ID",
+					Prompt:      "our account ID is the unique number located in your Basecamp URL. For example, in `https://app.basecamp.com/6258233/projects`, the account ID is 6258233.`", // nolint:lll
 				},
 			},
 		},
