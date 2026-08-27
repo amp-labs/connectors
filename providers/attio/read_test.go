cd228392886d34cac2e56b8c6c685bd3801f9ddd
Ali Iqbal <ali.dev01@invo.email>
2026-08-27 16:39:22 +0500

    [CON-3465] fix(Attio)/Record Reference Associations (#3329)
    * Extract associate records
    
    * Added unit tests
    
    * Added JSON to for companies with association
    
    * Fix parse to get associations
    
    * Fix read and record
    
    * Fix read and record tests
    
    * Fix lint

--- providers/attio/read_test.go
+++ providers/attio/read_test.go
@@ -22,6 +22,7 @@ func TestRead(t *testing.T) { // nolint:funlen,gocognit,cyclop
 	responseNotes := testutils.DataFromFile(t, "notes.json")
 	responseTasks := testutils.DataFromFile(t, "tasks.json")
 	responseCompanies := testutils.DataFromFile(t, "companies_read.json")
+	responseCompaniesWithAssociations := testutils.DataFromFile(t, "companies_with_associations.json")
 
 	tests := []testconn.TestCaseRead{
 		{
@@ -201,6 +202,90 @@ func TestRead(t *testing.T) { // nolint:funlen,gocognit,cyclop
 										},
 										"value":          "Attio",
 										"attribute_type": "text",
+									},
+								},
+							},
+						},
+					},
+				},
+				NextPage: "500",
+				Done:     false,
+			},
+			ExpectedErrs: nil,
+		},
+		{
+			Name: "Read companies with record-reference associations",
+			Input: common.ReadParams{
+				ObjectName:        "companies",
+				Fields:            connectors.Fields("name"),
+				AssociatedObjects: []string{"people"},
+			},
+			Server: mockserver.Conditional{
+				Setup: mockserver.ContentJSON(),
+				If:    mockcond.Path("/v2/objects/companies/records/query"),
+				Then:  mockserver.Response(http.StatusOK, responseCompaniesWithAssociations),
+			}.Server(),
+			Expected: &common.ReadResult{
+				Rows: 1,
+				Data: []common.ReadResultRow{
+					{
+						Id: "2db97cee-6c6b-4486-ae52-db8e4b6f44e9",
+						Fields: map[string]any{
+							"name": []any{
+								map[string]any{
+									"active_from":  "2025-03-12T07:55:38.981000000Z",
+									"active_until": nil,
+									"created_by_actor": map[string]any{
+										"type": "system",
+										"id":   nil,
+									},
+									"value":          "Attio",
+									"attribute_type": "text",
+								},
+							},
+						},
+						Associations: map[string][]common.Association{
+							"people": {
+								{ObjectId: "891dcbfc-9141-415d-9b2a-2238a6cc012d"},
+								{ObjectId: "5e3fb280-007b-495a-a530-9354bde01de1"},
+							},
+						},
+						Raw: map[string]any{
+							"id": map[string]any{
+								"workspace_id": "63d34516-b287-4c27-9d28-fe2adbebcd50",
+								"object_id":    "1fa986a6-952e-4e92-ba01-acca61a7b616",
+								"record_id":    "2db97cee-6c6b-4486-ae52-db8e4b6f44e9",
+							},
+							"created_at": "2025-03-12T07:55:38.327000000Z",
+							"values": map[string]any{
+								"name": []any{
+									map[string]any{
+										"active_from":  "2025-03-12T07:55:38.981000000Z",
+										"active_until": nil,
+										"created_by_actor": map[string]any{
+											"type": "system",
+											"id":   nil,
+										},
+										"value":          "Attio",
+										"attribute_type": "text",
+									},
+								},
+								"team": []any{
+									map[string]any{
+										"active_from":      "2025-03-12T07:55:39.000000000Z",
+										"active_until":     nil,
+										"created_by_actor": map[string]any{"type": "workspace-member", "id": "073f4c74-b60d-4de9-992a-0f799b5e442e"},
+										"attribute_type":   "record-reference",
+										"target_object":    "people",
+										"target_record_id": "891dcbfc-9141-415d-9b2a-2238a6cc012d",
+									},
+									map[string]any{
+										"active_from":      "2025-03-12T07:55:39.100000000Z",
+										"active_until":     nil,
+										"created_by_actor": map[string]any{"type": "workspace-member", "id": "073f4c74-b60d-4de9-992a-0f799b5e442e"},
+										"attribute_type":   "record-reference",
+										"target_object":    "people",
+										"target_record_id": "5e3fb280-007b-495a-a530-9354bde01de1",
 									},
 								},
 							},
