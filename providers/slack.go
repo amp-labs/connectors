cf365e84fa0536ab9983997e0479a59ed8be132c
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2026-08-27 19:38:34 +0300

    [CON-3464] feat(slack): Oauth2 custom query param (#3321)

--- providers/slack.go
+++ providers/slack.go
@@ -1,11 +1,14 @@
 package providers
 
-const Slack Provider = "slack"
+const (
+	Slack          Provider = "slack"
+	SlackUserScope Provider = "slackUserScope"
+)
 
-func init() {
+func init() { // nolint:funlen
 	// Slack configuration
 	SetInfo(Slack, ProviderInfo{
-		DisplayName: "Slack",
+		DisplayName: "Slack Bot Scope",
 		AuthType:    Oauth2,
 		BaseURL:     "https://slack.com/api",
 		Oauth2Opts: &Oauth2Opts{
@@ -30,6 +33,59 @@ func init() {
 			Read:      true,
 			Subscribe: true,
 			Write:     true,
+		},
+		SubscribeRequirements: &SubscribeRequirements{
+			SubscribeByAPI: new(false),
+		},
+		Media: &Media{
+			DarkMode: &MediaTypeDarkMode{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1724225856/media/wo2jw59mssz2pk1eczur.png",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1724225856/media/wo2jw59mssz2pk1eczur.png",
+			},
+			Regular: &MediaTypeRegular{
+				IconURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722059419/media/slack_1722059417.svg",
+				LogoURL: "https://res.cloudinary.com/dycvts6vp/image/upload/v1722059450/media/slack_1722059449.svg",
+			},
+		},
+		PostAuthInfoNeeded: true,
+		Metadata: &ProviderMetadata{
+			PostAuthentication: []MetadataItemPostAuthentication{
+				{
+					Name: "teamId",
+				},
+			},
+		},
+	})
+
+	SetInfo(SlackUserScope, ProviderInfo{
+		DisplayName: "Slack User Scope",
+		AuthType:    Oauth2,
+		BaseURL:     "https://slack.com/api",
+		Oauth2Opts: &Oauth2Opts{
+			GrantType:                 AuthorizationCode,
+			AuthURL:                   "https://slack.com/oauth/v2/authorize",
+			TokenURL:                  "https://slack.com/api/oauth.v2.user.access",
+			ExplicitScopesRequired:    true,
+			ExplicitWorkspaceRequired: false,
+			// https://docs.slack.dev/authentication/installing-with-oauth#how
+			// > Bot scopes should be added as scope=<bot_scope>,
+			// > and user scopes should be added as user_scope=<user_scope>.
+			ScopeQueryParam: "user_scope",
+			TokenMetadataFields: TokenMetadataFields{
+				ScopesField: "scope",
+			},
+		},
+		Support: Support{
+			BulkWrite: BulkWriteSupport{
+				Insert: false,
+				Update: false,
+				Upsert: false,
+				Delete: false,
+			},
+			Proxy:     false,
+			Read:      false,
+			Subscribe: false,
+			Write:     false,
 		},
 		SubscribeRequirements: &SubscribeRequirements{
 			SubscribeByAPI: new(false),
