2155058b651a776ee93fbb235fbf7b054703c73e
ejoo <ejazkarimhunzai@gmail.com>
2026-09-08 17:51:33 +0500

    feat(reply): add Reply metadata connector

--- connector/new.go
+++ connector/new.go
@@ -119,6 +119,7 @@ import (
 	"github.com/amp-labs/connectors/providers/pylon"
 	"github.com/amp-labs/connectors/providers/quickbooks"
 	"github.com/amp-labs/connectors/providers/recurly"
+	"github.com/amp-labs/connectors/providers/reply"
 	"github.com/amp-labs/connectors/providers/revenuecat"
 	"github.com/amp-labs/connectors/providers/ringcentral"
 	"github.com/amp-labs/connectors/providers/sageintacct"
@@ -286,6 +287,7 @@ var connectorConstructors = map[providers.Provider]outputConstructorFunc{ // nol
 	providers.QuickBooks:                        wrapper(newQuickbooksConnector),
 	providers.QuickbooksSandbox:                 wrapper(newQuickbooksSandboxConnector),
 	providers.Recurly:                           wrapper(newRecurlyConnector),
+	providers.Reply:                             wrapper(newReplyConnector),
 	providers.RevenueCat:                        wrapper(newRevenueCatConnector),
 	providers.RingCentral:                       wrapper(newRingCentral),
 	providers.SageIntacct:                       wrapper(newSageIntacctConnector),
@@ -899,6 +901,12 @@ func newSellsyConnector(
 	params common.ConnectorParams,
 ) (*sellsy.Connector, error) {
 	return sellsy.NewConnector(params)
+}
+
+func newReplyConnector(
+	params common.ConnectorParams,
+) (*reply.Connector, error) {
+	return reply.NewConnector(params)
 }
 
 func newServiceNowConnector(
