fb1c6857058949f40ac72d6ce3dae297f062d1b8
Joseph Karage <josephbkarage@gmail.com>
2026-09-11 12:32:04 +0300

    [CON-2200, CON-2201] feat: add ramp read, write connector (#3004)
    * add ramp metadata connector
    
    * use OpenAPI file
    
    * read & write
    
    * rw
    
    * extract data const
    
    * lint
    
    * fix: use demo in testings

--- providers/ramp/connector.go
+++ providers/ramp/connector.go
@@ -3,7 +3,10 @@ package ramp
 import (
 	"github.com/amp-labs/connectors/common"
 	"github.com/amp-labs/connectors/internal/components"
+	"github.com/amp-labs/connectors/internal/components/operations"
+	"github.com/amp-labs/connectors/internal/components/reader"
 	"github.com/amp-labs/connectors/internal/components/schema"
+	"github.com/amp-labs/connectors/internal/components/writer"
 	"github.com/amp-labs/connectors/providers"
 	"github.com/amp-labs/connectors/providers/ramp/metadata"
 )
@@ -32,6 +35,33 @@ func constructor(base *components.Connector) (*Connector, error) {
 	connector.SchemaProvider = schema.NewOpenAPISchemaProvider(
 		connector.ProviderContext.Module(),
 		metadata.Schemas,
+	)
+
+	registry, err := components.NewEndpointRegistry(supportedOperations())
+	if err != nil {
+		return nil, err
+	}
+
+	connector.Reader = reader.NewHTTPReader(
+		connector.HTTPClient().Client,
+		registry,
+		connector.ProviderContext.Module(),
+		operations.ReadHandlers{
+			BuildRequest:  connector.buildReadRequest,
+			ParseResponse: connector.parseReadResponse,
+			ErrorHandler:  common.InterpretError,
+		},
+	)
+
+	connector.Writer = writer.NewHTTPWriter(
+		connector.HTTPClient().Client,
+		registry,
+		connector.ProviderContext.Module(),
+		operations.WriteHandlers{
+			BuildRequest:  connector.buildWriteRequest,
+			ParseResponse: connector.parseWriteResponse,
+			ErrorHandler:  common.InterpretError,
+		},
 	)
 
 	return connector, nil
