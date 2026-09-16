0286add749ff3ea8bf033901839d509a2f3ce72b
ejoo <ejazkarimhunzai@gmail.com>
2026-09-16 10:54:48 +0500

    style(acculynx): format the nestedObjects comment

--- providers/acculynx/read.go
+++ providers/acculynx/read.go
@@ -126,9 +126,10 @@ type nestedSpec struct {
 	leafSuffix   string
 }
 
-//nestedObjects is a map of objects which require a fan-out of parent objects to child objects.
-//for example, for the object "jobs/contacts", the parent object is "jobs" and the leaf suffix is "contacts".
-//we first need to list all jobs, and then for each job, we need to list all their contacts.
+// nestedObjects is a map of objects which require a fan-out of parent objects to child objects.
+// For example, for the object "jobs/contacts", the parent object is "jobs" and the leaf suffix is "contacts".
+// We first need to list all jobs, and then for each job, we need to list all their contacts.
+//
 //nolint:gochecknoglobals
 var nestedObjects = datautils.Map[string, nestedSpec]{
 	"jobs/contacts":          {parentObject: objectJobs, leafSuffix: "contacts"},
